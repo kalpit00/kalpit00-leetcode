6f9a7900c6983ec1227260b5cee381bfaa248a98
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-03-25 21:03:22 -0400

    File updated at 3/25/2025, 9:03:20 PM

--- leetcode-sub/2033-MinimumOperationstoMakeaUni-ValueGrid/2033-MinimumOperationstoMakeaUni-ValueGrid.java
+++ leetcode-sub/2033-MinimumOperationstoMakeaUni-ValueGrid/2033-MinimumOperationstoMakeaUni-ValueGrid.java
@@ -1,9 +1,8 @@
-// Last updated: 3/25/2025, 8:59:09 PM
+// Last updated: 3/25/2025, 9:03:20 PM
 class Solution {
     public int minOperations(int[][] grid, int x) {
-        int m = grid.length, n = grid[0].length, k = m * n, 
-        idx = 0, min = Integer.MAX_VALUE;
-        int[] pre = new int[k], suf = new int[k], nums = new int[k];
+        int m = grid.length, n = grid[0].length, k = m * n, idx = 0, count = 0;
+        int[] nums = new int[k];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] % x != grid[0][0] % x) {
@@ -13,17 +12,13 @@ class Solution {
             }
         }
         Arrays.sort(nums);
-        for (int i = 1; i < k; i++) {
-            pre[i] = pre[i - 1] + nums[i - 1];
+        int median = nums[k / 2];
+        for (int num : nums) {
+            if (num % x != median % x) {
+                return -1;
+            }
+            count += Math.abs(median - num) / x;
         }
-        for (int i = k - 2; i >= 0; i--) {
-            suf[i] = suf[i + 1] + nums[i + 1];
-        }
-        for (int i = 0; i < k; i++) {
-            int left = (nums[i] * i - pre[i]) / x;
-            int right = (suf[i] - nums[i] * (k - i - 1)) / x;
-            min = Math.min(min, left + right);
-        }
-        return min;
+        return count;
     }
 }
