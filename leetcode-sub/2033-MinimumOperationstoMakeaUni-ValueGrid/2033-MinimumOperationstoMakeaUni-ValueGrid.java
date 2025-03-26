66386454be16e7e8c01efd5e49ed23609807f5a5
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-03-25 21:23:20 -0400

    File updated at 3/25/2025, 9:23:17 PM

--- leetcode-sub/2033-MinimumOperationstoMakeaUni-ValueGrid/2033-MinimumOperationstoMakeaUni-ValueGrid.java
+++ leetcode-sub/2033-MinimumOperationstoMakeaUni-ValueGrid/2033-MinimumOperationstoMakeaUni-ValueGrid.java
@@ -1,14 +1,17 @@
-// Last updated: 3/25/2025, 9:04:12 PM
+// Last updated: 3/25/2025, 9:23:17 PM
 class Solution {
     public int minOperations(int[][] grid, int x) {
-        int m = grid.length, n = grid[0].length, k = m * n, idx = 0, count = 0;
+        int m = grid.length, n = grid[0].length, k = m * n, count = 0, 
+        min = Integer.MAX_VALUE, max = Integer.MIN_VALUE, idx = 0;
         int[] nums = new int[k];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 nums[idx++] = grid[i][j];
+                min = Math.min(min, grid[i][j]);
+                max = Math.max(max, grid[i][j]);
             }
         }
-        Arrays.sort(nums);
+        bucketSort(nums, min, max);
         int median = nums[k / 2];
         for (int num : nums) {
             if (num % x != median % x) {
@@ -17,5 +20,18 @@ class Solution {
             count += Math.abs(median - num) / x;
         }
         return count;
+    }
+    public void bucketSort(int[] nums, int min, int max) {
+        int size = max - min + 1;
+        int[] buckets = new int[size];
+        for (int num : nums) {
+            buckets[num - min]++;
+        }
+        int index = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < buckets[i]; j++) {
+                nums[index++] = i + min;
+            }
+        }
     }
 }
