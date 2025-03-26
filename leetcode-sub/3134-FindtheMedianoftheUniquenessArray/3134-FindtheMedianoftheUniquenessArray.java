1fd3a0421e6dbb7f3e083381fcd9347d47de8bf9
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-03-25 22:46:43 -0400

    Create file

---
+++ leetcode-sub/3134-FindtheMedianoftheUniquenessArray/3134-FindtheMedianoftheUniquenessArray.java
@@ -0,0 +1,39 @@
+// Last updated: 3/25/2025, 10:46:43 PM
+class Solution {
+    public int medianOfUniquenessArray(int[] nums) {
+        int n = nums.length, start = 1, end = n, ans = -1;
+        long total = (long) n * (n + 1) / 2;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (countCompleteSubarrays(nums, mid) >= (total + 1) / 2) {
+                ans = mid;
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+    public long countCompleteSubarrays(int[] nums, int k) {
+        int counter = 0, left = 0, right = 0, n = nums.length;
+        long count = 0;
+        int[] map = new int[100001];
+        while (right < n) {
+            map[nums[right]]++;
+            if (map[nums[right]] == 1) {
+                counter++;
+            }
+            right++;
+            while (counter > k) {
+                map[nums[left]]--;
+                if (map[nums[left]] == 0) {
+                    counter--;
+                }
+                left++;
+            }
+            count += right - left;
+        }
+        return count;
+    }
+}
