7c3c5131739a68d0464973dc16eaf39b9801178f
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-03-26 23:10:20 -0400

    File updated at 3/26/2025, 11:10:19 PM

--- leetcode-sub/3275-K-thNearestObstacleQueries/3275-K-thNearestObstacleQueries.java
+++ leetcode-sub/3275-K-thNearestObstacleQueries/3275-K-thNearestObstacleQueries.java
@@ -1,12 +1,13 @@
-// Last updated: 3/26/2025, 11:09:14 PM
+// Last updated: 3/26/2025, 11:10:19 PM
 class Solution {
     public int[] resultsArray(int[][] queries, int k) {
         int n = queries.length;
         int[] res = new int[n];
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
         for (int i = 0; i < n; i++) {
-            int dist = Math.abs(queries[i][0]) + Math.abs(queries[i][1]);
-            maxHeap.offer(dist);
+            int a = queries[i][0] < 0 ? -queries[i][0] : queries[i][0];
+            int b = queries[i][1] < 0 ? -queries[i][1] : queries[i][1];
+            maxHeap.offer(a + b);
             if (maxHeap.size() > k) {
                 maxHeap.poll();
             }
