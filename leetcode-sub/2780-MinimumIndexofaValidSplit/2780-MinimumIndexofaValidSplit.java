0077a8186917d99e6f61168faf6f42441681470d
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-03-27 12:28:02 -0400

    File updated at 3/27/2025, 12:28:02 PM

--- leetcode-sub/2780-MinimumIndexofaValidSplit/2780-MinimumIndexofaValidSplit.java
+++ leetcode-sub/2780-MinimumIndexofaValidSplit/2780-MinimumIndexofaValidSplit.java
@@ -1,4 +1,4 @@
-// Last updated: 3/26/2025, 11:36:10 PM
+// Last updated: 3/27/2025, 12:28:02 PM
 class Solution {
     public int minimumIndex(List<Integer> nums) {
         int n = nums.size(), max = 0, count = 0, total = 0, left = 0;
@@ -12,7 +12,7 @@ class Solution {
         for (int i = 0; i < n - 1; i++) {
             left += nums.get(i) == max ? 1 : 0;
             int right = total - left;
-            if (left * 2 > i + 1 && right * 2 > n - i - 1) {
+            if (left > (i + 1) / 2 && right > (n - i - 1) / 2) {
                 return i;
             }
         }
