bccf308d26f607397c00194c6b8e00afc458d6b9
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-03-27 13:03:41 -0400

    File updated at 3/27/2025, 1:03:41 PM

--- leetcode-sub/915-PartitionArrayintoDisjointIntervals/915-PartitionArrayintoDisjointIntervals.java
+++ leetcode-sub/915-PartitionArrayintoDisjointIntervals/915-PartitionArrayintoDisjointIntervals.java
@@ -1,19 +1,21 @@
-// Last updated: 3/27/2025, 1:01:27 PM
+// Last updated: 3/27/2025, 1:03:41 PM
 class Solution {
     public int partitionDisjoint(int[] nums) {
-        int currMax = nums[0];
-        int possibleMax = nums[0];
-        int length = 1;
-        
-        for (int i = 1; i < nums.length; ++i) {
-            if (nums[i] < currMax) {
-                length = i + 1;
-                currMax = possibleMax;
-            } else {
-                possibleMax = Math.max(possibleMax, nums[i]);
+        int n = nums.length;
+        int[] pre = new int[n], suf = new int[n];
+        pre[0] = nums[0];
+        suf[n - 1] = nums[n - 1];
+        for (int i = 1; i < n; i++) {
+            pre[i] = Math.max(pre[i - 1], nums[i]);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            suf[i] = Math.min(suf[i + 1], nums[i]);
+        }
+        for (int i = 0; i < n - 1; i++) {            
+            if (pre[i] <= suf[i + 1]) {
+                return i + 1;
             }
         }
-        
-        return length;
+        return -1;
     }
 }
