0996ef974a0200d8b24267b1d76d0b2979f5bd27
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-03-26 23:10:49 -0400

    File updated at 3/26/2025, 11:10:49 PM

--- leetcode-sub/3275-K-thNearestObstacleQueries/3275-K-thNearestObstacleQueries.java
+++ leetcode-sub/3275-K-thNearestObstacleQueries/3275-K-thNearestObstacleQueries.java
@@ -1,17 +1,24 @@
-// Last updated: 3/26/2025, 11:10:19 PM
+// Last updated: 3/26/2025, 11:10:49 PM
 class Solution {
     public int[] resultsArray(int[][] queries, int k) {
-        int n = queries.length;
-        int[] res = new int[n];
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
-        for (int i = 0; i < n; i++) {
-            int a = queries[i][0] < 0 ? -queries[i][0] : queries[i][0];
-            int b = queries[i][1] < 0 ? -queries[i][1] : queries[i][1];
-            maxHeap.offer(a + b);
-            if (maxHeap.size() > k) {
-                maxHeap.poll();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> (b - a));
+        int len = queries.length;
+        int[] res = new int[len];
+        for(int i = 0; i < len; i++){
+            int now = Math.abs(queries[i][0]) + Math.abs(queries[i][1]);
+            if(pq.size() < k){
+                pq.offer(now);
+            }else{
+                if(pq.peek() > now){
+                    pq.poll();
+                    pq.offer(now);
+                }
             }
-            res[i] = maxHeap.size() == k ? maxHeap.peek() : -1;
+            if(pq.size() < k){
+                res[i] = -1;
+            }else{
+                res[i] = pq.peek();
+            }
         }
         return res;
     }
