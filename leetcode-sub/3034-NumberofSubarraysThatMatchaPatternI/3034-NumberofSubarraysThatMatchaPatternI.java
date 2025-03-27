9b0de03d307c51545af4c3cd7f7c8f4b4b432bc2
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-03-27 03:10:11 -0400

    File updated at 3/27/2025, 3:10:11 AM

--- leetcode-sub/3034-NumberofSubarraysThatMatchaPatternI/3034-NumberofSubarraysThatMatchaPatternI.java
+++ leetcode-sub/3034-NumberofSubarraysThatMatchaPatternI/3034-NumberofSubarraysThatMatchaPatternI.java
@@ -1,29 +1,18 @@
-// Last updated: 3/27/2025, 3:06:35 AM
+// Last updated: 3/27/2025, 3:10:11 AM
 class Solution {
     public int countMatchingSubarrays(int[] nums, int[] pattern) {
         int n = nums.length, m = pattern.length, count = 0;
         for (int i = 0; i < n - m; i++) {
+            boolean flag = true;
             for (int j = 0; j < m; j++) {
-                if (pattern[j] == 1) {
-                    if (nums[i + j + 1] <= nums[i + j]) {
-                        count--;
-                        break;
-                    }
-                }
-                else if (pattern[j] == 0) {
-                    if (nums[i + j + 1] != nums[i + j]) {
-                        count--;
-                        break;
-                    }
-                }
-                else if (pattern[j] == -1) {
-                    if (nums[i + j + 1] >= nums[i + j]) {
-                        count--;
-                        break;
-                    }
+                if ((pattern[j] == 1 && nums[i + j + 1] <= nums[i + j]) ||
+                    (pattern[j] == 0 && nums[i + j + 1] != nums[i + j]) ||
+                    (pattern[j] == -1 && nums[i + j + 1] >= nums[i + j])) {
+                    flag = false;
+                    break;
                 }
             }
-            count++;
+            count += flag ? 1 : 0;
         }
         return count;
     }
