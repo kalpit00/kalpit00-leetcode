daf885234568c6eb132cdafedc3cf8bf5bb2b116
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-03-30 19:22:36 -0400

    File updated at 3/30/2025, 7:22:36 PM

--- leetcode-sub/1957-DeleteCharacterstoMakeFancyString/1957-DeleteCharacterstoMakeFancyString.java
+++ leetcode-sub/1957-DeleteCharacterstoMakeFancyString/1957-DeleteCharacterstoMakeFancyString.java
@@ -1,4 +1,4 @@
-// Last updated: 3/30/2025, 7:19:31 PM
+// Last updated: 3/30/2025, 7:22:36 PM
 class Solution {
     public String makeFancyString(String s) {
         char[] chars = s.toCharArray();
@@ -10,7 +10,7 @@ class Solution {
             while (i < n - 1 && chars[i] == chars[i + 1]) {
                 streak++;
                 i++;
-            }
+            } // append 'c' twice if streak > 1, or append only once
             sb.append(c);
             if (streak > 1) {
                 sb.append(c);
