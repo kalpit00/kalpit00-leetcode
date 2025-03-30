79c81b8ef8fc380be37555ccf496c35efe2bd02f
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-03-30 19:15:42 -0400

    File updated at 3/30/2025, 7:15:42 PM

--- leetcode-sub/763-PartitionLabels/763-PartitionLabels.java
+++ leetcode-sub/763-PartitionLabels/763-PartitionLabels.java
@@ -1,45 +1,35 @@
-// Last updated: 3/29/2025, 8:41:35 PM
+// Last updated: 3/30/2025, 7:15:42 PM
 class Solution {
     public List<Integer> partitionLabels(String s) {
         int[][] map = new int[26][2];
-        for (int[] r : map) {
-            Arrays.fill(r, -1);
-        }
         for (int i = 0; i < s.length(); i++) {
             int c = s.charAt(i) - 'a';
-            map[c][0] = map[c][0] == -1 ? i : map[c][0];
-            map[c][1] = i;
+            map[c][0] = map[c][0] == 0 ? i + 1 : map[c][0];
+            map[c][1] = i + 1;
         }
         List<int[]> intervals = new ArrayList<>();
         for (int[] interval : map) {
-            if (interval[0] != -1) {
+            if (interval[0] != 0) {
                 intervals.add(interval);
             }
         }
-        intervals = merge(intervals);
-        List<Integer> res = new ArrayList<>();
-        for (int[] interval : intervals) {
-            int start = interval[0], end = interval[1];
-            res.add(end - start + 1);
-        }
-        return res;
+        return merge(intervals);
     }
-    public List<int[]> merge(List<int[]> intervals) {
+    public List<Integer> merge(List<int[]> intervals) {
         Collections.sort(intervals, (a,b) -> a[0] - b[0]);
-        List<int[]> res = new ArrayList();
+        List<Integer> res = new ArrayList<>();
         int[] prev = intervals.get(0);
         for (int i = 1; i < intervals.size(); i++) {
             int[] curr = intervals.get(i);
             if (prev[1] < curr[0]) {
-                res.add(prev);
+                res.add(prev[1] - prev[0] + 1);
                 prev = curr;
             } else {
                 prev[0] = Math.min(prev[0], curr[0]);
                 prev[1] = Math.max(prev[1], curr[1]);
             }
         }
-        res.add(prev);
-        // return res.toArray(new int[0][]);
+        res.add(prev[1] - prev[0] + 1);
         return res;
     }
 }
