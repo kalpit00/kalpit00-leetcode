3597cb1fcdff6dd15077e0192d245eb9554c7476
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-04-01 21:54:49 -0400

    File updated at 4/1/2025, 9:54:49 PM

--- leetcode-sub/2873-MaximumValueofanOrderedTripletI/2873-MaximumValueofanOrderedTripletI.java
+++ leetcode-sub/2873-MaximumValueofanOrderedTripletI/2873-MaximumValueofanOrderedTripletI.java
@@ -1,11 +1,19 @@
-// Last updated: 4/1/2025, 9:52:24 PM
+// Last updated: 4/1/2025, 9:54:49 PM
 class Solution {
-    public long maximumTripletValue(int[] A) {
-        long res = 0, maxa = 0, maxab = 0;
-        for (int a : A) {
-            res = Math.max(res, 1L * maxab * a);
-            maxab = Math.max(maxab, maxa - a);
-            maxa = Math.max(maxa, a);
+    public long maximumTripletValue(int[] nums) {
+        int n = nums.length;
+        long res = 0;
+        int[] pre = new int[n], suf = new int[n];
+        pre[0] = nums[0]; // or use int_max
+        suf[n - 1] = nums[n - 1]; // use int_min
+        for (int i = 1; i < n; i++) {
+            pre[i] = Math.max(pre[i - 1], nums[i]);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            suf[i] = Math.max(suf[i + 1], nums[i]);
+        }
+        for (int i = 1; i < n - 1; i++) {
+            res = Math.max(res, (long) (pre[i - 1] - nums[i]) * suf[i + 1]);
         }
         return res;
     }
