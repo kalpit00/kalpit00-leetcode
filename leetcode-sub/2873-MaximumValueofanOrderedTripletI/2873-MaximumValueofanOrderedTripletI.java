b9f3f4f7b672e4860ef056fc4b601d355ff5b724
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-04-02 05:11:13 -0400

    File updated at 4/2/2025, 5:11:13 AM

--- leetcode-sub/2873-MaximumValueofanOrderedTripletI/2873-MaximumValueofanOrderedTripletI.java
+++ leetcode-sub/2873-MaximumValueofanOrderedTripletI/2873-MaximumValueofanOrderedTripletI.java
@@ -1,18 +1,14 @@
-// Last updated: 4/1/2025, 9:58:32 PM
+// Last updated: 4/2/2025, 5:11:13 AM
 class Solution {
-    public long maximumTripletValue(int[] nums) {
-        int n = nums.length;
-        long res = 0;
-        int[] pre = new int[n], suf = new int[n];
-        pre[0] = nums[0];
-        suf[n - 1] = nums[n - 1];
-        for (int i = 1; i < n; i++) {
-            pre[i] = Math.max(pre[i - 1], nums[i]);
-            suf[n - i - 1] = Math.max(suf[n - i], nums[n - i - 1]);
-        } // can just combine pre[] and suf[] passes into one!
-        for (int i = 1; i < n - 1; i++) {
-            res = Math.max(res, (long) (pre[i - 1] - nums[i]) * suf[i + 1]);
+    public int arithmeticTriplets(int[] nums, int diff) {
+        Set<Integer> set = new HashSet<>();
+        int count = 0;
+        for (int num : nums) {
+            if (set.contains(num - diff) && set.contains(num - 2 * diff)) {
+                count++;
+            }
+            set.add(num);
         }
-        return res;
+        return count;
     }
 }
