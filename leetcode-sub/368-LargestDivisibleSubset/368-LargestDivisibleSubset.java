e83d8a16fd4e7c5c80db73d97f6846698970367c
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-04-06 04:53:33 -0400

    File updated at 4/6/2025, 4:53:33 AM

--- leetcode-sub/368-LargestDivisibleSubset/368-LargestDivisibleSubset.java
+++ leetcode-sub/368-LargestDivisibleSubset/368-LargestDivisibleSubset.java
@@ -1,28 +1,28 @@
-// Last updated: 4/6/2025, 4:51:21 AM
+// Last updated: 4/6/2025, 4:53:33 AM
 class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
         int n = nums.length, max = 1, maxIndex = 0;
         Arrays.sort(nums);
-        int[] dp = new int[n];
-        List<Integer>[] prev = new ArrayList[n];
+        int[] lds = new int[n];
+        List<Integer>[] dp = new ArrayList[n];
         for (int i = 0; i < n; i++) {
-            dp[i] = 1;
-            prev[i] = new ArrayList<>();
-            prev[i].add(nums[i]);
+            lds[i] = 1;
+            dp[i] = new ArrayList<>();
+            dp[i].add(nums[i]);
         }
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                if (nums[i] % nums[j] == 0 && dp[i] < 1 + dp[j]) {
-                    dp[i] = 1 + dp[j];
-                    prev[i] = new ArrayList<>(prev[j]); // deep copy
-                    prev[i].add(nums[i]);
+                if (nums[i] % nums[j] == 0 && lds[i] < 1 + lds[j]) {
+                    lds[i] = 1 + lds[j];
+                    dp[i] = new ArrayList<>(dp[j]); //deep copy use NEW keyword
+                    dp[i].add(nums[i]);
                 }
             }
-            if (dp[i] > max) {
-                max = dp[i];
+            if (lds[i] > max) {
+                max = lds[i];
                 maxIndex = i;
             }
         }
-        return prev[maxIndex];
+        return dp[maxIndex];
     }
 }
