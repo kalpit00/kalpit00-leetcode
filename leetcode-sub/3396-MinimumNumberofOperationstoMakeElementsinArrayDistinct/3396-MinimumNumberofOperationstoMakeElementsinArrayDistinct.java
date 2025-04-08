e1f66038e140563ce5e81437fcd2ce68b52f3962
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-04-08 04:43:00 -0400

    File updated at 4/8/2025, 4:43:00 AM

--- leetcode-sub/3396-MinimumNumberofOperationstoMakeElementsinArrayDistinct/3396-MinimumNumberofOperationstoMakeElementsinArrayDistinct.java
+++ leetcode-sub/3396-MinimumNumberofOperationstoMakeElementsinArrayDistinct/3396-MinimumNumberofOperationstoMakeElementsinArrayDistinct.java
@@ -1,12 +1,12 @@
-// Last updated: 4/8/2025, 4:42:45 AM
+// Last updated: 4/8/2025, 4:43:00 AM
 class Solution {
     public int minimumOperations(int[] nums) {
-        boolean[] visited = new boolean[128];
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (visited[nums[i]]) {
+        int n = nums.length;
+        boolean[] seen = new boolean[101];
+        for(int i = n - 1 ; i >= 0 ; i--){
+            if(seen[nums[i]])
                 return i / 3 + 1;
-            }
-            visited[nums[i]] = true;
+            else seen[nums[i]] = true;
         }
         return 0;
     }
