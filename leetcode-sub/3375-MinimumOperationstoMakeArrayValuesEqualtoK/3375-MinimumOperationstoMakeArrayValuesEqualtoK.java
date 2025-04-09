97922ba003518cb560d80e98809ea0f897e36b3b
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-04-08 20:24:30 -0400

    File updated at 4/8/2025, 8:24:30 PM

--- leetcode-sub/3375-MinimumOperationstoMakeArrayValuesEqualtoK/3375-MinimumOperationstoMakeArrayValuesEqualtoK.java
+++ leetcode-sub/3375-MinimumOperationstoMakeArrayValuesEqualtoK/3375-MinimumOperationstoMakeArrayValuesEqualtoK.java
@@ -1,15 +1,17 @@
-// Last updated: 4/8/2025, 8:23:13 PM
+// Last updated: 4/8/2025, 8:24:30 PM
 class Solution {
     public int minOperations(int[] nums, int k) {
-        Set<Integer> set = new HashSet<>();
+        int[] map = new int[101];
+        int count = 0;
         for (int num : nums) {
             if (num < k) {
                 return -1;
             }
-            if (num > k){
-                set.add(num);
+            if (num > k) {
+                count += map[num] == 0 ? 1 : 0;
+                map[num]++;
             }
         }
-        return set.size();
+        return count;
     }
 }
