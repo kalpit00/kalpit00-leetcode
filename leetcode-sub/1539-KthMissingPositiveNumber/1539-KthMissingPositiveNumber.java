a8f373b986bb0842858fdc67d3519c721cf2d3ed
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-04-09 21:33:26 -0400

    Create file

---
+++ leetcode-sub/1539-KthMissingPositiveNumber/1539-KthMissingPositiveNumber.java
@@ -0,0 +1,18 @@
+// Last updated: 4/9/2025, 9:33:25 PM
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            set.add(num);
+        }
+
+        int i = 1;
+        while (k != 0) {
+            if (!set.contains(i)) {
+                k--;
+            }
+            i++;
+        }
+        return i - 1;
+    }
+}
