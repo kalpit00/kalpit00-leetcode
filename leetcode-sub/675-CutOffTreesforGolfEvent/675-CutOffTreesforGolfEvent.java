379635a234fa6a9e0271044808b5deea2fe88ac8
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-04-12 01:14:39 -0400

    File updated at 4/12/2025, 1:14:39 AM

--- leetcode-sub/675-CutOffTreesforGolfEvent/675-CutOffTreesforGolfEvent.java
+++ leetcode-sub/675-CutOffTreesforGolfEvent/675-CutOffTreesforGolfEvent.java
@@ -1,4 +1,4 @@
-// Last updated: 4/11/2025, 8:41:53 PM
+// Last updated: 4/12/2025, 1:14:39 AM
 class Solution {
     int[][] dir = new int[][]{{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
     public int cutOffTree(List<List<Integer>> forest) {
@@ -15,7 +15,8 @@ class Solution {
         }
         Collections.sort(nums, (a, b) -> a[2] - b[2]);
         for (int i = 1; i < nums.size(); i++) {
-            int steps = bfs(forest, nums.get(i - 1), nums.get(i), m, n);
+            int[] src = nums.get(i - 1), dest = nums.get(i);
+            int steps = bidirectional_bfs(forest, src, dest, m, n);
             if (steps == -1) {
                 return -1;
             }
@@ -23,28 +24,39 @@ class Solution {
         }
         return sum;
     }
-    private int bfs(List<List<Integer>> forest, int[] src, int[] dest, 
-    int m, int n) {
-        boolean[][] visited = new boolean[m][n];
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(src);
-        visited[src[0]][src[1]] = true;
+    private int bidirectional_bfs(List<List<Integer>> forest, int[] src, int[] dest, int m, int n) {
+        if (src[0] == dest[0] && src[1] == dest[1]) return 0;
+        Queue<int[]> queue1 = new LinkedList<>(), queue2 = new LinkedList<>();
+        boolean[][] visited1 = new boolean[m][n], visited2 = new boolean[m][n];
+        queue1.offer(src);
+        queue2.offer(dest);
+        visited1[src[0]][src[1]] = true;
+        visited2[dest[0]][dest[1]] = true;
         int steps = 0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+            if (queue1.size() > queue2.size()) {
+                Queue<int[]> tempQueue = queue1;
+                queue1 = queue2;
+                queue2 = tempQueue;
+                boolean[][] tempSet = visited1; 
+                visited1 = visited2; 
+                visited2 = tempSet;
+            }
+            int size = queue1.size();
             for (int i = 0; i < size; i++) {
-                int[] node = queue.poll();
+                int[] node = queue1.poll();
                 int x = node[0], y = node[1];
-                if (x == dest[0] && y == dest[1]) {
-                    return steps;
-                }
                 for (int[] d : dir) {
                     int r = x + d[0], c = y + d[1];
-                    if (r >= 0 && r < m && c >= 0 && c < n && !visited[r][c] &&
-                    forest.get(r).get(c) != 0) {
-                        visited[r][c] = true;
-                        queue.offer(new int[]{r, c});
+                    if (r < 0 || r >= m || c < 0 || c >= n || 
+                    forest.get(r).get(c) == 0 || visited1[r][c]) {
+                        continue;
                     }
+                    if (visited2[r][c]) {
+                        return steps + 1;
+                    }
+                    visited1[r][c] = true;
+                    queue1.offer(new int[]{r, c});
                 }
             }
             steps++;
