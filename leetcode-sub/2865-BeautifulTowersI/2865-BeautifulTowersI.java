6016515ae58a16c4acc54f344401addb5590658c
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-04-15 03:17:53 -0400

    File updated at 4/15/2025, 3:17:53 AM

--- leetcode-sub/2865-BeautifulTowersI/2865-BeautifulTowersI.java
+++ leetcode-sub/2865-BeautifulTowersI/2865-BeautifulTowersI.java
@@ -1,33 +1,13 @@
-// Last updated: 4/15/2025, 1:30:42 AM
+// Last updated: 4/15/2025, 3:17:53 AM
 class Solution {
-    public long maximumSumOfHeights(int[] heights) {
-        int n = heights.length, top = -1;
-        int[] stack = new int[n], PSE = new int[n], NSE = new int[n];
-        Arrays.fill(PSE, -1);
-        Arrays.fill(NSE, n);
-        for (int i = 0; i < n; i++) {
-            while (top != -1 && heights[stack[top]] > heights[i]) {
-                int t = stack[top--];
-                NSE[t] = i;
-            }
-            if (top != -1) {
-                PSE[i] = stack[top];
-            }
-            stack[++top] = i;
+    public boolean validMountainArray(int[] nums) {
+        int n = nums.length, i = 0, j = n - 1;
+        while (i < n - 2 && nums[i] < nums[i + 1]) {
+            i++;
         }
-        long[] pre = new long[n], suf = new long[n];
-        for (int i = 0; i < n; i++) {
-            pre[i] = (i - PSE[i]) * (long) heights[i];
-            pre[i] += PSE[i] != -1 ? pre[PSE[i]] : 0;
+        while (j > 1 && nums[j] < nums[j - 1]) {
+            j--;
         }
-        for (int i = n - 1; i >= 0; i--) {
-            suf[i] = (NSE[i] - i) * (long) heights[i];
-            suf[i] += NSE[i] != n ? suf[NSE[i]] : 0;
-        }
-        long max = 0;
-        for (int i = 0; i < n; i++) {
-            max = Math.max(max, pre[i] + suf[i] - heights[i]);
-        }
-        return max;
+        return n < 3 ? false : i == j;
     }
 }
