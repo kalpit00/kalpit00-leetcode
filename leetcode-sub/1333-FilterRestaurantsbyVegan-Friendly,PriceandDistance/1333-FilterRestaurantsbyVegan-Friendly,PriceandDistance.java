21aa983366f563f8a24c9bdaed95d8606b2c4956
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-04-21 13:10:36 -0400

    File updated at 4/21/2025, 1:10:36 PM

--- leetcode-sub/1333-FilterRestaurantsbyVegan-Friendly,PriceandDistance/1333-FilterRestaurantsbyVegan-Friendly,PriceandDistance.java
+++ leetcode-sub/1333-FilterRestaurantsbyVegan-Friendly,PriceandDistance/1333-FilterRestaurantsbyVegan-Friendly,PriceandDistance.java
@@ -1,20 +1,13 @@
-// Last updated: 4/21/2025, 1:09:26 PM
+// Last updated: 4/21/2025, 1:10:36 PM
 class Solution {
     public List<Integer> filterRestaurants(int[][] restaurants, int veganFriendly, int maxPrice, int maxDistance) {
-        List<Integer> res = new ArrayList<>();        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> 
-        a[1] != b[1] ? b[1] - a[1] : b[0] - a[0]);
-        for (int[] item : restaurants) {
-            int id = item[0], rating = item[1], vegan = item[2],
-            price = item[3], distance = item[4];
-            if (price <= maxPrice && distance <= maxDistance && 
-            (veganFriendly == 0 || veganFriendly == 1 && vegan == 1)) {
-                pq.offer(item);
-            }
-        }
-        while (!pq.isEmpty()) {
-            res.add(pq.poll()[0]);
-        }
+        List<Integer> res = new ArrayList<>();
+        Queue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] == b[1] ? b[0] - a[0] : b[1] - a[1]);
+        for (int[] r : restaurants)
+        	if ((veganFriendly == 0 || r[2] == veganFriendly) && r[3] <= maxPrice && r[4] <= maxDistance)
+        		pq.offer(r);
+        while (!pq.isEmpty())
+        	res.add(pq.poll()[0]);
         return res;
     }
 }
