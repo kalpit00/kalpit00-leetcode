230a65df3079f83379a90cd2cb45ca4a81b62722
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-04-23 00:34:00 -0400

    File updated at 4/23/2025, 12:34:00 AM

--- leetcode-sub/3209-NumberofSubarraysWithANDValueofK/3209-NumberofSubarraysWithANDValueofK.java
+++ leetcode-sub/3209-NumberofSubarraysWithANDValueofK/3209-NumberofSubarraysWithANDValueofK.java
@@ -1,21 +1,21 @@
-// Last updated: 4/20/2025, 2:06:33 PM
+// Last updated: 4/23/2025, 12:34:00 AM
 class Solution {
     public long countSubarrays(int[] nums, int k) {
         int n = nums.length;
-        long ans = 0;
+        long count = 0;
         SparseTable table = new SparseTable(nums);
         for (int i = 0; i < n; i++) {
-            int first = search(i, k, table, nums, true);
-            int last = search(i, k, table, nums, false);
-            if (table.query(i, first) == k) {
-                ans += (last - first + 1);
+            int start = search(i, k, table, nums, true);
+            int end = search(i, k, table, nums, false);
+            if (table.query(i, start) == k) {
+                count += (end - start + 1);
             }
         }
-        return ans;
+        return count;
     }
     private int search(int i, int k, SparseTable table, 
     int[] nums, boolean first) {
-        int ans = -1, start = 0, end = nums.length - 1;
+        int n = nums.length, ans = -1, start = 0, end = n - 1;
         while (start <= end) {
             int mid = start + (end - start)/2;
             int target = table.query(i, mid);
@@ -40,24 +40,23 @@ class Solution {
     class SparseTable {
         int[][] table;
         int k, n;
-        public SparseTable(int[] a) {
-            n = a.length;
+        public SparseTable(int[] nums) {
+            n = nums.length;
             k = 22;
-            table = new int[k][n];
+            table = new int[n][k];
             for (int i = 0; i < n; i++) {
-                table[0][i] = a[i];
+                table[i][0] = nums[i];
             }
-            for (int i = 1; i < k; i++) {
-                for (int j = 0; j + (1 << i) <= n; j++) {
-                    table[i][j] = table[i - 1][j] & 
-                    table[i - 1][j + (1 << (i - 1))];
+            for (int j = 1; j < k; j++) {
+                for (int i = 0; i + (1 << j) <= n; i++) {
+                    table[i][j] = table[i][j - 1] & table[i + (1 << (j - 1))][j - 1];
                 }
             }
         }
         public int query(int l, int r) {
             if (l > r) return Integer.MAX_VALUE;
             int x = 31 - Integer.numberOfLeadingZeros(r - l + 1);
-            return table[x][l] & table[x][r - (1 << x) + 1];
+            return table[l][x] & table[r - (1 << x) + 1][x];
         }
     }
 }
