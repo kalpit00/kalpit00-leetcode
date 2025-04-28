3cbae68234adbc9baed02bf2074c3767f20a6c75
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-04-28 17:06:15 -0400

    Create file

---
+++ leetcode-sub/137-SingleNumberII/137-SingleNumberII.java
@@ -0,0 +1,13 @@
+// Last updated: 4/28/2025, 5:06:15 PM
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+            if (map.get(num) == 3) {
+                map.remove(num);
+            }
+        }
+        return map.keySet().iterator().next();
+    }
+}
