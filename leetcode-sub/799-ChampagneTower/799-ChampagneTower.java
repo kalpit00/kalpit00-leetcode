2ccc84958c890991f3751d727c51a979eaefe0b8
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-04-28 15:15:28 -0400

    File updated at 4/28/2025, 3:15:28 PM

--- leetcode-sub/799-ChampagneTower/799-ChampagneTower.java
+++ leetcode-sub/799-ChampagneTower/799-ChampagneTower.java
@@ -1,18 +1,19 @@
-// Last updated: 4/28/2025, 3:09:05 PM
+// Last updated: 4/28/2025, 3:15:28 PM
 class Solution {
     public double champagneTower(int poured, int query_row, int query_glass) {
-        double[][] dp = new double[102][102];
-        dp[0][0] = (double) poured;
+        double[] prev = new double[102], curr = new double[102];
+        prev[0] = (double) poured;
         for (int i = 1; i <= query_row; i++) {
-            double n = dp[i - 1][0];
-            dp[i][0] = Math.max(0.0, (n - 1) / 2); 
+            double n = prev[0];
+            curr[0] = Math.max(0.0, (n - 1) / 2); 
             for (int j = 1; j < i; j++) {
-                double n1 = dp[i - 1][j - 1], n2 = dp[i - 1][j];
+                double n1 = prev[j - 1], n2 = prev[j];
                 double mid = Math.max(0.0, (n1 - 1) / 2) + Math.max(0.0, (n2 - 1) / 2);
-                dp[i][j] = mid;
+                curr[j] = mid;
             }
-            dp[i][i] = dp[i][0];
+            curr[i] = curr[0];
+            System.arraycopy(curr, 0, prev, 0, i + 1);
         }
-        return Math.min(1, dp[query_row][query_glass]);
+        return Math.min(1, prev[query_glass]);
     }
 }
