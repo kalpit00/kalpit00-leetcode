ca9b6806bb142ccf9b8da9f155c5d54b6f9ea15b
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-04-28 15:47:32 -0400

    File updated at 4/28/2025, 3:47:31 PM

--- leetcode-sub/762-PrimeNumberofSetBitsinBinaryRepresentation/762-PrimeNumberofSetBitsinBinaryRepresentation.java
+++ leetcode-sub/762-PrimeNumberofSetBitsinBinaryRepresentation/762-PrimeNumberofSetBitsinBinaryRepresentation.java
@@ -1,12 +1,16 @@
-// Last updated: 4/28/2025, 3:46:17 PM
+// Last updated: 4/28/2025, 3:47:31 PM
 class Solution {
     public int countPrimeSetBits(int left, int right) {
         boolean[] notPrime = sieve(33);
         int count = 0;
         for (int i = left; i <= right; i++) {
-            count += !notPrime[Integer.bitCount(i)] ? 1 : 0;
+            count += isPrime(Integer.bitCount(i)) ? 1 : 0;
         }
         return count;
+    }
+    private boolean isPrime(int x) {
+        return (x == 2 || x == 3 || x == 5 || x == 7 ||
+                x == 11 || x == 13 || x == 17 || x == 19);
     }
     private boolean[] sieve(int n) {
         boolean[] notPrime = new boolean[n];
