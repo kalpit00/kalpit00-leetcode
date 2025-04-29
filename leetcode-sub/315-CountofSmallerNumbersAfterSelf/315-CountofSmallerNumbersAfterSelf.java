1a12cbf88f278f2027321bf61c240f2fcdc31789
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-04-29 15:04:05 -0400

    File updated at 4/29/2025, 3:04:05 PM

--- leetcode-sub/315-CountofSmallerNumbersAfterSelf/315-CountofSmallerNumbersAfterSelf.java
+++ leetcode-sub/315-CountofSmallerNumbersAfterSelf/315-CountofSmallerNumbersAfterSelf.java
@@ -1,4 +1,4 @@
-// Last updated: 4/29/2025, 2:54:06 PM
+// Last updated: 4/29/2025, 3:04:05 PM
 class Solution {
     public List<Integer> countSmaller(int[] nums) {
         int n = nums.length, min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
@@ -6,48 +6,48 @@ class Solution {
             min = Math.min(min, num);
             max = Math.max(max, num);
         }
-        FenwickTree fenwickTree = new FenwickTree(max - min + 1);
+        SegmentTree segmentTree = new SegmentTree(max - min + 1);
         Integer[] res = new Integer[n];
         for (int i = n - 1; i >= 0; i--) {
             int idx = nums[i] - min;
-            res[i] = fenwickTree.query(idx - 1);
-            fenwickTree.update(idx, 1);
+            res[i] = segmentTree.query(0, idx);
+            segmentTree.update(idx, 1);
         }
         return Arrays.asList(res);
     }
-    class FenwickTree {
+    class SegmentTree {
         int[] tree;
         int n;
 
-        public FenwickTree(int n) {
+        public SegmentTree(int n) {
             this.n = n;
-            this.tree = new int[n + 1];
+            this.tree = new int[2 * n];
         }
-        public int query(int index) {
+
+        public int query(int left, int right) {
+            left += n;
+            right += n;
             int sum = 0;
-            index++;
-            while (index > 0) {
-                sum += tree[index];
-                index -= index & (-index);
+            while (left < right) {
+                if ((left & 1) == 1) { // left % 2 == 1
+                    sum += tree[left++];
+                }
+                if ((right & 1) == 1) { // right % 2 == 1
+                    sum += tree[--right];
+                }
+                left >>= 1; // left /= 2
+                right >>= 1; // right /= 2
             }
             return sum;
         }
 
         public void update(int index, int val) {
-            index++;
-            while (index < tree.length) {
-                tree[index] += val;
-                index += index & (-index);
+            index += n;
+            tree[index] += val;
+            while (index > 1) {
+                index >>= 1; // index /= 2
+                tree[index] = tree[2 * index] + tree[2 * index + 1];
             }
-        }
-        public int rangeQuery(int left, int right) {
-            int sum = 0;
-            left++;
-            while (left <= right) {
-                sum += tree[right];
-                right -= right & (-right);
-            }
-            return sum;
         }
     }
 }
