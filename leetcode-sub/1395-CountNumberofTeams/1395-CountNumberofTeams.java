f1ab2d4d646218cf7dd5190ddef2df15ec0dd356
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-04-29 18:50:41 -0400

    File updated at 4/29/2025, 6:50:41 PM

--- leetcode-sub/1395-CountNumberofTeams/1395-CountNumberofTeams.java
+++ leetcode-sub/1395-CountNumberofTeams/1395-CountNumberofTeams.java
@@ -1,20 +1,20 @@
-// Last updated: 4/29/2025, 6:49:44 PM
+// Last updated: 4/29/2025, 6:50:41 PM
 class Solution {
     public int numTeams(int[] rating) {
         int n = rating.length, max = Integer.MIN_VALUE, count = 0;
         for (int num : rating) {
             max = Math.max(max, num);
         }
-        FenwickTree leftTree = new FenwickTree(max + 1);
-        FenwickTree rightTree = new FenwickTree(max + 1);
+        SegmentTree leftTree = new SegmentTree(max + 1);
+        SegmentTree rightTree = new SegmentTree(max + 1);
         for (int num : rating) {
             rightTree.update(num, 1);
         }
         for (int i = 0; i < n; i++) {
             rightTree.update(rating[i], -1);
-            int lessLeft = leftTree.query(rating[i] - 1);
+            int lessLeft = leftTree.query(0, rating[i]);
             int greaterLeft = i - lessLeft;
-            int lessRight = rightTree.query(rating[i] - 1);
+            int lessRight = rightTree.query(0, rating[i]);
             int greaterRight = (n - i - 1) - lessRight;
             count += lessLeft * greaterRight; // [i < j < k]
             count += greaterLeft * lessRight; // [i > j > k]
@@ -22,39 +22,39 @@ class Solution {
         }
         return count;
     }
-    class FenwickTree {
+    class SegmentTree {
         int[] tree;
         int n;
 
-        public FenwickTree(int n) {
+        public SegmentTree(int n) {
             this.n = n;
-            this.tree = new int[n + 1];
+            this.tree = new int[2 * n];
         }
-        public int query(int index) {
+
+        public int query(int left, int right) {
+            left += n;
+            right += n;
             int sum = 0;
-            index++;
-            while (index > 0) {
-                sum += tree[index];
-                index -= index & (-index);
+            while (left < right) {
+                if ((left & 1) == 1) { // left % 2 == 1
+                    sum += tree[left++];
+                }
+                if ((right & 1) == 1) { // right % 2 == 1
+                    sum += tree[--right];
+                }
+                left >>= 1; // left /= 2
+                right >>= 1; // right /= 2
             }
             return sum;
         }
 
         public void update(int index, int val) {
-            index++;
-            while (index < tree.length) {
-                tree[index] += val;
-                index += index & (-index);
+            index += n;
+            tree[index] += val;
+            while (index > 1) {
+                index >>= 1; // index /= 2
+                tree[index] = tree[2 * index] + tree[2 * index + 1];
             }
-        }
-        public int rangeQuery(int left, int right) {
-            int sum = 0;
-            left++;
-            while (left <= right) {
-                sum += tree[right];
-                right -= right & (-right);
-            }
-            return sum;
         }
     }
 }
