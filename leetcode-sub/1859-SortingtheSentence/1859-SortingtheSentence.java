8639a069b835174896a853eedd24303117f3f976
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-04-29 23:48:22 -0400

    File updated at 4/29/2025, 11:48:22 PM

--- leetcode-sub/1859-SortingtheSentence/1859-SortingtheSentence.java
+++ leetcode-sub/1859-SortingtheSentence/1859-SortingtheSentence.java
@@ -1,13 +1,12 @@
-// Last updated: 4/29/2025, 11:46:17 PM
+// Last updated: 4/29/2025, 11:48:22 PM
 class Solution {
     public int minOperations(int n) {
         int count = 0;
         while (n > 0) {
             count++;
             int x = (int) (Math.log(n) / Math.log(2));
-            int prev = (int) Math.pow(2, x);
-            int next = (int) Math.pow(2, x + 1);
-            n = n - prev <= next - n ? n - prev : next - n;
+            int prev = (int) Math.pow(2, x), next = (int) Math.pow(2, x + 1);
+            n = Math.min(n - prev, next - n);
         }
         return count;
     }
