33cfd4a5eafee2c5d9473e0af2fe7fee48944111
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-05-01 21:17:41 -0400

    File updated at 5/1/2025, 9:17:41 PM

--- leetcode-sub/838-PushDominoes/838-PushDominoes.java
+++ leetcode-sub/838-PushDominoes/838-PushDominoes.java
@@ -1,27 +1,57 @@
-// Last updated: 5/1/2025, 9:10:40 PM
+// Last updated: 5/1/2025, 9:17:41 PM
 class Solution {
-    public String pushDominoes(String s) {
-        int n = s.length(), f = 0;
-        char[] arr = s.toCharArray();
-        int[] force = new int[n];
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == 'R') f = n;
-            else if (arr[i] == 'L') f = 0;
-            else f = Math.max(f - 1, 0);
-            force[i] += f;
+    public String pushDominoes(String dominoes) {
+        int N = dominoes.length();
+        char[] arr = dominoes.toCharArray();
+        int[] dis = new int[N];
+        int pushDist = 0;
+        boolean push = false;
+        for(int l=N-1;l>=0;l--){
+            if(arr[l]=='L'){
+                pushDist=0;
+                push=true;
+            }
+            else if(arr[l]=='R'){
+                pushDist=0;
+                push=false;
+                dis[l]=-1;
+            } 
+            
+            if(push){
+                dis[l]= pushDist;
+                pushDist++;
+            }else{
+                dis[l]= Integer.MAX_VALUE;
+            }
         }
-        f = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            if (arr[i] == 'L') f = n;
-            else if (arr[i] == 'R') f = 0;
-            else f = Math.max(f - 1, 0);
-            force[i] -= f;
+        //System.out.println(Arrays.toString(dis));
+
+        pushDist = 0;
+        push = false;
+        for(int i=0;i<N;i++){
+            if(arr[i]=='R'){
+                pushDist=0;
+                push=true;
+            }
+
+            if(arr[i]=='L'){
+                pushDist=0;
+                push=false;
+            }  
+
+            if(arr[i] == '.'){
+                if(push && pushDist == dis[i]){
+                    arr[i] = '.';
+                }
+                else if(push && pushDist < dis[i]){
+                    arr[i] = 'R';
+                } else if(dis[i]!= Integer.MAX_VALUE){
+                    arr[i] = 'L';
+                }
+            }
+            if(push)pushDist++;
         }
-        for (int i = 0; i < n; i++) {
-            if (force[i] > 0) arr[i] = 'R';
-            else if (force[i] < 0) arr[i] = 'L';
-            else arr[i] = '.';
-        }
+        //System.out.println(new String(arr));
         return new String(arr);
     }
 }
