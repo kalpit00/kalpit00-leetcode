b58d49ee55628a2867e65602532bb057cbef255b
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-05-02 21:34:28 -0400

    File updated at 5/2/2025, 9:34:28 PM

--- leetcode-sub/3462-MaximumSumWithatMostKElements/3462-MaximumSumWithatMostKElements.java
+++ leetcode-sub/3462-MaximumSumWithatMostKElements/3462-MaximumSumWithatMostKElements.java
@@ -1,29 +1,20 @@
-// Last updated: 5/2/2025, 9:31:31 PM
+// Last updated: 5/2/2025, 9:34:28 PM
 class Solution {
     public long maxSum(int[][] grid, int[] limits, int k) {
-        int n = limits.length, m = grid[0].length;
-        long sum = 0;
-        List<Integer> list = new ArrayList<>();
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                pq.offer(grid[i][j]);
-                if (pq.size() > limits[i]) {
-                    pq.poll();
-                }
-            }
-            while (!pq.isEmpty()) {
-                list.add(pq.poll());
+        List<Integer> list = new ArrayList();
+        for (int i = 0; i < grid.length; i++) {
+            Arrays.sort(grid[i]);
+            int limit = grid[i].length - limits[i];
+            for (int j = grid[i].length - 1; j >= limit; j--) {
+                list.add(grid[i][j]);
             }
         }
-        for (int num : list) {
-            pq.offer(num);
-            if (pq.size() > k) {
-                pq.poll();
-            }
-        }
-        while (!pq.isEmpty()) {
-            sum += pq.poll();
+
+        list.sort(Comparator.reverseOrder());
+
+        long sum = 0L;
+        for (int i = 0; i < k; i++) {
+            sum += list.get(i);
         }
         return sum;
     }
