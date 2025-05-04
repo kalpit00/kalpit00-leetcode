e2870235ce2f4c498c729c68dce6a282c14d881e
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-05-04 00:57:13 -0400

    File updated at 5/4/2025, 12:57:12 AM

--- leetcode-sub/2407-LongestIncreasingSubsequenceII/2407-LongestIncreasingSubsequenceII.java
+++ leetcode-sub/2407-LongestIncreasingSubsequenceII/2407-LongestIncreasingSubsequenceII.java
@@ -1,26 +1,19 @@
-// Last updated: 5/4/2025, 12:45:28 AM
+// Last updated: 5/4/2025, 12:57:12 AM
 class Solution {
     public int lengthOfLIS(int[] nums, int k) {
-        int n = nums.length, size = 0, maxLen = 0;
+        int n = nums.length, size = 0, max = 0;
         for (int num : nums) size = Math.max(size, num);
-        
         SegmentTreeLazyMax segmentTree = new SegmentTreeLazyMax(size + 2);
-        int[] arr = new int[size + 2];  // Track original values
-        
+        int[] arr = new int[size + 2]; 
         for (int num : nums) {
-            int left = Math.max(0, num - k);
-            int right = num - 1;  // we want strictly less than num
-            
-            int maxInRange = (left <= right) ? segmentTree.query(left, right) : 0;
-            int newVal = maxInRange + 1;
-            
-            int diff = newVal - arr[num];
-            arr[num] = newVal;
-            segmentTree.update(num, num, diff);  // point update as range
-            
-            maxLen = Math.max(maxLen, newVal);
+            int left = Math.max(0, num - k), right = num - 1;
+            int res = (left <= right) ? segmentTree.query(left, right) + 1 : 1;
+            int diff = res - arr[num];
+            arr[num] = res;
+            segmentTree.update(num, num, diff);
+            max = Math.max(max, res);
         }
-        return maxLen;
+        return max;
     }
 
     class SegmentTreeLazyMax {
@@ -35,7 +28,7 @@ class Solution {
 
         private void push(int node, int l, int r) {
             if (lazy[node] != 0) {
-                tree[node] += lazy[node];
+                tree[node] += (r - l + 1) * lazy[node];
                 if (l != r) { // Not a leaf
                     lazy[2 * node] += lazy[node];
                     lazy[2 * node + 1] += lazy[node];
@@ -48,17 +41,17 @@ class Solution {
             update(1, 0, n - 1, l, r, val);
         }
 
-        private void update(int node, int nodeL, int nodeR, int l, int r, int val) {
-            push(node, nodeL, nodeR);
-            if (nodeR < l || nodeL > r) return;
-            if (l <= nodeL && nodeR <= r) {
+        private void update(int node, int l, int r, int ql, int qr, int val) {
+            push(node, l, r);
+            if (qr < l || r < ql) return;
+            if (ql <= l && r <= qr) {
                 lazy[node] += val;
-                push(node, nodeL, nodeR);
+                push(node, l, r);
                 return;
             }
-            int mid = (nodeL + nodeR) / 2;
-            update(2 * node, nodeL, mid, l, r, val);
-            update(2 * node + 1, mid + 1, nodeR, l, r, val);
+            int mid = l + (r - l) / 2;
+            update(2 * node, l, mid, ql, qr, val);
+            update(2 * node + 1, mid + 1, r, ql, qr, val);
             tree[node] = Math.max(tree[2 * node], tree[2 * node + 1]);
         }
 
@@ -66,13 +59,13 @@ class Solution {
             return query(1, 0, n - 1, l, r);
         }
 
-        private int query(int node, int nodeL, int nodeR, int l, int r) {
-            push(node, nodeL, nodeR);
-            if (nodeR < l || nodeL > r) return 0;
-            if (l <= nodeL && nodeR <= r) return tree[node];
-            int mid = (nodeL + nodeR) / 2;
-            int leftMax = query(2 * node, nodeL, mid, l, r);
-            int rightMax = query(2 * node + 1, mid + 1, nodeR, l, r);
+        private int query(int node, int l, int r, int ql, int qr) {
+            push(node, l, r);
+            if (qr < l || r < ql) return 0;
+            if (ql <= l && r <= qr) return tree[node];
+            int mid = l + (r - l) / 2;
+            int leftMax = query(2 * node, l, mid, ql, qr);
+            int rightMax = query(2 * node + 1, mid + 1, r, ql, qr);
             return Math.max(leftMax, rightMax);
         }
     }
