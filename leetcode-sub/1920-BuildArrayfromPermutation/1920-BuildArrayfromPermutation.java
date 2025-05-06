cc183f932173bb852cc38b3940eeef718bdd6f91
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-05-05 23:05:11 -0400

    Create file

---
+++ leetcode-sub/1920-BuildArrayfromPermutation/1920-BuildArrayfromPermutation.java
@@ -0,0 +1,11 @@
+// Last updated: 5/5/2025, 11:05:11 PM
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
