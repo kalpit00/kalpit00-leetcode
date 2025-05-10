fc78cc193e98e473b6e3ae061827083ea6699048
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-05-10 06:42:57 -0400

    File updated at 5/10/2025, 3:42:57 AM

--- leetcode-sub/2444-CountSubarraysWithFixedBounds/2444-CountSubarraysWithFixedBounds.java
+++ leetcode-sub/2444-CountSubarraysWithFixedBounds/2444-CountSubarraysWithFixedBounds.java
@@ -1,30 +1,30 @@
-// Last updated: 5/10/2025, 3:41:29 AM
+// Last updated: 5/10/2025, 3:42:57 AM
 class Solution {
     public long countSubarrays(int[] nums, int minK, int maxK) {
         int n = nums.length, left = 0, right = 0;
         long count = 0;
-        Queue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[1] - a[1]);
-        Queue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        Deque<Integer> maxDeque = new ArrayDeque<>();
+        Deque<Integer> minDeque = new ArrayDeque<>();   
         while (right < n) {
-            while (!maxHeap.isEmpty() && maxHeap.peek()[1] <= nums[right]) {
-                maxHeap.poll();
-            }
-            while (!minHeap.isEmpty() && minHeap.peek()[1] >= nums[right]) {
-                minHeap.poll();
-            }
-            maxHeap.add(new int[]{right, nums[right]});
-            minHeap.add(new int[]{right, nums[right]});
+            while (!maxDeque.isEmpty() && nums[maxDeque.peekLast()] <= nums[right]) {
+                maxDeque.pollLast();
+            } // monodeque steps to carry max and mins on peek
+            while (!minDeque.isEmpty() && nums[minDeque.peekLast()] >= nums[right]) {
+                minDeque.pollLast();
+            } // offer INDICES for max and min elements, NOT elements
+            maxDeque.add(right);
+            minDeque.add(right);
+// if curr ele is outside of range [minK, maxK], ALL eles in between invalid
             if (nums[right] < minK || nums[right] > maxK) {
-                left = right + 1;
-                minHeap.clear();
-                maxHeap.clear();
-            } 
-            if (!minHeap.isEmpty() && !maxHeap.isEmpty() && 
-            minHeap.peek()[1] == minK && maxHeap.peek()[1] == maxK) {  
-                count += Math.min(maxHeap.peek()[0], minHeap.peek()[0]) - left + 1;
-            }
+                left = right + 1; // move left to right + 1
+                minDeque.clear(); // empty the deques!
+                maxDeque.clear();
+            } // SHRINK left step, no need for WHILE, a one-shot IF suffices
+            if (!minDeque.isEmpty() && !maxDeque.isEmpty() && nums[minDeque.peekFirst()] == minK && nums[maxDeque.peekFirst()] == maxK) {  
+                count += Math.min(maxDeque.peekFirst(), minDeque.peekFirst()) - left + 1; // same as doing count += (right - left)
+            } // except right = Math.min(max.peek, min.peek)
             right++;
-        }
+        } // and we do right++ AFTER 2nd while, so count += right - left + 1
         return count;
     }
 }
