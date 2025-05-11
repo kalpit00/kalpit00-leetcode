d7f181c190b0b9e247e2b5b759d22c68dc0aa66c
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-05-11 06:47:33 -0400

    Create file

---
+++ leetcode-sub/1550-ThreeConsecutiveOdds/1550-ThreeConsecutiveOdds.java
@@ -0,0 +1,12 @@
+// Last updated: 5/11/2025, 6:47:32 AM
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n - 1; i++) {
+            if (arr[i - 1] % 2 != 0 && arr[i] % 2 != 0 && arr[i+1] % 2 != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
