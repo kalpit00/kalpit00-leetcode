5ab89cb8839c7df7065d3518a38485839014c4b3
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-05-14 23:15:49 -0400

    Create file

---
+++ leetcode-sub/2900-LongestUnequalAdjacentGroupsSubsequenceI/2900-LongestUnequalAdjacentGroupsSubsequenceI.java
@@ -0,0 +1,13 @@
+// Last updated: 5/14/2025, 11:15:48 PM
+class Solution {
+    public List<String> getLongestSubsequence(String[] words, int[] groups) {
+        List<String> ans = new ArrayList<>();
+        int n = words.length;
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || groups[i] != groups[i - 1]) {
+                ans.add(words[i]);
+            }
+        }
+        return ans;
+    }
+}
