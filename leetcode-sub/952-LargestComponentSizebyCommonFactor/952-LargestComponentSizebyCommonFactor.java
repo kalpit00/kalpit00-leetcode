9fcea5990623d17923d2a63df8934c72e8a14372
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-05-19 19:22:39 -0400

    File updated at 5/19/2025, 7:22:39 PM

--- leetcode-sub/952-LargestComponentSizebyCommonFactor/952-LargestComponentSizebyCommonFactor.java
+++ leetcode-sub/952-LargestComponentSizebyCommonFactor/952-LargestComponentSizebyCommonFactor.java
@@ -1,68 +1,70 @@
-// Last updated: 5/19/2025, 7:19:38 PM
+// Last updated: 5/19/2025, 7:22:39 PM
 class Solution {
     public int largestComponentSize(int[] nums) {
-        int n = nums.length;
-        int max = 1;
+        int n = nums.length, size = 0, max = 0;
         for (int num : nums) {
-            max = Math.max(max, num);
+            size = Math.max(size, num);
         }
-        int[] map = new int[max + 1];
+        int[] map = new int[size + 1];
         Arrays.fill(map, -1);
-        boolean[] check = new boolean[max + 1];
-        
+        List<Integer> primes = sieve(size + 1);;
         DSU dsu = new DSU(n);
         for (int i = 0; i < n; i++) {
             map[nums[i]] = i;
         }
-        for (int p = 2; p <= max; p++) {
-            if (check[p]) {
-                continue;
+        for (int i : primes) {
+            int idx = -1;
+            for (int j = 1; i * j <= size; j++) {
+                if (map[i * j] == -1) continue;
+                idx = idx == -1 ? map[i * j] : idx;
+                dsu.union(idx, map[i * j]);
             }
-
-            int first = -1;
-            for (int div = p; div <= max; div += p) {
-                if (map[div] >= 0) {
-                    if (first == -1) {
-                        first = map[div];
-                    } else {
-                        dsu.union(first, map[div]);
-                    }
+        }  
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, dsu.size[dsu.findParent(i)]);
+        } // or also dsu.max will work, it maxes each size[pu, pv] when unioning
+        return max; // but for NO unions, we have to take max of all size[]
+    }
+    public List<Integer> sieve(int n) {
+        List<Integer> primes = new ArrayList<>();
+        boolean[] notPrime = new boolean[n];
+        notPrime[0] = notPrime[1] = true;
+        for (int i = 2; i < n; i++) {
+            if (!notPrime[i]) {
+                primes.add(i);
+                for (int j = 2; i*j < n; j++) {
+                    notPrime[i*j] = true;
                 }
-                check[div] = true;
             }
         }
-        
-        return dsu.getMaxComponent();
+        return primes;
     }
 
     class DSU {
-        private int[] parent;
-        private int[] size;
-        private int maxSize;
-
+        int[] size, parent;
+        int componentCount, max;
         public DSU(int n) {
-            parent = new int[n];
             size = new int[n];
-            maxSize = 1;
-
+            parent = new int[n];
+            componentCount = n;
+            max = 0;
             for (int i = 0; i < n; i++) {
-                parent[i] = i;
                 size[i] = 1;
+                parent[i] = i;
             }
         }
-        
+
         public int findParent(int node) {
             if (node == parent[node]) {
                 return node;
             }
             return parent[node] = findParent(parent[node]);
         }
-        
+
         public void union(int u, int v) {
             int pu = findParent(u), pv = findParent(v);
-            if (pu == pv) {
-                return;
-            }
+            if (pu == pv) return;
+
             if (size[pu] < size[pv]) {
                 parent[pu] = pv;
                 size[pv] += size[pu];
@@ -70,11 +72,8 @@ class Solution {
                 parent[pv] = pu;
                 size[pu] += size[pv];
             }
-            maxSize = Math.max(maxSize, Math.max(size[pu], size[pv]));
-        }
-        
-        public int getMaxComponent() {
-            return maxSize;
+            componentCount--;
+            max = Math.max(max, Math.max(size[pu], size[pv]));
         }
     }
 }
