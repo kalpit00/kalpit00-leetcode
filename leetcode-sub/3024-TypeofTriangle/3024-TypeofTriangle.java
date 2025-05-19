0f09488ff8d02f28a1972928f6069c4ff0ea1d36
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-05-18 22:17:25 -0400

    Create file

---
+++ leetcode-sub/3024-TypeofTriangle/3024-TypeofTriangle.java
@@ -0,0 +1,15 @@
+// Last updated: 5/18/2025, 10:17:25 PM
+class Solution {
+    public String triangleType(int[] nums) {
+        Arrays.sort(nums);
+        if (nums[0] + nums[1] <= nums[2]) {
+            return "none";
+        } else if (nums[0] == nums[2]) {
+            return "equilateral";
+        } else if (nums[0] == nums[1] || nums[1] == nums[2]) {
+            return "isosceles";
+        } else {
+            return "scalene";
+        }
+    }
+}
