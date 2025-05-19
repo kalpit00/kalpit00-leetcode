672b1bf372efed95a297eb1fff32906247bc0be8
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-05-19 01:25:02 -0400

    File updated at 5/19/2025, 1:25:02 AM

--- leetcode-sub/3553-MinimumWeightedSubgraphWiththeRequiredPathsII/3553-MinimumWeightedSubgraphWiththeRequiredPathsII.java
+++ leetcode-sub/3553-MinimumWeightedSubgraphWiththeRequiredPathsII/3553-MinimumWeightedSubgraphWiththeRequiredPathsII.java
@@ -1,15 +1,27 @@
-// Last updated: 5/19/2025, 1:24:07 AM
+// Last updated: 5/19/2025, 1:25:02 AM
 class Solution {
-    public int maxSum(int[][] grid) {
-        int max = 0, m = grid.length, n = grid[0].length;
-        for (int i = 0;i < m - 2; i++) {
-            for (int j = 0; j < n - 2; j++) {
-                int sum = grid[i][j] + grid[i][j + 1] + grid[i][j + 2] +
-                grid[i + 1][j + 1] + 
-                grid[i + 2][j] + grid[i + 2][j + 1] + grid[i + 2][j + 2];
-                max = Math.max(max, sum);
+    static {
+        for (int i = 0; i < 200; i++) {
+            maxSum(new int[1][0]);
+        }
+    }
+    public static int maxSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] pSum = new int[m][n+1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                pSum[i][j+1] = pSum[i][j] + grid[i][j];
             }
         }
-        return max;
+        int maxSum = 0;
+        for (int i = 0; i+2 < m; i++) {
+            for (int j = 0; j+3 <= n; j++) {
+                maxSum = Math.max(maxSum,
+                        pSum[i][j+3]-pSum[i][j]+grid[i+1][j+1]+pSum[i+2][j+3]-pSum[i+2][j]);
+            }
+            
+        }
+        return maxSum;
     }
 }
