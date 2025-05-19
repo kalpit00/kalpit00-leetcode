77d37d5e9bf97081f7e3f2fbf572aa5402816c2a
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-05-19 01:10:55 -0400

    File updated at 5/19/2025, 1:10:55 AM

--- leetcode-sub/3553-MinimumWeightedSubgraphWiththeRequiredPathsII/3553-MinimumWeightedSubgraphWiththeRequiredPathsII.java
+++ leetcode-sub/3553-MinimumWeightedSubgraphWiththeRequiredPathsII/3553-MinimumWeightedSubgraphWiththeRequiredPathsII.java
@@ -1,11 +1,11 @@
-// Last updated: 5/19/2025, 1:10:40 AM
+// Last updated: 5/19/2025, 1:10:55 AM
 class Solution {
     public int minimizeArrayValue(int[] nums) {
         long sum = 0, max = 0; // max the prefixSum Average!
         int n = nums.length;
         for (int i = 0; i < n; i++) {
             sum += nums[i]; // there are (i + 1) elements in sum[0..i]
-            max = Math.max(max, (long) Math.ceil((double) sum / (i + 1)));
+            max = Math.max(max, (sum + i) / (i + 1));
         } // avg is simply sum / i + 1, get the ceiling!
         return (int) max;
     }
