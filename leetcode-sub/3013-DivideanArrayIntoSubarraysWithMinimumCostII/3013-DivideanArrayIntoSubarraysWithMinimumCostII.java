d27a85da786918108a6601654cc5a0e5bd42e6e1
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-05-20 18:37:15 -0400

    Create file

---
+++ leetcode-sub/3013-DivideanArrayIntoSubarraysWithMinimumCostII/3013-DivideanArrayIntoSubarraysWithMinimumCostII.java
@@ -0,0 +1,51 @@
+// Last updated: 5/20/2025, 6:37:15 PM
+class Solution {
+    int mod = 1000000007;
+    public int countSteppingNumbers(String low, String high) {
+        String a = helper(low), b = high;
+        int m = a.equals("0") ? 0 : a.length(), n = b.length();
+        Long[][][][] dp2 = new Long[n][2][11][2];
+        long res2 = solve(b, n, 0, 1, -1, 1, dp2);
+        if (m == 0) {
+            return (int) res2 % mod;
+        }
+        Long[][][][] dp1 = new Long[m][2][11][2];
+        long res1 = solve(a, m, 0, 1, -1, 1, dp1);
+        return (int) ((res2 - res1 + mod) % mod);
+    }
+        private long solve(String num, int m, int idx, int tight, int prev,
+        int leadingZero, Long[][][][] dp) {
+        if (idx == m) {
+            return leadingZero == 0 ? 1 : 0;
+        }
+        if (dp[idx][tight][prev + 1][leadingZero] != null) {
+            return dp[idx][tight][prev + 1][leadingZero];
+        }
+        int limit = tight == 1 ? num.charAt(idx) - '0' : 9;
+        long count = 0;
+        for (int i = 0; i <= limit; i++) {
+            int newTight = tight == 1 && i == limit ? 1 : 0;
+            int newLeadingZero = (leadingZero == 1 && i == 0) ? 1 : 0;
+            if (leadingZero == 1 || prev == -1 || Math.abs(i - prev) == 1) {
+                count = (count + solve(num, m, idx + 1, newTight, i,
+                newLeadingZero, dp)) % mod;
+            }
+        }
+        return dp[idx][tight][prev + 1][leadingZero] = count % mod;
+    }
+    private String helper(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        int i = sb.length() - 1;
+        while (i >= 0 && sb.charAt(i) == '0') {
+            sb.setCharAt(i, '9');
+            i--;
+        }
+        if (i >= 0) {
+            sb.setCharAt(i, (char)(sb.charAt(i) - 1));
+        }
+        if (sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+        return sb.toString();
+    }
+}
