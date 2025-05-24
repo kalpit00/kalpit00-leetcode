926be53f3537685fd319305c7d4cac280ca066e6
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-05-23 20:06:39 -0400

    File updated at 5/23/2025, 8:06:39 PM

--- leetcode-sub/2942-FindWordsContainingCharacter/2942-FindWordsContainingCharacter.java
+++ leetcode-sub/2942-FindWordsContainingCharacter/2942-FindWordsContainingCharacter.java
@@ -1,11 +1,13 @@
-// Last updated: 5/23/2025, 8:03:46 PM
+// Last updated: 5/23/2025, 8:06:39 PM
 class Solution {
     public List<Integer> findWordsContaining(String[] words, char x) {
         int n = words.length;
         List<Integer> res = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            for (char c : words[i].toCharArray()) {
-                if (c == x) {
+            char[] s = words[i].toCharArray();
+            int m = s.length;
+            for (int j = 0; j < m; j++) {
+                if (s[j] == x || s[m - j - 1] == x) {
                     res.add(i);
                     break;
                 }
