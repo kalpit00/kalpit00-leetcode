fdea0223fbadccf6fdb3293fb3e389f27f4cfa3a
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-05-24 00:34:08 -0400

    Create file

---
+++ leetcode-sub/895-MaximumFrequencyStack/895-MaximumFrequencyStack.java
@@ -0,0 +1,24 @@
+// Last updated: 5/24/2025, 12:34:07 AM
+class FreqStack {
+    Map<Integer,Integer> map;
+    PriorityQueue<int[]> maxHeap;
+    int size;
+    public FreqStack() {
+        size = 0;
+        map = new HashMap<>();
+        maxHeap = new PriorityQueue<>((a, b) -> a[1] != b[1] ? b[1] - a[1] : 
+        b[2] - a[2]); // <val, freq, size>
+    }  // sort on freq first, maxHeap, then on height/size of stack!
+    
+    public void push(int val) {
+        map.put(val, map.getOrDefault(val, 0) + 1);
+        size++; // grow the stack vertically, used for tiebreakers!
+        maxHeap.offer(new int[]{val, map.get(val), size});
+    }
+    
+    public int pop() {
+        int val = maxHeap.poll()[0];
+        map.put(val, map.getOrDefault(val, 0) - 1);
+        return val;
+    }
+}
