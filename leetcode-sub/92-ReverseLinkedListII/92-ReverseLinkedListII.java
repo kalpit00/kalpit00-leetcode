cbc041bee2bd906b360132b89079dd99234a403d
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-05-25 22:36:55 -0400

    File updated at 5/25/2025, 10:36:55 PM

--- leetcode-sub/92-ReverseLinkedListII/92-ReverseLinkedListII.java
+++ leetcode-sub/92-ReverseLinkedListII/92-ReverseLinkedListII.java
@@ -1,33 +1,32 @@
-// Last updated: 5/25/2025, 5:42:51 PM
+// Last updated: 5/25/2025, 10:36:55 PM
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
         if (head == null || left == right) {
             return head;
         } 
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode prev = dummy;
-        for (int i = 1; i < left; i++) {
+        ListNode dummy = new ListNode(-1), prev = dummy;
+        dummy.next = head; // move prev to 1 node before 'left' index element
+        for (int i = 0; i < left - 1; i++) {
             prev = prev.next;
-        }
+        } // store the 'left' index item at curr (prev.next)
         ListNode curr = prev.next, after = curr;
         for (int i = left; i <= right; i++) {
             after = after.next;
-        }
-        ListNode ans = reverseList(curr, after);
-        prev.next = ans;
-        curr.next = after;
+        } // and store the node at 'right' + 1 index item in 'after'
+        ListNode ans = reverseList(curr, after); // ans is reversed head!
+        prev.next = ans; // connect prev.next to reversed head.
+        curr.next = after; // connect tail of reversed LL to 'after'
         return dummy.next;
     }
-
+    // Rev LL1 + 'after' var to rev until a certain node in LL, not till null
     public ListNode reverseList(ListNode head, ListNode after) {
         ListNode prev = null, curr = head;
-        while (curr != after) {
+        while (curr != after) { // curr goes till 'after' node
             ListNode next = curr.next;
-            curr.next = prev;
+            curr.next = prev; 
             prev = curr;
             curr = next;
-        }
+        } // in classic rev LL1, curr goes till null, so we reversed entire LL!
         return prev;
     }
 }
