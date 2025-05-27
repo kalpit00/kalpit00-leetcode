ebc5c8e13e0b603abe4fa15544702487b96aa070
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-05-26 20:21:02 -0400

    File updated at 5/26/2025, 8:21:02 PM

--- leetcode-sub/2894-DivisibleandNon-divisibleSumsDifference/2894-DivisibleandNon-divisibleSumsDifference.java
+++ leetcode-sub/2894-DivisibleandNon-divisibleSumsDifference/2894-DivisibleandNon-divisibleSumsDifference.java
@@ -1,15 +1,7 @@
-// Last updated: 5/26/2025, 8:20:37 PM
+// Last updated: 5/26/2025, 8:21:02 PM
 class Solution {
-    public int differenceOfSums(int n, int m) {
-        int num1 = 0, num2 = 0;
-        for (int i = 1; i <= n; i++) {
-            if (i % m != 0) {
-                num1 += i;
-            }
-            else {
-                num2 += i;
-            }
-        }
-        return num1 - num2;
+    public int differenceOfSums(int b, int v) {
+        int k = b / v;
+        return (b *(b + 1))/ 2 - k * (k+ 1)* v;
     }
 }
