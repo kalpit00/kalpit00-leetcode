ce39fc84ee80c84dffe48b1157ed11d0d7b8f43e
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-05-30 14:44:23 -0400

    File updated at 5/30/2025, 2:44:23 PM

--- leetcode-sub/2316-CountUnreachablePairsofNodesinanUndirectedGraph/2316-CountUnreachablePairsofNodesinanUndirectedGraph.java
+++ leetcode-sub/2316-CountUnreachablePairsofNodesinanUndirectedGraph/2316-CountUnreachablePairsofNodesinanUndirectedGraph.java
@@ -1,52 +1,51 @@
-// Last updated: 5/30/2025, 2:31:29 PM
+// Last updated: 5/30/2025, 2:44:23 PM
 class Solution {
-    public long countPairs(int n, int[][] edges) {
-        DSU dsu = new DSU(n);
+    public int[] timeTaken(int[][] edges) {
+        int n = edges.length + 1;
+        int[] ans = new int[n];
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
         for (int[] edge : edges) {
             int u = edge[0], v = edge[1];
-            dsu.union(u, v);
+            adj.get(u).add(v);
+            adj.get(v).add(u);
         }
-        long count = 0;
-        Set<Integer> set = new HashSet<>();
+        int dist[] = bfs(n, adj, 0), max = getMax(dist);
+        int dist1[] = bfs(n, adj, max), secondMax = getMax(dist1);
+        int dist2[] = bfs(n, adj, secondMax);
         for (int i = 0; i < n; i++) {
-            set.add(dsu.findParent(i));
+            ans[i] = Math.max(dist1[i], dist2[i]) - (2 - (i % 2));
         }
-        for (int i : set) {
-            count += (long) dsu.size[i] * (n - dsu.size[i]);
-        }
-        return count / 2;
+        return ans;
     }
-    class DSU {
-        int[] size, parent;
-        int componentCount;
-        public DSU(int n) {
-            size = new int[n];
-            parent = new int[n];
-            componentCount = n;
-            for (int i = 0; i < n; i++) {
-                size[i] = 1;
-                parent[i] = i;
-            }
-        }
 
-        public int findParent(int node) {
-            if (node == parent[node]) {
-                return node;
+    private int[] bfs(int n, List<List<Integer>> adj, int src) {
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MIN_VALUE);
+        Queue<Integer> queue = new LinkedList<>();
+        dist[src] = (src % 2 == 1) ? 1 : 2;
+        queue.offer(src);
+        while (!queue.isEmpty()) {
+            int v = queue.poll();
+            for (int u : adj.get(v)) {
+                if (dist[u] != Integer.MIN_VALUE) continue;
+                int wt = ((u % 2 == 1) ? 1 : 2);
+                dist[u] = dist[v] + wt;
+                queue.offer(u);
             }
-            return parent[node] = findParent(parent[node]);
         }
-
-        public void union(int u, int v) {
-            int pu = findParent(u), pv = findParent(v);
-            if (pu == pv) return;
-            if (size[pu] < size[pv]) {
-                parent[pu] = pv;
-                size[pv] += size[pu];
-            } else {
-                parent[pv] = pu;
-                size[pu] += size[pv];
+        return dist;
+    }
+    private int getMax(int[] nums) {
+        int n = nums.length, max = Integer.MIN_VALUE, ans = -1;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+                ans = i;
             }
-            componentCount--;
         }
+        return ans;
     }
 }
