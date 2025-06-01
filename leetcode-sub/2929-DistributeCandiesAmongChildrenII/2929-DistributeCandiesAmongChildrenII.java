65cd8eb3d43ab443631f4f31b56b11b90a8e5837
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-05-31 20:20:27 -0400

    File updated at 5/31/2025, 8:20:26 PM

--- leetcode-sub/2929-DistributeCandiesAmongChildrenII/2929-DistributeCandiesAmongChildrenII.java
+++ leetcode-sub/2929-DistributeCandiesAmongChildrenII/2929-DistributeCandiesAmongChildrenII.java
@@ -1,10 +1,12 @@
-// Last updated: 5/31/2025, 8:19:02 PM
+// Last updated: 5/31/2025, 8:20:26 PM
 class Solution {
     public long distributeCandies(int n, int limit) {
         long count = 0;
-        for (int i = Math.max(0, n - 2 * limit); i <= Math.min(limit, n); i++) {
-            int j = Math.min(limit, n - i) - Math.max(0, n - i - limit) + 1;
-            count += Math.max(0, j);
+        for (int i = 0; i <= Math.min(limit, n); i++) {
+            if (n - i > 2 * limit) {
+                continue;
+            }
+            count += Math.min(n - i, limit) - Math.max(0, n - i - limit) + 1;
         }
         return count;
     }
