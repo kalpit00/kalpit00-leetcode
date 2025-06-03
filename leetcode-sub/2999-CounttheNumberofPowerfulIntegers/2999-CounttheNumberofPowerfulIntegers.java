abb49f3ec101062c760a4bc504852153ff624556
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-03 01:54:13 -0400

    File updated at 6/3/2025, 1:54:13 AM

--- leetcode-sub/2999-CounttheNumberofPowerfulIntegers/2999-CounttheNumberofPowerfulIntegers.java
+++ leetcode-sub/2999-CounttheNumberofPowerfulIntegers/2999-CounttheNumberofPowerfulIntegers.java
@@ -1,35 +1,47 @@
-// Last updated: 4/9/2025, 9:01:47 PM
+// Last updated: 6/3/2025, 1:54:13 AM
 class Solution {
-    public long numberOfPowerfulInt(long start, long finish, int limit, String s) {
+    public long numberOfPowerfulInt(long start, long finish, int threshold, String s) {
+        for (char c : s.toCharArray()) {
+            if (c - '0' > threshold) {
+                return 0;
+            }
+        }
         String b = String.valueOf(finish), a = String.valueOf(start - 1);
         int m = a.length(), n = b.length(), k = s.length();
-        long x = 0, y = 0;        
         Long[][] dp1 = new Long[17][2], dp2 = new Long[17][2];
-        if (k <= n) {
-            x = solve(0, 1, b, s, limit, n, k, dp1);
-        }
-        if (k <= m) {
-            y = solve(0, 1, a, s, limit, m, k, dp2);
-        }
-        return x - y;
+        long res2 = k <= n ? solve(0, 1, b, s, threshold, n, k, dp1) : 0;
+        long res1 = k <= m ? solve(0, 1, a, s, threshold, m, k, dp2) : 0;
+        return res2 - res1;
     }
 
-    private long solve(int idx, int tight, String num, String s, int limit, int m, int k, Long[][] dp) {
+    private long solve(int idx, int tight, String num, String s, int threshold, int m, int k, Long[][] dp) {
         if (idx == m) {
             return 1;
         }
         if (dp[idx][tight] != null) {
             return dp[idx][tight];
         }
-        int lower = 0, upper = tight == 1 ? Math.min(limit, num.charAt(idx) - '0') : limit, sufIdx = m - k;
-        if (sufIdx <= idx) {
-            lower = s.charAt(idx - sufIdx) - '0';
-            upper = Math.min(upper, s.charAt(idx - sufIdx) - '0');
-        }
         long count = 0;
-        for (int i = lower; i <= upper; i++) {
-            int newTight = tight == 1 && i == num.charAt(idx) - '0' ? 1 : 0;
-            count += solve(idx + 1, newTight, num, s, limit, m, k, dp);
+        if (idx >= m - k) {
+            int digit = s.charAt(idx - (m - k)) - '0';
+            if (digit <= threshold) {
+                int limit = tight == 1 ? num.charAt(idx) - '0' : threshold;
+                if (digit <= limit) {
+                    int newTight = (tight == 1 && 
+                    digit == num.charAt(idx) - '0') ? 1 : 0;
+                    count += solve(idx + 1, newTight, num, s, threshold, 
+                    m, k, dp);
+                }
+            }
+        } 
+        else {
+            int limit = tight == 1 ? Math.min(threshold, num.charAt(idx) - '0')
+            : threshold;
+            for (int i = 0; i <= limit; i++) {
+                int newTight = (tight == 1 && i == num.charAt(idx) - '0') 
+                ? 1 : 0;
+                count += solve(idx + 1, newTight, num, s, threshold, m, k, dp);
+            }
         }
         return dp[idx][tight] = count;
     }
