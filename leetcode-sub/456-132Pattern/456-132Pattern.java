5daafe5b83be0e21f702b2204cec55179a70c642
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-02 23:02:12 -0400

    File updated at 6/2/2025, 11:02:12 PM

--- leetcode-sub/456-132Pattern/456-132Pattern.java
+++ leetcode-sub/456-132Pattern/456-132Pattern.java
@@ -1,27 +1,28 @@
-// Last updated: 6/2/2025, 10:50:14 PM
+// Last updated: 6/2/2025, 11:02:12 PM
 class Solution {
     public boolean find132pattern(int[] nums) {
-        int n = nums.length, min = Integer.MAX_VALUE, top = -1;
+        int n = nums.length, top = -1;
         if (n < 3) return false;
-        int[][] stack = new int[n][2]; // <i, pre[i]>
-        int[] PGE = new int[n];
+        int[] stack = new int[n], pre = new int[n], PGE = new int[n];
+        pre[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            pre[i] = Math.min(pre[i - 1], nums[i]);
+        }
         // Arrays.fill(PGE, -1);
         for (int k = 0; k < n; k++) { // strict PGE! so unstrict in while loop
-            while (top != -1 && nums[stack[top][0]] <= nums[k]) {
-                // int t = stack[top][0];
+            while (top != -1 && nums[stack[top]] <= nums[k]) {
+                // int t = stack[top];
                 // NGE[t] = k;
                 top--; // its redundant, we don't need NGE[] nor PGE[]
             }
             if (top != -1) {
-                int j = stack[top][0]; // j = 3, k = 2, 32 found
+                int j = stack[top]; // j = 3, k = 2, 32 found
                 PGE[k] = j; // now at stack top 'j', check prefix i : [0 .. j]
-                if (stack[top][1] < nums[k]) {
-                    return true;
-                } // nums[i] < nums[k] found via pre, so 132 formed
+                if (pre[j] < nums[k]) {
+                    return true; // pre[j] == nums[i]!
+                } // nums[i] < nums[k], so 132 formed
             }
-            stack[++top][0] = k;
-            stack[top][1] = min;
-            min = Math.min(min, nums[k]);
+            stack[++top] = k;
         }
         return false;
     }
