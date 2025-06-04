43af5ebd13c35fc804b18286e42febeecc57fc6b
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-04 02:38:25 -0400

    File updated at 6/4/2025, 2:38:25 AM

--- leetcode-sub/1313-DecompressRun-LengthEncodedList/1313-DecompressRun-LengthEncodedList.java
+++ leetcode-sub/1313-DecompressRun-LengthEncodedList/1313-DecompressRun-LengthEncodedList.java
@@ -1,18 +1,18 @@
-// Last updated: 6/4/2025, 2:29:51 AM
+// Last updated: 6/4/2025, 2:38:25 AM
 class Solution {
     public int longestValidSubstring(String word, List<String> forbidden) {
         Trie trie = new Trie();
         for (String str : forbidden) {
-            trie.insert(str.toCharArray());
+            trie.insert(str);
         }
         char[] s = word.toCharArray();
         int n = s.length, left = 0, right = 0, max = 0;
         while (right < n) {
             int i = trie.searchSuffix(s, left, right);
-            right++;
+            right++; // look for any forbidden words in s[left .. right]
             if (i != -1) {
                 left = i + 1;
-            }
+            } // 'i' is where a forbidden word started, move left to 'i + 1' 
             max = Math.max(max, right - left);
         }
         return max;
@@ -24,18 +24,19 @@ class Solution {
             root = new TrieNode();
         }
         
-        public void insert(char[] s) {
-            int n = s.length;
+        public void insert(String word) {
+            int n = word.length();
             TrieNode node = root;
             for (int i = n - 1; i >= 0; i--) {
-                if (node.child[s[i] - 'a'] == null) {
-                    node.child[s[i] - 'a'] = new TrieNode();
+                char ch = word.charAt(i);
+                if (node.child[ch - 'a'] == null) {
+                    node.child[ch - 'a'] = new TrieNode();
                 }
-                node = node.child[s[i] - 'a'];
+                node = node.child[ch - 'a'];
             }
             node.isEnd = true;
         } 
-// find 'last' index of a Tnode with isEnd true -> a forbidden word ends there
+// find 'last' index of a Tnode with isEnd true -> a forbidden word starts there
         public int searchSuffix(char[] s, int left, int right) {
             TrieNode node = root; // s[left ... right] from right to left, i--
             for (int i = right; i >= left; i--) {
@@ -45,10 +46,10 @@ class Solution {
                 node = node.child[s[i] - 'a'];
                 if (node.isEnd) {
                     return i; // [left ... i ... right]
-                } // [left, i] invalid range as some forbidden word ends at 'i'
+                } // [left, i] invalid range as some forbidden word start at 'i'
             } // [i + 1, right] still valid, so notice we set left = i + 1
             return -1;
-        } // no forbidden words' suffix found in [left, right], ret -1
+        } // no forbidden words' found in [left, right], ret -1
         
         class TrieNode {
             TrieNode[] child = new TrieNode[26];
