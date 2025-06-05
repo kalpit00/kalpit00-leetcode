9f56fca3fa4355f2151ac2c62ebbea4158e78cef
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-05 17:20:40 -0400

    File updated at 6/5/2025, 5:20:40 PM

--- leetcode-sub/2708-MaximumStrengthofaGroup/2708-MaximumStrengthofaGroup.java
+++ leetcode-sub/2708-MaximumStrengthofaGroup/2708-MaximumStrengthofaGroup.java
@@ -1,18 +1,21 @@
-// Last updated: 6/5/2025, 5:12:58 PM
-public class Solution {
+// Last updated: 6/5/2025, 5:20:40 PM
+class Solution {
     public long maxStrength(int[] nums) {
-        Arrays.sort(nums);
-        long result = 1;
-        int size = nums.length;
-        int count = 0;
-
-        for (int i = 0; i < size; ++i) {
-            if (result * nums[i] > 0 || (i + 1 < size && nums[i + 1] < 0)) {
-                result *= nums[i];
+        if (nums.length == 1) return nums[0];
+        long prod = 1;
+        boolean flag = false;
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE, count = 0;
+        for (int num : nums) {
+            max = Math.max(max, num);
+            if (num == 0) continue;
+            flag = true;
+            prod *= num;
+            if (num < 0) {
                 count++;
+                min = Math.min(min, -num);
             }
         }
-
-        return count > 0 ? result : nums[size - 1];
+        if (max == 0 && count <= 1) return 0;
+        return !flag ? 0 : prod >= 0 ? prod : -prod / min;
     }
 }
