c38f982cd12cd2c6d40d77c52d57b8b27e780de5
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-06 13:34:03 -0400

    File updated at 6/6/2025, 1:34:03 PM

--- leetcode-sub/3111-MinimumRectanglestoCoverPoints/3111-MinimumRectanglestoCoverPoints.java
+++ leetcode-sub/3111-MinimumRectanglestoCoverPoints/3111-MinimumRectanglestoCoverPoints.java
@@ -1,14 +1,23 @@
-// Last updated: 6/6/2025, 1:13:30 PM
+// Last updated: 6/6/2025, 1:34:03 PM
 class Solution {
-    public int minRectanglesToCoverPoints(int[][] points, int w) {
-        Arrays.sort(points, (a, b) -> a[0] - b[0]);
-        int count = 0, prev = -1;
-        for (int[] point : points) {
-            if (point[0] > prev) {
-                count++;
-                prev = point[0] + w;
+    public List<String> subdomainVisits(String[] cpdomains) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : cpdomains) {
+            String[] s = word.split("\\s+");
+            int freq = Integer.parseInt(s[0]);
+            String[] domains = s[1].split("\\.");
+            int n = domains.length;
+            String key = "";
+            for (int i = n - 1; i >= 0; i--) {
+                key = domains[i] + (i < n - 1 ? "." : "") + key;
+                map.put(key, map.getOrDefault(key, 0) + freq);
             }
         }
-        return count;
+        List<String> res = new ArrayList<>();
+        for (String key : map.keySet()) {
+            int freq = map.get(key);
+            res.add(freq + " " + key);
+        }
+        return res;
     }
 }
