d30a452cc52c138b4cfdda8bad176944d99ca858
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-08 01:27:07 -0400

    File updated at 6/8/2025, 1:27:07 AM

--- leetcode-sub/480-SlidingWindowMedian/480-SlidingWindowMedian.java
+++ leetcode-sub/480-SlidingWindowMedian/480-SlidingWindowMedian.java
@@ -1,41 +1,41 @@
-// Last updated: 6/8/2025, 12:53:24 AM
+// Last updated: 6/8/2025, 1:27:07 AM
 class Solution {
     public double[] medianSlidingWindow(int[] nums, int k) {
         int n = nums.length, idx = 1;
         double[] res = new double[n - k + 1];
-        TreeSet<int[]> left = new TreeSet<>((a, b) -> a[0] != b[0] ? 
-        Integer.compare(b[0], a[0]) : b[1] - a[1]); // maxHeap
-        TreeSet<int[]> right = new TreeSet<>((a, b) -> a[0] != b[0] ? 
-        Integer.compare(a[0], b[0]) : a[1] - b[1]); // minHeap
+        TreeSet<int[]> maxHeap = new TreeSet<>((a, b) -> a[0] != b[0] ? 
+        Integer.compare(b[0], a[0]) : b[1] - a[1]); // left
+        TreeSet<int[]> minHeap = new TreeSet<>((a, b) -> a[0] != b[0] ? 
+        Integer.compare(a[0], b[0]) : a[1] - b[1]); // right
         for (int i = 0; i < k; i++) {
-            left.add(new int[]{nums[i], i}); 
-        } // add the first "k" elements and balance
-        balance(left, right);
-        res[0] = median(k, nums, left, right); // get median for first k
+            add(maxHeap, minHeap, nums, i);
+        }
+        res[0] = median(k, nums, maxHeap, minHeap);
         for (int i = k; i < n; i++) {
-            int[] key = new int[]{nums[i - k], i - k};
-            if (!left.remove(key)) { 
-                right.remove(key);
+            int[] old = new int[]{nums[i - k], i - k};
+            if (!maxHeap.remove(old)) { // min/maxHeaps swappable here!
+                minHeap.remove(old);
             }
-            right.add(new int[]{nums[i], i});
-            left.add(right.pollFirst());
-            balance(left, right);
-            res[idx++] = median(k, nums, left, right);
+            add(maxHeap, minHeap, nums, i);
+            res[idx++] = median(k, nums, maxHeap, minHeap);
         }
         return res;
     }
-    private void balance(TreeSet<int[]> left, TreeSet<int[]> right) {
-        while (left.size() > right.size()) {
-            right.add(left.pollFirst());
+    private void add(TreeSet<int[]> maxHeap, TreeSet<int[]> minHeap, 
+    int[] nums, int i) {
+        maxHeap.add(new int[]{nums[i], i});
+        minHeap.add(maxHeap.pollFirst());
+        while (minHeap.size() > maxHeap.size()) {
+            maxHeap.add(minHeap.pollFirst());
         }
     }
-    private double median(int k, int[] nums, TreeSet<int[]> left, 
-    TreeSet<int[]> right) {
+    private double median(int k, int[] nums, TreeSet<int[]> maxHeap, 
+    TreeSet<int[]> minHeap) {
         if (k % 2 == 0) {
-            return ((double) left.first()[0] + right.first()[0]) / 2;
+            return ((double) maxHeap.first()[0] + minHeap.first()[0]) / 2;
         }
         else {
-            return (double) right.first()[0];
+            return (double) maxHeap.first()[0];
         }
     }
 }
