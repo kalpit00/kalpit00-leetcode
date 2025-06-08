6052249b96af76acc5520851b57d079383c23dad
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-08 04:29:41 -0400

    File updated at 6/8/2025, 4:29:41 AM

--- leetcode-sub/3505-MinimumOperationstoMakeElementsWithinKSubarraysEqual/3505-MinimumOperationstoMakeElementsWithinKSubarraysEqual.java
+++ leetcode-sub/3505-MinimumOperationstoMakeElementsWithinKSubarraysEqual/3505-MinimumOperationstoMakeElementsWithinKSubarraysEqual.java
@@ -1,4 +1,4 @@
-// Last updated: 6/8/2025, 3:34:17 AM
+// Last updated: 6/8/2025, 4:29:41 AM
 class Solution {
     public long minOperations(int[] nums, int x, int k) {
         long[] cost = medianSlidingWindow(nums, x);
@@ -26,9 +26,9 @@ class Solution {
         int[] medians = new int[n - k + 1];
         long[] cost = new long[n - k + 1];
         TreeSet<int[]> maxHeap = new TreeSet<>((a, b) -> a[0] != b[0] ? 
-        Integer.compare(b[0], a[0]) : b[1] - a[1]); // left
+        b[0] - a[0] : b[1] - a[1]); // left
         TreeSet<int[]> minHeap = new TreeSet<>((a, b) -> a[0] != b[0] ? 
-        Integer.compare(a[0], b[0]) : a[1] - b[1]); // right
+        a[0] - b[0] : a[1] - b[1]); // right
         long[] sum = new long[2]; // <maxHeapSum/leftSum, minHeapSum/rightSum>
         for (int i = 0; i < k; i++) {
             add(maxHeap, minHeap, nums, i, sum);
@@ -53,14 +53,15 @@ class Solution {
     }
     private void add(TreeSet<int[]> maxHeap, TreeSet<int[]> minHeap, 
     int[] nums, int i, long[] sum) {
-        maxHeap.add(new int[]{nums[i], i});
-        sum[0] += nums[i] - maxHeap.first()[0];
-        sum[1] += maxHeap.first()[0];
+        sum[0] += nums[i]; // add nums[i] to maxHp
+        maxHeap.add(new int[]{nums[i], i}); // move top from maxHp to minHp
+        sum[1] += maxHeap.first()[0]; // so add to minHp
+        sum[0] -= maxHeap.first()[0]; // and subtract from maxHp
         minHeap.add(maxHeap.pollFirst());
         if (minHeap.size() > maxHeap.size()) {
-            sum[1] -= minHeap.first()[0];
-            sum[0] += minHeap.first()[0];
-            maxHeap.add(minHeap.pollFirst());
+            sum[1] -= minHeap.first()[0]; // so subtract from minHp
+            sum[0] += minHeap.first()[0]; // and add to maxHp
+            maxHeap.add(minHeap.pollFirst()); // since move from minHP to maxHP
         }
     }
     private int median(int k, int[] nums, TreeSet<int[]> maxHeap, 
