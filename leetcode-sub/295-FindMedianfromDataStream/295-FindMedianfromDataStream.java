83360d5cc6f093dc3d770bfdff2d099a2e9f99a5
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-08 02:16:02 -0400

    Create file

---
+++ leetcode-sub/295-FindMedianfromDataStream/295-FindMedianfromDataStream.java
@@ -0,0 +1,20 @@
+// Last updated: 6/8/2025, 2:16:02 AM
+class MedianFinder {
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> a - b);
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+    boolean even = true;
+    public MedianFinder() {
+        
+    }
+    public void addNum(int num) {
+        maxHeap.add(num);
+        minHeap.add(maxHeap.poll());
+        if (minHeap.size() > maxHeap.size()) {
+            maxHeap.add(minHeap.poll());
+        }
+        even = !even;
+    }
+    public double findMedian() {
+        return (!even) ? maxHeap.peek() : (maxHeap.peek() + minHeap.peek()) / 2.0;
+    }
+}
