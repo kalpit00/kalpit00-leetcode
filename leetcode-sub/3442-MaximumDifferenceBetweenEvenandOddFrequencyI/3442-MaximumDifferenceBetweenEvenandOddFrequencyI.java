a4b6e5e7e9af53eb7643d4cc87954d388eda1097
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-09 20:07:36 -0400

    File updated at 6/9/2025, 8:07:36 PM

--- leetcode-sub/3442-MaximumDifferenceBetweenEvenandOddFrequencyI/3442-MaximumDifferenceBetweenEvenandOddFrequencyI.java
+++ leetcode-sub/3442-MaximumDifferenceBetweenEvenandOddFrequencyI/3442-MaximumDifferenceBetweenEvenandOddFrequencyI.java
@@ -1,15 +1,15 @@
-// Last updated: 6/9/2025, 8:07:03 PM
+// Last updated: 6/9/2025, 8:07:36 PM
 class Solution {
     public int maxDifference(String s) {
-        int maxOdd = 0, minEven = Integer.MAX_VALUE;
+        int max = 0, min = Integer.MAX_VALUE;
         int[] map = new int[26];
         for (char c : s.toCharArray()) {
             map[c - 'a']++;
         }
         for (int i = 0; i < 26; i++) {
-            maxOdd = map[i] % 2 == 1 ? Math.max(maxOdd, map[i]) : maxOdd;
-            minEven = map[i] != 0 && map[i] % 2 == 0 ? Math.min(minEven, map[i]) : minEven;
+            max = map[i] % 2 == 1 ? Math.max(max, map[i]) : max;
+            min = map[i] != 0 && map[i] % 2 == 0 ? Math.min(min, map[i]) : min;
         }
-        return maxOdd - minEven;
+        return max - min;
     }
 }
