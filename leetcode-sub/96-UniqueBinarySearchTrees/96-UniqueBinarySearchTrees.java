64d40b1b3381a7d3a5d8264add6c75e6df194747
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-11 15:34:08 -0400

    File updated at 6/11/2025, 3:34:08 PM

--- leetcode-sub/96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.java
+++ leetcode-sub/96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.java
@@ -1,4 +1,4 @@
-// Last updated: 6/11/2025, 3:32:11 PM
+// Last updated: 6/11/2025, 3:34:08 PM
 class Solution {
     public List<TreeNode> generateTrees(int n) {
         List<TreeNode>[][] dp = new List[n + 1][n + 1];
@@ -16,30 +16,15 @@ class Solution {
         for (int k = i; k <= j; k++) {
             List<TreeNode> left = solve(i, k - 1, dp);
             List<TreeNode> right = solve(k + 1, j, dp);
-            List<TreeNode[]> pairs = product(left, right);
-            for (TreeNode[] pair : pairs) {
-                TreeNode curr = new TreeNode(k);
-                curr.left = pair[0];
-                curr.right = pair[1];
-                res.add(curr);
+            for (TreeNode a : left) {
+                for (TreeNode b: right) {
+                    TreeNode node = new TreeNode(k);
+                    node.left = a;
+                    node.right = b;
+                    res.add(node);
+                }
             }
         }
         return dp[i][j] = res;
-    }
-    private List<TreeNode[]> product(List<TreeNode> left, List<TreeNode> right){
-        List<TreeNode[]> list = new ArrayList<>();
-        for (TreeNode a: left) {
-            for (TreeNode b: right) {
-                list.add(new TreeNode[]{deepCopy(a), deepCopy(b)});
-            }
-        }
-        return list;
-    }
-    private TreeNode deepCopy(TreeNode root) {
-        if (root == null) return null;
-        TreeNode curr = new TreeNode(root.val);
-        curr.left  = deepCopy(root.left);
-        curr.right = deepCopy(root.right);
-        return curr;
     }
 }
