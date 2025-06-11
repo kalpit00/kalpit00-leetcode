1cead527bcd5d7da549875928f73ab81ec900cb9
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-10 20:59:09 -0400

    File updated at 6/10/2025, 8:59:09 PM

--- leetcode-sub/312-BurstBalloons/312-BurstBalloons.java
+++ leetcode-sub/312-BurstBalloons/312-BurstBalloons.java
@@ -1,18 +1,34 @@
-// Last updated: 6/10/2025, 8:53:31 PM
+// Last updated: 6/10/2025, 8:59:09 PM
 class Solution {
     public int maxCoins(int[] nums) {
         int n = nums.length;
-        int[][] coins = new int[n + 1][n + 1];
-        // coins[i][j] is max coins by bursing in [i,j) range
-        for (int j = 1; j <= n; j++) {
-            for (int i = j - 1; i >= 0; i--) {
-                int max = 0; // k is index of last balloon bursted in [i,j)
-                for (int k = i; k <= j - 1; k++) {
-                    max = Math.max(max, coins[i][k] + coins[k + 1][j] + nums[k] * (j == n ? 1 : nums[j]) * (i == 0 ? 1 : nums[i - 1]));
-                }
-                coins[i][j] = max;
-            }
+        int[] balloons = new int[n + 2];
+        balloons[0] = balloons[n + 1] = 1;
+        for (int i = 0; i < n; i++) {
+            balloons[i + 1] = nums[i];
         }
-        return coins[0][n];
+
+        int[][] dp = new int[n + 2][n + 2];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        return burst(balloons, dp, 0, n + 1);
+    }
+
+    private int burst(int[] balloons, int[][] dp, int left, int right) {
+        if (left + 1 == right) return 0;
+        if (dp[left][right] != -1) return dp[left][right];
+
+        int max = 0;
+        for (int i = left + 1; i < right; i++) {
+            int coins = balloons[left] * balloons[i] * balloons[right]
+                      + burst(balloons, dp, left, i)
+                      + burst(balloons, dp, i, right);
+            max = Math.max(max, coins);
+        }
+
+        dp[left][right] = max;
+        return max;
     }
 }
