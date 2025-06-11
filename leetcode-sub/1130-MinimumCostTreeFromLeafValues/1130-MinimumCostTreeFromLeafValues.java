b3eece16c278fe664b6ede5b9d24fbf36e0f0dbf
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-11 02:16:31 -0400

    File updated at 6/11/2025, 2:16:30 AM

--- leetcode-sub/1130-MinimumCostTreeFromLeafValues/1130-MinimumCostTreeFromLeafValues.java
+++ leetcode-sub/1130-MinimumCostTreeFromLeafValues/1130-MinimumCostTreeFromLeafValues.java
@@ -1,4 +1,4 @@
-// Last updated: 6/11/2025, 2:16:19 AM
+// Last updated: 6/11/2025, 2:16:30 AM
 class Solution {
     public int mctFromLeafValues(int[] arr) {
         int n = arr.length;
@@ -14,15 +14,17 @@ class Solution {
         }
         int min = Integer.MAX_VALUE;
         for (int k = i; k < j; k++) {
-            int left = arr[i], right = arr[j];
-            for (int t = i; t <= k; t++) {
-                left = Math.max(left, arr[t]);
-            }
-            for (int t = k + 1; t <= j; t++) {
-                right = Math.max(right, arr[t]);
-            }
-            min = Math.min(min, left * right + solve(i, k, arr, dp) + solve(k + 1, j, arr, dp));
+            int left = getMax(arr, i, k), right = getMax(arr, k + 1, j);
+            min = Math.min(min, left * right + solve(i, k, arr, dp) + 
+            solve(k + 1, j, arr, dp));
         }
         return dp[i][j] = min;
+    }
+    private int getMax(int[] arr, int start, int end) {
+        int max = Math.max(arr[start], arr[end]);
+        for (int i = start; i <= end; i++) {
+            max = Math.max(max, arr[i]);
+        }
+        return max;
     }
 }
