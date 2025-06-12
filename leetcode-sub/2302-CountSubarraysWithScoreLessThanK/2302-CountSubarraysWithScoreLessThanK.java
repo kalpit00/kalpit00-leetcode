df310267f0221378deea63732e16c390805a16df
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-12 17:43:51 -0400

    File updated at 6/12/2025, 5:43:51 PM

--- leetcode-sub/2302-CountSubarraysWithScoreLessThanK/2302-CountSubarraysWithScoreLessThanK.java
+++ leetcode-sub/2302-CountSubarraysWithScoreLessThanK/2302-CountSubarraysWithScoreLessThanK.java
@@ -1,18 +1,26 @@
-// Last updated: 4/28/2025, 4:42:52 AM
+// Last updated: 6/12/2025, 5:43:51 PM
 class Solution {
-    public long countSubarrays(int[] nums, long k) {
-        int left = 0, right = 0, n = nums.length;
-        long count = 0, sum = 0;
-        while (right < n) {
-            sum += nums[right]; // running Sum
-            right++;
-            // score = sum * len of subarr
-            while (sum * (right - left) >= k) {
-                sum -= nums[left];
-                left++;
+    public int lenLongestFibSubseq(int[] nums) {
+        int n = nums.length, max = 0;
+        int[][] dp = new int[n][n];
+        for (int i = 2; i < n; i++) {
+            int start = 0, end = i - 1;
+            while (start < end) {
+                int sum = nums[start] + nums[end];
+                if (sum > nums[i]) {
+                    end--;
+                }
+                else if (sum < nums[i]) {
+                    start++;
+                }
+                else {
+                    dp[end][i] = Math.max(dp[end][i], dp[start][end] + 1);
+                    max = Math.max(max, dp[end][i]);
+                    start++;
+                    end--;
+                }
             }
-            count += right - left;
         }
-        return count;
+        return max == 0 ? 0 : max + 2;
     }
 }
