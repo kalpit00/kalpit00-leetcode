54182caff3c62d84cb1037422c74dbf6aa062cc3
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-15 16:39:27 -0400

    Create file

---
+++ leetcode-sub/3443-MaximumManhattanDistanceAfterKChanges/3443-MaximumManhattanDistanceAfterKChanges.java
@@ -0,0 +1,16 @@
+// Last updated: 6/15/2025, 4:39:26 PM
+class Solution {
+    public int maxDistance(String str, int k) {
+        int n = 0, s = 0, e = 0, w = 0, max = 0;
+        for (char c : str.toCharArray()) {
+            n += c == 'N' ? 1 : 0;
+            s += c == 'S' ? 1 : 0;
+            e += c == 'E' ? 1 : 0;
+            w += c == 'W' ? 1 : 0;
+            int y = Math.min(k, Math.min(n, s));
+            int x = Math.min(k - y, Math.min(w, e));
+            max = Math.max(max, Math.abs(n - s) + Math.abs(w - e) + 2*(x + y));
+        }
+        return max;
+    }
+}
