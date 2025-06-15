09a298b816097d165f9d9418483e7f033cf7e9ca
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-15 02:05:13 -0400

    File updated at 6/15/2025, 2:05:13 AM

--- leetcode-sub/1247-MinimumSwapstoMakeStringsEqual/1247-MinimumSwapstoMakeStringsEqual.java
+++ leetcode-sub/1247-MinimumSwapstoMakeStringsEqual/1247-MinimumSwapstoMakeStringsEqual.java
@@ -1,18 +1,32 @@
-// Last updated: 6/15/2025, 2:02:52 AM
+// Last updated: 6/15/2025, 2:05:13 AM
 class Solution {
     public int minimumSwap(String s1, String s2) {
-        int x1 = 0, x2 = 0, y1 = 0, y2 = 0, n = s1.length();
-        for (int i = 0; i < n; i++) {
-            char c1 = s1.charAt(i), c2 = s2.charAt(i);
-            if (c1 == c2) continue;
-            x1 += c1 == 'x' ? 1 : 0;
-            x2 += c2 == 'x' ? 1 : 0;
-            y1 += c1 == 'y' ? 1 : 0;
-            y2 += c2 == 'y' ? 1 : 0;
+        int n=s1.length();
+        int x=0,y=0;
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            char ch1=s1.charAt(i);
+            char ch2=s2.charAt(i);
+            if(ch1=='x' && ch2=='y')
+            {
+                x++;
+            }
+            if(ch2=='x' && ch1=='y')
+            {
+                y++;
+            }
+            
         }
-        if ((x1 + x2) % 2 != 0 || (y1 + y2) % 2 != 0) {
+        if(x%2!=y%2)
+        {
             return -1;
         }
-        return x1 / 2 + y1 / 2 + (x1 % 2) * 2;
+      int ans=x/2+y/2;
+      if(x%2==1)
+      {
+        ans+=2;
+      }
+      return ans;
     }
 }
