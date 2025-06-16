a0b2452f6dbceca4be0b5f4c8308f1a8034d4e92
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-16 16:39:43 -0400

    Create file

---
+++ leetcode-sub/1047-RemoveAllAdjacentDuplicatesInString/1047-RemoveAllAdjacentDuplicatesInString.java
@@ -0,0 +1,25 @@
+// Last updated: 6/16/2025, 4:39:43 PM
+class Solution {
+    public String removeDuplicates(String s) {
+        return removeDuplicates(s, 2);
+    }
+    public String removeDuplicates(String s, int k) {
+        Stack<int[]> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            int count = !stack.isEmpty() && stack.peek()[0] == c ? 
+            stack.peek()[1] + 1 : 1;
+            stack.push(new int[]{c, count});
+            if (count == k) {
+                while (!stack.isEmpty() && count > 0) {
+                    stack.pop();
+                    count--;
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append((char) stack.pop()[0]);
+        }
+        return sb.reverse().toString();
+    }
+}
