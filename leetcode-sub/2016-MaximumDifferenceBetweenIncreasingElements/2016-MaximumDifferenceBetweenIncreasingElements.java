d165bf5c8b02c71a183756f87949424e2da76ab5
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-15 20:32:21 -0400

    File updated at 6/15/2025, 8:32:21 PM

--- leetcode-sub/2016-MaximumDifferenceBetweenIncreasingElements/2016-MaximumDifferenceBetweenIncreasingElements.java
+++ leetcode-sub/2016-MaximumDifferenceBetweenIncreasingElements/2016-MaximumDifferenceBetweenIncreasingElements.java
@@ -1,4 +1,4 @@
-// Last updated: 6/15/2025, 8:31:20 PM
+// Last updated: 6/15/2025, 8:32:21 PM
 class Solution {
     public int maximumDifference(int[] nums) {
         int n = nums.length, max = -1;
@@ -6,8 +6,6 @@ class Solution {
         pre[0] = nums[0];
         for (int i = 1; i < n; i++) {
             pre[i] = Math.min(pre[i - 1], nums[i]);
-        }
-        for (int i = 1; i < n; i++) {
             if (pre[i - 1] < nums[i]) {
                 max = Math.max(max, nums[i] - pre[i - 1]);
             }
