10e79e0ef2295369a09cb909fdec0d0c605f9795
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-16 20:11:09 -0400

    File updated at 6/16/2025, 8:11:09 PM

--- leetcode-sub/866-PrimePalindrome/866-PrimePalindrome.java
+++ leetcode-sub/866-PrimePalindrome/866-PrimePalindrome.java
@@ -1,23 +1,25 @@
-// Last updated: 6/16/2025, 7:54:36 PM
+// Last updated: 6/16/2025, 8:11:09 PM
 class Solution {
     public int primePalindrome(int n) {
-        while (true) {
-            if (n == reverse(n) && isPrime(n)) {
-                return n;
+        for (int i = n; i <= 200000000; i++) {
+            i = i > 10000000 && i < 100000000 ? 100000000 : i;
+            // no palindromes between [10000000, 100000000], move 'i' ahead
+            if (i == reverse(i) && isPrime(i)) {
+                return i;
             }
-            n++;
-            if (10000000 < n && n < 100000000) {
-                n = 100000000;
-            } // Any even length palindrome must be divisble by 11
-        } // so we will skip numbers N = [10,000,000, 99,999,999]
+        }
+        return -1;
     }
 
     public boolean isPrime(int n) {
-        if (n < 2) return false;
-        for (int i = 2; i * i <= n; i++)
+        if (n < 2) {
+            return false;
+        } 
+        for (int i = 2; i * i <= n; i++) {
             if (n % i == 0) {
                 return false;
-            } 
+            }
+        }
         return true;
     }
 
