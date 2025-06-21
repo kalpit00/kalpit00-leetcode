347ddeff3e68fde7c9235d3a19750ddbb689a680
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-21 18:30:49 -0400

    File updated at 6/21/2025, 6:30:49 PM

--- leetcode-sub/2477-MinimumFuelCosttoReporttotheCapital/2477-MinimumFuelCosttoReporttotheCapital.java
+++ leetcode-sub/2477-MinimumFuelCosttoReporttotheCapital/2477-MinimumFuelCosttoReporttotheCapital.java
@@ -1,4 +1,4 @@
-// Last updated: 6/21/2025, 6:24:20 PM
+// Last updated: 6/21/2025, 6:30:49 PM
 class Solution {
     public long minimumFuelCost(int[][] roads, int seats) {
         int n = roads.length + 1;
@@ -15,7 +15,6 @@ class Solution {
             indegree[v]++;
         }
         long[] dp = new long[n];
-        boolean[] visited = new boolean[n];
         Arrays.fill(dp, 1);
         Queue<long[]> queue = new LinkedList<>();
         for (int i = 1; i < n; i++) {
@@ -23,26 +22,24 @@ class Solution {
                 queue.offer(new long[]{i, 1});
             }
         }
-        long sum = 0;
         while (!queue.isEmpty()) {
             long[] item = queue.poll();
             int node = (int) item[0];
-            if (visited[node]) continue;
-            visited[node] = true;
             long dist = item[1];
             for (int neighbor : adj.get(node)) {
-                if (!visited[neighbor]) {
-                    dp[neighbor] += dist;
-                    indegree[neighbor]--;
-                    if (indegree[neighbor] == 1 && neighbor != 0) {
-                        queue.offer(new long[]{neighbor, dp[neighbor]});
-                    }
+                if (indegree[neighbor] == 0) continue;
+                dp[neighbor] += dist; // skip visited here instead
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 1 && neighbor != 0) {
+                    queue.offer(new long[]{neighbor, dp[neighbor]});
                 }
-            }
+            } // use indegree = 0 as visited trick! avoid boolean[] visited
+            indegree[node] = 0;
         }
+        long sum = 0;
         for (int i = 1; i < n; i++) {
-            sum += Math.ceil((double) dp[i] / seats);
-        }
+            sum += (dp[i] + seats - 1) / seats;
+        } // trick to avoid cast <==> ceil((double) dp[i] / seats)
         return sum;
     }
 }
