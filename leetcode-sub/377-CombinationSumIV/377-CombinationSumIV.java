6d982c1fb7a425689b0ab292c9c1d5c8ff305d08
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-23 19:47:41 -0400

    File updated at 6/23/2025, 7:47:41 PM

--- leetcode-sub/377-CombinationSumIV/377-CombinationSumIV.java
+++ leetcode-sub/377-CombinationSumIV/377-CombinationSumIV.java
@@ -1,21 +1,21 @@
-// Last updated: 6/23/2025, 7:46:25 PM
+// Last updated: 6/23/2025, 7:47:41 PM
 class Solution {
     int mod = 1000000007;
     public int numberOfWays(int a, int b) {
         int n = (int) Math.round(Math.pow(a, 1.0 / b));
-        int[] nums = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            nums[i] = (int) Math.pow(i, b);
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = (int) Math.pow(i + 1, b);
         }
-        Integer[][] dp = new Integer[n + 1][a + 1];
-        return solve(1, nums, 0, a, dp);
+        Integer[][] dp = new Integer[n][a];
+        return solve(0, nums, 0, a, dp);
     }
 
     private int solve(int i, int[] nums, int sum, int target, Integer[][] dp) {
         if (sum == target) {
             return 1;
         }
-        if (i >= nums.length || sum > target) {
+        if (i >= nums.length || sum >= target) {
             return 0;
         }
         if (dp[i][sum] != null) {
