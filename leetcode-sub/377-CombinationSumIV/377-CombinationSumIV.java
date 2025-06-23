4fac58b555bdfb271918627d2d72ec808247f131
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-23 19:46:26 -0400

    File updated at 6/23/2025, 7:46:25 PM

--- leetcode-sub/377-CombinationSumIV/377-CombinationSumIV.java
+++ leetcode-sub/377-CombinationSumIV/377-CombinationSumIV.java
@@ -1,26 +1,31 @@
-// Last updated: 6/23/2025, 2:31:32 PM
+// Last updated: 6/23/2025, 7:46:25 PM
 class Solution {
-    public int combinationSum4(int[] nums, int target) {
-        int n = nums.length;
-        Integer[][] dp = new Integer[n][target];
-        return solve(0, n, nums, 0, target, dp);
-    }
-    private int solve(int i, int n, int[] nums, int sum, 
-    int target, Integer[][] dp) {       
-        if (i >= n || sum > target) {
-            return 0;
+    int mod = 1000000007;
+    public int numberOfWays(int a, int b) {
+        int n = (int) Math.round(Math.pow(a, 1.0 / b));
+        int[] nums = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            nums[i] = (int) Math.pow(i, b);
         }
+        Integer[][] dp = new Integer[n + 1][a + 1];
+        return solve(1, nums, 0, a, dp);
+    }
+
+    private int solve(int i, int[] nums, int sum, int target, Integer[][] dp) {
         if (sum == target) {
             return 1;
         }
+        if (i >= nums.length || sum > target) {
+            return 0;
+        }
         if (dp[i][sum] != null) {
             return dp[i][sum];
-        }
-        int notTake = solve(i + 1, n, nums, sum, target, dp);
+        } 
+        int notTake = solve(i + 1, nums, sum, target, dp);
         int take = 0;
         if (sum + nums[i] <= target) {
-            take += solve(0, n, nums, sum + nums[i], target, dp);
+            take = solve(i + 1, nums, sum + nums[i], target, dp);
         }
-        return dp[i][sum] = take + notTake;
+        return dp[i][sum] = (take + notTake) % mod;
     }
 }
