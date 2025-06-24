c37a03c8563e85a86f3c7d38f989e6065fe057a4
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-24 19:53:23 -0400

    File updated at 6/24/2025, 7:53:23 PM

--- leetcode-sub/279-PerfectSquares/279-PerfectSquares.java
+++ leetcode-sub/279-PerfectSquares/279-PerfectSquares.java
@@ -1,34 +1,15 @@
-// Last updated: 6/24/2025, 4:12:44 PM
+// Last updated: 6/24/2025, 7:53:23 PM
 class Solution {
-    public int numSquares(int num) {
-        int n = (int) Math.sqrt(num);
-        int[] nums = new int[n];
+    public int nthUglyNumber(int n) {
+        TreeSet<Long> set = new TreeSet<>();
+        set.add(1L);
+        Long num = 1L;
         for (int i = 0; i < n; i++) {
-            nums[i] = (i + 1) * (i + 1);
+            num = set.pollFirst(); // poll 'n' times for nth ugly number!
+            set.add(num * 2);
+            set.add(num * 3);
+            set.add(num * 5);
         }
-        Integer[][] dp = new Integer[n][num + 1];
-        int res = solve(0, nums, 0, num, dp);
-        return res == Integer.MAX_VALUE ? -1 : res;
-    }
-    
-    private int solve(int i, int[] nums, int sum, int target, Integer[][] dp) {
-        if (sum == target) {
-            return 0;
-        }
-        if (i >= nums.length || sum > target) {
-            return Integer.MAX_VALUE;
-        }
-        if (dp[i][sum] != null) {
-            return dp[i][sum];
-        }
-        int notTake = solve(i + 1, nums, sum, target, dp);
-        int take = Integer.MAX_VALUE;
-        if (nums[i] <= target - sum) {
-            int res = solve(i, nums, sum + nums[i], target, dp);
-            if (res != Integer.MAX_VALUE) {
-                take = 1 + res;
-            }
-        }
-        return dp[i][sum] = Math.min(take, notTake);
+        return num.intValue();
     }
 }
