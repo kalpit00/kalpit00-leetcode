9c3f2aea2b079e267de8d771e4dc167b7f401ce3
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-24 01:58:30 -0400

    File updated at 6/24/2025, 1:58:30 AM

--- leetcode-sub/2200-FindAllK-DistantIndicesinanArray/2200-FindAllK-DistantIndicesinanArray.java
+++ leetcode-sub/2200-FindAllK-DistantIndicesinanArray/2200-FindAllK-DistantIndicesinanArray.java
@@ -1,32 +1,29 @@
-// Last updated: 6/24/2025, 1:55:28 AM
+// Last updated: 6/24/2025, 1:58:30 AM
 class Solution {
     public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
         int n = nums.length, m = n + 2 * k;
         int[] arr = new int[n + 2 * k];
         for (int i = 0; i < k; i++) {
-            arr[i] = -1;
-            arr[m - i - 1] = -1;
+            arr[i] = 2000;
+            arr[m - i - 1] = 2000;
         }
         for (int i = 0; i < n; i++) {
             arr[i + k] = nums[i];
         }
         List<Integer> res = new ArrayList<>();
-        Map<Integer, Integer> map = new HashMap<>();
+        int[] map = new int[2001];
         for (int i = 0; i <= 2 * k; i++) {
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            map[arr[i]]++;
         }
-        if (map.containsKey(key)) {
+        if (map[key] > 0) {
             res.add(0);
         }
         for (int i = 1; i < n; i++) {
-            map.put(arr[i - 1], map.get(arr[i - 1]) - 1);
-            if (map.get(arr[i - 1]) == 0) {
-                map.remove(arr[i - 1]);
-            }
-            map.put(arr[i + 2 * k], map.getOrDefault(arr[i + 2 * k], 0) + 1);
-            if (map.containsKey(key)) {
+            map[arr[i - 1]]--;
+            map[arr[i + 2 * k]]++;
+            if (map[key] > 0) {
                 res.add(i);
-            } 
+            }
         }
         return res;
     }
