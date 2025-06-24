25204a56a1d57666fc758c6e7c37092e7c334617
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-24 14:37:04 -0400

    Create file

---
+++ leetcode-sub/2787-WaystoExpressanIntegerasSumofPowers/2787-WaystoExpressanIntegerasSumofPowers.java
@@ -0,0 +1,28 @@
+// Last updated: 6/24/2025, 2:37:03 PM
+class Solution {
+    int mod = 1000000007;
+    public int numberOfWays(int a, int b) {
+        int n = (int) Math.round(Math.pow(a, 1.0 / b));
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = (int) Math.pow(i + 1, b);
+        }
+        Integer[][] dp = new Integer[n][a];
+        return solve(0, nums, 0, a, dp);
+    }
+
+    private int solve(int i, int[] nums, int sum, int target, Integer[][] dp) {
+        if (sum == target) {
+            return 1;
+        }
+        if (i >= nums.length || sum >= target) {
+            return 0;
+        }
+        if (dp[i][sum] != null) {
+            return dp[i][sum];
+        } 
+        int notTake = solve(i + 1, nums, sum, target, dp);
+        int take = solve(i + 1, nums, sum + nums[i], target, dp);
+        return dp[i][sum] = (take + notTake) % mod;
+    }
+}
