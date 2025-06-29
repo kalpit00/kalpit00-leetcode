e97464616a6ce7157d22b49165e6c892462ad4a3
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-28 20:20:40 -0400

    File updated at 6/28/2025, 8:20:40 PM

--- leetcode-sub/1498-NumberofSubsequencesThatSatisfytheGivenSumCondition/1498-NumberofSubsequencesThatSatisfytheGivenSumCondition.java
+++ leetcode-sub/1498-NumberofSubsequencesThatSatisfytheGivenSumCondition/1498-NumberofSubsequencesThatSatisfytheGivenSumCondition.java
@@ -1,30 +1,24 @@
-// Last updated: 6/28/2025, 8:18:21 PM
+// Last updated: 6/28/2025, 8:20:40 PM
 class Solution {
+    int mod = 1000000007;
     public int numSubseq(int[] nums, int target) {
-        int n = nums.length;
-        int mod = 1_000_000_007;
+        int n = nums.length, count = 0, left = 0, right = n - 1;
         Arrays.sort(nums);
-        
-        // Precompute the value of 2 to the power of each value.
         int[] power = new int[n];
         power[0] = 1;
-        for (int i = 1; i < n; ++i) {
+        for (int i = 1; i < n; i++) {
             power[i] = (power[i - 1] * 2) % mod;
-        }
-        
-        int answer = 0;
-        int left = 0, right = n - 1;
-
+        } // pow[i] =  2^(i) with MOD!
         while (left <= right) {
             if (nums[left] + nums[right] <= target) {
-                answer += power[right - left];
-                answer %= mod;
+                count += power[right - left]; // 2^(r - l) overflows integer 
+                count %= mod;
                 left++;
-            } else {
+            }
+            else {
                 right--;
             }
         }
-        
-        return answer;
+        return count;
     }
 }
