45e0d98378bb4d0c50bbf251b01e3848d3449c25
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-06-29 20:07:12 -0400

    Create file

---
+++ leetcode-sub/594-LongestHarmoniousSubsequence/594-LongestHarmoniousSubsequence.java
@@ -0,0 +1,16 @@
+// Last updated: 6/29/2025, 8:07:12 PM
+class Solution {
+    public int findLHS(int[] nums) {
+        int max = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int key : map.keySet()) {
+            if (map.containsKey(key + 1)) {
+                max = Math.max(max, map.get(key) + map.get(key + 1));
+            }
+        }
+        return max;
+    }
+}
