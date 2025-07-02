494ab735ced164bbdbc2fd1302341313967a482c
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-07-01 21:22:08 -0400

    File updated at 7/1/2025, 9:22:08 PM

--- leetcode-sub/3333-FindtheOriginalTypedStringII/3333-FindtheOriginalTypedStringII.java
+++ leetcode-sub/3333-FindtheOriginalTypedStringII/3333-FindtheOriginalTypedStringII.java
@@ -1,51 +1,37 @@
-// Last updated: 7/1/2025, 9:21:14 PM
+// Last updated: 7/1/2025, 9:22:08 PM
 class Solution {
-
-    private static final int mod = 1000000007;
-
+    private static final long MOD = (long)1e9 + 7;
     public int possibleStringCount(String word, int k) {
+        List<Integer> list = new ArrayList<>();
         int n = word.length();
-        int cnt = 1;
-        List<Integer> freq = new ArrayList<>();
-        for (int i = 1; i < n; ++i) {
-            if (word.charAt(i) == word.charAt(i - 1)) {
-                ++cnt;
-            } else {
-                freq.add(cnt);
-                cnt = 1;
+        int i = 0;
+        while (i < n) {
+            int j = i+1;
+            while (j < n && word.charAt(j) == word.charAt(j-1)) j++;
+            list.add(j - i);
+            i = j;
+        }
+        int m = list.size();
+        long[] power = new long[m];
+        power[m-1] = list.get(m-1);
+        for (i = m-2; i >= 0; i--) {
+            power[i] = (power[i+1] * list.get(i)) % MOD;
+        }
+        if (m >= k) return (int)power[0];
+
+        long[][] dp = new long[m][k-m+1];
+        for (i = 0; i < k-m+1; i++) {
+            if (list.get(m-1) + i + m > k) dp[m-1][i] = list.get(m-1) - (k-m-i);
+        }
+        for (i = m-2; i >= 0; i--) {
+            long sum = (dp[i+1][k-m] * list.get(i)) % MOD;
+            for (int j = k-m; j >= 0; j--) {
+                sum += dp[i+1][j];
+                if (j + list.get(i) > k-m) sum = (sum - dp[i+1][k-m] + MOD) % MOD;
+                else sum = (sum - dp[i+1][j+list.get(i)] + MOD) % MOD;
+                dp[i][j] = sum;
             }
         }
-        freq.add(cnt);
-
-        long ans = 1;
-        for (int o : freq) {
-            ans = (ans * o) % mod;
-        }
-        if (freq.size() >= k) {
-            return (int) ans;
-        }
-
-        int[] f = new int[k];
-        int[] g = new int[k];
-        f[0] = 1;
-        Arrays.fill(g, 1);
-        for (int i = 0; i < freq.size(); ++i) {
-            int[] f_new = new int[k];
-            for (int j = 1; j < k; ++j) {
-                f_new[j] = g[j - 1];
-                if (j - freq.get(i) - 1 >= 0) {
-                    f_new[j] = (f_new[j] - g[j - freq.get(i) - 1] + mod) % mod;
-                }
-            }
-            int[] g_new = new int[k];
-            g_new[0] = f_new[0];
-            for (int j = 1; j < k; ++j) {
-                g_new[j] = (g_new[j - 1] + f_new[j]) % mod;
-            }
-            f = f_new;
-            g = g_new;
-        }
-
-        return (int) ((ans - g[k - 1] + mod) % mod);
+        return (int)dp[0][0];
     }
 }
