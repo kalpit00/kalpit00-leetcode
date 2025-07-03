da35bb97d31392fea675c9ae5866b0fd0df08829
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-07-02 21:26:51 -0400

    Create file

---
+++ leetcode-sub/3304-FindtheK-thCharacterinStringGameI/3304-FindtheK-thCharacterinStringGameI.java
@@ -0,0 +1,17 @@
+// Last updated: 7/2/2025, 9:26:51 PM
+class Solution {
+
+    public char kthCharacter(int k) {
+        int ans = 0;
+        int t;
+        while (k != 1) {
+            t = 31 - Integer.numberOfLeadingZeros(k);
+            if ((1 << t) == k) {
+                t--;
+            }
+            k = k - (1 << t);
+            ans++;
+        }
+        return (char) ('a' + ans);
+    }
+}
