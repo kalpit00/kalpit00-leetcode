6ac07227fa98f12241b0d2214b3bf5772270c90e
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-07-04 00:15:56 -0400

    File updated at 7/4/2025, 12:15:56 AM

--- leetcode-sub/2386-FindtheK-SumofanArray/2386-FindtheK-SumofanArray.java
+++ leetcode-sub/2386-FindtheK-SumofanArray/2386-FindtheK-SumofanArray.java
@@ -1,27 +1,42 @@
-// Last updated: 7/4/2025, 12:08:46 AM
-class Solution {
+// Last updated: 7/4/2025, 12:15:56 AM
+import java.util.*;
+public class Solution {
     public long kSum(int[] nums, int k) {
-        long sum = 0, total = 0;
         int n = nums.length;
+        // Step 1: Calculate maxSum by taking all positive numbers
+        long maxSum = 0;
         for (int i = 0; i < n; i++) {
-            total += nums[i] > 0 ? nums[i] : 0;
-            nums[i] = nums[i] < 0 ? -nums[i] : nums[i];
+            if (nums[i] > 0) maxSum += nums[i];
+            nums[i] = Math.abs(nums[i]); // convert to abs
         }
+        // Step 2: Sort the array
         Arrays.sort(nums);
-        PriorityQueue<long[]> pq = new PriorityQueue<>((a, b) -> Long.compare(a[0], b[0]));
-        pq.offer(new long[]{nums[0], 0});
-        while (k > 1) {
-            long[] item = pq.poll();
-            int i = (int) item[1]++;
-            long val = item[0];
-            sum = val;
-            if (i < n - 1) {
-                item[0] += nums[i + 1];
-                pq.offer(new long[]{val - nums[i] + nums[i + 1], i + 1});
-                pq.offer(item);
+        // Step 3: Min-heap to track next smallest subtractions
+        PriorityQueue<Pair> heap = new PriorityQueue<>((a, b) -> Long.compare(a.sum, b.sum));
+        heap.offer(new Pair(nums[0], 0));
+        int count = 1; // We already have the largest (maxSum)
+        long res = maxSum;
+
+        while (count < k) {
+            Pair current = heap.poll();
+            res = maxSum - current.sum; // k-th largest = maxSum - k-th smallest subtraction
+            count++;
+            if (current.index + 1 < n) {
+                // Case 1: Include next element
+                heap.offer(new Pair(current.sum + nums[current.index + 1], current.index + 1));
+                // Case 2: Replace current element with next
+                heap.offer(new Pair(current.sum - nums[current.index] + nums[current.index + 1], current.index + 1));
             }
-            k--;
         }
-        return total - sum;
+        return res;
+    }
+    static class Pair {
+        long sum;
+        int index;
+
+        public Pair(long sum, int index) {
+            this.sum = sum;
+            this.index = index;
+        }
     }
 }
