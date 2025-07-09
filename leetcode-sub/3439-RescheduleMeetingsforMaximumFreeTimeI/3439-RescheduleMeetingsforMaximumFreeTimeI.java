3d94053074faac2f6407f35d3e33f8c42f9c48fa
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-07-08 23:03:04 -0400

    File updated at 7/8/2025, 11:03:04 PM

--- leetcode-sub/3439-RescheduleMeetingsforMaximumFreeTimeI/3439-RescheduleMeetingsforMaximumFreeTimeI.java
+++ leetcode-sub/3439-RescheduleMeetingsforMaximumFreeTimeI/3439-RescheduleMeetingsforMaximumFreeTimeI.java
@@ -1,16 +1,20 @@
-// Last updated: 7/8/2025, 10:53:43 PM
+// Last updated: 7/8/2025, 11:03:04 PM
 class Solution {
     public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
-        int n = startTime.length, res = 0;
-        int[] pre = new int[n + 1];
-        for (int i = 0; i < n; i++) {
-            pre[i + 1] = pre[i] + endTime[i] - startTime[i];
+        int n = startTime.length, sum = 0, max = 0;
+        for (int i = 0; i < k; i++) {
+            sum += endTime[i] - startTime[i];
         }
-        for (int i = k - 1; i < n; i++) {
-            int right = i == n - 1 ? eventTime : startTime[i + 1];
-            int left = i == k - 1 ? 0 : endTime[i - k];
-            res = Math.max(res, right - left - (pre[i + 1] - pre[i - k + 1]));
+        int left = 0, right = (k == n) ? eventTime : startTime[k];
+        max = Math.max(max, right - left - sum);
+        for (int i = k; i < n; i++) {
+            int incoming = endTime[i] - startTime[i];
+            int outgoing = endTime[i - k] - startTime[i - k];
+            sum += incoming - outgoing;
+            left = endTime[i - k];
+            right = (i == n - 1) ? eventTime : startTime[i + 1];
+            max = Math.max(max, right - left - sum);
         }
-        return res;
+        return max;
     }
 }
