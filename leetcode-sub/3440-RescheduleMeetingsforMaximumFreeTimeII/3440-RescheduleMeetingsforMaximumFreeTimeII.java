1009a2099126f0f0fa4ec0386c3cbfac9d7ca150
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-07-09 23:42:49 -0400

    Create file

---
+++ leetcode-sub/3440-RescheduleMeetingsforMaximumFreeTimeII/3440-RescheduleMeetingsforMaximumFreeTimeII.java
@@ -0,0 +1,23 @@
+// Last updated: 7/9/2025, 11:42:48 PM
+class Solution {
+    public int maxFreeTime(int eventTime, int[] startTime, int[] endTime) {
+        int n = startTime.length, res = 0;
+        for (int i = 0, t1 = 0, t2 = 0; i < n; i++) {
+            int left1 = i == 0 ? 0 : endTime[i - 1];
+            int right1 = i == n - 1 ? eventTime : startTime[i + 1];
+            if (endTime[i] - startTime[i] <= t1) {
+                res = Math.max(res, right1 - left1);
+            }
+            t1 = Math.max(t1, startTime[i] - (i == 0 ? 0 : endTime[i - 1]));
+            res = Math.max(res, right1 - left1 - (endTime[i] - startTime[i]));
+            int left2 = i == n - 1 ? 0 : endTime[n - i - 2];
+            int right2 = i == 0 ? eventTime : startTime[n - i];
+            if (endTime[n - i - 1] - startTime[n - i - 1] <= t2) {
+                res = Math.max(res, right2 - left2);
+            }
+            t2 = Math.max(t2, (i == 0 ? eventTime : startTime[n - i]) -
+            endTime[n - i - 1]);
+        }
+        return res;
+    }
+}
