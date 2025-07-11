1785d25e4e42150db6cd32455f556fb7c81c3101
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-07-11 18:43:24 -0400

    File updated at 7/11/2025, 6:43:24 PM

--- leetcode-sub/2065-MaximumPathQualityofaGraph/2065-MaximumPathQualityofaGraph.java
+++ leetcode-sub/2065-MaximumPathQualityofaGraph/2065-MaximumPathQualityofaGraph.java
@@ -1,4 +1,4 @@
-// Last updated: 7/11/2025, 6:24:53 PM
+// Last updated: 7/11/2025, 6:43:24 PM
 class Solution {
     public int[] countServers(int n, int[][] logs, int x, int[] queries) {
         int m = queries.length, left = 0, right = 0, count = 0, k = logs.length;
@@ -13,19 +13,17 @@ class Solution {
         for (int[] query : nums) {
             int start = query[0], end = start + x, idx = query[1];
             while (right < k && logs[right][1] <= end) {
-                int key = logs[right][0];
-                if (map[key] == 0) {
+                if (map[logs[right][0]] == 0) {
                     count++;
                 }
-                map[key]++;
+                map[logs[right][0]]++;
                 right++;
             }
             while (left < k && logs[left][1] < start) {
-                int key = logs[left][0];
-                if (map[key] == 1) {
+                if (map[logs[left][0]] == 1) {
                     count--;
                 }
-                map[key]--;
+                map[logs[left][0]]--;
                 left++;
             }
             res[idx] = n - count;
