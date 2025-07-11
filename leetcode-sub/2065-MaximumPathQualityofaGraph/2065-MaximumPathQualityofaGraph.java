3ef3450355da7b58281b293cfcccd89fbb9ced12
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-07-11 18:24:53 -0400

    File updated at 7/11/2025, 6:24:53 PM

--- leetcode-sub/2065-MaximumPathQualityofaGraph/2065-MaximumPathQualityofaGraph.java
+++ leetcode-sub/2065-MaximumPathQualityofaGraph/2065-MaximumPathQualityofaGraph.java
@@ -1,10 +1,9 @@
-// Last updated: 7/11/2025, 6:22:59 PM
+// Last updated: 7/11/2025, 6:24:53 PM
 class Solution {
     public int[] countServers(int n, int[][] logs, int x, int[] queries) {
-        int m = queries.length, left = 0, right = 0, k = logs.length;
+        int m = queries.length, left = 0, right = 0, count = 0, k = logs.length;
         int[][] nums = new int[m][2];
-        int[] res = new int[m];
-        Map<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[m], map = new int[100001];
         for (int i = 0; i < m; i++) {
             nums[i][0] = queries[i] - x;
             nums[i][1] = i;
@@ -15,18 +14,21 @@ class Solution {
             int start = query[0], end = start + x, idx = query[1];
             while (right < k && logs[right][1] <= end) {
                 int key = logs[right][0];
-                map.put(key, map.getOrDefault(key, 0) + 1);
+                if (map[key] == 0) {
+                    count++;
+                }
+                map[key]++;
                 right++;
             }
             while (left < k && logs[left][1] < start) {
                 int key = logs[left][0];
-                map.put(key, map.get(key) - 1);
-                if (map.get(key) == 0) {
-                    map.remove(key);
+                if (map[key] == 1) {
+                    count--;
                 }
+                map[key]--;
                 left++;
             }
-            res[idx] = n - map.size();
+            res[idx] = n - count;
         }
         return res;
     }
