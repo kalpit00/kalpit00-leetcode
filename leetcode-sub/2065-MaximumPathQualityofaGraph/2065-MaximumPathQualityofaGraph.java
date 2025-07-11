a8d60014e665bbb65decf2d8f5bdcff633c86775
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-07-11 18:23:00 -0400

    File updated at 7/11/2025, 6:22:59 PM

--- leetcode-sub/2065-MaximumPathQualityofaGraph/2065-MaximumPathQualityofaGraph.java
+++ leetcode-sub/2065-MaximumPathQualityofaGraph/2065-MaximumPathQualityofaGraph.java
@@ -1,36 +1,33 @@
-// Last updated: 7/11/2025, 1:05:06 AM
+// Last updated: 7/11/2025, 6:22:59 PM
 class Solution {
-    int max = 0;
-    public int maximalPathQuality(int[] values, int[][] edges, int maxTime) {
-        int n = values.length;
-        List<List<int[]>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            adj.add(new ArrayList<>());
+    public int[] countServers(int n, int[][] logs, int x, int[] queries) {
+        int m = queries.length, left = 0, right = 0, k = logs.length;
+        int[][] nums = new int[m][2];
+        int[] res = new int[m];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < m; i++) {
+            nums[i][0] = queries[i] - x;
+            nums[i][1] = i;
         }
-        for (int[] edge : edges) {
-            int u = edge[0], v = edge[1], wt = edge[2];
-            adj.get(u).add(new int[]{v, wt});
-            adj.get(v).add(new int[]{u, wt});
+        Arrays.sort(nums, (a, b) -> a[0] - b[0]);
+        Arrays.sort(logs, (a, b) -> a[1] - b[1]);
+        for (int[] query : nums) {
+            int start = query[0], end = start + x, idx = query[1];
+            while (right < k && logs[right][1] <= end) {
+                int key = logs[right][0];
+                map.put(key, map.getOrDefault(key, 0) + 1);
+                right++;
+            }
+            while (left < k && logs[left][1] < start) {
+                int key = logs[left][0];
+                map.put(key, map.get(key) - 1);
+                if (map.get(key) == 0) {
+                    map.remove(key);
+                }
+                left++;
+            }
+            res[idx] = n - map.size();
         }
-        int[] visited = new int[n]; // stores no of times node i is visited
-        visited[0]++; // start by visited node 0 and taking its value[0]
-        dfs(adj, 0, values[0], maxTime, values, visited);
-        return max;
-    }
-    private void dfs(List<List<int[]>> adj, int i, int sum, int remainingTime,
-    int[] values, int[] visited) {
-        if (i == 0) { // when reach 0 again, max across all possible pathSums
-            max = Math.max(max, sum);
-        }
-        for (int[] neighbor : adj.get(i)) {
-            int j = neighbor[0], time = neighbor[1];
-            if (remainingTime < time) {
-                continue;
-            } // if not enough time to go to next neighbor, skip him
-            visited[j]++; // add values[j] if first time seeing node "j"
-            dfs(adj, j, sum + (visited[j] == 1 ? values[j] : 0),
-            remainingTime - time, values, visited);
-            visited[j]--; // backtrack
-        }
+        return res;
     }
 }
