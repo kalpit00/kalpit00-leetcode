360f11e9480a6a864f461876d9fa48ec27acd729
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-07-13 19:44:37 -0400

    File updated at 7/13/2025, 7:44:37 PM

--- leetcode-sub/2410-MaximumMatchingofPlayersWithTrainers/2410-MaximumMatchingofPlayersWithTrainers.java
+++ leetcode-sub/2410-MaximumMatchingofPlayersWithTrainers/2410-MaximumMatchingofPlayersWithTrainers.java
@@ -1,16 +1,15 @@
-// Last updated: 7/13/2025, 7:43:58 PM
+// Last updated: 7/13/2025, 7:44:37 PM
 class Solution {
     public int matchPlayersAndTrainers(int[] players, int[] trainers) {
         Arrays.sort(players);
         Arrays.sort(trainers);
-        int m = players.length, n = trainers.length, i = 0, j = 0, count = 0;
+        int m = players.length, n = trainers.length, i = 0, j = 0;
         while (i < m && j < n) {
             if (players[i] <= trainers[j]) {
                 i++;
-                count++;
             }
             j++;
         }
-        return count;
+        return i;
     }
 }
