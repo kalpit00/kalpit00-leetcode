f38235938d3733ed724cf8f819b2df57527ee710
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-07-13 20:08:40 -0400

    Create file

---
+++ leetcode-sub/1290-ConvertBinaryNumberinaLinkedListtoInteger/1290-ConvertBinaryNumberinaLinkedListtoInteger.java
@@ -0,0 +1,11 @@
+// Last updated: 7/13/2025, 8:08:40 PM
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        int num = head.val;
+        while (head.next != null) {
+            num = num * 2 + head.next.val;
+            head = head.next;    
+        }
+        return num;
+    }
+}
