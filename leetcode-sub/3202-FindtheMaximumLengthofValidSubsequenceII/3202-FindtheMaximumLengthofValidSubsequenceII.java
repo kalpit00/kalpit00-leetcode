b10ec1cba75006d48ed23dda8aba26d83a9689f4
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-07-16 21:16:57 -0400

    File updated at 7/16/2025, 9:16:57 PM

--- leetcode-sub/3202-FindtheMaximumLengthofValidSubsequenceII/3202-FindtheMaximumLengthofValidSubsequenceII.java
+++ leetcode-sub/3202-FindtheMaximumLengthofValidSubsequenceII/3202-FindtheMaximumLengthofValidSubsequenceII.java
@@ -1,8 +1,11 @@
-// Last updated: 7/16/2025, 8:51:49 PM
+// Last updated: 7/16/2025, 9:16:57 PM
 class Solution {
     public int maximumLength(int[] nums, int k) {
-        int n = nums.length, max = 0;
+        int n = nums.length, max = 1;
         int[][] dp = new int[n][k];
+        for (int[] r : dp) {
+            Arrays.fill(r, 1);
+        }
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 int sum = (nums[j] + nums[i]) % k;
@@ -10,6 +13,6 @@ class Solution {
                 max = Math.max(max, dp[i][sum]);
             }
         }
-        return max + 1;
+        return max;
     }
 }
