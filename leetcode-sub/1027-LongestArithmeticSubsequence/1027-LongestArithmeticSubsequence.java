6898021e948e0bde9598f57c83105bad9ad8161e
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-07-16 21:16:48 -0400

    File updated at 7/16/2025, 9:16:48 PM

--- leetcode-sub/1027-LongestArithmeticSubsequence/1027-LongestArithmeticSubsequence.java
+++ leetcode-sub/1027-LongestArithmeticSubsequence/1027-LongestArithmeticSubsequence.java
@@ -1,8 +1,11 @@
-// Last updated: 7/16/2025, 8:56:11 PM
+// Last updated: 7/16/2025, 9:16:48 PM
 class Solution {
     public int longestArithSeqLength(int[] nums) {
-        int n = nums.length, max = 0, k = 500;
+        int n = nums.length, max = 1, k = 500;
         int[][] dp = new int[n][2 * k + 1];
+        for (int[] r : dp) {
+            Arrays.fill(r, 1);
+        }
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 int d = nums[i] - nums[j] + k;
@@ -10,6 +13,6 @@ class Solution {
                 max = Math.max(max, dp[i][d]);
             }
         }
-        return max + 1;
+        return max;
     }
 }
