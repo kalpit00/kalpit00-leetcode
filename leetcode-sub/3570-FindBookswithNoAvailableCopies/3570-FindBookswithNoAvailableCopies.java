96155e10e5ce2ebed1c583b0025ee4dff3e3c72e
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-07-20 13:31:40 -0400

    File updated at 7/20/2025, 1:31:39 PM

--- leetcode-sub/3570-FindBookswithNoAvailableCopies/3570-FindBookswithNoAvailableCopies.java
+++ leetcode-sub/3570-FindBookswithNoAvailableCopies/3570-FindBookswithNoAvailableCopies.java
@@ -1,20 +1,20 @@
-// Last updated: 7/20/2025, 1:30:46 PM
+// Last updated: 7/20/2025, 1:31:39 PM
 class Solution {
     public int flipgame(int[] fronts, int[] backs) {
-        Set<Integer> set = new HashSet<>();
+        boolean[] visited = new boolean[2001];
         for (int i = 0; i < fronts.length; i++) {
             if (fronts[i] == backs[i]) {
-                set.add(fronts[i]);
+                visited[fronts[i]] = true;
             }
         }
         int min = 9999;
         for (int num : fronts) {
-            if (!set.contains(num)) {
+            if (!visited[num]) {
                 min = Math.min(min, num);
             }
-        }  
+        }
         for (int num : backs) {
-            if (!set.contains(num)) {
+            if (!visited[num]) {
                 min = Math.min(min, num);
             }
         }
