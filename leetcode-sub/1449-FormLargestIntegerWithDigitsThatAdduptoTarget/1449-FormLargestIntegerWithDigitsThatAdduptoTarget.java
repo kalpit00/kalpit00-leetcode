ea91ffa960447e4de6c68c5d34a284272645f0aa
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-07-20 02:22:18 -0400

    File updated at 7/20/2025, 2:22:18 AM

--- leetcode-sub/1449-FormLargestIntegerWithDigitsThatAdduptoTarget/1449-FormLargestIntegerWithDigitsThatAdduptoTarget.java
+++ leetcode-sub/1449-FormLargestIntegerWithDigitsThatAdduptoTarget/1449-FormLargestIntegerWithDigitsThatAdduptoTarget.java
@@ -1,36 +1,45 @@
-// Last updated: 7/20/2025, 2:19:07 AM
+// Last updated: 7/20/2025, 2:22:18 AM
 class Solution {
     public String largestNumber(int[] cost, int target) {
-        String[] dp = new String[target + 1];
-        Arrays.fill(dp, null);
-        dp[0] = "";
-        String res = dfs(target, cost, dp);
-        return res.equals("0") ? "0" : res;
-    }
-    private String dfs(int sum, int[] cost, String[] dp) {
-        if (sum < 0) {
-            return "0";
+
+        String[][] dp=new String[cost.length+1][target+1];
+
+        for(String arr[]:dp){
+            Arrays.fill(arr,null);
         }
-        if (dp[sum] != null) {
-            return dp[sum];
-        }
-        String max = "0";
-        for (int d = 9; d >= 1; d--) {
-            if (sum >= cost[d - 1]) {
-                String sub = dfs(sum - cost[d - 1], cost, dp);
-                if (!sub.equals("0")) {
-                    String candidate = d + sub;
-                    if (isLarger(candidate, max)) {
-                        max = candidate;
-                    }
-                }
-            }
-        }
-        return dp[sum] = max;
+        return f(cost.length-1,cost,target,dp);    
     }
 
-    private boolean isLarger(String a, String b) {
-        return a.length() > b.length() || 
-        (a.length() == b.length() && a.compareTo(b) > 0);
+    public String f(int i,int[] cost,int target,String[][] dp){
+
+        if(target==0){
+            return "";
+        }
+        if(i<0){
+           return "0";  
+            
+        }
+        if(dp[i][target]!=null){
+            return dp[i][target];
+        }
+        String pick="";
+        String max="0";
+
+        if(target>=cost[i]){
+            pick=f(i,cost,target-cost[i],dp);
+            if(!pick.equals("0")){
+                max=(i+1)+pick;
+            }
+        }
+
+        String notpick=f(i-1,cost,target,dp);
+
+        if(notpick.length()>max.length() || notpick.length()==max.length() && notpick.compareTo(max)>0  ){
+            max=notpick;
+        }
+
+        return dp[i][target]=max;
+
+
     }
 }
