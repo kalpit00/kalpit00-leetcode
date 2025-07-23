2b0466c7c4c109149ad4f34feb1e1ec24ef84efa
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-07-23 13:29:00 -0400

    File updated at 7/23/2025, 1:29:00 PM

--- leetcode-sub/2671-FrequencyTracker/2671-FrequencyTracker.java
+++ leetcode-sub/2671-FrequencyTracker/2671-FrequencyTracker.java
@@ -1,37 +1,25 @@
-// Last updated: 7/23/2025, 1:27:46 PM
+// Last updated: 7/23/2025, 1:29:00 PM
 class FrequencyTracker {
-    Map<Integer, Integer> map;
-    Map<Integer, Set<Integer>> freqMap;
+    int[] map, freq;
+
     public FrequencyTracker() {
-        map = new HashMap<>();
-        freqMap = new HashMap<>();
-        freqMap.put(0, new HashSet<>());
+        map = new int[100001];
+        freq = new int[100001];
     }
-    
+
     public void add(int number) {
-        int oldFreq = map.getOrDefault(number, 0);
-        freqMap.get(oldFreq).remove(number);
-        map.put(number, oldFreq + 1);
-        freqMap.putIfAbsent(oldFreq + 1, new HashSet<>());
-        freqMap.get(oldFreq + 1).add(number);
+        freq[map[number]++]--;
+        freq[map[number]]++;
     }
-    
-    public void deleteOne(int number) {        
-        if (map.containsKey(number)) {
-            int oldFreq = map.get(number);
-            freqMap.get(oldFreq).remove(number);
-            map.put(number, oldFreq - 1);
-            if (map.get(number) == 0) {
-                map.remove(number);
-            }
-            else {
-                freqMap.get(oldFreq - 1).add(number);
-            }
+
+    public void deleteOne(int number) {
+        if (map[number] > 0) {
+            freq[map[number]--]--;
+            freq[map[number]]++;
         }
     }
-    
+
     public boolean hasFrequency(int frequency) {
-        return freqMap.containsKey(frequency) && 
-        !freqMap.get(frequency).isEmpty();
+        return freq[frequency] > 0;
     }
 }
