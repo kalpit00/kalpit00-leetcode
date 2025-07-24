0d18a6e89acf3d905dc872a8d145a28a80f42c69
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-07-24 15:04:37 -0400

    File updated at 7/24/2025, 3:04:37 PM

--- leetcode-sub/2772-ApplyOperationstoMakeAllArrayElementsEqualtoZero/2772-ApplyOperationstoMakeAllArrayElementsEqualtoZero.java
+++ leetcode-sub/2772-ApplyOperationstoMakeAllArrayElementsEqualtoZero/2772-ApplyOperationstoMakeAllArrayElementsEqualtoZero.java
@@ -1,37 +1,69 @@
-// Last updated: 7/24/2025, 3:03:43 PM
+// Last updated: 7/24/2025, 3:04:37 PM
 class Solution {
     public boolean checkArray(int[] nums, int k) {
         int n = nums.length;
-        FenwickTree bit = new FenwickTree(n + 1);
+        SegmentTreeLazy segmentTree = new SegmentTreeLazy(n);
         for (int i = 0; i < n; i++) {
-            int curr = nums[i] - bit.query(i);
+            int curr = nums[i] - segmentTree.query(i, i);
             if (curr < 0) return false;
             if (curr > 0) {
                 if (i + k > n) return false;
-                bit.update(i, curr);
-                bit.update(i + k, -curr);
+                segmentTree.update(i, i + k - 1, curr);
             }
         }
         return true;
     }
 
-    class FenwickTree {
-        int[] tree;
+    class SegmentTreeLazy {
+        int[] tree, lazy;
         int n;
 
-        public FenwickTree(int n) {
-            this.n = n;
-            tree = new int[n + 2];
+        public SegmentTreeLazy(int size) {
+            n = size;
+            tree = new int[4 * n];
+            lazy = new int[4 * n];
         }
 
-        void update(int i, int delta) {
-            for (i++; i <= n; i += i & -i) tree[i] += delta;
+        void push(int node, int l, int r) {
+            if (lazy[node] != 0) {
+                tree[node] += (r - l + 1) * lazy[node];
+                if (l != r) {
+                    lazy[2 * node] += lazy[node];
+                    lazy[2 * node + 1] += lazy[node];
+                }
+                lazy[node] = 0;
+            }
         }
 
-        int query(int i) {
-            int sum = 0;
-            for (i++; i > 0; i -= i & -i) sum += tree[i];
-            return sum;
+        public void update(int l, int r, int val) {
+            update(1, 0, n - 1, l, r, val);
+        }
+
+        private void update(int node, int l, int r, int ql, int qr, int val) {
+            push(node, l, r);
+            if (qr < l || r < ql) return;
+            if (ql <= l && r <= qr) {
+                lazy[node] += val;
+                push(node, l, r);
+                return;
+            }
+            int mid = l + (r - l) / 2;
+            update(2 * node, l, mid, ql, qr, val);
+            update(2 * node + 1, mid + 1, r, ql, qr, val);
+            tree[node] = tree[2 * node] + tree[2 * node + 1];
+        }
+
+        public int query(int l, int r) {
+            return query(1, 0, n - 1, l, r);
+        }
+
+        private int query(int node, int l, int r, int ql, int qr) {
+            push(node, l, r);
+            if (qr < l || r < ql) return 0;
+            if (ql <= l && r <= qr) return tree[node];
+            int mid = l + (r - l) / 2;
+            return query(2 * node, l, mid, ql, qr) +
+                   query(2 * node + 1, mid + 1, r, ql, qr);
         }
     }
 }
