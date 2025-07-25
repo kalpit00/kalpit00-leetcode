14b85cc6b08d488f622e2a6e68e5bdf07478083b
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-07-25 12:28:48 -0400

    File updated at 7/25/2025, 12:28:48 PM

--- leetcode-sub/3216-LexicographicallySmallestStringAfteraSwap/3216-LexicographicallySmallestStringAfteraSwap.java
+++ leetcode-sub/3216-LexicographicallySmallestStringAfteraSwap/3216-LexicographicallySmallestStringAfteraSwap.java
@@ -1,24 +1,17 @@
-// Last updated: 7/25/2025, 12:27:19 PM
+// Last updated: 7/25/2025, 12:28:48 PM
 class Solution {
     public String getSmallestString(String s) {
-        String min = s;
         char[] arr = s.toCharArray();
-        int n = arr.length;
-        for (int i = 1; i < n; i++) {
-            if ((arr[i] - '0') % 2 == (arr[i - 1] - '0') % 2) {
-                swap(arr, i - 1, i);
-                String str = new String(arr);
-                if (str.compareTo(min) < 0) {
-                    min = str;
-                }
-                swap(arr, i - 1, i);
+        for (int i = 1; i < arr.length; i++) {
+            int left = arr[i - 1] - '0';
+            int right = arr[i] - '0';
+            if ((left % 2 == right % 2) && (arr[i ] < arr[i-1])) {
+                char temp = arr[i];
+                arr[i] = arr[i - 1];
+                arr[i - 1] = temp;
+                break;
             }
         }
-        return min;
-    }
-    private void swap(char[] arr, int i, int j) {
-        char c = arr[i];
-        arr[i] = arr[j];
-        arr[j] = c;
+        return new String(arr);
     }
 }
