e31b292ba311af1aab48fd2f9c49062f65e3f9e1
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-07-26 14:34:31 -0400

    File updated at 7/26/2025, 2:34:31 PM

--- leetcode-sub/841-KeysandRooms/841-KeysandRooms.java
+++ leetcode-sub/841-KeysandRooms/841-KeysandRooms.java
@@ -1,21 +1,22 @@
-// Last updated: 7/26/2025, 2:33:16 PM
+// Last updated: 7/26/2025, 2:34:31 PM
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        int n = rooms.size();
-        Set<Integer> visited = new HashSet<>();
+        int n = rooms.size(), count = 1;
+        boolean[] visited = new boolean[n];
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(0);
-        visited.add(0);
+        visited[0] = true;
         while (!queue.isEmpty()) {
             int room = queue.poll();
             for (int key : rooms.get(room)) {
-                if (!visited.contains(key)) {
-                    visited.add(key);
+                if (!visited[key]) {
+                    visited[key] = true;
+                    count++;
                     queue.offer(key);
                 }
             }
         }
-        return visited.size() == n;
+        return count == n;
     }
 
 }
