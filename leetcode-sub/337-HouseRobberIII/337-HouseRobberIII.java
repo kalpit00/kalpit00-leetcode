2d6d2aad3bf1bbc38865bf41c714cffd6eb7c110
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-07-26 14:05:48 -0400

    Create file

---
+++ leetcode-sub/337-HouseRobberIII/337-HouseRobberIII.java
@@ -0,0 +1,20 @@
+// Last updated: 7/26/2025, 2:05:48 PM
+class Solution {
+    public int rob(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int[] dp = helper(root);
+        return Math.max(dp[0], dp[1]);
+    }
+    private int[] helper(TreeNode node) {
+        if (node == null) {
+            return new int[]{0, 0};
+        }
+        int[] left = helper(node.left);
+        int[] right = helper(node.right);
+        int take = node.val + left[1] + right[1];
+        int notTake = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        return new int[]{take, notTake};
+    }
+}
