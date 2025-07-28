addcf9ac23ebac64efd027e2a4ccde6f02070e6c
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-07-27 20:28:08 -0400

    File updated at 7/27/2025, 8:28:08 PM

--- leetcode-sub/2044-CountNumberofMaximumBitwise-ORSubsets/2044-CountNumberofMaximumBitwise-ORSubsets.java
+++ leetcode-sub/2044-CountNumberofMaximumBitwise-ORSubsets/2044-CountNumberofMaximumBitwise-ORSubsets.java
@@ -1,23 +1,20 @@
-// Last updated: 7/27/2025, 8:27:59 PM
+// Last updated: 7/27/2025, 8:28:08 PM
 class Solution {
+    int count = 0;
     public int countMaxOrSubsets(int[] nums) {
         int n = nums.length, max = 0;
         for (int num : nums) {
             max |= num;
         }
-        Integer[][] dp = new Integer[n][1 << 17];
-        return solve(0, n, nums, 0, max, dp);
+        solve(0, n, nums, 0, max);
+        return count;
     }
-    private int solve(int i, int n, int[] nums, int xOr, int max, 
-    Integer[][] dp) {
+    private void solve(int i, int n, int[] nums, int xOr, int max) {
         if (i >= n) {
-            return xOr == max ? 1 : 0;
+            count += xOr == max ? 1 : 0;
+            return;
         }
-        if (dp[i][xOr] != null) {
-            return dp[i][xOr];
-        }
-        int take = solve(i + 1, n, nums, xOr | nums[i], max, dp);
-        int notTake = solve(i + 1, n, nums, xOr, max, dp);
-        return dp[i][xOr] = take + notTake;
+        solve(i + 1, n, nums, xOr | nums[i], max);
+        solve(i + 1, n, nums, xOr, max);
     }
 }
