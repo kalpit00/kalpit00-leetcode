38e23d0639b74f29ac14740eb9888673c2325719
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-07-30 17:20:55 -0400

    File updated at 7/30/2025, 5:20:54 PM

--- leetcode-sub/1452-PeopleWhoseListofFavoriteCompaniesIsNotaSubsetofAnotherList/1452-PeopleWhoseListofFavoriteCompaniesIsNotaSubsetofAnotherList.java
+++ leetcode-sub/1452-PeopleWhoseListofFavoriteCompaniesIsNotaSubsetofAnotherList/1452-PeopleWhoseListofFavoriteCompaniesIsNotaSubsetofAnotherList.java
@@ -1,16 +1,10 @@
-// Last updated: 7/30/2025, 5:19:45 PM
+// Last updated: 7/30/2025, 5:20:54 PM
 class Solution {
     public String arrangeWords(String text) {
         String[] words = text.toLowerCase().split(" ");
-        int n = words.length;
-        String[][] res = new String[n][2];
-        for (int i = 0; i < n; i++) {
-            res[i] = new String[]{words[i], String.valueOf(i)};
-        }
-        Arrays.sort(res, (a, b) -> a[0].length() != b[0].length() ? a[0].length() - b[0].length() : Integer.parseInt(a[1]) - Integer.parseInt(b[1]));
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
         StringBuilder sb = new StringBuilder();
-        for (String[] item : res) {
-            String word = item[0];
+        for (String word : words) {
             if (sb.isEmpty()) {
                 sb.append(Character.toUpperCase(word.charAt(0)));
                 sb.append(word.substring(1));
