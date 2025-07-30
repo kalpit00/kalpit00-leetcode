ccb7b6405f28aa99d0c779b07a72f4f5218d02f8
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-07-30 17:05:03 -0400

    File updated at 7/30/2025, 5:05:02 PM

--- leetcode-sub/2343-QueryKthSmallestTrimmedNumber/2343-QueryKthSmallestTrimmedNumber.java
+++ leetcode-sub/2343-QueryKthSmallestTrimmedNumber/2343-QueryKthSmallestTrimmedNumber.java
@@ -1,17 +1,17 @@
-// Last updated: 7/30/2025, 5:01:19 PM
+// Last updated: 7/30/2025, 5:05:02 PM
 class Solution {
     public int[] smallestTrimmedNumbers(String[] arr, int[][] queries) {
         int x = arr[0].length(), n = arr.length, m = queries.length;
-        String[][] nums = new String[n][2];
+        Node[] nums = new Node[n];
         for (int i = 0; i < n; i++) {
-            nums[i] = new String[]{arr[i], String.valueOf(i)};
+            nums[i] = new Node(arr[i], i);
         }
         int[][] ans = new int[x][n];
         int[] res = new int[m];
         for (int i = x - 1; i >= 0; i--) {
             radixSort(nums, i);
             for (int j = 0; j < n; j++) {
-                ans[i][j] = Integer.parseInt(nums[j][1]);
+                ans[i][j] = nums[j].index;
             }
         }
         for (int i = 0; i < m; i++) {
@@ -21,24 +21,32 @@ class Solution {
         return res;
     }
     
-    private void radixSort(String[][] nums, int k) {
+    private void radixSort(Node[] nums, int k) {
         int n = nums.length;
         int[] map = new int[10];        
-        String[][] res = new String[n][2];
+        Node[] res = new Node[n];
         for (int i = 0; i < n; i++) {
-            int d = nums[i][0].charAt(k) - '0';
+            int d = nums[i].val.charAt(k) - '0';
             map[d]++;
         }        
         for (int i = 1; i < 10; i++) {
             map[i] += map[i - 1];
         }        
         for (int i = n - 1; i >= 0; i--) {
-            int d = nums[i][0].charAt(k) - '0';
+            int d = nums[i].val.charAt(k) - '0';
             res[map[d] - 1] = nums[i];
             map[d]--;
         }
         for (int i = 0; i < n; i++) {
             nums[i] = res[i];
+        }
+    }
+    class Node {
+        String val;
+        int index;
+        public Node (String val, int index) {
+            this.val = val;
+            this.index = index;
         }
     }
 }
