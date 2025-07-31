f71d35eaa03a3452f0e7b39b3b814f857619a028
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-07-31 17:39:14 -0400

    File updated at 7/31/2025, 5:39:14 PM

--- leetcode-sub/912-SortanArray/912-SortanArray.java
+++ leetcode-sub/912-SortanArray/912-SortanArray.java
@@ -1,55 +1,39 @@
-// Last updated: 7/30/2025, 12:49:12 AM
+// Last updated: 7/31/2025, 5:39:14 PM
 class Solution {
     public int[] sortArray(int[] nums) {
-        int n = nums.length, k = 0, t = 0, j = 0, idx = 0;
-        for (int num : nums) {
-            k += num < 0 ? 1 : 0;
+        if (nums.length <= 1) {
+            return nums;
         }
-        int[] neg = new int[k], pos = new int[n - k];
-        for (int num : nums) {
-            if (num < 0) {
-                neg[t++] = -num;
+        int n = nums.length, k = n / 2;
+        int[] left = new int[k], right = new int[n - k];
+        for (int i = 0; i < k; i++) {
+            left[i] = nums[i];
+        }
+        for (int i = k; i < n; i++) {
+            right[i - k] = nums[i];
+        }
+        left = sortArray(left);
+        right = sortArray(right);
+        return merge(left, right);
+    }
+    
+    private int[] merge(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length, i = 0, j = 0, k = 0;
+        int[] res = new int[m + n];
+        while (i < m && j < n) {
+            if (nums1[i] <= nums2[j]) {
+                res[k++] = nums1[i++];
             }
             else {
-                pos[j++] = num;
+                res[k++] = nums2[j++];
             }
         }
-        radixSort(neg);
-        radixSort(pos);
-        for (int i = k - 1; i >= 0; i--) {
-            nums[idx++] = -neg[i];
+        while (i < m) {
+            res[k++] = nums1[i++];
         }
-        for (int i = 0; i < n - k; i++) {
-            nums[idx++] = pos[i];
+        while (j < n) {
+            res[k++] = nums2[j++];
         }
-        return nums;
-    }
-
-    private void radixSort(int[] nums) {
-        int max = 0, n = nums.length;
-        for (int i = 0; i < n; i++) {
-            max = Math.max(max, nums[i]);
-        }
-        for (int exp = 1; max / exp > 0; exp *= 10) {
-            digitSort(nums, n, exp);
-        }
-    }
-    private void digitSort(int[] nums, int n, int exp) {
-        int[] res = new int[n], map = new int[10];
-        for (int i = 0; i < n; i++) {
-            int d = (nums[i] / exp) % 10;
-            map[d]++;
-        }
-        for (int i = 1; i < 10; i++) {
-            map[i] += map[i - 1];
-        }
-        for (int i = n - 1; i >= 0; i--) {
-            int d = (nums[i] / exp) % 10;
-            res[map[d] - 1] = nums[i];
-            map[d]--;
-        }
-        for (int i = 0; i < n; i++) {
-            nums[i] = res[i];
-        }
+        return res;
     }
 }
