e44aa60f2d89b4358dea4dc4af7603468666f612
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-01 00:16:26 -0400

    File updated at 8/1/2025, 12:16:26 AM

--- leetcode-sub/912-SortanArray/912-SortanArray.java
+++ leetcode-sub/912-SortanArray/912-SortanArray.java
@@ -1,42 +1,7 @@
-// Last updated: 7/31/2025, 5:40:21 PM
+// Last updated: 8/1/2025, 12:16:26 AM
 class Solution {
     public int[] sortArray(int[] nums) {
-        return mergeSort(nums);
-    }
-    private int[] mergeSort(int[] nums) {
-        if (nums.length <= 1) {
-            return nums;
-        }
-        int n = nums.length, k = n / 2;
-        int[] left = new int[k], right = new int[n - k];
-        for (int i = 0; i < k; i++) {
-            left[i] = nums[i];
-        }
-        for (int i = k; i < n; i++) {
-            right[i - k] = nums[i];
-        }
-        left = mergeSort(left);
-        right = mergeSort(right);
-        return merge(left, right);
-    }
-    
-    private int[] merge(int[] nums1, int[] nums2) {
-        int m = nums1.length, n = nums2.length, i = 0, j = 0, k = 0;
-        int[] res = new int[m + n];
-        while (i < m && j < n) {
-            if (nums1[i] <= nums2[j]) {
-                res[k++] = nums1[i++];
-            }
-            else {
-                res[k++] = nums2[j++];
-            }
-        }
-        while (i < m) {
-            res[k++] = nums1[i++];
-        }
-        while (j < n) {
-            res[k++] = nums2[j++];
-        }
-        return res;
+        Arrays.sort(nums);
+        return nums;
     }
 }
