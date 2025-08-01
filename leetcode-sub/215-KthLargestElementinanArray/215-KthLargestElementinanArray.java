b8843712531c9d7c110e507f2eccfec8262f6c64
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-01 14:18:59 -0400

    File updated at 8/1/2025, 2:18:58 PM

--- leetcode-sub/215-KthLargestElementinanArray/215-KthLargestElementinanArray.java
+++ leetcode-sub/215-KthLargestElementinanArray/215-KthLargestElementinanArray.java
@@ -1,51 +1,41 @@
-// Last updated: 8/1/2025, 2:04:14 AM
+// Last updated: 8/1/2025, 2:18:58 PM
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        nums = sortArray(nums);
-        return nums[nums.length - k];
-    }
-    public int[] sortArray(int[] nums) {
-        return quickSort(nums);
-    }
-    
-    private int[] quickSort(int[] nums) {
         int n = nums.length;
-        if (n <= 1) {
-            return nums;
+        return quickSelect(nums, 0, n - 1, n - k);
+    } // [n - k]th smallest = k'th largest
+
+    private int quickSelect(int[] nums, int start, int end, int k) {
+        if (start == end) {
+            return nums[start];
+        } // take random pivot choice between index [start .. end]
+        int pivotIndex = start + new Random().nextInt(end - start + 1);
+        int pivot = partition(nums, start, end, pivotIndex);
+        if (pivot == k) {
+            return nums[k];
         }
-        int pivot = nums[new Random().nextInt(n)];
-        int l = 0, r = 0, m = 0, k = 0, j = 0;
-        for (int i = 0; i < n; i++) {
-            k += nums[i] == pivot ? 1 : 0;
-            m += nums[i] < pivot ? 1 : 0;
+        else if (pivot > k) {
+            return quickSelect(nums, start, pivot - 1, k);
         }
-        int[] left = new int[m], mid = new int[k], right = new int[n - m - k];
-        for (int i = 0; i < n; i++) {
-            if (nums[i] < pivot) {
-                left[l++] = nums[i];
-            } else if (nums[i] == pivot) {
-                mid[j++] = nums[i];
-            } else {
-                right[r++] = nums[i];
-            }
+        else {
+            return quickSelect(nums, pivot + 1, end, k);
         }
-        left = quickSort(left);
-        right = quickSort(right);        
-        return merge(left, mid, right);
     }
-    
-    private int[] merge(int[] left, int[] mid, int[] right) {
-        int m = left.length, j = mid.length, n = right.length, k = 0;
-        int[] res = new int[m + j + n];
-        for (int i = 0; i < m; i++) {
-            res[k++] = left[i];
-        }        
-        for (int i = 0; i < j; i++) {
-            res[k++] = mid[i];
-        }        
-        for (int i = 0; i < n; i++) {
-            res[k++] = right[i];
-        }
-        return res;
+    private int partition(int[] nums, int start, int end, int pivotIndex) {
+        int pivot = nums[pivotIndex]; 
+        swap(nums, pivotIndex, end); // temporarily put pivot on end
+        int j = start;
+        for (int i = start; i < end; i++) { // nums[start ... end]
+            if (nums[i] < pivot) {
+                swap(nums, i, j++);
+            } // place all elements < pivot in front, in [start .. j .. end]
+        } // [start .. j] now has all nums < pivot
+        swap(nums, j, end); // move pivot just AFTER the 'j'th pos
+        return j;
+    } // return 'j' which is summing elements < k, thereby O(N) sort
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
