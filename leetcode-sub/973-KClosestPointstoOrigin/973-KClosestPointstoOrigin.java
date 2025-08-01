2c52bb7b74442b460e0d8aa44b20d7c528eb7811
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-01 01:42:47 -0400

    File updated at 8/1/2025, 1:42:47 AM

--- leetcode-sub/973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.java
+++ leetcode-sub/973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.java
@@ -1,66 +1,45 @@
-// Last updated: 8/1/2025, 1:39:29 AM
+// Last updated: 8/1/2025, 1:42:47 AM
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        int n = points.length;
-        Point[] nums = new Point[n];
-        for (int i = 0; i < n; i++) {
-            int x = points[i][0], y = points[i][1], val = x * x + y * y;
-            nums[i] = new Point(x, y, val);
+        shuffle(points);
+        int l = 0;
+        int r = points.length - 1;
+        int p = points.length;
+        while(p != k){
+            p = partition(points, l, r);
+
+            if(p < k)
+                l = p + 1;
+            else if(p > k)
+                r = p - 1;
         }
-        nums = quickSort(nums);
-        int[][] res = new int[k][2];
-        for (int i = 0; i < k; i++) {
-            res[i] = new int[]{nums[i].x, nums[i].y};
-        }
-        return res;
+
+        return Arrays.copyOfRange(points, 0, k);
     }
-    private Point[] quickSort(Point[] nums) {
-        int n = nums.length;
-        if (n <= 1) {
-            return nums;
-        }
-        int pivot = nums[new Random().nextInt(n)].val;
-        int l = 0, r = 0, m = 0, k = 0, j = 0;
-        for (int i = 0; i < n; i++) {
-            k += nums[i].val == pivot ? 1 : 0;
-            m += nums[i].val < pivot ? 1 : 0;
-        }
-        Point[] left = new Point[m], mid = new Point[k], 
-        right = new Point[n - m - k];
-        for (int i = 0; i < n; i++) {
-            if (nums[i].val < pivot) {
-                left[l++] = nums[i];
-            } else if (nums[i].val == pivot) {
-                mid[j++] = nums[i];
-            } else {
-                right[r++] = nums[i];
-            }
-        }
-        left = quickSort(left);
-        right = quickSort(right);        
-        return merge(left, mid, right);
+
+    private void shuffle(int[][] points){
+        Random rnd = new Random();
+        for(int i = points.length - 1; i > 0; i--)
+            swap(points, rnd.nextInt(i + 1), i);
     }
-    
-    private Point[] merge(Point[] left, Point[] mid, Point[] right) {
-        int m = left.length, j = mid.length, n = right.length, k = 0;
-        Point[] res = new Point[m + j + n];
-        for (int i = 0; i < m; i++) {
-            res[k++] = left[i];
-        }        
-        for (int i = 0; i < j; i++) {
-            res[k++] = mid[i];
-        }        
-        for (int i = 0; i < n; i++) {
-            res[k++] = right[i];
+
+    private int partition(int[][] points, int l, int r){
+        int i = l;
+        for(int j = l; j < r; j++){
+            if(euclidian(points[j]) <= euclidian(points[r]))
+                swap(points, i++, j);
         }
-        return res;
+        swap(points, i, r);
+        return i;
     }
-    class Point {
-        int x, y, val;
-        public Point(int x, int y, int val) {
-            this.x = x;
-            this.y = y;
-            this.val = val;
-        }
+
+    private int euclidian(int[] p){
+        return p[0] * p[0] + p[1] * p[1];
+    }
+
+    private void swap(int[][] points, int a, int b){
+        int[] tmp = points[a];
+        points[a] = points[b];
+        points[b] = tmp;
     }
 }
