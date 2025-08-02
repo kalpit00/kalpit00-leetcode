2ee8e647e4122b842a8f44392de8f6c0588212de
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-01 23:38:11 -0400

    File updated at 8/1/2025, 11:38:11 PM

--- leetcode-sub/324-WiggleSortII/324-WiggleSortII.java
+++ leetcode-sub/324-WiggleSortII/324-WiggleSortII.java
@@ -1,14 +1,60 @@
-// Last updated: 8/1/2025, 11:24:06 PM
+// Last updated: 8/1/2025, 11:38:11 PM
 class Solution {
     public void wiggleSort(int[] nums) {
-        int n = nums.length, idx = n - 1;
-        int[] arr = nums.clone();
-        Arrays.sort(arr);
-        for (int i = 1; i < n; i += 2) {
-            nums[i] = arr[idx--];
+        int n = nums.length;
+        int median = quickSelect(nums, 0, n - 1, (n - 1) / 2);
+        int idx = 0, i = 1, j = n % 2 == 0 ? n - 2 : n - 1;
+        while (idx < n) {
+            if (nums[idx] > median && (idx > i || (idx <= i && idx % 2 == 0))) {
+                swap(nums, idx, i);
+                i += 2;
+            }
+    else if (nums[idx] < median && (idx < j || (idx >= j && idx % 2 == 1))) {
+                swap(nums, idx, j);
+                j -= 2;
+            }
+            else {
+                idx++;
+            }
         }
-        for (int i = 0; i < n; i += 2) {
-            nums[i] = arr[idx--];
+    }
+
+    private int quickSelect(int[] nums, int start, int end, int k) {
+        if (start >= end) {
+            return nums[start];
+        } // take random pivot choice between index [start .. end]
+        int pivotIndex = start + new Random().nextInt(end - start + 1);
+        int pivot[] = partition(nums, start, end, pivotIndex);
+        if (pivot[0] > k) {
+            return quickSelect(nums, start, pivot[0] - 1, k);
         }
+        else if (pivot[1] < k) {
+            return quickSelect(nums, pivot[1] + 1, end, k);
+        }
+        else {
+            return nums[k];
+        }
+    }
+    // Dutch National Flag Algorithm
+    private int[] partition(int[] nums, int start, int end, int pivotIndex) {
+        int pivot = nums[pivotIndex]; 
+        int i = start, idx = start, j = end;
+        while (idx <= j && i < j) {
+            if (nums[idx] < pivot) {
+                swap(nums, i++, idx++);
+            }
+            else if (nums[idx] > pivot) {
+                swap(nums, j--, idx);
+            }
+            else {
+                idx++;
+            }
+        }
+        return new int[]{i, j};
+    }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
