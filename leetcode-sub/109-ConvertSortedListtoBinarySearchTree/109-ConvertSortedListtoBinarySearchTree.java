7d7389a8d996b301b5883dac32791f480df9445f
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-02 19:03:06 -0400

    File updated at 8/2/2025, 7:03:06 PM

--- leetcode-sub/109-ConvertSortedListtoBinarySearchTree/109-ConvertSortedListtoBinarySearchTree.java
+++ leetcode-sub/109-ConvertSortedListtoBinarySearchTree/109-ConvertSortedListtoBinarySearchTree.java
@@ -1,14 +1,14 @@
-// Last updated: 8/2/2025, 7:01:38 PM
+// Last updated: 8/2/2025, 7:03:06 PM
 class Solution {
     public TreeNode sortedListToBST(ListNode head) {
         if (head == null) return null;
         ListNode tail = head;
-        int n = 0;
+        int n = 1; // start with 1 because going till temp.next != null
         while (tail.next != null) {
             n++;
             tail = tail.next;
         }
-        return mergeSort(head, tail, 0, n);
+        return mergeSort(head, tail, 0, n - 1);
     }
     private TreeNode mergeSort(ListNode head, ListNode tail, 
     int start, int end) {
