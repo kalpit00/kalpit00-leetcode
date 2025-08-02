7ef517e164a1277f0fa3f46b7f40885e30cfcd0d
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-02 05:25:26 -0400

    File updated at 8/2/2025, 5:25:26 AM

--- leetcode-sub/2561-RearrangingFruits/2561-RearrangingFruits.java
+++ leetcode-sub/2561-RearrangingFruits/2561-RearrangingFruits.java
@@ -1,7 +1,7 @@
-// Last updated: 8/2/2025, 5:14:29 AM
+// Last updated: 8/2/2025, 5:25:26 AM
 class Solution {
     public long minCost(int[] basket1, int[] basket2) {
-        Map<Integer, Integer> map = new TreeMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         int min = Integer.MAX_VALUE;
         for (int num : basket1) {
             map.put(num, map.getOrDefault(num, 0) + 1);
@@ -21,11 +21,57 @@ class Solution {
                 list.add(key);
             }
         }
-        Collections.sort(list);
+        int n = list.size();
+        if (n == 0) {
+            return 0;
+        }
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = list.get(i);
+        }
+        quickSelect(nums, 0, n - 1, (n - 1) / 2);
         long res = 0;
-        for (int i = 0; i < list.size() / 2; i++) {
-            res += Math.min(2 * min, list.get(i));
+        for (int i = 0; i < n / 2; i++) {
+            res += Math.min(2 * min, nums[i]);
         }
         return res;
+    }
+    private int quickSelect(int[] nums, int start, int end, int k) {
+        if (start >= end) {
+            return nums[start];
+        } // take random pivot choice between index [start .. end]
+        int pivotIndex = start + new Random().nextInt(end - start + 1);
+        int pivot[] = partition(nums, start, end, pivotIndex);
+        if (pivot[0] > k) {
+            return quickSelect(nums, start, pivot[0] - 1, k);
+        }
+        else if (pivot[1] < k) {
+            return quickSelect(nums, pivot[1] + 1, end, k);
+        }
+        else {
+            return nums[k];
+        }
+    }
+    // Dutch National Flag Algorithm
+    private int[] partition(int[] nums, int start, int end, int pivotIndex) {
+        int pivot = nums[pivotIndex]; 
+        int i = start, idx = start, j = end;
+        while (idx <= j && i < j) {
+            if (nums[idx] < pivot) {
+                swap(nums, i++, idx++);
+            }
+            else if (nums[idx] > pivot) {
+                swap(nums, j--, idx);
+            }
+            else {
+                idx++;
+            }
+        }
+        return new int[]{i, j};
+    }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
