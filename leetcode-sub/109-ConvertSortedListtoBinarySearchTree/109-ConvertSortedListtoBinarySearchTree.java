b6a69c1c7ff102fdd1d37a3c77073c71e2874a6b
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-02 19:15:02 -0400

    File updated at 8/2/2025, 7:15:02 PM

--- leetcode-sub/109-ConvertSortedListtoBinarySearchTree/109-ConvertSortedListtoBinarySearchTree.java
+++ leetcode-sub/109-ConvertSortedListtoBinarySearchTree/109-ConvertSortedListtoBinarySearchTree.java
@@ -1,30 +1,27 @@
-// Last updated: 8/2/2025, 7:03:06 PM
+// Last updated: 8/2/2025, 7:15:02 PM
 class Solution {
+    ListNode head;
     public TreeNode sortedListToBST(ListNode head) {
         if (head == null) return null;
-        ListNode tail = head;
-        int n = 1; // start with 1 because going till temp.next != null
-        while (tail.next != null) {
+        this.head = head;
+        ListNode temp = head;
+        int n = 0;
+        while (temp != null) {
             n++;
-            tail = tail.next;
+            temp = temp.next;
         }
-        return mergeSort(head, tail, 0, n - 1);
+        return mergeSort(0, n - 1);
     }
-    private TreeNode mergeSort(ListNode head, ListNode tail, 
-    int start, int end) {
+    private TreeNode mergeSort(int start, int end) {
         if (start > end) {
             return null;
         }
         int mid = start + (end - start)/2;
-        ListNode slow = head, fast = head, prev = null;
-        while (fast != tail && fast.next != tail) {
-            prev = slow;
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        TreeNode res = new TreeNode(slow.val);
-        res.left = mergeSort(head, prev, start, mid - 1);
-        res.right = mergeSort(slow.next, tail, mid + 1, end);
-        return res;
+        TreeNode left = mergeSort(start, mid - 1);
+        TreeNode root = new TreeNode(head.val);
+        head = head.next; // CRUCIAL step
+        root.left = left;
+        root.right = mergeSort(mid + 1, end);
+        return root;
     }
 }
