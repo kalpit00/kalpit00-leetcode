1b1f88db8c64c3d048eb1a961dd6b8bf83cdcc2a
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-01 20:13:45 -0400

    File updated at 8/1/2025, 8:13:45 PM

--- leetcode-sub/912-SortanArray/912-SortanArray.java
+++ leetcode-sub/912-SortanArray/912-SortanArray.java
@@ -1,47 +1,39 @@
-// Last updated: 8/1/2025, 1:14:35 AM
+// Last updated: 8/1/2025, 8:13:45 PM
 class Solution {    
     public int[] sortArray(int[] nums) {
-        return quickSort(nums);
-    }
-    
-    private int[] quickSort(int[] nums) {
         int n = nums.length;
-        if (n <= 1) {
-            return nums;
+        quickSort(nums, 0, n - 1);
+        return nums;
+    }
+    private void quickSort(int[] nums, int start, int end) {
+        if (start >= end) {
+            return;
         }
-        int pivot = nums[new Random().nextInt(n)];
-        int l = 0, r = 0, m = 0, k = 0, j = 0;
-        for (int i = 0; i < n; i++) {
-            k += nums[i] == pivot ? 1 : 0;
-            m += nums[i] < pivot ? 1 : 0;
-        }
-        int[] left = new int[m], mid = new int[k], right = new int[n - m - k];
-        for (int i = 0; i < n; i++) {
-            if (nums[i] < pivot) {
-                left[l++] = nums[i];
-            } else if (nums[i] == pivot) {
-                mid[j++] = nums[i];
-            } else {
-                right[r++] = nums[i];
+        int pivotIndex = start + new Random().nextInt(end - start + 1);
+        int[] pivot = partition(nums, start, end, pivotIndex);
+        quickSort(nums, start, pivot[0] - 1);
+        quickSort(nums, pivot[1] + 1, end);
+    }
+    // Dutch National Flag Algorithm
+    private int[] partition(int[] nums, int start, int end, int pivotIndex) {
+        int pivot = nums[pivotIndex]; 
+        int i = start, idx = start, j = end;
+        while (idx <= j && i < j) {
+            if (nums[idx] < pivot) {
+                swap(nums, i++, idx++);
+            }
+            else if (nums[idx] > pivot) {
+                swap(nums, j--, idx);
+            }
+            else {
+                idx++;
             }
         }
-        left = quickSort(left);
-        right = quickSort(right);        
-        return merge(left, mid, right);
+        return new int[]{i, j};
     }
-    
-    private int[] merge(int[] left, int[] mid, int[] right) {
-        int m = left.length, j = mid.length, n = right.length, k = 0;
-        int[] res = new int[m + j + n];
-        for (int i = 0; i < m; i++) {
-            res[k++] = left[i];
-        }        
-        for (int i = 0; i < j; i++) {
-            res[k++] = mid[i];
-        }        
-        for (int i = 0; i < n; i++) {
-            res[k++] = right[i];
-        }
-        return res;
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
