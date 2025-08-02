234e4c65a935eb744861ca0d50ac50feef4bea7d
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-02 17:46:23 -0400

    File updated at 8/2/2025, 5:46:23 PM

--- leetcode-sub/2426-NumberofPairsSatisfyingInequality/2426-NumberofPairsSatisfyingInequality.java
+++ leetcode-sub/2426-NumberofPairsSatisfyingInequality/2426-NumberofPairsSatisfyingInequality.java
@@ -1,49 +1,59 @@
-// Last updated: 4/29/2025, 6:42:57 PM
+// Last updated: 8/2/2025, 5:46:23 PM
 class Solution {
+    long count = 0;
+    int diff;
     public long numberOfPairs(int[] nums1, int[] nums2, int diff) {
-        int shift = 20000, n = nums1.length;
-        SegmentTree segmentTree = new SegmentTree(100001);
-        long res = 0;
+        int n = nums1.length;
+        this.diff = diff;
+        int[] nums = new int[n];
         for (int i = 0; i < n; i++) {
-            int k = nums1[i] - nums2[i];
-            res += segmentTree.query(0, k + diff + shift + 1);
-            segmentTree.update(k + shift, 1);
+            nums[i] = nums1[i] - nums2[i];
+        }
+        mergeSort(nums);
+        return count;
+    }
+    private int[] mergeSort(int[] nums) {
+        if (nums.length <= 1) {
+            return nums;
+        }
+        int n = nums.length, k = n / 2;
+        int[] left = new int[k], right = new int[n - k];
+        for (int i = 0; i < k; i++) {
+            left[i] = nums[i];
+        }
+        for (int i = k; i < n; i++) {
+            right[i - k] = nums[i];
+        }
+        left = mergeSort(left);
+        right = mergeSort(right);
+        return merge(left, right);
+    }
+    
+    private int[] merge(int[] left, int[] right) {
+        int m = left.length, n = right.length, i = 0, j = 0, k = 0;
+        int[] res = new int[m + n];
+        for (i = 0; i < m; i++) {
+            while (j < n && left[i] > right[j] + diff) {
+                j++;
+            }
+            count += (n - j);
+        }
+        i = 0;
+        j = 0;
+        while (i < m && j < n) {
+            if (left[i] <= right[j]) {
+                res[k++] = left[i++];
+            }
+            else {
+                res[k++] = right[j++];
+            }
+        }
+        while (i < m) {
+            res[k++] = left[i++];
+        }
+        while (j < n) {
+            res[k++] = right[j++];
         }
         return res;
-    }
-    class SegmentTree {
-        int[] tree;
-        int n;
-
-        public SegmentTree(int n) {
-            this.n = n;
-            this.tree = new int[2 * n];
-        }
-
-        public int query(int left, int right) {
-            left += n;
-            right += n;
-            int sum = 0;
-            while (left < right) {
-                if ((left & 1) == 1) { // left % 2 == 1
-                    sum += tree[left++];
-                }
-                if ((right & 1) == 1) { // right % 2 == 1
-                    sum += tree[--right];
-                }
-                left >>= 1; // left /= 2
-                right >>= 1; // right /= 2
-            }
-            return sum;
-        }
-
-        public void update(int index, int val) {
-            index += n;
-            tree[index] += val;
-            while (index > 1) {
-                index >>= 1; // index /= 2
-                tree[index] = tree[2 * index] + tree[2 * index + 1];
-            }
-        }
     }
 }
