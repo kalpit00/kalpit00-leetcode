8995131bfbbed8f3dc2d8f285d4e4f80e64f7884
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-01 23:18:35 -0400

    File updated at 8/1/2025, 11:18:34 PM

--- leetcode-sub/973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.java
+++ leetcode-sub/973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.java
@@ -1,45 +1,54 @@
-// Last updated: 8/1/2025, 1:42:47 AM
+// Last updated: 8/1/2025, 11:18:34 PM
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        shuffle(points);
-        int l = 0;
-        int r = points.length - 1;
-        int p = points.length;
-        while(p != k){
-            p = partition(points, l, r);
-
-            if(p < k)
-                l = p + 1;
-            else if(p > k)
-                r = p - 1;
+        int n = points.length;
+        int[][] nums = new int[n][3], res = new int[k][2];
+        for (int i = 0; i < n; i++) {
+            int x = points[i][0], y = points[i][1], val = x * x + y * y;
+            nums[i] = new int[]{x, y, val};
         }
-
-        return Arrays.copyOfRange(points, 0, k);
-    }
-
-    private void shuffle(int[][] points){
-        Random rnd = new Random();
-        for(int i = points.length - 1; i > 0; i--)
-            swap(points, rnd.nextInt(i + 1), i);
-    }
-
-    private int partition(int[][] points, int l, int r){
-        int i = l;
-        for(int j = l; j < r; j++){
-            if(euclidian(points[j]) <= euclidian(points[r]))
-                swap(points, i++, j);
+        quickSelect(nums, 0, n - 1, k - 1);
+        for (int i = 0; i < k; i++) {
+            res[i] = new int[]{nums[i][0], nums[i][1]};
         }
-        swap(points, i, r);
-        return i;
+        return res;
     }
-
-    private int euclidian(int[] p){
-        return p[0] * p[0] + p[1] * p[1];
+    private int quickSelect(int[][] nums, int start, int end, int k) {
+        if (start >= end) {
+            return nums[start][2];
+        } // take random pivot choice between index [start .. end]
+        int pivotIndex = start + new Random().nextInt(end - start + 1);
+        int pivot[] = partition(nums, start, end, pivotIndex);
+        if (pivot[0] > k) {
+            return quickSelect(nums, start, pivot[0] - 1, k);
+        }
+        else if (pivot[1] < k) {
+            return quickSelect(nums, pivot[1] + 1, end, k);
+        }
+        else {
+            return nums[k][2];
+        }
     }
-
-    private void swap(int[][] points, int a, int b){
-        int[] tmp = points[a];
-        points[a] = points[b];
-        points[b] = tmp;
+    // Dutch National Flag Algorithm
+    private int[] partition(int[][] nums, int start, int end, int pivotIndex) {
+        int pivot = nums[pivotIndex][2]; 
+        int i = start, idx = start, j = end;
+        while (idx <= j && i < j) {
+            if (nums[idx][2] < pivot) {
+                swap(nums, i++, idx++);
+            }
+            else if (nums[idx][2] > pivot) {
+                swap(nums, j--, idx);
+            }
+            else {
+                idx++;
+            }
+        }
+        return new int[]{i, j};
+    }
+    private void swap(int[][] nums, int i, int j) {
+        int[] temp = nums[i].clone();
+        System.arraycopy(nums[j], 0, nums[i], 0, 3);
+        System.arraycopy(temp, 0, nums[j], 0, 3); 
     }
 }
