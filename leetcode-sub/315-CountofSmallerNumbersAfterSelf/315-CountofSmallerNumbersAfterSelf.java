c49fc116b47a82edbd5b5a8aa2243fdd81d220b1
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-02 17:10:11 -0400

    File updated at 8/2/2025, 5:10:10 PM

--- leetcode-sub/315-CountofSmallerNumbersAfterSelf/315-CountofSmallerNumbersAfterSelf.java
+++ leetcode-sub/315-CountofSmallerNumbersAfterSelf/315-CountofSmallerNumbersAfterSelf.java
@@ -1,53 +1,55 @@
-// Last updated: 4/29/2025, 3:04:05 PM
+// Last updated: 8/2/2025, 5:10:10 PM
 class Solution {
     public List<Integer> countSmaller(int[] nums) {
-        int n = nums.length, min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
-        for (int num : nums) {
-            min = Math.min(min, num);
-            max = Math.max(max, num);
+        int n = nums.length;
+        int[] map = new int[n];
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = nums[i];
+            arr[i][1] = i;
         }
-        SegmentTree segmentTree = new SegmentTree(max - min + 1);
-        Integer[] res = new Integer[n];
-        for (int i = n - 1; i >= 0; i--) {
-            int idx = nums[i] - min;
-            res[i] = segmentTree.query(0, idx);
-            segmentTree.update(idx, 1);
+        mergeSort(arr, map);
+        List<Integer> res = new ArrayList<>();
+        for (int num : map) {
+            res.add(num);
         }
-        return Arrays.asList(res);
+        return res;
     }
-    class SegmentTree {
-        int[] tree;
-        int n;
 
-        public SegmentTree(int n) {
-            this.n = n;
-            this.tree = new int[2 * n];
+    private int[][] mergeSort(int[][] nums, int[] map) {
+        if (nums.length <= 1) return nums;
+        int n = nums.length, k = n / 2;
+        int[][] left = new int[k][2], right = new int[n - k][2];
+        for (int i = 0; i < k; i++) {
+            left[i] = nums[i];
         }
-
-        public int query(int left, int right) {
-            left += n;
-            right += n;
-            int sum = 0;
-            while (left < right) {
-                if ((left & 1) == 1) { // left % 2 == 1
-                    sum += tree[left++];
-                }
-                if ((right & 1) == 1) { // right % 2 == 1
-                    sum += tree[--right];
-                }
-                left >>= 1; // left /= 2
-                right >>= 1; // right /= 2
-            }
-            return sum;
+        for (int i = k; i < n; i++) {
+            right[i - k] = nums[i];
         }
+        left = mergeSort(left, map);
+        right = mergeSort(right, map);
+        return merge(left, right, map);
+    }
 
-        public void update(int index, int val) {
-            index += n;
-            tree[index] += val;
-            while (index > 1) {
-                index >>= 1; // index /= 2
-                tree[index] = tree[2 * index] + tree[2 * index + 1];
+    private int[][] merge(int[][] left, int[][] right, int[] map) {
+        int m = left.length, n = right.length, i = 0, j = 0, k = 0;
+        int[][] res = new int[m + n][2];
+        while (i < m && j < n) {
+            if (left[i][0] <= right[j][0]) {
+                map[left[i][1]] += j;
+                res[k++] = left[i++];
+            }  // just count all 'j' greater eles on the right 
+            else {
+                res[k++] = right[j++];
             }
         }
+        while (i < m) {  // same here, add += 'j'
+            map[left[i][1]] += j;
+            res[k++] = left[i++];
+        }
+        while (j < n) {
+            res[k++] = right[j++];
+        }
+        return res;
     }
 }
