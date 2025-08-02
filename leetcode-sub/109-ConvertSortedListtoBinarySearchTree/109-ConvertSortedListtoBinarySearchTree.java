26329e53d2bc05bec0b6f84cec7c6c32f8f39518
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-02 19:01:38 -0400

    File updated at 8/2/2025, 7:01:38 PM

--- leetcode-sub/109-ConvertSortedListtoBinarySearchTree/109-ConvertSortedListtoBinarySearchTree.java
+++ leetcode-sub/109-ConvertSortedListtoBinarySearchTree/109-ConvertSortedListtoBinarySearchTree.java
@@ -1,58 +1,30 @@
-// Last updated: 8/2/2025, 6:53:20 PM
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// Last updated: 8/2/2025, 7:01:38 PM
 class Solution {
     public TreeNode sortedListToBST(ListNode head) {
-        if(head == null)
-        {
+        if (head == null) return null;
+        ListNode tail = head;
+        int n = 0;
+        while (tail.next != null) {
+            n++;
+            tail = tail.next;
+        }
+        return mergeSort(head, tail, 0, n);
+    }
+    private TreeNode mergeSort(ListNode head, ListNode tail, 
+    int start, int end) {
+        if (start > end) {
             return null;
         }
-
-        if(head.next == null)
-        {
-            return new TreeNode(head.val);
-        }
-
-        ListNode slow = head, fast = head, slow_prev = null;
-
-        while(fast != null && fast.next != null)
-        {
-            slow_prev = slow;
+        int mid = start + (end - start)/2;
+        ListNode slow = head, fast = head, prev = null;
+        while (fast != tail && fast.next != tail) {
+            prev = slow;
             slow = slow.next;
             fast = fast.next.next;
         }
-
-
-        TreeNode root = new TreeNode(slow.val);
-
-        slow_prev.next = null;
-        
-        root.left = sortedListToBST(head);
-        root.right = sortedListToBST(slow.next);
-
-        return root;
+        TreeNode res = new TreeNode(slow.val);
+        res.left = mergeSort(head, prev, start, mid - 1);
+        res.right = mergeSort(slow.next, tail, mid + 1, end);
+        return res;
     }
 }
