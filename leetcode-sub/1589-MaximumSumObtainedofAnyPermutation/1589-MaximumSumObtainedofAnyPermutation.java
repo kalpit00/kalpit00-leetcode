8483327db59de871f23736eaf1dc25469427cbec
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-08 17:56:53 -0400

    File updated at 8/8/2025, 5:56:53 PM

--- leetcode-sub/1589-MaximumSumObtainedofAnyPermutation/1589-MaximumSumObtainedofAnyPermutation.java
+++ leetcode-sub/1589-MaximumSumObtainedofAnyPermutation/1589-MaximumSumObtainedofAnyPermutation.java
@@ -1,24 +1,15 @@
-// Last updated: 8/8/2025, 5:54:02 PM
+// Last updated: 8/8/2025, 5:56:53 PM
 class Solution {
-    public int maxSumRangeQuery(int[] nums, int[][] requests) {
-        int m = requests.length, n = nums.length, mod = 1000000007;
-        long sum = 0;
-        int[] count = new int[n];
-        for (int[] request : requests) {
-            int start = request[0], end = request[1], val = 1;
-            count[start] += val;
-            if (end + 1 < n) {
-                count[end + 1] -= val;
+    public int countEven(int num) {
+        int count = 0;
+        for (int i = 1; i <= num; i++) {
+            int n = i, sum = 0;
+            while (n > 0) {
+                sum += n % 10;
+                n /= 10;
             }
+            count += sum % 2 == 0 ? 1 : 0;
         }
-        for (int i = 1 ; i < n; i++) {
-            count[i] += count[i - 1];
-        }
-        Arrays.sort(nums);
-        Arrays.sort(count);
-        for (int i = 0; i < n; i++) {
-            sum += 1L * nums[i] * count[i];
-        }
-        return (int) (sum % mod);
+        return count;
     }
 }
