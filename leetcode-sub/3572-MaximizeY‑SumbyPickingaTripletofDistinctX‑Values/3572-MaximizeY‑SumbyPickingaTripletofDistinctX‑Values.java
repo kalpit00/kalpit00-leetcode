e9f0d76dd65be240620989addd26d3b1dc008c6c
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-09 00:42:21 -0400

    File updated at 8/9/2025, 12:42:21 AM

--- leetcode-sub/3572-MaximizeY‑SumbyPickingaTripletofDistinctX‑Values/3572-MaximizeY‑SumbyPickingaTripletofDistinctX‑Values.java
+++ leetcode-sub/3572-MaximizeY‑SumbyPickingaTripletofDistinctX‑Values/3572-MaximizeY‑SumbyPickingaTripletofDistinctX‑Values.java
@@ -1,32 +1,26 @@
-// Last updated: 8/9/2025, 12:38:48 AM
+// Last updated: 8/9/2025, 12:42:21 AM
 class Solution {
     public int maxSumDistinctTriplet(int[] x, int[] y) {
-        int n = x.length;
-        int[] map = new int[1000001];
-        for (int i = 0; i < n; i++) {
-            map[x[i]] = Math.max(map[x[i]], y[i]);
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < x.length; i++){
+            map.put(x[i], Math.max(map.getOrDefault(x[i], 0), y[i]));
         }
-        int[] max = helper(map);
-        if (max[0] == -1 || max[1] == -1 || max[2] == -1) {
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(3,Collections.reverseOrder());
+        for(int val : map.values()){
+            maxHeap.offer(val);
+        }
+
+        if(maxHeap.size() < 3){
             return -1;
         }
-        return max[0] + max[1] + max[2];
-    }
-    private int[] helper(int[] nums) {
-        int max1 = -1, max2 = -1, max3 = -1;
-        for (int num : nums) {
-            if (num == 0) continue;
-            if (num > max1) {
-                max3 = max2;
-                max2 = max1;
-                max1 = num;
-            } else if (num > max2) {
-                max3 = max2;
-                max2 = num;
-            } else if (num > max3) {
-                max3 = num;
-            }
+
+        int sum = 0;
+        // this garantee ordering
+        for (int i = 0; i < 3; i++) {
+            sum += maxHeap.poll();
         }
-        return new int[] {max1, max2, max3};
+
+        return sum;
     }
 }
