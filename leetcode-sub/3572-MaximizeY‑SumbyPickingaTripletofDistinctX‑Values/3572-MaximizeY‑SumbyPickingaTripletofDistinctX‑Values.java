fecea6dc49ecc7919acc4603a0c03a0bd258cd1a
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-09 00:43:58 -0400

    File updated at 8/9/2025, 12:43:58 AM

--- leetcode-sub/3572-MaximizeY‑SumbyPickingaTripletofDistinctX‑Values/3572-MaximizeY‑SumbyPickingaTripletofDistinctX‑Values.java
+++ leetcode-sub/3572-MaximizeY‑SumbyPickingaTripletofDistinctX‑Values/3572-MaximizeY‑SumbyPickingaTripletofDistinctX‑Values.java
@@ -1,26 +1,60 @@
-// Last updated: 8/9/2025, 12:42:21 AM
+// Last updated: 8/9/2025, 12:43:58 AM
+/**
+Sort: nlgn
+PQ: nlg3 -> n, space n
+List: n, but also space 1
+ */
 class Solution {
     public int maxSumDistinctTriplet(int[] x, int[] y) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i = 0; i < x.length; i++){
-            map.put(x[i], Math.max(map.getOrDefault(x[i], 0), y[i]));
+        int i = 0, j = 0, k = 0;
+        int iV = 0, jV = 0, kV = 0;
+
+        for (int idx = 0; idx < x.length; ++idx) {
+            if (x[idx] == i) {
+                iV = Math.max(iV, y[idx]);
+            } else if (x[idx] == j) {
+                jV = Math.max(jV, y[idx]);
+            } else if (x[idx] == k) {
+                kV = Math.max(kV, y[idx]);
+            } else if (y[idx] > iV || y[idx] > jV || y[idx] > kV) {
+                int min = Math.min(iV, Math.min(jV, kV));
+                if (min == iV) {
+                    iV = y[idx];
+                    i = x[idx];
+                } else if (min == jV) {
+                    jV = y[idx];
+                    j = x[idx];
+                } else {
+                    kV = y[idx];
+                    k = x[idx];
+                }
+            }
         }
 
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(3,Collections.reverseOrder());
-        for(int val : map.values()){
-            maxHeap.offer(val);
-        }
-
-        if(maxHeap.size() < 3){
-            return -1;
-        }
-
-        int sum = 0;
-        // this garantee ordering
-        for (int i = 0; i < 3; i++) {
-            sum += maxHeap.poll();
-        }
-
-        return sum;
+        return (iV==0 || jV==0 || kV==0) ? -1 : (iV+jV+kV);
     }
+
+    // public int maxSumDistinctTriplet(int[] x, int[] y) {
+    //     Map<Integer, Integer> map = new HashMap<>();
+    //     for (int i = 0; i < x.length; ++i) {
+    //         map.put(x[i], Math.max(map.getOrDefault(x[i], 0), y[i]));
+    //     }
+    //     if (map.size() < 3) {
+    //         return -1;
+    //     }
+
+    //     PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    //     for (int value : map.values()) {
+    //         minHeap.add(value);
+    //         if (minHeap.size() > 3) {
+    //             minHeap.poll();
+    //         }
+    //     }
+        
+    //     int sum = 0;
+    //     while (!minHeap.isEmpty()) {
+    //         sum += minHeap.poll();
+    //     }
+    //     return sum;
+    // }
 }
