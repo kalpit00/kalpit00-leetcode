9118a5e6dfc1abd9725b4f4b43b63f65674c87f2
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-10 01:51:54 -0400

    Create file

---
+++ leetcode-sub/3563-LexicographicallySmallestStringAfterAdjacentRemovals/3563-LexicographicallySmallestStringAfterAdjacentRemovals.java
@@ -0,0 +1,34 @@
+// Last updated: 8/10/2025, 1:51:52 AM
+class Solution {
+    public void findSecretWord(String[] words, Master master) {
+        Random random = new Random();
+        int count = 0;
+        for (int i = 0; i < 10 && count != 6; i++) {
+            String guess = words[random.nextInt(words.length)];
+            count = master.guess(guess);
+            List<String> candidates = new ArrayList<>();
+            for (String word : words) {
+                if (count == helper(guess, word)) {
+                    candidates.add(word);
+                }
+            }
+            words = candidates.toArray(new String[0]);
+        }
+    }
+    private int helper(String s1, String s2) {
+        int count = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) == s2.charAt(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+/**
+ * // This is the Master's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface Master {
+ *     public int guess(String word) {}
+ * }
+ */
