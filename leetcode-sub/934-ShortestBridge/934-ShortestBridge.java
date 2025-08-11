721a79529326b722e7efc48bed1122215d3407c2
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-11 17:32:51 -0400

    File updated at 8/11/2025, 5:32:51 PM

--- leetcode-sub/934-ShortestBridge/934-ShortestBridge.java
+++ leetcode-sub/934-ShortestBridge/934-ShortestBridge.java
@@ -1,14 +1,15 @@
-// Last updated: 8/11/2025, 5:31:59 PM
+// Last updated: 8/11/2025, 5:32:51 PM
 class Solution {
     int[][] dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     public int shortestBridge(int[][] grid) {
         int n = grid.length, steps = -1;
         Queue<int[]> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[n][n];
         outer: // LABEL new trick
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
-                    dfs(grid, i, j, queue, n);
+                    dfs(grid, i, j, visited, queue, n);
                     break outer;
                 }
             }
@@ -23,11 +24,10 @@ class Solution {
                 }
                 for (int[] d : dir) {
                     int r = x + d[0], c = y + d[1];
-                    if (r >= 0 && r < n && c >= 0 && c < n && grid[r][c] != 2) {
+                    if (r >= 0 && r < n && c >= 0 && c < n && 
+                    !visited[r][c]) {
+                        visited[r][c] = true;
                         queue.offer(new int[]{r, c});
-                        if (grid[r][c] == 0) {
-                            grid[r][c] = 2; // only mark the 0 to 2
-                        } // as we need to return the first 1-cell we poll
                     }
                 }
             }
@@ -36,14 +36,17 @@ class Solution {
         return -1;
     }
 
-    private void dfs(int[][] grid, int i, int j, Queue<int[]> queue, int n) {
-        if (i < 0 || i >= n || j < 0 || j >= n || grid[i][j] != 1) {
+    private void dfs(int[][] grid, int i, int j, boolean[][] visited,
+    Queue<int[]> queue, int n) {
+        if (i < 0 || i >= n || j < 0 || j >= n || grid[i][j] != 1 || 
+        visited[i][j]) {
             return;
         }
         grid[i][j] = 2; // color first island cells to 2!!
+        visited[i][j] = true;
         queue.offer(new int[]{i, j});
         for (int[] d : dir) {
-            dfs(grid, i + d[0], j + d[1], queue, n);
+            dfs(grid, i + d[0], j + d[1], visited, queue, n);
         }
     }
 }   
