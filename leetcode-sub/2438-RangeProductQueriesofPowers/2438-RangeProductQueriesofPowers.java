3ef9936f31bd99baa1be57e5a94dbb59c9b6dea1
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-10 20:56:52 -0400

    Create file

---
+++ leetcode-sub/2438-RangeProductQueriesofPowers/2438-RangeProductQueriesofPowers.java
@@ -0,0 +1,42 @@
+// Last updated: 8/10/2025, 8:56:52 PM
+class Solution {
+    int mod = 1000000007;
+
+    public int[] productQueries(int n, int[][] queries) {
+        char[] arr = Integer.toBinaryString(n).toCharArray();
+        int m = queries.length, k = 0, idx = 0, t = arr.length;
+        for (char c : arr) {
+            if (c == '1') k++;
+        }
+        int[] res = new int[m], nums = new int[k];
+        for (int i = t - 1; i >= 0; i--) {
+            if (arr[i] == '1') {
+                nums[idx++] = 1 << (t - i - 1);
+            }
+        }
+        
+        long[] pre = new long[k + 1];
+        pre[0] = 1;
+        for (int i = 1; i <= k; i++) {
+            pre[i] = (pre[i - 1] * nums[i - 1]) % mod;
+        }
+        
+        for (int i = 0; i < m; i++) {
+            int l = queries[i][0], r = queries[i][1];
+            res[i] = (int) ((pre[r + 1] * modPow(pre[l], mod - 2)) % mod);
+        } // (a / b) % mod == (a * b^(mod - 2)) % mod -> Fermats Little theorem
+        return res;
+    }
+
+    private long modPow(long base, long exp) {
+        long res = 1;
+        while (exp > 0) {
+            if ((exp % 2) == 1) {
+                res = (res * base) % mod;
+            }
+            base = (base * base) % mod;
+            exp >>= 1;
+        }
+        return res;
+    }
+}
