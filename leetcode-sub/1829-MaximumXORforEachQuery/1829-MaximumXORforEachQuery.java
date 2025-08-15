657cdd93efd972f578265717e6138ebefa5fabb7
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-15 05:21:26 -0400

    File updated at 8/15/2025, 5:21:26 AM

--- leetcode-sub/1829-MaximumXORforEachQuery/1829-MaximumXORforEachQuery.java
+++ leetcode-sub/1829-MaximumXORforEachQuery/1829-MaximumXORforEachQuery.java
@@ -1,4 +1,4 @@
-// Last updated: 8/15/2025, 5:20:39 AM
+// Last updated: 8/15/2025, 5:21:26 AM
 class Solution {
     public long[] findMaxSum(int[] nums1, int[] nums2, int k) {
         int n = nums1.length;
@@ -8,18 +8,13 @@ class Solution {
             nums[i][0] = nums1[i];
             nums[i][1] = i;
         }
-        // Arrays.sort(nums, (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
         Arrays.sort(nums, (a, b) -> a[0] - b[0]);
         long sum = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int i = 0; i < n; i++) {
             int idx = nums[i][1];
-            if (i > 0 && nums[i][0] == nums[i - 1][0]) {
-                res[idx] = res[nums[i - 1][1]];
-            }
-            else {
-                res[idx] = sum;
-            }
+            res[idx] = (i > 0 && nums[i][0] == nums[i - 1][0]) ? 
+            res[nums[i - 1][1]] : sum;
             sum += nums2[idx];
             pq.offer(nums2[idx]);
             if (pq.size() > k) {
