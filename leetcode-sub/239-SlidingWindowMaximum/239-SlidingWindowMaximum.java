638f4df55f1954beedf9674c0694f1cef2bad175
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-18 04:32:46 -0400

    File updated at 8/18/2025, 4:32:46 AM

--- leetcode-sub/239-SlidingWindowMaximum/239-SlidingWindowMaximum.java
+++ leetcode-sub/239-SlidingWindowMaximum/239-SlidingWindowMaximum.java
@@ -1,34 +1,19 @@
-// Last updated: 8/18/2025, 12:31:45 AM
+// Last updated: 8/18/2025, 4:32:46 AM
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
-        int[] res = new int[n - k + 1];
-        SparseTable table = new SparseTable(nums);
+        int[] res = new int[n - k + 1], pre = new int[n], suf = new int[n];
+        pre[0] = nums[0];
+        suf[n - 1] = nums[n - 1];
+        for (int i = 1; i < n; i++) {
+            pre[i] = i % k == 0 ? nums[i] : Math.max(pre[i - 1], nums[i]);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            suf[i] = i % k == 0 ? nums[i] : Math.max(suf[i + 1], nums[i]);
+        }
         for (int i = 0; i <= n - k; i++) {
-            res[i] = table.query(i, i + k - 1);
+            res[i] = Math.max(suf[i], pre[i + k - 1]);
         }
         return res;
-    }
-    class SparseTable {
-        int[][] table;
-        int k, n;
-        public SparseTable(int[] nums) {
-            n = nums.length;
-            k = (int) (Math.log(n) / Math.log(2)) + 1;
-            table = new int[n][k];
-            for (int i = 0; i < n; i++) {
-                table[i][0] = nums[i];
-            }
-            for (int j = 1; j < k; j++) {
-                for (int i = 0; i + (1 << j) <= n; i++) {
-                    table[i][j] = Math.max(table[i][j - 1], table[i + (1 << (j - 1))][j - 1]);
-                }
-            }
-        }
-        public int query(int l, int r) {
-            if (l > r) return Integer.MAX_VALUE;
-            int x = 31 - Integer.numberOfLeadingZeros(r - l + 1);
-            return Math.max(table[l][x], table[r - (1 << x) + 1][x]);
-        }
     }
 }
