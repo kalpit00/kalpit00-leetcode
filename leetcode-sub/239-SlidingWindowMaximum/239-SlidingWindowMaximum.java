b49e220dc13f047f504b4158543f945225a9e70a
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-18 00:31:45 -0400

    File updated at 8/18/2025, 12:31:45 AM

--- leetcode-sub/239-SlidingWindowMaximum/239-SlidingWindowMaximum.java
+++ leetcode-sub/239-SlidingWindowMaximum/239-SlidingWindowMaximum.java
@@ -1,20 +1,34 @@
-// Last updated: 6/9/2025, 1:08:25 AM
+// Last updated: 8/18/2025, 12:31:45 AM
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int n = nums.length, idx = 1;
+        int n = nums.length;
         int[] res = new int[n - k + 1];
-        TreeSet<int[]> maxHeap = new TreeSet<>((a, b) -> a[0] != b[0] ? 
-        Integer.compare(b[0], a[0]) : b[1] - a[1]); // left
-        for (int i = 0; i < k; i++) {
-            maxHeap.add(new int[]{nums[i], i});
-        }
-        res[0] = maxHeap.first()[0];
-        for (int i = k; i < n; i++) {
-            int[] old = new int[]{nums[i - k], i - k};
-            maxHeap.remove(old);
-            maxHeap.add(new int[]{nums[i], i});
-            res[idx++] = maxHeap.first()[0];
+        SparseTable table = new SparseTable(nums);
+        for (int i = 0; i <= n - k; i++) {
+            res[i] = table.query(i, i + k - 1);
         }
         return res;
+    }
+    class SparseTable {
+        int[][] table;
+        int k, n;
+        public SparseTable(int[] nums) {
+            n = nums.length;
+            k = (int) (Math.log(n) / Math.log(2)) + 1;
+            table = new int[n][k];
+            for (int i = 0; i < n; i++) {
+                table[i][0] = nums[i];
+            }
+            for (int j = 1; j < k; j++) {
+                for (int i = 0; i + (1 << j) <= n; i++) {
+                    table[i][j] = Math.max(table[i][j - 1], table[i + (1 << (j - 1))][j - 1]);
+                }
+            }
+        }
+        public int query(int l, int r) {
+            if (l > r) return Integer.MAX_VALUE;
+            int x = 31 - Integer.numberOfLeadingZeros(r - l + 1);
+            return Math.max(table[l][x], table[r - (1 << x) + 1][x]);
+        }
     }
 }
