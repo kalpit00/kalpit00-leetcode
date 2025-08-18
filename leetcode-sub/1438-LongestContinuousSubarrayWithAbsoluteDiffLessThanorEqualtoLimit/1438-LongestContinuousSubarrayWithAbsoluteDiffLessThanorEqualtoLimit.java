e7529b17627a2c1b12482bea73719575e264085d
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-18 17:50:07 -0400

    File updated at 8/18/2025, 5:50:07 PM

--- leetcode-sub/1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit/1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit.java
+++ leetcode-sub/1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit/1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit.java
@@ -1,33 +1,45 @@
-// Last updated: 6/9/2025, 12:24:07 AM
+// Last updated: 8/18/2025, 5:50:07 PM
 class Solution {
     public int longestSubarray(int[] nums, int limit) {
+        SparseTable table = new SparseTable(nums);
         int n = nums.length, left = 0, right = 0, max = 0;
-        Deque<Integer> maxDeque = new ArrayDeque<>();
-        Deque<Integer> minDeque = new ArrayDeque<>();   
         while (right < n) {
-            while (!maxDeque.isEmpty() && 
-            nums[maxDeque.peekLast()] < nums[right]) {
-                maxDeque.pollLast();
-            }
-            while (!minDeque.isEmpty() &&
-            nums[minDeque.peekLast()] > nums[right]) {
-                minDeque.pollLast();
-            }
-            maxDeque.add(right);
-            minDeque.add(right);
-            right++;
-            while (!maxDeque.isEmpty() && !minDeque.isEmpty() && 
-            nums[maxDeque.peekFirst()] - nums[minDeque.peekFirst()] > limit) {
-                if (maxDeque.peekFirst() == left) {
-                    maxDeque.pollFirst();
-                }
-                if (minDeque.peekFirst() == left) {
-                    minDeque.pollFirst();
-                }
+            while (left < n && table.queryMax(left, right) - 
+            table.queryMin(left, right) > limit) {
                 left++;
             }
-            max = Math.max(max, right - left);
+            max = Math.max(max, right - left + 1);
+            right++;
         }
         return max;
+    }
+    class SparseTable {
+        int[][][] table;
+        int k, n;
+        public SparseTable(int[] nums) {
+            n = nums.length;
+            k = (int) (Math.log(n) / Math.log(2)) + 1;
+            table = new int[n][k][2];
+            for (int i = 0; i < n; i++) {
+                table[i][0][0] = nums[i];
+                table[i][0][1] = nums[i];
+            }
+            for (int j = 1; j < k; j++) {
+                for (int i = 0; i + (1 << j) <= n; i++) {
+                    table[i][j][0] = Math.max(table[i][j - 1][0], table[i + (1 << (j - 1))][j - 1][0]);
+                    table[i][j][1] = Math.min(table[i][j - 1][1], table[i + (1 << (j - 1))][j - 1][1]);
+                }
+            }
+        }
+        public int queryMax(int l, int r) {
+            if (l > r) return Integer.MAX_VALUE;
+            int x = 31 - Integer.numberOfLeadingZeros(r - l + 1);
+            return Math.max(table[l][x][0], table[r - (1 << x) + 1][x][0]);
+        }
+        public int queryMin(int l, int r) {
+            if (l > r) return Integer.MIN_VALUE;
+            int x = 31 - Integer.numberOfLeadingZeros(r - l + 1);
+            return Math.min(table[l][x][1], table[r - (1 << x) + 1][x][1]);
+        }
     }
 }
