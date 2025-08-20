3256fd1801871019aa36b3c2b6c30e266602f1ed
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-20 03:00:16 -0400

    File updated at 8/20/2025, 3:00:16 AM

--- leetcode-sub/2338-CounttheNumberofIdealArrays/2338-CounttheNumberofIdealArrays.java
+++ leetcode-sub/2338-CounttheNumberofIdealArrays/2338-CounttheNumberofIdealArrays.java
@@ -1,62 +1,18 @@
-// Last updated: 4/22/2025, 12:47:11 AM
+// Last updated: 8/20/2025, 3:00:16 AM
 class Solution {
-
-    static int MOD = 1000000007;
-    static int MAX_N = 10010;
-    static int MAX_P = 15; // There are up to 15 prime factors
-    static int[][] c = new int[MAX_N + MAX_P][MAX_P + 1];
-    static int[] sieve = new int[MAX_N]; // Minimum prime factor
-    static List<Integer>[] ps = new List[MAX_N]; // List of prime factor counts
-
-    public Solution() {
-        if (c[0][0] == 1) {
-            return;
+    public boolean isSubstringPresent(String s) {
+        String rev = new StringBuilder(s).reverse().toString();
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < s.length() - 1; i++) {
+            String key = rev.charAt(i) + "" + rev.charAt(i + 1);
+            set.add(key);
         }
-
-        for (int i = 0; i < MAX_N; i++) {
-            ps[i] = new ArrayList<>();
-        }
-
-        for (int i = 2; i < MAX_N; i++) {
-            if (sieve[i] == 0) {
-                for (int j = i; j < MAX_N; j += i) {
-                    if (sieve[j] == 0) {
-                        sieve[j] = i;
-                    }
-                }
+        for (int i = 0; i < s.length() - 1; i++) {
+            String key = s.charAt(i) + "" + s.charAt(i + 1);
+            if (set.contains(key)) {
+                return true;
             }
         }
-
-        for (int i = 2; i < MAX_N; i++) {
-            int x = i;
-            while (x > 1) {
-                int p = sieve[x], cnt = 0;
-                while (x % p == 0) {
-                    x /= p;
-                    cnt++;
-                }
-                ps[i].add(cnt);
-            }
-        }
-
-        c[0][0] = 1;
-        for (int i = 1; i < MAX_N + MAX_P; i++) {
-            c[i][0] = 1;
-            for (int j = 1; j <= Math.min(i, MAX_P); j++) {
-                c[i][j] = (c[i - 1][j] + c[i - 1][j - 1]) % MOD;
-            }
-        }
-    }
-
-    public int idealArrays(int n, int maxValue) {
-        long ans = 0;
-        for (int x = 1; x <= maxValue; x++) {
-            long mul = 1;
-            for (int p : ps[x]) {
-                mul = (mul * c[n + p - 1][p]) % MOD;
-            }
-            ans = (ans + mul) % MOD;
-        }
-        return (int) ans;
+        return false;
     }
 }
