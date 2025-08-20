87559769266501686cfc3d4a0f99f8644bdcebe5
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-20 15:09:48 -0400

    File updated at 8/20/2025, 3:09:48 PM

--- leetcode-sub/2338-CounttheNumberofIdealArrays/2338-CounttheNumberofIdealArrays.java
+++ leetcode-sub/2338-CounttheNumberofIdealArrays/2338-CounttheNumberofIdealArrays.java
@@ -1,7 +1,29 @@
-// Last updated: 8/20/2025, 3:06:12 AM
+// Last updated: 8/20/2025, 3:09:48 PM
 class Solution {
-    public int numberOfSteps(int num) {
-        return num == 0 ? 0 : 
-        31 - Integer.numberOfLeadingZeros(num) + Integer.bitCount(num);
+    public int maxScore(int[] nums) {
+        int n = nums.length;
+        Integer[][] dp = new Integer[n][1 << n];
+        return solve(1, n, 0, nums, dp);
+    }
+    private int solve(int i, int n, int mask, int[] nums, Integer[][] dp) {
+        if (i > n / 2) {
+            return 0;
+        }
+        if (dp[i][mask] != null) {
+            return dp[i][mask];
+        }
+        int max = Integer.MIN_VALUE;
+        for (int j = 0; j < n; j++) {
+            for (int k = j + 1; k < n; k++) {
+                int newMask = (1 << j) | (1 << k);
+                if ((mask & newMask) != 0) continue;
+                max = Math.max(max, i * gcd(nums[j], nums[k]) + 
+                solve(i + 1, n, mask | newMask, nums, dp));
+            }
+        }
+        return dp[i][mask] = max;
+    }
+    private int gcd(int x, int y) {
+        return y == 0 ? x : gcd(y, x % y);
     }
 }
