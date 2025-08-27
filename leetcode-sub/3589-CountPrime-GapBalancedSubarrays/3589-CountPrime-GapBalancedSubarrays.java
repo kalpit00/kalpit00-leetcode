b3cef4e4d86f8a7dc31add828bba6ce762efca76
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-27 19:47:16 -0400

    File updated at 8/27/2025, 7:47:16 PM

--- leetcode-sub/3589-CountPrime-GapBalancedSubarrays/3589-CountPrime-GapBalancedSubarrays.java
+++ leetcode-sub/3589-CountPrime-GapBalancedSubarrays/3589-CountPrime-GapBalancedSubarrays.java
@@ -1,35 +1,33 @@
-// Last updated: 8/27/2025, 7:45:45 PM
+// Last updated: 8/27/2025, 7:47:16 PM
 class Solution {
     public int primeSubarray(int[] nums, int k) {
-        int n = nums.length, left1 = 0, left2 = 0, right = 0, 
-        max = 0, res = 0, count = 0;
+        int n = nums.length, left = 0, right = 0, max = 0, res = 0, count = 0;
         for (int num : nums) {
             max = Math.max(max, num);
         }
         boolean[] notPrime = sieve(max + 1);
         TreeMap<Integer, Integer> map = new TreeMap<>();
-        while (right < n) {
-            if (!notPrime[nums[right]]) {
+        for (int i = 0; i < n; i++) {
+            if (!notPrime[nums[i]]) {
                 count++;
-                map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
             }
             while (count > 1) {
-                if (!notPrime[nums[left1]]) {
+                if (!notPrime[nums[right]]) {
                     count--;
                 }
-                left1++;
+                right++;
             }
             while (!map.isEmpty() && map.lastKey() - map.firstKey() > k) {
-                if (!notPrime[nums[left2]]) {
-                    map.put(nums[left2], map.get(nums[left2]) - 1);
-                    if (map.get(nums[left2]) == 0) {
-                        map.remove(nums[left2]);
+                if (!notPrime[nums[left]]) {
+                    map.put(nums[left], map.get(nums[left]) - 1);
+                    if (map.get(nums[left]) == 0) {
+                        map.remove(nums[left]);
                     }
                 }
-                left2++;
+                left++;
             }
-            res += left1 - left2; // res += (r - l2 + 1) - (r - l1 + 1)
-            right++;
+            res += right - left;
         }
         return res;
     }
