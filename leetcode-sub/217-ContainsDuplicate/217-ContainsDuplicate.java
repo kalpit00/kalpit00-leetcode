bcb2b84fbeb685c1ba4d0fc6323dab09d081436f
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-27 03:26:00 -0400

    File updated at 8/27/2025, 3:25:59 AM

--- leetcode-sub/217-ContainsDuplicate/217-ContainsDuplicate.java
+++ leetcode-sub/217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -1,28 +1,15 @@
-// Last updated: 8/27/2025, 3:25:15 AM
+// Last updated: 8/27/2025, 3:25:59 AM
 class Solution {
     public String breakPalindrome(String palindrome) {
-        char[] arr = palindrome.toCharArray();
-        int n = arr.length, idx = 0;
-        if (n == 1) {
-            return "";
-        }
-        boolean flag = false;
-        char c = 'a';
-        for (int i = 0; i < n; i++) {
-            if (arr[i] != 'a') {
-                idx = i;
-                c = arr[i];
-                arr[i] = 'a';
-                break;
+        char[] s = palindrome.toCharArray();
+        int n = s.length;
+        for (int i = 0; i < n / 2; i++) {
+            if (s[i] != 'a') {
+                s[i] = 'a';
+                return String.valueOf(s);
             }
         }
-        for (int i = 0; i < n; i++) {
-            flag = arr[i] != 'a' ? true : flag;
-        }
-        if (!flag) {
-            arr[idx] = c;
-            arr[n - 1] = 'b';
-        }
-        return new String(arr);
+        s[n - 1] = 'b';
+        return n < 2 ? "" : String.valueOf(s);
     }
 }
