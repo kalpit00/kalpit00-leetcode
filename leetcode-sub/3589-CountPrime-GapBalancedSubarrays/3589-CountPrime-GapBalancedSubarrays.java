ddf41205fc317ad62f6198788908d98f0d624298
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-27 20:09:11 -0400

    File updated at 8/27/2025, 8:09:10 PM

--- leetcode-sub/3589-CountPrime-GapBalancedSubarrays/3589-CountPrime-GapBalancedSubarrays.java
+++ leetcode-sub/3589-CountPrime-GapBalancedSubarrays/3589-CountPrime-GapBalancedSubarrays.java
@@ -1,46 +1,66 @@
-// Last updated: 8/27/2025, 7:47:16 PM
+// Last updated: 8/27/2025, 8:09:10 PM
 class Solution {
-    public int primeSubarray(int[] nums, int k) {
-        int n = nums.length, left = 0, right = 0, max = 0, res = 0, count = 0;
-        for (int num : nums) {
-            max = Math.max(max, num);
-        }
-        boolean[] notPrime = sieve(max + 1);
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        for (int i = 0; i < n; i++) {
-            if (!notPrime[nums[i]]) {
-                count++;
-                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-            }
-            while (count > 1) {
-                if (!notPrime[nums[right]]) {
-                    count--;
+    private static final int M = 50000;
+    private static boolean[] p = new boolean[M + 1];
+    private static boolean done = false;
+
+    private void sieve() {
+        if (!done) {
+            Arrays.fill(p, true);
+            p[0] = p[1] = false;
+            for (int i = 2; i * i <= M; i++) {
+                if (p[i]) {
+                    for (int j = i * i; j <= M; j += i)
+                        p[j] = false;
                 }
-                right++;
             }
-            while (!map.isEmpty() && map.lastKey() - map.firstKey() > k) {
-                if (!notPrime[nums[left]]) {
-                    map.put(nums[left], map.get(nums[left]) - 1);
-                    if (map.get(nums[left]) == 0) {
-                        map.remove(nums[left]);
-                    }
-                }
-                left++;
-            }
-            res += right - left;
+            done = true;
         }
-        return res;
     }
-    public boolean[] sieve(int n) {
-        boolean[] notPrime = new boolean[n];
-        notPrime[0] = notPrime[1] = true;
-        for (int i = 2; i < n; i++) {
-            if (!notPrime[i]) {
-                for (int j = 2; i*j < n; j++) {
-                    notPrime[i*j] = true;
-                }
+
+    public int primeSubarray(int[] a, int k) {
+        sieve();
+        int n = a.length;
+        long ans = 0;
+
+        int[] f = new int[n];
+        for (int i = 0; i < n; i++) f[i] = p[a[i]] ? 1 : 0;
+
+        LinkedList<Integer> minQ = new LinkedList<>();
+        LinkedList<Integer> maxQ = new LinkedList<>();
+        LinkedList<Integer> primes = new LinkedList<>();
+
+        int l = 0;
+
+        for (int r = 0; r < n; r++) {
+            if (f[r] == 1) {
+                while (!minQ.isEmpty() && a[minQ.peekLast()] >= a[r]) minQ.pollLast();
+                minQ.addLast(r);
+
+                while (!maxQ.isEmpty() && a[maxQ.peekLast()] <= a[r]) maxQ.pollLast();
+                maxQ.addLast(r);
+
+                primes.addLast(r);
+            }
+
+            while (!minQ.isEmpty() && !maxQ.isEmpty()) {
+                int minV = a[minQ.peekFirst()];
+                int maxV = a[maxQ.peekFirst()];
+
+                if (maxV - minV > k) {
+                    if (minQ.peekFirst() == l) minQ.pollFirst();
+                    if (maxQ.peekFirst() == l) maxQ.pollFirst();
+                    if (!primes.isEmpty() && primes.peekFirst() == l) primes.pollFirst();
+                    l++;
+                } else break;
+            }
+
+            if (primes.size() >= 2) {
+                int x = primes.get(primes.size() - 2);
+                ans += (x - l + 1);
             }
         }
-        return notPrime;
+
+        return (int) ans;  
     }
 }
