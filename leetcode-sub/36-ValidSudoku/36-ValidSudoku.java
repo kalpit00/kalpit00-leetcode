754c2db0b9c37f52bf1d97ce057a3cf9a65f09d5
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-29 21:27:00 -0400

    File updated at 8/29/2025, 9:26:59 PM

--- leetcode-sub/36-ValidSudoku/36-ValidSudoku.java
+++ leetcode-sub/36-ValidSudoku/36-ValidSudoku.java
@@ -1,27 +1,28 @@
-// Last updated: 8/29/2025, 9:25:28 PM
+// Last updated: 8/29/2025, 9:26:59 PM
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         int n = board.length, k = (int) Math.sqrt(n);
-        boolean[][] rows = new boolean[n][10], cols = new boolean[n][10];
-        boolean[][][] boxes = new boolean[k][k][10];
+        boolean[][] rows = new boolean[n][n], cols = new boolean[n][n];
+        boolean[][][] boxes = new boolean[k][k][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 char c = board[i][j];
                 if (c == '.') {
                     continue;
                 }
-                if (rows[i][c - '0']) {
+                int idx = c - '1';
+                if (rows[i][idx]) {
                     return false;
                 }
-                if (cols[j][c - '0']) {
+                if (cols[j][idx]) {
                     return false;
                 }
-                if (boxes[i/k][j/k][c - '0']) {
+                if (boxes[i/k][j/k][idx]) {
                     return false;
                 }
-                rows[i][c - '0'] = true;
-                cols[j][c - '0'] = true;
-                boxes[i/k][j/k][c - '0'] = true;
+                rows[i][idx] = true;
+                cols[j][idx] = true;
+                boxes[i/k][j/k][idx] = true;
             }
         }
         return true;
