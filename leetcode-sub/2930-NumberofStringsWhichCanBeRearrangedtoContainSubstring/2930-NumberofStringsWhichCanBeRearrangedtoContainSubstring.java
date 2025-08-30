3753982829bb42283827a7d6db3d34bb626fefce
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-30 17:55:14 -0400

    File updated at 8/30/2025, 5:55:14 PM

--- leetcode-sub/2930-NumberofStringsWhichCanBeRearrangedtoContainSubstring/2930-NumberofStringsWhichCanBeRearrangedtoContainSubstring.java
+++ leetcode-sub/2930-NumberofStringsWhichCanBeRearrangedtoContainSubstring/2930-NumberofStringsWhichCanBeRearrangedtoContainSubstring.java
@@ -1,36 +1,24 @@
-// Last updated: 8/30/2025, 5:51:08 PM
+// Last updated: 8/30/2025, 5:55:14 PM
 class Solution {
-    int mod = 1000000007;
-    public int stringCount(int n) {
-        if (n < 4) {
-            return 0;
+    private long pow(long x, long n, long mod) {
+        long result = 1, p = x % mod;
+        while (n != 0) {
+            if ((n & 1) != 0) {
+                result = (result * p) % mod;
+            }
+            p = (p * p) % mod;
+            n >>= 1;
         }
-        Long[][][][] dp = new Long[n][3][2][2];
-        return (int) solve(0, n, 0, 0, 0, dp);
+        return result;
     }
-    private long solve(int i, int n, int e, int l, int t, Long[][][][] dp) {
-        if (i == n) {
-            return e >= 2 && t >= 1 && l >= 1 ? 1 : 0;
-        }
-        if (dp[i][e][l][t] != null) {
-            return dp[i][e][l][t];
-        }
-        long ans = 0;
-        for (int j = 0; j < 26; j++) {
-            char c = (char) (j + 'a'); // [0, 25] -> [a, z]
-            if (c == 'e' && e < 2) {
-                ans = ans + (solve(i + 1, n, e + 1, l, t, dp) % mod);
-            }
-            else if (c == 'l' && l < 1) {
-                ans = ans + (solve(i + 1, n, e, l + 1, t, dp) % mod);
-            }
-            else if (c == 't' && t < 1) {
-                ans = ans + (solve(i + 1, n, e, l, t + 1, dp) % mod);
-            }
-            else { // any char other than [e, l, t]
-                ans = ans + (solve(i + 1, n, e, l, t, dp) % mod);
-            }
-        }
-        return dp[i][e][l][t] = ans % mod;
+
+    public int stringCount(int n) {
+        long mod = (int)1e9 + 7;
+        return (int)(((
+            + pow(26, n, mod)
+            - (n + 75) * pow(25, n - 1, mod)
+            + (2 * n + 72) * pow(24, n - 1, mod)
+            - (n + 23) * pow(23, n - 1, mod)
+        ) % mod + mod) % mod);
     }
 }
