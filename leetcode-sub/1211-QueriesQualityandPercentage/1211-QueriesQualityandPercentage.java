e709cb42a39c0c44ea792806948e5eddfcc894bb
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-08-31 02:22:00 -0400

    File updated at 8/31/2025, 2:22:00 AM

--- leetcode-sub/1211-QueriesQualityandPercentage/1211-QueriesQualityandPercentage.java
+++ leetcode-sub/1211-QueriesQualityandPercentage/1211-QueriesQualityandPercentage.java
@@ -1,8 +1,13 @@
-// Last updated: 8/31/2025, 2:20:16 AM
+// Last updated: 8/31/2025, 2:22:00 AM
 class Solution {
     public boolean doesAliceWin(String s) {
-        for (char ch : s.toCharArray()) {
+        int n = s.length();
+        for (int i = 0; i <= n / 2; i++) {
+            char ch = s.charAt(i), c = s.charAt(n - i - 1);
             if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                return true;
+            }
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
                 return true;
             }
         }
