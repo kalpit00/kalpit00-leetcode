2c03a98618fc6e770004aef64a2bee4e4defe302
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-09-02 17:35:25 -0400

    File updated at 9/2/2025, 5:35:25 PM

--- leetcode-sub/2455-AverageValueofEvenNumbersThatAreDivisiblebyThree/2455-AverageValueofEvenNumbersThatAreDivisiblebyThree.java
+++ leetcode-sub/2455-AverageValueofEvenNumbersThatAreDivisiblebyThree/2455-AverageValueofEvenNumbersThatAreDivisiblebyThree.java
@@ -1,13 +1,21 @@
-// Last updated: 9/2/2025, 5:31:25 PM
+// Last updated: 9/2/2025, 5:35:25 PM
 class Solution {
-    public int averageValue(int[] nums) {
-        int count = 0, sum = 0;
-        for (int num : nums) {
-            if (num % 6 == 0) {
-                count++;
-                sum += num;
+    public int[] getNoZeroIntegers(int n) {
+        for (int i = 1; i <= n / 2; i++) {
+            int j = n - i;
+            if (helper(i) && helper(j)) {
+                return new int[]{i, j};
             }
         }
-        return count == 0 ? 0 : sum / count;
+        return new int[]{-1, -1};
+    }
+    private boolean helper(int num) {
+        while (num > 0) {
+            if (num % 10 == 0) {
+                return false;
+            }
+            num /= 10;
+        }
+        return true;
     }
 }
