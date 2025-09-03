6f04fe34e31761d278a79627384327f9f451ebd6
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-09-03 13:20:50 -0400

    Create file

---
+++ leetcode-sub/2556-DisconnectPathinaBinaryMatrixbyatMostOneFlip/2556-DisconnectPathinaBinaryMatrixbyatMostOneFlip.java
@@ -0,0 +1,19 @@
+// Last updated: 9/3/2025, 1:20:50 PM
+class Solution {
+    public boolean isPossibleToCutPath(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        dfs(grid, 0, 0, m, n);
+        grid[0][0] = 1;
+        return !dfs(grid, 0, 0, m, n);
+    }
+    private boolean dfs(int[][] grid, int r, int c, int m, int n) {
+        if (r == m - 1 && c == n - 1) {
+            return true; // path to [m-1][n-1] found, count 1
+        }
+        if (r >= m || c >= n || r < 0 || c < 0 || grid[r][c] == 0) {
+            return false;
+        }
+        grid[r][c] = 0;
+        return dfs(grid, r + 1, c, m, n) || dfs(grid, r, c + 1, m, n);
+    }
+}
