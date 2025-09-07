a34b5a874ac07452353452cea553505b704cfe92
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-09-06 21:04:16 -0400

    File updated at 9/6/2025, 9:04:15 PM

--- leetcode-sub/1304-FindNUniqueIntegersSumuptoZero/1304-FindNUniqueIntegersSumuptoZero.java
+++ leetcode-sub/1304-FindNUniqueIntegersSumuptoZero/1304-FindNUniqueIntegersSumuptoZero.java
@@ -1,13 +1,13 @@
-// Last updated: 9/6/2025, 9:00:58 PM
+// Last updated: 9/6/2025, 9:04:15 PM
 class Solution {
     public int[] sumZero(int n) {
         int sum = 0;
         int[] res = new int[n];
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < n / 2; i++) {
             res[i] = i + 1;
-            sum += i + 1;
+            res[n - i - 1] = -(i + 1);
         }
-        res[n - 1] = -sum;
+        // res[n / 2] = 0; // redudant as int[] have 0s by default
         return res;
     }
 }
