04779c21a7f29f68f1dd5f284a20dc273e15fab2
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-09-09 03:54:34 -0400

    File updated at 9/9/2025, 3:54:34 AM

--- leetcode-sub/1154-DayoftheYear/1154-DayoftheYear.java
+++ leetcode-sub/1154-DayoftheYear/1154-DayoftheYear.java
@@ -1,22 +1,22 @@
-// Last updated: 9/9/2025, 3:43:22 AM
+// Last updated: 9/9/2025, 3:54:34 AM
 class Solution {
     public int minValidStrings(String[] words, String target) {
         int n = words.length, k = target.length(), count = 0;
         List<int[]> list = new ArrayList<>();
         for (String word : words) {
             list.add(buildLPS((word + "#" + target).toCharArray()));
-        }
-        while (k > 0) {
+        } // precompute lps[] for str = (words[i] + '#' + target)
+        while (k > 0) { // O(nk) = 10^5, is fine!
             int max = 0;
             for (int i = 0; i < n; i++) {
                 max = Math.max(max, list.get(i)[words[i].length() + k]);
-            }
+            } // take lps[words[i].len + k] to get MATCH of prefix = suffix
             if (max == 0) {
                 return -1;
             }
-            count++;
+            count++; // lower k by max!
             k -= max;
-        }
+        } // this way, while k > 0 tries to match entire 'target' of length k!
         return count;
     }
     private int[] buildLPS(char[] pattern) {
