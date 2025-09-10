2b4e80c3237e1ebe7e7bdde49636ffa685bb6193
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-09-09 20:05:14 -0400

    File updated at 9/9/2025, 8:05:14 PM

--- leetcode-sub/3532-PathExistenceQueriesinaGraphI/3532-PathExistenceQueriesinaGraphI.java
+++ leetcode-sub/3532-PathExistenceQueriesinaGraphI/3532-PathExistenceQueriesinaGraphI.java
@@ -1,57 +1,34 @@
-// Last updated: 9/9/2025, 8:03:48 PM
+// Last updated: 9/9/2025, 8:05:14 PM
 class Solution {
-    public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
-        DSU dsu = new DSU(n);
-        int[][] arr = new int[n][2];
-        for (int i = 0; i < n; i++) {
-            arr[i] = new int[]{nums[i], i};
-        }
-        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
-        for (int i = 1; i < n; i++) {
-            if (arr[i][0] - arr[i - 1][0] <= maxDiff) {
-                dsu.union(arr[i][1], arr[i - 1][1]);
-            }
-        }
-        int m = queries.length;
-        boolean[] res = new boolean[m];
-        for (int i = 0; i < m; i++) {
-            int u = queries[i][0], v = queries[i][1];
-            res[i] = dsu.findParent(u) == dsu.findParent(v);
-        }
-        return res;
+    
+public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+        int[] component = new int[n];
+int compId = 0;
+component[0] = compId;
+for (int i = 1; i < n; i++) {
+    if (nums[i] - nums[i-1] <= maxDiff) {
+        component[i] = compId;
+    } else {
+        compId++;
+        component[i] = compId;
     }
-    class DSU {
-        int[] size, parent;
-        int componentCount;
-        public DSU(int n) {
-            size = new int[n];
-            parent = new int[n];
-            componentCount = n;
-            for (int i = 0; i < n; i++) {
-                size[i] = 1;
-                parent[i] = i;
-            }
-        }
+}
 
-        public int findParent(int node) {
-            if (node == parent[node]) {
-                return node;
-            }
-            return parent[node] = findParent(parent[node]);
-        }
-
-        public boolean union(int u, int v) {
-            int pu = findParent(u), pv = findParent(v);
-            if (pu == pv) return false;
-            if (size[pu] < size[pv]) {
-                parent[pu] = pv;
-                size[pv] += size[pu];
-            } else {
-                parent[pv] = pu;
-                size[pu] += size[pv];
-            }
-            componentCount--;
-            return true;
-        }
+// Answer queries
+boolean[] response = new boolean[queries.length];
+for (int i = 0; i < queries.length; i++) {
+    int u = queries[i][0];
+    int v = queries[i][1];
+    response[i] = component[u] == component[v];
+}
+        return response;
     }
+
+    int findParent(int[] parent, int node) {
+    while (parent[node] != node) {
+        parent[node] = parent[parent[node]]; // path compression
+        node = parent[node];
+    }
+    return node;
+}
 }
