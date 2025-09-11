7d9ffeac2d5841214062e0898ba63f5f877bb5e7
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-09-10 20:04:35 -0400

    File updated at 9/10/2025, 8:04:35 PM

--- leetcode-sub/90-SubsetsII/90-SubsetsII.java
+++ leetcode-sub/90-SubsetsII/90-SubsetsII.java
@@ -1,7 +1,8 @@
-// Last updated: 9/10/2025, 4:38:46 PM
+// Last updated: 9/10/2025, 8:04:35 PM
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         int n = nums.length;
+        Arrays.sort(nums);
         Set<List<Integer>> set = new HashSet<>();
         List<Integer> list = new ArrayList<>();
         solve(0, n, nums, set, list);
@@ -10,9 +11,7 @@ class Solution {
     private void solve(int i, int n, int[] nums,
     Set<List<Integer>> set, List<Integer> list) {
         if (i >= n) {
-            List<Integer> sorted = new ArrayList<>(list);
-            Collections.sort(sorted);
-            set.add(sorted);
+            set.add(new ArrayList<>(list));
             return;
         }
         list.add(nums[i]);
