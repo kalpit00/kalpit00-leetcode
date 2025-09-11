79339d58bf3fc58b03cd41f9fbbb0d60e7ae9732
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-09-10 23:21:46 -0400

    File updated at 9/10/2025, 11:21:46 PM

--- leetcode-sub/47-PermutationsII/47-PermutationsII.java
+++ leetcode-sub/47-PermutationsII/47-PermutationsII.java
@@ -1,23 +1,25 @@
-// Last updated: 9/10/2025, 11:18:32 PM
+// Last updated: 9/10/2025, 11:21:46 PM
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
         int n = nums.length;
-        Set<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> res = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        backtrack(0, n, list, set, nums);
-        return new ArrayList<>(set);
+        backtrack(0, n, list, res, nums);
+        return res;
     }
     
     public void backtrack(int mask, int n,
-    List<Integer> list, Set<List<Integer>> set, int[] nums) {
+    List<Integer> list, List<List<Integer>> res, int[] nums) {
         if (list.size() >= n) {
-            set.add(new ArrayList<>(list));
+            res.add(new ArrayList<>(list));
             return;
         }
         for (int i = 0; i < n; i++) {
-            if ((mask & (1 << i)) != 0) continue;
+            if (((mask & (1 << i)) != 0) || (i > 0 && nums[i] == nums[i-1] &&
+            (mask & (1 << (i - 1))) == 0)) continue;
             list.add(nums[i]);
-            backtrack(mask | (1 << i), n, list, set, nums);
+            backtrack(mask | (1 << i), n, list, res, nums);
             list.remove(list.size() - 1);
         }
     }
