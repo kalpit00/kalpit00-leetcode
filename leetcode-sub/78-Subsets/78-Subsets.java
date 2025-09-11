547631f3609c158d06a305e0f5eb58064ccdeba2
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-09-10 20:02:24 -0400

    File updated at 9/10/2025, 8:02:24 PM

--- leetcode-sub/78-Subsets/78-Subsets.java
+++ leetcode-sub/78-Subsets/78-Subsets.java
@@ -1,21 +1,21 @@
-// Last updated: 9/10/2025, 8:00:54 PM
+// Last updated: 9/10/2025, 8:02:24 PM
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         int n = nums.length;
-        Set<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> res = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        solve(0, n, nums, set, list);
-        return new ArrayList<>(set);
+        solve(0, n, nums, res, list);
+        return res;
     }
     private void solve(int i, int n, int[] nums,
-    Set<List<Integer>> set, List<Integer> list) {
+    List<List<Integer>> res, List<Integer> list) {
         if (i >= n) {
-            set.add(new ArrayList<>(list));
+            res.add(new ArrayList<>(list));
             return;
         }
         list.add(nums[i]);
-        solve(i + 1, n, nums, set, list);
+        solve(i + 1, n, nums, res, list);
         list.remove(list.size() - 1);
-        solve(i + 1, n, nums, set, list);
+        solve(i + 1, n, nums, res, list);
     }
 }
