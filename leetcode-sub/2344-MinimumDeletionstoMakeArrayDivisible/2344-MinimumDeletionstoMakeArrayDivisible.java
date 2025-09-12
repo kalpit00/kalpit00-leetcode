346bfd94a66fb3e5b88dfef5845acee19a06cdb6
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-09-12 00:22:20 -0400

    File updated at 9/12/2025, 12:22:20 AM

--- leetcode-sub/2344-MinimumDeletionstoMakeArrayDivisible/2344-MinimumDeletionstoMakeArrayDivisible.java
+++ leetcode-sub/2344-MinimumDeletionstoMakeArrayDivisible/2344-MinimumDeletionstoMakeArrayDivisible.java
@@ -1,23 +1,25 @@
-// Last updated: 9/12/2025, 12:21:10 AM
+// Last updated: 9/12/2025, 12:22:20 AM
 class Solution {
     public int minOperations(int[] nums, int[] numsDivide) {
-        int gcd = numsDivide[0], sum = 0;
-        for (int i = 1; i < numsDivide.length; i++) {
-            gcd = gcd(gcd, numsDivide[i]);
+        int g=numsDivide[0];
+        for(int i=1;i<numsDivide.length;i++){
+        g=gcd(g,numsDivide[i]);
         }
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        for (int key : map.keySet()) {        
-            if (gcd % key == 0) {
-                return sum;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(g%nums[i]==0){
+                return i;
             }
-            sum += map.get(key);
         }
-        return sum != 0 ? -1 : sum;
-    }
-    private int gcd(int x, int y) {
-        return y == 0 ? x : gcd(y, x % y);
-    }
+            return -1;
+
+        }
+        private int gcd(int a,int b){
+            while(b!=0){
+                int temp=a%b;
+                a=b;
+                b=temp;
+            }
+            return a;
+        }
 }
