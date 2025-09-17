dd249b2f036f7cb18246f073892c4c0726c9f6d8
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-09-17 04:45:00 -0400

    File updated at 9/17/2025, 4:45:00 AM

--- leetcode-sub/3169-CountDaysWithoutMeetings/3169-CountDaysWithoutMeetings.java
+++ leetcode-sub/3169-CountDaysWithoutMeetings/3169-CountDaysWithoutMeetings.java
@@ -1,15 +1,17 @@
-// Last updated: 9/17/2025, 4:41:34 AM
+// Last updated: 9/17/2025, 4:45:00 AM
 class Solution {
     public int maxHeight(int[][] grid) {
         int n = grid.length, max = 0;
-        int[] dp = new int[n];
         for (int[] item : grid) {
             Arrays.sort(item); // first sort each <l, w, h> -> interchangable
         } // then sort grid based on desc order on 1st dim -> 2nd -> 3rd
         Arrays.sort(grid, (a, b) -> a[0] != b[0] ? b[0] - a[0] : 
         a[1] != b[1] ? b[1] - a[1] : b[2] - a[2]);
+        int[] dp = new int[n];
         for (int i = 0; i < n; i++) {
             dp[i] = grid[i][2];
+        }
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 if (grid[i][0] <= grid[j][0] && grid[i][1] <= grid[j][1] &&
                 grid[i][2] <= grid[j][2]) {
