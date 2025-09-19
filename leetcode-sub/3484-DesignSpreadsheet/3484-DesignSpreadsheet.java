0ef17f7130798ceff89728dcd1992713a174dcd5
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-09-18 20:41:26 -0400

    File updated at 9/18/2025, 8:41:26 PM

--- leetcode-sub/3484-DesignSpreadsheet/3484-DesignSpreadsheet.java
+++ leetcode-sub/3484-DesignSpreadsheet/3484-DesignSpreadsheet.java
@@ -1,4 +1,4 @@
-// Last updated: 9/18/2025, 8:38:38 PM
+// Last updated: 9/18/2025, 8:41:26 PM
 class Spreadsheet {
     int[][] map;
     public Spreadsheet(int rows) {
@@ -7,7 +7,7 @@ class Spreadsheet {
     
     public void setCell(String cell, int value) {
         int j = cell.charAt(0) - 'A';
-        int i = Integer.parseInt(cell.substring(1));
+        int i = parseInt(cell, 1);
         map[i][j] = value;
     }
     
@@ -22,28 +22,27 @@ class Spreadsheet {
         int x = 0, y = 0;
         if (Character.isLetter(a.charAt(0))) {
             int j = a.charAt(0) - 'A';
-            int i = Integer.parseInt(a.substring(1));
+            int i = parseInt(a, 1);
             x = map[i][j];
         }
         else {
-            x = Integer.parseInt(a);
+            x = parseInt(a, 0);
         }
         if (Character.isLetter(b.charAt(0))) {
             int n = b.charAt(0) - 'A';
-            int m = Integer.parseInt(b.substring(1));            
+            int m = parseInt(b, 1);
             y = map[m][n];
         }
         else {
-            y = Integer.parseInt(b);
+            y = parseInt(b, 0);
         }
         return x + y;
     }
+    private int parseInt(String s, int start) {
+        int num = 0;
+        for (int i = start; i < s.length(); i++) {
+            num = (num * 10) + s.charAt(i) - '0';
+        }
+        return num;
+    }
 }
-
-/**
- * Your Spreadsheet object will be instantiated and called as such:
- * Spreadsheet obj = new Spreadsheet(rows);
- * obj.setCell(cell,value);
- * obj.resetCell(cell);
- * int param_3 = obj.getValue(formula);
- */
