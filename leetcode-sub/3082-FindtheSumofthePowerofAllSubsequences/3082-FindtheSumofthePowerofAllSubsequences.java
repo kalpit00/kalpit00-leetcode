a881e915890572a8b236bf981baef809fd52945a
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-09-21 17:07:45 -0400

    File updated at 9/21/2025, 5:07:45 PM

--- leetcode-sub/3082-FindtheSumofthePowerofAllSubsequences/3082-FindtheSumofthePowerofAllSubsequences.java
+++ leetcode-sub/3082-FindtheSumofthePowerofAllSubsequences/3082-FindtheSumofthePowerofAllSubsequences.java
@@ -1,4 +1,4 @@
-// Last updated: 4/12/2025, 3:35:24 AM
+// Last updated: 9/21/2025, 5:07:45 PM
 class Solution {
     int mod = 1000000007;
     public int sumOfPower(int[] nums, int k) {
@@ -9,7 +9,11 @@ class Solution {
     private int solve(int[] nums, int i, int n, int k, int sum, int count, 
     Integer[][][] dp) {
         if (sum == k) {
-            return helper(n - count); // ftn to calc 2^(x) with mod!
+            long res = 1L;
+            for (int j = 0; j < n - count; j++) {
+                res = (res * 2) % mod;
+            }
+            return (int) res;
         }
         if (i >= n || sum > k) {
             return 0;
@@ -20,20 +24,5 @@ class Solution {
         int take = solve(nums, i + 1, n, k, sum + nums[i], count + 1, dp);
         int notTake = solve(nums, i + 1, n, k, sum, count, dp);
         return dp[i][count][sum] = (take + notTake) % mod;
-    }
-    private int helper(int n) {
-        long res = 1L;
-        for (int i = 0; i < n; i++) {
-           res = (res * 2) % mod;
-        }
-        return (int) res;
-        // long a = 2;
-        // while (n > 0) {
-        //     if (n % 2 == 1) {
-        //         res = (res * a) % mod;
-        //     }
-        //     a = (a * a) % mod;
-        //     n /= 2;
-        // }
     }
 }
