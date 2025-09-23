95475f00c1f03c45f4889b8419c81052fa5925aa
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-09-23 04:19:13 -0400

    File updated at 9/23/2025, 4:19:12 AM

--- leetcode-sub/1686-StoneGameVI/1686-StoneGameVI.java
+++ leetcode-sub/1686-StoneGameVI/1686-StoneGameVI.java
@@ -1,22 +1,30 @@
-// Last updated: 6/5/2025, 1:17:41 AM
+// Last updated: 9/23/2025, 4:19:12 AM
 class Solution {
-    public int stoneGameVI(int[] aliceValues, int[] bobValues) {
-        int n = aliceValues.length, aliceSum = 0, bobSum = 0;
-        int[][] map = new int[n][3];
-        for (int i = 0; i < n; i++) {
-            map[i][0] = aliceValues[i] + bobValues[i];
-            map[i][1] = aliceValues[i];
-            map[i][2] = bobValues[i];
+    public int totalNumbers(int[] digits) {
+        int[] map = new int[10];
+        boolean flag = false;
+        for (int num : digits) {
+            map[num]++;
+            flag = num % 2 == 0 ? true : flag;
         }
-        Arrays.sort(map, (a, b) -> b[0] - a[0]);
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                aliceSum += map[i][1];
-            } 
-            else {
-                bobSum += map[i][2];
+        if (!flag) {
+            return 0; // no even digits to put on last place
+        }
+        int count = 0;
+        for (int i = 1; i < 10; i++) {
+            if (map[i] == 0) continue;
+            map[i]--;
+            for (int j = 0; j < 10; j++) {
+                if (map[j] == 0) continue;
+                map[j]--;
+                for (int k = 0; k < 10; k += 2) {
+                    if (map[k] == 0) continue;
+                    count++;
+                }
+                map[j]++;
             }
+            map[i]++;
         }
-        return aliceSum > bobSum ? 1 : aliceSum == bobSum ? 0 : -1;
+        return count;
     }
 }
