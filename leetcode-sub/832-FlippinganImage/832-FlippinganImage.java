2cb648da07b0880ea6bc5087ba9fab8f85e72862
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-09-26 22:59:04 -0400

    File updated at 9/26/2025, 10:59:03 PM

--- leetcode-sub/832-FlippinganImage/832-FlippinganImage.java
+++ leetcode-sub/832-FlippinganImage/832-FlippinganImage.java
@@ -1,4 +1,4 @@
-// Last updated: 9/26/2025, 10:56:50 PM
+// Last updated: 9/26/2025, 10:59:03 PM
 class Solution {
     public int[][] flipAndInvertImage(int[][] image) {
         int n = image.length;
@@ -10,14 +10,15 @@ class Solution {
     private void helper(int[] nums) {
         int n = nums.length;
         for (int i = 0; i < n / 2; i++) {
-            int temp = nums[i];
-            nums[i] = nums[n - i - 1];
-            nums[n - i - 1] = temp;
+            swap(nums, i, n - i - 1);
             nums[i] = 1 - nums[i];
             nums[n - i - 1] = 1 - nums[n - i - 1];
         }
-        if (n % 2 == 1) {
-            nums[n / 2] = 1 - nums[n / 2];
-        }
+        nums[n / 2] = n % 2 == 1 ? 1 - nums[n / 2] : nums[n / 2];
+    }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
