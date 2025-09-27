18335a4319b16298bc2064fa3286866d44d3b4d4
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-09-26 20:48:14 -0400

    File updated at 9/26/2025, 8:48:14 PM

--- leetcode-sub/912-SortanArray/912-SortanArray.java
+++ leetcode-sub/912-SortanArray/912-SortanArray.java
@@ -1,35 +1,25 @@
-// Last updated: 8/1/2025, 8:13:45 PM
-class Solution {    
+// Last updated: 9/26/2025, 8:48:14 PM
+class Solution {
     public int[] sortArray(int[] nums) {
         int n = nums.length;
         quickSort(nums, 0, n - 1);
         return nums;
     }
     private void quickSort(int[] nums, int start, int end) {
-        if (start >= end) {
-            return;
-        }
-        int pivotIndex = start + new Random().nextInt(end - start + 1);
-        int[] pivot = partition(nums, start, end, pivotIndex);
-        quickSort(nums, start, pivot[0] - 1);
-        quickSort(nums, pivot[1] + 1, end);
+        if (start >= end) return;
+        int pivotIndex = hoarePartition(nums, start, end);
+        quickSort(nums, start, pivotIndex);
+        quickSort(nums, pivotIndex + 1, end);
     }
-    // Dutch National Flag Algorithm
-    private int[] partition(int[] nums, int start, int end, int pivotIndex) {
-        int pivot = nums[pivotIndex]; 
-        int i = start, idx = start, j = end;
-        while (idx <= j && i < j) {
-            if (nums[idx] < pivot) {
-                swap(nums, i++, idx++);
-            }
-            else if (nums[idx] > pivot) {
-                swap(nums, j--, idx);
-            }
-            else {
-                idx++;
-            }
+
+    private int hoarePartition(int[] nums, int start, int end) {
+        int pivot = nums[start], i = start - 1, j = end + 1;
+        while (true) {
+            do { i++; } while (nums[i] < pivot);
+            do { j--; } while (nums[j] > pivot);
+            if (i >= j) return j;
+            swap(nums, i, j);
         }
-        return new int[]{i, j};
     }
     private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
