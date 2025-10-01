7d74cbd4813ea10f1edacb3ee47e001eed549de0
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-09-30 23:43:11 -0400

    Create file

---
+++ leetcode-sub/1518-WaterBottles/1518-WaterBottles.java
@@ -0,0 +1,6 @@
+// Last updated: 9/30/2025, 11:43:10 PM
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        return numBottles + (numBottles - 1) / (numExchange - 1);
+    }
+}
