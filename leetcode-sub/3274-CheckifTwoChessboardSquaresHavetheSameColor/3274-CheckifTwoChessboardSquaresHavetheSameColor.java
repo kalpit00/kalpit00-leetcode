41d28c4a0fb5a9546afae64519489290b1aa5a28
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-10-04 06:10:10 -0400

    Create file

---
+++ leetcode-sub/3274-CheckifTwoChessboardSquaresHavetheSameColor/3274-CheckifTwoChessboardSquaresHavetheSameColor.java
@@ -0,0 +1,8 @@
+// Last updated: 10/4/2025, 6:10:10 AM
+class Solution {
+    public boolean checkTwoChessboards(String coordinate1, String coordinate2) {
+        int sum1 = coordinate1.charAt(0) + coordinate1.charAt(1);
+        int sum2 = coordinate2.charAt(0) + coordinate2.charAt(1);
+        return (sum1 % 2) == (sum2 % 2);
+    }
+}
