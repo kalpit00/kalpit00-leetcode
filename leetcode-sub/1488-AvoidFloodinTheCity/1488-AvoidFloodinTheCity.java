e496b95e4bc937603110700fbece59c6df4477c3
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-10-07 00:33:17 -0400

    File updated at 10/7/2025, 12:33:17 AM

--- leetcode-sub/1488-AvoidFloodinTheCity/1488-AvoidFloodinTheCity.java
+++ leetcode-sub/1488-AvoidFloodinTheCity/1488-AvoidFloodinTheCity.java
@@ -1,28 +1,62 @@
-// Last updated: 10/6/2025, 11:14:40 PM
+// Last updated: 10/7/2025, 12:33:17 AM
 class Solution {
     public int[] avoidFlood(int[] rains) {
-        // the previous appeared idx of rains[i]
+        int n = rains.length;
+        DSU dsu = new DSU(n + 1);
+        int[] res = new int[n];
         Map<Integer, Integer> map = new HashMap<>();
-        TreeSet<Integer> zeros = new TreeSet<>();
-        int[] res = new int[rains.length];
-        for (int i = 0; i < rains.length; i++) {
-            if (rains[i] == 0) {
-                zeros.add(i);
-            } else {
-                if (map.containsKey(rains[i])) {
-                    // find the location of zero that can be used to empty rains[i]
-                    Integer next = zeros.ceiling(map.get(rains[i]));
-                    if (next == null) return new int[0];
-                    res[next] = rains[i];
-                    zeros.remove(next);
-                }
-                res[i] = -1;
-				map.put(rains[i], i);
+        for (int i = 0; i < n; i++) {
+            int lake = rains[i];
+            if (lake == 0) {
+                res[i] = 1;
+                continue;
             }
-        }
-        for (int i : zeros) {
-            res[i] = 1;
+            Integer j = map.get(lake);
+            if (j != null) {
+                int dry = dsu.findParent(j + 1);
+                if (dry >= i) {
+                    return new int[0];
+                }
+                res[dry] = lake;
+                dsu.parent[dry] = dsu.findParent(dry + 1);
+            }
+            res[i] = -1;
+            dsu.parent[i] = i + 1;
+            map.put(lake, i);
         }
         return res;
+    }
+    public class DSU {
+        int[] parent;
+        int[] size;
+        public DSU (int n) {
+            parent = new int[n];
+            size = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+        public int findParent(int node) {
+            if (node == parent[node]) {
+                return node;
+            }
+            return parent[node] = findParent(parent[node]);
+        }
+        public boolean union(int u, int v) {
+            int pu = findParent(u), pv = findParent(v);
+            if (pu == pv) {
+                return false;
+            }
+            if (size[pu] < size[pv]) {
+                parent[pu] = pv;
+                size[pv] += size[pu];
+            }
+            else {
+                parent[pv] = pu;
+                size[pu] += size[pv];
+            }
+            return true;
+        }
     }
 }
