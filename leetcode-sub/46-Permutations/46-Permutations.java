1c021872b36d412d7e8cfe0ef64eb4c83e021a0c
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-10-24 03:17:11 -0400

    File updated at 10/24/2025, 3:17:10 AM

--- leetcode-sub/46-Permutations/46-Permutations.java
+++ leetcode-sub/46-Permutations/46-Permutations.java
@@ -1,24 +1,55 @@
-// Last updated: 9/10/2025, 11:15:45 PM
+// Last updated: 10/24/2025, 3:17:10 AM
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         int n = nums.length;
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        backtrack(0, n, list, res, nums);
-        return res;
+        Set<List<Integer>> set = new HashSet<>();
+        List<Integer> arr = listToArray(nums);
+        while (!set.contains(arr)) {
+            set.add(arr);
+            nextPermutation(nums);
+            arr = listToArray(nums);
+        }
+        return new ArrayList<>(set);
     }
-    
-    public void backtrack(int mask, int n,
-    List<Integer> list, List<List<Integer>> res, int[] nums) {
-        if (list.size() >= n) {
-            res.add(new ArrayList<>(list));
+    private List<Integer> listToArray(int[] nums) {
+        List<Integer> arr = new ArrayList<>();
+        for (int num : nums) {
+            arr.add(num);
+        }
+        return arr;
+    }
+    public void nextPermutation(int[] nums) {
+        int n = nums.length, i = n - 2, j = n - 1;
+        for (i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                break;
+            }
+        }
+        if (i < 0) {
+            reverse(nums, 0, n - 1);
             return;
         }
-        for (int i = 0; i < n; i++) {
-            if ((mask & (1 << i)) != 0) continue;
-            list.add(nums[i]);
-            backtrack(mask | (1 << i), n, list, res, nums);
-            list.remove(list.size() - 1);
+        for (j = n - 1; j >= 0; j--) {
+            if (nums[j] > nums[i]) {
+                break;
+            }
         }
+        swap(nums, i, j);
+        reverse(nums, i + 1, n - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        int i = start, j = end;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
