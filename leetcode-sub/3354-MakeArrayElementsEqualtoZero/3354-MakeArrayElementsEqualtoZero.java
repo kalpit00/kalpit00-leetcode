070bda5de64b8ad0490bf9e18576a7f08a0eddab
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-10-27 21:13:53 -0400

    File updated at 10/27/2025, 9:13:53 PM

--- leetcode-sub/3354-MakeArrayElementsEqualtoZero/3354-MakeArrayElementsEqualtoZero.java
+++ leetcode-sub/3354-MakeArrayElementsEqualtoZero/3354-MakeArrayElementsEqualtoZero.java
@@ -1,59 +1,24 @@
-// Last updated: 10/27/2025, 9:13:32 PM
+// Last updated: 10/27/2025, 9:13:53 PM
 class Solution {
     public int countValidSelections(int[] nums) {
-        int n = nums.length;
-        int validSelections = 0;
 
-        for (int i = 0; i < n; i++) {
+        int count = 0, len = nums.length;
+        int sum = 0;
+        for (int num : nums) sum += num;
+
+        int halfSum = 0;
+
+        for (int i = 0; i < len; i++) {
+            halfSum += nums[i];
             if (nums[i] == 0) {
-                // Try starting at index i with initial direction right (1)
-                if (isValidStart(nums, i, 1)) {
-                    validSelections++;
-                }
-                // Try starting at index i with initial direction left (-1)
-                if (isValidStart(nums, i, -1)) {
-                    validSelections++;
-                }
-            }
-        }
-        
-        return validSelections;
-    }
 
-    private boolean isValidStart(int[] originalNums, int start, int initialDirection) {
-        int[] nums = Arrays.copyOf(originalNums, originalNums.length);
-        int n = nums.length;
-        int curr = start;
-        int direction = initialDirection;
-        
-        // Initial move after selection: curr is at a 0, so move in the chosen direction.
-        curr += direction;
-
-        while (curr >= 0 && curr < n) {
-            if (nums[curr] == 0) {
-                // Case 1: nums[curr] == 0. Continue in the current direction.
-                curr += direction;
-            } else { // nums[curr] > 0
-                // Case 2: nums[curr] > 0.
-                
-                // Decrement nums[curr] by 1.
-                nums[curr]--;
-                
-                // Reverse the movement direction.
-                direction = -direction;
-                
-                // Take a step in the new direction.
-                curr += direction;
+                if (2 * halfSum == sum) {
+                    count += 2;
+                } else if (Math.abs(sum - 2*halfSum) == 1) {
+                    count++;
+                } 
             }
         }
-        
-        // Check if all elements are now 0.
-        for (int num : nums) {
-            if (num != 0) {
-                return false;
-            }
-        }
-        
-        return true;
+        return count;
     }
 }
