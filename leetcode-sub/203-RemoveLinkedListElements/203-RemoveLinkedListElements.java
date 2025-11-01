3b090c2480a47c1bd0245bbd37c78eb4ef6a4d63
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-10-31 22:01:10 -0400

    File updated at 10/31/2025, 10:01:09 PM

--- leetcode-sub/203-RemoveLinkedListElements/203-RemoveLinkedListElements.java
+++ leetcode-sub/203-RemoveLinkedListElements/203-RemoveLinkedListElements.java
@@ -1,21 +1,22 @@
-// Last updated: 10/31/2025, 10:00:34 PM
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+// Last updated: 10/31/2025, 10:01:09 PM
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-  while (head != null && head.val == val) head = head.next;
-        ListNode curr = head;
-        while (curr != null && curr.next != null)
-            if (curr.next.val == val) curr.next = curr.next.next;
-            else curr = curr.next;
-        return head;
+        ListNode newHead = head;
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+        newHead = head;
+        ListNode dummy = new ListNode(-1);
+        dummy.next = newHead;
+        ListNode temp = dummy;
+        while (temp != null && temp.next != null) {
+            if (temp.next.val == val) {
+                temp.next = temp.next.next;
+            }
+            else {
+                temp = temp.next;
+            }
+        }
+        return dummy.next;
     }
 }
