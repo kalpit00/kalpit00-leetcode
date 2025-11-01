f417e1342e4da8936f9820b64666d583f207769c
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-10-31 23:24:38 -0400

    File updated at 10/31/2025, 11:24:38 PM

--- leetcode-sub/2487-RemoveNodesFromLinkedList/2487-RemoveNodesFromLinkedList.java
+++ leetcode-sub/2487-RemoveNodesFromLinkedList/2487-RemoveNodesFromLinkedList.java
@@ -1,14 +1,20 @@
-// Last updated: 10/31/2025, 11:03:29 PM
+// Last updated: 10/31/2025, 11:24:38 PM
 class Solution {
     public ListNode removeNodes(ListNode head) {
         Set<ListNode> set = new HashSet<>();
-        Stack<ListNode> stack = new Stack<>();
+        int n = 0, top = -1;
         ListNode temp = head;
         while (temp != null) {
-            while (!stack.isEmpty() && stack.peek().val < temp.val) {
-                set.add(stack.pop());
+            n++;
+            temp = temp.next;
+        }
+        temp = head;
+        ListNode[] stack = new ListNode[n];
+        while (temp != null) {
+            while (top >= 0 && stack[top].val < temp.val) {
+                set.add(stack[top--]);
             }
-            stack.push(temp);
+            stack[++top] = temp;
             temp = temp.next;
         }
         while (head != null && set.contains(head)) {
