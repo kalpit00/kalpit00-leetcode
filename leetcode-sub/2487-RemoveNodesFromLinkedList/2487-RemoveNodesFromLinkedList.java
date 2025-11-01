251f639a747466b5df3232a4d37fe89cbb3707ac
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-10-31 23:03:29 -0400

    File updated at 10/31/2025, 11:03:29 PM

--- leetcode-sub/2487-RemoveNodesFromLinkedList/2487-RemoveNodesFromLinkedList.java
+++ leetcode-sub/2487-RemoveNodesFromLinkedList/2487-RemoveNodesFromLinkedList.java
@@ -1,27 +1,27 @@
-// Last updated: 10/31/2025, 10:38:45 PM
+// Last updated: 10/31/2025, 11:03:29 PM
 class Solution {
     public ListNode removeNodes(ListNode head) {
-        head = reverseList(head);
+        Set<ListNode> set = new HashSet<>();
+        Stack<ListNode> stack = new Stack<>();
         ListNode temp = head;
-        while (temp != null && temp.next != null) {
-            if (temp.next.val < temp.val) { // instead of checking next==val
-                temp.next = temp.next.next; // check if its < curr.val!
+        while (temp != null) {
+            while (!stack.isEmpty() && stack.peek().val < temp.val) {
+                set.add(stack.pop());
             }
-            else {
+            stack.push(temp);
+            temp = temp.next;
+        }
+        while (head != null && set.contains(head)) {
+            head = head.next;
+        }
+        temp = head;
+        while (temp != null && temp.next != null) {
+            if (set.contains(temp.next)) {
+                temp.next = temp.next.next;
+            } else {
                 temp = temp.next;
             }
         }
-        return reverseList(head);
-    }
-    public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-        while (curr != null) {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
+        return head;
     }
 }
