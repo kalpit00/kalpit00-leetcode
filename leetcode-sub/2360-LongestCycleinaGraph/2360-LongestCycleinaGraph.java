193ad02a9674b4070ca979d7ff8fea935a269cb6
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-11-08 05:35:29 -0500

    File updated at 11/8/2025, 5:35:28 AM

--- leetcode-sub/2360-LongestCycleinaGraph/2360-LongestCycleinaGraph.java
+++ leetcode-sub/2360-LongestCycleinaGraph/2360-LongestCycleinaGraph.java
@@ -1,70 +1,53 @@
-// Last updated: 11/8/2025, 5:33:16 AM
+// Last updated: 11/8/2025, 5:35:28 AM
 class Solution {
-    int flag = 0;
     public int longestCycle(int[] edges) {
         int n = edges.length;
-        List<List<Integer>> adj = new ArrayList<>();
+        boolean[] visit = new boolean[n];
+        int[] indegree = new int[n];
+
+        // Count indegree of each node.
+        for (int edge : edges) {
+            if (edge != -1) {
+                indegree[edge]++;
+            }
+        }
+
+        // Kahn's algorithm starts.
+        Queue<Integer> q = new LinkedList<>();
         for (int i = 0; i < n; i++) {
-            adj.add(new ArrayList<>());
+            if (indegree[i] == 0) {
+                q.offer(i);
+            }
         }
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            visit[node] = true;
+            int neighbor = edges[node];
+            if (neighbor != -1) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    q.offer(neighbor);
+                }
+            }
+        }
+        // Kahn's algorithm ends.
+
+        int answer = -1;
         for (int i = 0; i < n; i++) {
-            if (edges[i] != -1) {
-                adj.get(i).add(edges[i]);
+            if (!visit[i]) {
+                int neighbor = edges[i];
+                int count = 1;
+                visit[i] = true;
+                // Iterate in the cycle.
+                while (neighbor != i) {
+                    visit[neighbor] = true;
+                    count++;
+                    neighbor = edges[neighbor];
+                }
+                answer = Math.max(answer, count);
             }
         }
-        int res = kosaraju(n, adj);
-        return flag == 0 ? -1 : res;
-    }
-    public int kosaraju(int n, List<List<Integer>> adj) {
-        Stack<Integer> stack = new Stack<>();
-        int[] visited = new int[n];
-        for (int i = 0; i < n; i++) {
-            if (visited[i] == 0) {
-                dfs(adj, i, stack, visited);
-            }
-        }
-        List<List<Integer>> revAdj = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            visited[i] = 0;
-            revAdj.add(new ArrayList<>());
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j : adj.get(i)) {
-                revAdj.get(j).add(i);
-            }
-        }
-        int max = 1;
-        while (!stack.isEmpty()) {
-            int node = stack.pop();
-            if (visited[node] == 0) {
-                max = Math.max(max, reverseDfs(revAdj, node, visited));
-            }
-        }
-        return max;
-    }
-    private void dfs(List<List<Integer>> adj, int node, 
-    Stack<Integer> stack, int[] visited) {
-        visited[node] = 1;
-        for (int neighbor : adj.get(node)) {
-            if (visited[neighbor] == 1) {
-                flag = 1;
-            }
-            else if (visited[neighbor] == 0) {
-                dfs(adj, neighbor, stack, visited);
-            }
-        }
-        stack.push(node);
-        visited[node] = 2;
-    }
-    private int reverseDfs(List<List<Integer>> revAdj, int node, 
-    int[] visited) {
-        int size = 1;
-        visited[node] = 1;
-        for (int neighbor : revAdj.get(node)) {
-            if (visited[neighbor] == 0) {
-                size += reverseDfs(revAdj, neighbor, visited);
-            }
-        }
-        return size;
+        return answer;
     }
 }
