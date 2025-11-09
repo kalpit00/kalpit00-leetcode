f66d9a1b06a55f2bd894f09655fe869af1cc7313
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-11-08 20:35:55 -0500

    File updated at 11/8/2025, 8:35:55 PM

--- leetcode-sub/1568-MinimumNumberofDaystoDisconnectIsland/1568-MinimumNumberofDaystoDisconnectIsland.java
+++ leetcode-sub/1568-MinimumNumberofDaystoDisconnectIsland/1568-MinimumNumberofDaystoDisconnectIsland.java
@@ -1,26 +1,21 @@
-// Last updated: 11/8/2025, 8:17:06 PM
+// Last updated: 11/8/2025, 8:35:55 PM
 class Solution {
-    int timer = 1;
     int[][] dir = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    int timer = 1;
     public int minDays(int[][] grid) {
         int m = grid.length, n = grid[0].length;
-        int numOfIsland = countIslands(grid);
-        if (numOfIsland != 1) return 0;
-        int[][] low = new int[m][n], time = new int[m][n];
-        int lands = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    lands++;
-                }
-                time[i][j] = -1;
-            }
+        int[] numOfIsland = countIslands(grid);
+        if (numOfIsland[0] != 1) return 0;
+        if (numOfIsland[1] == 1) return 1;
+        int[][] low = new int[m][n], time = new int[m][n],
+        visited = new int[m][n], parent = new int[m][n];
+        for (int[] mat : parent) {
+            Arrays.fill(mat, -1);
         }
-        if (lands == 1) return 1;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1 && time[i][j] == -1) {
-                    if (tarjans(i, j, grid, low, time, -1)) {
+                if (grid[i][j] == 1 && visited[i][j] == 0) {
+                    if (tarjan(i, j, grid, low, time, visited, parent)) {
                         return 1;
                     }
                 }
@@ -29,60 +24,68 @@ class Solution {
         return 2;
     }
 
-    private boolean tarjans(int i, int j, int[][] grid, 
-    int[][] low, int[][] time, int p) {
+    private boolean tarjan(int i, int j, int[][] grid, int[][] low, int[][] time, int[][] visited, int[][] parent) {
         low[i][j] = time[i][j] = timer;
-        int child = 0, m = grid.length, n = grid[0].length;
+        visited[i][j] = 1;
         timer++;
+        int m = grid.length, n = grid[0].length, count = 0;
         for (int[] d : dir) {
-            int x = d[0] + i, y = d[1] + j, idx = n * i + j;
-            if (x >= 0 && x < m && 
-            y >= 0 && y < n && grid[x][y] == 1) {
-                if (time[x][y] == -1) {
-                    child++;
-                    if (tarjans(x, y, grid, low, time, idx)) {
-                        return true;
-                    }
-                    low[i][j] = Math.min(low[i][j], low[x][y]);
-                    if (low[x][y] >= time[i][j] && p > -1) {
-                        return true;
-                    }
-                } 
-                else if (p != idx) {
-                    low[i][j] = Math.min(low[i][j], time[x][y]);
+            int x = d[0] + i, y = d[1] + j;
+            if (x < 0 || y < 0 || x >= m || y >= n || visited[x][y] == 2 
+            || grid[x][y] == 0) {
+                continue;
+            }
+            int neighbor = n * x + y;
+            if (neighbor == parent[i][j]) {
+                continue;
+            }
+            
+            if (visited[x][y] == 1) { // gray to gray, BACK EDGE
+                low[i][j] = Math.min(low[i][j], time[x][y]);
+            } 
+            else if (visited[x][y] == 0) {
+                count++;
+                parent[x][y] = n * i + j;
+                if (tarjan(x, y, grid, low, time, visited, parent)) {
+                    return true;
+                }
+                low[i][j] = Math.min(low[i][j], low[x][y]);
+                if (low[x][y] >= time[i][j] && parent[i][j] > -1) {
+                    return true;
                 }
             }
         }
-        if (p == -1 && child > 1) {
+        visited[i][j] = 2;
+        if (parent[i][j] == -1 && count > 1) {
             return true;
         }
         return false;
     }
 
-    private int countIslands(int[][] grid) {
-        int count = 0, m = grid.length, n = grid[0].length;
-        boolean[][] visited = new boolean[m][n];
+    private int[] countIslands(int[][] grid) {
+        int count = 0, m = grid.length, n = grid[0].length, lands = 0;
+        int[][] visited = new int[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (!visited[i][j] && grid[i][j] == 1) {
+                lands += grid[i][j] == 1 ? 1 : 0;
+                if (visited[i][j] == 0 && grid[i][j] == 1) {
                     count++;
                     dfs(visited, grid, i, j, m, n);
                 }
             }
         }
-        return count;
+        return new int[]{count, lands};
     }
-
-    private void dfs(boolean[][] visited, int[][] grid, int i, int j, 
-    int m, int n) {
-        if (i < 0 || j < 0 || i >= m || 
-        j >= n || visited[i][j] || grid[i][j] == 0) {
-            return;
+    private void dfs(int[][] visited, int[][] grid, int i, int j, int m,int n) {
+        visited[i][j] = 1;
+        for (int[] d : dir) {
+            int x = d[0] + i, y = d[1] + j;
+            if (x < 0 || y < 0 || x >= m || y >= n || visited[x][y] == 1 
+            || grid[x][y] == 0) {
+                continue;
+            }
+            dfs(visited, grid, x, y, m, n);
         }
-        visited[i][j] = true;
-        dfs(visited, grid, i - 1, j, m, n);
-        dfs(visited, grid, i + 1, j, m, n);
-        dfs(visited, grid, i, j - 1, m, n);
-        dfs(visited, grid, i, j + 1, m, n);
+        // visited[i][j] = 2;
     }
 }
