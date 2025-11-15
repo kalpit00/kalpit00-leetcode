099e7e06098adb0ac482402ad8a3d1a574a9149d
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-11-14 19:59:42 -0500

    File updated at 11/14/2025, 7:59:42 PM

--- leetcode-sub/516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.java
+++ leetcode-sub/516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.java
@@ -1,16 +1,16 @@
-// Last updated: 11/14/2025, 7:58:34 PM
+// Last updated: 11/14/2025, 7:59:42 PM
 class Solution {
     public int longestPalindromeSubseq(String s) {
         char[] arr = s.toCharArray();
         int n = arr.length;
-        int[][] dp = new int[n][n];
-        for (int i = n - 1; i >= 0; i--) {
+        int[][] dp = new int[n+1][n+1];
+        for (int i = n; i > 0; i--) {
             dp[i][i] = 1;
-            for (int j = i + 1; j < n; j++) {
-                dp[i][j] = (arr[i] == arr[j]) ? 
+            for (int j = i + 1; j <= n; j++) {
+                dp[i][j] = (arr[i - 1] == arr[j - 1]) ? 
                 dp[i + 1][j - 1] + 2 : Math.max(dp[i + 1][j], dp[i][j - 1]);
             }
         }
-        return dp[0][n - 1];
+        return dp[1][n];
     }
 }
