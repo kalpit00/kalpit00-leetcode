ac20bb30cc1bee16f9fee3051494cc9b31aa908e
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-11-14 21:46:13 -0500

    File updated at 11/14/2025, 9:46:13 PM

--- leetcode-sub/1955-CountNumberofSpecialSubsequences/1955-CountNumberofSpecialSubsequences.java
+++ leetcode-sub/1955-CountNumberofSpecialSubsequences/1955-CountNumberofSpecialSubsequences.java
@@ -1,15 +1,10 @@
-// Last updated: 11/14/2025, 9:45:22 PM
+// Last updated: 11/14/2025, 9:46:13 PM
 class Solution {
     public int countSpecialSubsequences(int[] nums) {
         int mod = 1000000007;
         long[] dp = new long[3];
         for (int i : nums) {
-            if (i == 0) {
-                dp[i] += dp[i] + 1;
-            }
-            else {
-                dp[i] += dp[i] + dp[i - 1];
-            }
+            dp[i] += dp[i] + (i == 0 ? 1 : dp[i - 1]);
             dp[i] %= mod;
         }
         return (int) dp[2];
