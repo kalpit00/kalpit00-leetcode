b80c816eaafde9066a897fec45c5fa235b36c99a
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-11-16 00:12:08 -0500

    File updated at 11/16/2025, 12:12:07 AM

--- leetcode-sub/2511-MaximumEnemyFortsThatCanBeCaptured/2511-MaximumEnemyFortsThatCanBeCaptured.java
+++ leetcode-sub/2511-MaximumEnemyFortsThatCanBeCaptured/2511-MaximumEnemyFortsThatCanBeCaptured.java
@@ -1,16 +1,15 @@
-// Last updated: 11/16/2025, 12:08:21 AM
+// Last updated: 11/16/2025, 12:12:07 AM
 class Solution {
     public int captureForts(int[] nums) {
         int n = nums.length, max = 0;
         for (int i = 0; i < n; i++) {
-            int m = 1, left = -9, right = -9; // m = streak length
-            left = i > 0 ? nums[i - 1] : -9;
+            int m = 1, left = i > 0 ? nums[i - 1] : -2;
             boolean flag = nums[i] == 0;
             while (i < n - 1 && nums[i] == 0 && nums[i] == nums[i + 1]) {
                 i++;
                 m++;
             }
-            right = i < n - 1 ? nums[i + 1] : -9;
+            int right = i < n - 1 ? nums[i + 1] : -2;
             max = left + right == 0 && flag ? Math.max(max, m) : max;
         }
         return max;
