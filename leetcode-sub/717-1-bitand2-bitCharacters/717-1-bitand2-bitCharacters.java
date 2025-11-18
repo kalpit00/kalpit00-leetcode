a45d5c43082d5cade238b4d0199f23a68f4ac457
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-11-17 23:48:33 -0500

    File updated at 11/17/2025, 11:48:33 PM

--- leetcode-sub/717-1-bitand2-bitCharacters/717-1-bitand2-bitCharacters.java
+++ leetcode-sub/717-1-bitand2-bitCharacters/717-1-bitand2-bitCharacters.java
@@ -1,10 +1,10 @@
-// Last updated: 11/17/2025, 11:47:00 PM
+// Last updated: 11/17/2025, 11:48:33 PM
 class Solution {
     public boolean isOneBitCharacter(int[] bits) {
-        int i = 0;
-        while (i < bits.length - 1) {
-            i += bits[i] + 1;
+        int n = bits.length, i = 0;
+        for (i = 0; i < n - 1; i++) {
+            i += bits[i] == 1 ? 1 : 0; // for every 1, move to next
         }
-        return i == bits.length - 1;
+        return i == n - 1;
     }
 }
