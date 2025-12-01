a16ee3d2675be5f320714e476611990c04680bb6
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-11-30 20:31:11 -0500

    File updated at 11/30/2025, 8:31:11 PM

--- leetcode-sub/2141-MaximumRunningTimeofNComputers/2141-MaximumRunningTimeofNComputers.java
+++ leetcode-sub/2141-MaximumRunningTimeofNComputers/2141-MaximumRunningTimeofNComputers.java
@@ -1,23 +1,31 @@
-// Last updated: 11/30/2025, 8:12:57 PM
+// Last updated: 11/30/2025, 8:31:11 PM
 1class Solution {
-2    public long maxRunTime(int n, int[] batteries) {
-3        long sumPower = 0;
-4        for (int power : batteries)
-5            sumPower += power;
-6        long left = 1, right = sumPower / n;
-7        
-8        while (left < right){
-9            long target = right - (right - left) / 2;
-10            long extra = 0;
-11            
-12            for (int power : batteries)
-13                extra += Math.min(power, target);
-14
-15            if (extra >= (long)(n * target))
-16                left = target;
-17            else
-18                right = target - 1;
-19        }
-20        return left;
-21    }
-22}
+2    public long maxRunTime(int n, int[] nums) {
+3        long sum = 0;
+4        for (int num : nums) {
+5            sum += num;
+6        }
+7        long start = 1, end = sum / n, ans = -1;
+8        while (start <= end) {
+9            long mid = start + (end - start) / 2;
+10            if (helper(nums, mid, n)) {
+11                ans = mid;
+12                start = mid + 1;
+13            }
+14            else {
+15                end = mid - 1;
+16            }
+17        }
+18        return ans;
+19    }
+20    private boolean helper(int[] nums, long mid, int n) {
+21        long sum = 0;
+22        for (int num : nums) {
+23            sum += Math.min(num, mid);
+24            if (sum >= (long) (n * mid)) {
+25                return true;
+26            }
+27        }
+28        return sum >= (long) (n * mid);
+29    }
+30}
