3114096cbc39e2baae7b7430c213243f0480ee55
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-12-07 02:51:49 -0500

    File updated at 12/7/2025, 2:51:49 AM

--- leetcode-sub/1523-CountOddNumbersinanIntervalRange/1523-CountOddNumbersinanIntervalRange.java
+++ leetcode-sub/1523-CountOddNumbersinanIntervalRange/1523-CountOddNumbersinanIntervalRange.java
@@ -1,6 +1,6 @@
-// Last updated: 4/8/2025, 4:59:51 PM
-class Solution {
-    public int countOdds(int low, int high) {
-        return (high + 1) / 2 - low / 2;        
-    }
-}
+// Last updated: 12/7/2025, 2:51:49 AM
+1class Solution {
+2    public int countOdds(int low, int high) {
+3        return (high + 1) / 2 - low / 2;        
+4    }
+5}
