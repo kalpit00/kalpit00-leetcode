f5c6772f39241e76a66f4f5cced33c7afa9f966a
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-12-08 03:50:20 -0500

    File updated at 12/8/2025, 3:50:20 AM

--- leetcode-sub/146-LRUCache/146-LRUCache.java
+++ leetcode-sub/146-LRUCache/146-LRUCache.java
@@ -1,61 +1,63 @@
-// Last updated: 4/18/2025, 11:57:36 AM
-class LRUCache {
-    private class Node {
-        int key, value;
-        Node prev, next;
-        
-        Node(int key, int value) {
-            this.key = key;
-            this.value = value;
-        }
-    }
-    
-    private int capacity;
-    private Map<Integer, Node> map;
-    private Node head, tail;
-    
-    public LRUCache(int capacity) {
-        this.capacity = capacity;
-        this.map = new HashMap<>();
-        this.head = new Node(0, 0);
-        this.tail = new Node(0, 0);
-        head.next = tail;
-        tail.prev = head;
-    }
-    
-    public int get(int key) {
-        if (!map.containsKey(key)) {
-            return -1;
-        }
-        Node node = map.get(key);
-        remove(node);
-        insert(node);
-        return node.value;
-    }
-    
-    public void put(int key, int value) {
-        if (map.containsKey(key)) {
-            remove(map.get(key));
-        }
-        if (map.size() == capacity) {
-            remove(tail.prev);
-        }
-        insert(new Node(key, value));
-    }
-    
-    private void remove(Node node) {
-        map.remove(node.key);
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
-    }
-    
-    private void insert(Node node) {
-        map.put(node.key, node);
-        node.next = head.next;
-        node.prev = head;
-        // head.next.prev = node;
-        node.next.prev = node;
-        // head.next = node;
-        node.prev.next = node;
-    }
-}
+// Last updated: 12/8/2025, 3:50:20 AM
+1class LRUCache {
+2    private class Node {
+3        int key, value;
+4        Node prev, next;
+5        Node(int key, int value) {
+6            this.key = key;
+7            this.value = value;
+8        }
+9    }
+10    private int size;
+11    private int capacity;
+12    private Node[] map;
+13    private Node head, tail;
+14    
+15    public LRUCache(int capacity) {
+16        this.size = 0;
+17        this.capacity = capacity;
+18        this.map = new Node[10001];
+19        this.head = new Node(0, 0);
+20        this.tail = new Node(0, 0);
+21        head.next = tail;
+22        tail.prev = head;
+23    }
+24    
+25    public int get(int key) {
+26        if (map[key] == null) {
+27            return -1;
+28        }
+29        Node node = map[key];
+30        remove(node);
+31        insert(node);
+32        return node.value;
+33    }
+34    
+35    public void put(int key, int value) {
+36        if (map[key] != null) {
+37            remove(map[key]);
+38        }
+39        if (size == capacity) {
+40            remove(tail.prev);
+41        }
+42        insert(new Node(key, value));
+43    }
+44    
+45    private void remove(Node node) {
+46        map[node.key] = null;
+47        size--;
+48        node.prev.next = node.next;
+49        node.next.prev = node.prev;
+50    }
+51    
+52    private void insert(Node node) {
+53        map[node.key] = node;
+54        size++;
+55        node.next = head.next;
+56        node.prev = head;
+57        // head.next.prev = node;
+58        node.next.prev = node;
+59        // head.next = node;
+60        node.prev.next = node;
+61    }
+62}
