598092467b12b52d2e24fb3ee962794c8981e611
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-12-21 21:31:36 -0500

    File updated at 12/21/2025, 9:31:36 PM

--- leetcode-sub/960-DeleteColumnstoMakeSortedIII/960-DeleteColumnstoMakeSortedIII.java
+++ leetcode-sub/960-DeleteColumnstoMakeSortedIII/960-DeleteColumnstoMakeSortedIII.java
@@ -1,21 +1,34 @@
-// Last updated: 12/21/2025, 9:22:21 PM
+// Last updated: 12/21/2025, 9:31:36 PM
 1class Solution {
-2    public int minDeletionSize(String[] A) {
-3        int W = A[0].length();
-4        int[] dp = new int[W];
-5        Arrays.fill(dp, 1);
-6        for (int i = W-2; i >= 0; --i)
-7            search: for (int j = i+1; j < W; ++j) {
-8                for (String row: A)
-9                    if (row.charAt(i) > row.charAt(j))
-10                        continue search;
-11
-12                dp[i] = Math.max(dp[i], 1 + dp[j]);
-13            }
-14
-15        int kept = 0;
-16        for (int x: dp)
-17            kept = Math.max(kept, x);
-18        return W - kept;
-19    }
-20}
+2    public int minDeletionSize(String[] strs) {
+3        int n = strs[0].length();
+4        Integer[][] dp = new Integer[n][n + 1];
+5        return solve(0, -1, n, strs, dp);
+6    }
+7    private int solve(int i, int prev, int n, String[] strs, Integer[][] dp) {
+8        if (i >= n) {
+9            return 0;
+10        }
+11        if (dp[i][prev + 1] != null) {
+12            return dp[i][prev + 1];
+13        }
+14        int notTake = Integer.MAX_VALUE;
+15        if (prev == -1 || helper(i, prev, strs)) {
+16            notTake = solve(i + 1, i, n, strs, dp);
+17        }
+18        int take = 1 + solve(i + 1, prev, n, strs, dp);
+19        int min = Math.min(take, notTake);
+20        if (prev == -1) {
+21            return min;
+22        }
+23        return dp[i][prev + 1] = min;
+24    }
+25    private boolean helper(int j, int prev, String[] strs) {
+26        for (int i = 0; i < strs.length; i++) {
+27            if (strs[i].charAt(j) < strs[i].charAt(prev)) {
+28                return false;
+29            }
+30        }
+31        return true;
+32    }
+33}
