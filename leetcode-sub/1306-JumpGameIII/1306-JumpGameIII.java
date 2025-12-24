d09cfb3d2927f15f9a9e4ad20fac24bd38f236a6
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-12-24 17:33:20 -0500

    File updated at 12/24/2025, 5:33:20 PM

--- leetcode-sub/1306-JumpGameIII/1306-JumpGameIII.java
+++ leetcode-sub/1306-JumpGameIII/1306-JumpGameIII.java
@@ -1,4 +1,4 @@
-// Last updated: 12/24/2025, 5:32:48 PM
+// Last updated: 12/24/2025, 5:33:20 PM
 1class Solution {
 2    public boolean canReach(int[] arr, int src) {
 3        int n = arr.length;
@@ -11,14 +11,13 @@
 10            if (arr[i] == 0) {
 11                return true;
 12            }
-13            int[] idx = new int[]{i + arr[i], i - arr[i]};
-14            for (int j : idx) {
-15                if (j >= 0 && j < n && !visited[j]) { 
-16                    visited[j] = true;
-17                    queue.offer(j);
-18                }
-19            }
-20        }
-21        return false;
-22    }
-23}
+13            for (int j : new int[]{i + arr[i], i - arr[i]}) {
+14                if (j >= 0 && j < n && !visited[j]) { 
+15                    visited[j] = true;
+16                    queue.offer(j);
+17                }
+18            }
+19        }
+20        return false;
+21    }
+22}
