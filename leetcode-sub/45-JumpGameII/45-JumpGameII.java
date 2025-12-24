3a9dda6f47120b0295270a265a0ddab135724751
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-12-24 17:21:29 -0500

    File updated at 12/24/2025, 5:21:28 PM

--- leetcode-sub/45-JumpGameII/45-JumpGameII.java
+++ leetcode-sub/45-JumpGameII/45-JumpGameII.java
@@ -1,24 +1,27 @@
-// Last updated: 12/24/2025, 5:19:25 PM
+// Last updated: 12/24/2025, 5:21:28 PM
 1class Solution {
 2    public int jump(int[] nums) {
-3        int n = nums.length;
-4        Queue<int[]> queue = new LinkedList<>();
+3        int n = nums.length, steps = 0;
+4        Queue<Integer> queue = new LinkedList<>();
 5        boolean[] visited = new boolean[n];
-6        queue.offer(new int[]{0, 0});
+6        queue.offer(0);
 7        visited[0] = true;
 8        while (!queue.isEmpty()) {
-9            int[] node = queue.poll();
-10            int i = node[0], steps = node[1];
-11            if (i == n - 1) {
-12                return steps;
-13            }
-14            for (int j = i; j <= i + nums[i] && j < n; j++) {
-15                if (!visited[j]) {
-16                    visited[j] = true;
-17                    queue.offer(new int[]{j, steps + 1});
-18                }
-19            }
-20        }
-21        return -1;
-22    }
-23}
+9            int size = queue.size();
+10            for (int k = 0; k < size; k++) {
+11                int i = queue.poll();
+12                if (i == n - 1) {
+13                    return steps;
+14                }
+15                for (int j = i; j <= i + nums[i] && j < n; j++) {
+16                    if (!visited[j]) {
+17                        visited[j] = true;
+18                        queue.offer(j);
+19                    }
+20                }
+21            }
+22            steps++;
+23        }
+24        return -1;
+25    }
+26}
