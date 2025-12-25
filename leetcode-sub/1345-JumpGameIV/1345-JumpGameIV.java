b29a1f0e83391d0dcdb64d50c51fb57eed51d6a6
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-12-24 20:06:07 -0500

    File updated at 12/24/2025, 8:06:07 PM

--- leetcode-sub/1345-JumpGameIV/1345-JumpGameIV.java
+++ leetcode-sub/1345-JumpGameIV/1345-JumpGameIV.java
@@ -1,4 +1,4 @@
-// Last updated: 12/24/2025, 5:50:39 PM
+// Last updated: 12/24/2025, 8:06:07 PM
 1class Solution {
 2    public int minJumps(int[] nums) {
 3        int n = nums.length, steps = 0;
@@ -7,38 +7,48 @@
 6            map.putIfAbsent(nums[i], new ArrayList<>());
 7            map.get(nums[i]).add(i);
 8        }
-9        Queue<Integer> queue = new LinkedList<>();
-10        boolean[] visited = new boolean[n];
-11        queue.offer(0);
-12        visited[0] = true;
-13        while (!queue.isEmpty()) {
-14            int size = queue.size();
-15            for (int k = 0; k < size; k++) {
-16                int i = queue.poll();
-17                if (i == n - 1) {
-18                    return steps;
-19                }
-20                Set<Integer> set = new HashSet<>();
-21                if (i + 1 < n) {
-22                    set.add(i + 1);
-23                }
-24                if (i - 1 >= 0) {
-25                    set.add(i - 1);
-26                }
-27                for (int idx : map.get(nums[i])) {
-28                    if (idx == i) continue;
-29                    set.add(idx);
-30                }
-31                map.get(nums[i]).clear();
-32                for (int j : set) {
-33                    if (!visited[j]) {
-34                        visited[j] = true;
-35                        queue.offer(j);
-36                    }
-37                }
-38            }
-39            steps++;
-40        }
-41        return -1;
-42    }
-43}
+9        Queue<Integer> queue1 = new LinkedList<>(), queue2 = new LinkedList();
+10        boolean[] visited1 = new boolean[n], visited2 = new boolean[n];
+11        queue1.offer(0);
+12        queue2.offer(n - 1);
+13        visited1[0] = true;
+14        visited2[n - 1] = true;
+15        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+16            if (queue1.size() > queue2.size()) {
+17                Queue<Integer> tempQueue = queue1;
+18                queue1 = queue2;
+19                queue2 = tempQueue;
+20                boolean[] tempVisited = visited1;
+21                visited1 = visited2;
+22                visited2 = tempVisited;
+23            }
+24            int size = queue1.size();
+25            for (int k = 0; k < size; k++) {
+26                int i = queue1.poll();
+27                if (visited2[i]) {
+28                    return steps;
+29                }
+30                Set<Integer> set = new HashSet<>();
+31                if (i + 1 < n) {
+32                    set.add(i + 1);
+33                }
+34                if (i - 1 >= 0) {
+35                    set.add(i - 1);
+36                }
+37                for (int idx : map.get(nums[i])) {
+38                    if (idx == i) continue;
+39                    set.add(idx);
+40                }
+41                map.get(nums[i]).clear();
+42                for (int j : set) {
+43                    if (!visited1[j]) {
+44                        visited1[j] = true;
+45                        queue1.offer(j);
+46                    }
+47                }
+48            }
+49            steps++;
+50        }
+51        return -1;
+52    }
+53}
