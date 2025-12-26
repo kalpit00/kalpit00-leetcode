d0e26460165d1b3d8dcedb07f9cb61dead071764
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-12-26 05:26:12 -0500

    File updated at 12/26/2025, 5:26:11 AM

--- leetcode-sub/2483-MinimumPenaltyforaShop/2483-MinimumPenaltyforaShop.java
+++ leetcode-sub/2483-MinimumPenaltyforaShop/2483-MinimumPenaltyforaShop.java
@@ -1,4 +1,4 @@
-// Last updated: 12/26/2025, 5:22:19 AM
+// Last updated: 12/26/2025, 5:26:11 AM
 1class Solution {
 2    public int bestClosingTime(String customers) {
 3        char[] arr = customers.toCharArray();
@@ -6,17 +6,16 @@
 5        int[] pre = new int[n + 1], suf = new int[n + 1];
 6        for (int i = 1; i <= n; i++) {
 7            pre[i] = pre[i - 1] + (arr[i - 1] == 'N' ? 1 : 0);
-8            suf[n - i] = suf[n - i + 1] + (arr[n - i] == 'Y' ? 1 : 0);
-9        }
-10        for (int i = n; i >= 1; i--) {
-11            suf[n - i] = suf[n - i + 1] + (arr[n - i] == 'Y' ? 1 : 0);
-12        }
-13        for (int i = 0; i <= n; i++) {
-14            if (pre[i] + suf[i] < min) {
-15                min = pre[i] + suf[i];
-16                minIndex = i; // minIndex, not minVal
-17            }
-18        }
-19        return minIndex;
-20    }
-21}
+8        }
+9        for (int i = n - 2; i >= -1; i--) {
+10            suf[i + 1] = suf[i + 2] + (arr[i + 1] == 'Y' ? 1 : 0);
+11        }
+12        for (int i = 0; i <= n; i++) {
+13            if (pre[i] + suf[i] < min) {
+14                min = pre[i] + suf[i];
+15                minIndex = i; // minIndex, not minVal
+16            }
+17        }
+18        return minIndex;
+19    }
+20}
