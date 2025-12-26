29983fde659c4f9f0b525213a957015fa4a8501a
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-12-26 05:18:25 -0500

    File updated at 12/26/2025, 5:18:25 AM

--- leetcode-sub/2483-MinimumPenaltyforaShop/2483-MinimumPenaltyforaShop.java
+++ leetcode-sub/2483-MinimumPenaltyforaShop/2483-MinimumPenaltyforaShop.java
@@ -1,36 +1,19 @@
-// Last updated: 12/26/2025, 5:01:08 AM
+// Last updated: 12/26/2025, 5:18:25 AM
 1class Solution {
 2    public int bestClosingTime(String customers) {
-3        int curPenalty = 0;
-4        for (int i = 0; i < customers.length(); i++) {
-5            if (customers.charAt(i) == 'Y') {
-6                curPenalty++;
-7            }
-8        }
-9
-10        // Start with closing at hour 0, the penalty equals all 'Y' in closed hours.
-11        int minPenalty = curPenalty;
-12        int earliestHour = 0;
-13
-14        for (int i = 0; i < customers.length(); i++) {
-15            char ch = customers.charAt(i);
-16            
-17            // If status in hour i is 'Y', moving it to open hours decrement
-18            // penalty by 1. Otherwise, moving 'N' to open hours increment
-19            // penatly by 1.
-20            if (ch == 'Y') {
-21                curPenalty--;
-22            } else {
-23                curPenalty++;
-24            }
-25
-26            // Update earliestHour if a smaller penatly is encountered.
-27            if (curPenalty < minPenalty) {
-28                earliestHour = i + 1;
-29                minPenalty = curPenalty;
-30            }
-31        }
-32
-33        return earliestHour;
-34    }
-35}
+3        char[] arr = customers.toCharArray();
+4        int n = arr.length, min = Integer.MAX_VALUE, ans = n;
+5        int[] pre = new int[n + 1], suf = new int[n + 1];
+6        for (int i = 1; i <= n; i++) {
+7            pre[i] = pre[i - 1] + (arr[i - 1] == 'N' ? 1 : 0);
+8            suf[n - i] = suf[n - i + 1] + (arr[n - i] == 'Y' ? 1 : 0);
+9        }
+10        for (int i = n; i >= 0; i--) {
+11            if (pre[i] + suf[i] <= min) {
+12                min = pre[i] + suf[i];
+13                ans = i;
+14            }
+15        }
+16        return ans;
+17    }
+18}
