9572bba2e463be49031582a457572cbbc5b8ccff
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-12-30 08:03:29 -0500

    File updated at 12/30/2025, 8:03:28 AM

--- leetcode-sub/3434-MaximumFrequencyAfterSubarrayOperation/3434-MaximumFrequencyAfterSubarrayOperation.java
+++ leetcode-sub/3434-MaximumFrequencyAfterSubarrayOperation/3434-MaximumFrequencyAfterSubarrayOperation.java
@@ -1,23 +1,35 @@
-// Last updated: 12/30/2025, 8:01:39 AM
-1class Solution {
-2    public int maxFrequency(int[] nums, int k) {
-3        int n = nums.length, count = 0, max = 0;
-4        for (int num : nums) {
-5            count += num == k ? 1 : 0;
-6        }
-7        for (int x = 1; x <= 50; x++) {
-8            if (x == k) continue;
-9            max = Math.max(max, kadane(nums, x, k));
-10        }
-11        return count + max;
-12    }
-13    public int kadane(int[] nums, int x, int k) {
-14        int sum = 0, max = Integer.MIN_VALUE;
-15        for (int num : nums) {
-16            sum += num == x ? 1 : num == k ? -1 : 0;
-17            max = Math.max(max, sum);
-18            sum = sum < 0 ? 0 : sum;
-19        }
-20        return max;
-21    }
-22}
+// Last updated: 12/30/2025, 8:03:28 AM
+class Solution {
+    public int maxFrequency(int[] nums, int k) {
+        int[] freq = new int[51];
+        int ans = 0, cnt = 0;
+        for(int n : nums){
+            freq[n] = Math.max(freq[n], cnt) + 1;
+            if(n == k){
+                ans++;
+                cnt++;
+            }
+            ans = Math.max(ans, freq[n]);
+        }
+        return ans;
+
+        /*
+        int orig = 0, ans = 0;
+        for(int n : nums)
+            if(n == k) 
+                orig++;
+        for(int i = 1; i <= 50; i++){
+            if(i == k)
+                continue;
+            int cnt = 0, mx = 0;
+            for(int n : nums){
+                cnt += (n == i)? 1: (n == k)? -1: 0;
+                cnt = Math.max(cnt, 0);
+                mx = Math.max(mx, cnt);
+            }
+            ans = Math.max(ans, mx);
+        }
+        return orig + ans;
+        */
+    }
+}
