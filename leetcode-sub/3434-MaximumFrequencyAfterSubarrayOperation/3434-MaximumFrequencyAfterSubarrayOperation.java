7802a28b7c09481f205af8a4cd7a4a56c7422543
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-12-30 08:01:39 -0500

    File updated at 12/30/2025, 8:01:39 AM

--- leetcode-sub/3434-MaximumFrequencyAfterSubarrayOperation/3434-MaximumFrequencyAfterSubarrayOperation.java
+++ leetcode-sub/3434-MaximumFrequencyAfterSubarrayOperation/3434-MaximumFrequencyAfterSubarrayOperation.java
@@ -1,4 +1,4 @@
-// Last updated: 12/30/2025, 7:54:03 AM
+// Last updated: 12/30/2025, 8:01:39 AM
 1class Solution {
 2    public int maxFrequency(int[] nums, int k) {
 3        int n = nums.length, count = 0, max = 0;
@@ -7,24 +7,17 @@
 6        }
 7        for (int x = 1; x <= 50; x++) {
 8            if (x == k) continue;
-9            max = Math.max(max, kadane(helper(nums.clone(), x, k)));
+9            max = Math.max(max, kadane(nums, x, k));
 10        }
 11        return count + max;
 12    }
-13    public int kadane(int[] nums) {
+13    public int kadane(int[] nums, int x, int k) {
 14        int sum = 0, max = Integer.MIN_VALUE;
 15        for (int num : nums) {
-16            sum += num;
+16            sum += num == x ? 1 : num == k ? -1 : 0;
 17            max = Math.max(max, sum);
 18            sum = sum < 0 ? 0 : sum;
 19        }
 20        return max;
 21    }
-22    private int[] helper(int[] nums, int x, int k) {
-23        int n = nums.length;
-24        for (int i = 0; i < n; i++) {
-25            nums[i] = nums[i] == x ? 1 : nums[i] == k ? -1 : 0;
-26        }
-27        return nums;
-28    }
-29}
+22}
