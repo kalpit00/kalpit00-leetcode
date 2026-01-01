abec05d5a2d02837c98e5bb803ca843b24c9a998
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-01-01 06:23:04 -0500

    File updated at 1/1/2026, 6:23:04 AM

--- leetcode-sub/2765-LongestAlternatingSubarray/2765-LongestAlternatingSubarray.java
+++ leetcode-sub/2765-LongestAlternatingSubarray/2765-LongestAlternatingSubarray.java
@@ -1,14 +1,13 @@
-// Last updated: 1/1/2026, 6:20:26 AM
+// Last updated: 1/1/2026, 6:23:04 AM
 1class Solution {
 2    public int alternatingSubarray(int[] nums) {
 3        int n = nums.length, max = -1;
 4        int[] dp = new int[n];
-5        Arrays.fill(dp, -1);
-6        for (int i = 1; i < n; i++) {
-7            dp[i] = (dp[i - 1] != -1 && nums[i] == nums[i - 2]) ? dp[i - 1] + 1
-8            : (nums[i] - nums[i - 1] == 1) ? 2 : -1;
-9            max = Math.max(max, dp[i]);
-10        }
-11        return max;
-12    }
-13}
+5        for (int i = 1; i < n; i++) {
+6            dp[i] = (dp[i - 1] > 0 && nums[i] == nums[i - 2]) ? 
+7            dp[i - 1] + 1 : (nums[i] - nums[i - 1] == 1) ? 2 : -1;
+8            max = Math.max(max, dp[i]);
+9        }
+10        return max;
+11    }
+12}
