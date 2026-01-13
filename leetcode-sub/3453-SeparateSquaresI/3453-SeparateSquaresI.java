0b9789544722670f372d272c4ec11466fc765ba4
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-01-12 19:22:55 -0500

    File updated at 1/12/2026, 7:22:55 PM

--- leetcode-sub/3453-SeparateSquaresI/3453-SeparateSquaresI.java
+++ leetcode-sub/3453-SeparateSquaresI/3453-SeparateSquaresI.java
@@ -1,40 +1,34 @@
-// Last updated: 1/12/2026, 7:17:32 PM
+// Last updated: 1/12/2026, 7:22:55 PM
 1class Solution {
-2
-3    public double separateSquares(int[][] squares) {
-4        double max_y = 0;
-5        double total_area = 0;
-6        for (int[] sq : squares) {
-7            int y = sq[1];
-8            int l = sq[2];
-9            total_area += (double) l * l;
-10            max_y = Math.max(max_y, (double) (y + l));
-11        }
-12
-13        double lo = 0;
-14        double hi = max_y;
-15        double eps = 1e-5;
-16        while (Math.abs(hi - lo) > eps) {
-17            double mid = (hi + lo) / 2;
-18            if (check(mid, squares, total_area)) {
-19                hi = mid;
-20            } else {
-21                lo = mid;
-22            }
-23        }
-24
-25        return hi;
-26    }
-27
-28    private Boolean check(double limit_y, int[][] squares, double total_area) {
-29        double area = 0;
-30        for (int[] sq : squares) {
-31            int y = sq[1];
-32            int l = sq[2];
-33            if (y < limit_y) {
-34                area += (double) l * Math.min(limit_y - y, (double) l);
-35            }
-36        }
-37        return area >= total_area / 2;
-38    }
-39}
+2    public double separateSquares(int[][] squares) {
+3        double max = 0, sum = 0, ans = -1;
+4        for (int[] square : squares) {
+5            int y = square[1], l = square[2];
+6            sum += (double) l * l;
+7            max = Math.max(max, (double) (y + l));
+8        }
+9        double start = 0, end = max, eps = 1e-5;
+10        while (Math.abs(end - start) >= eps) {
+11            double mid = start + (end - start) / 2;
+12            if (helper(mid, squares, sum)) {
+13                end = mid;
+14                ans = mid;
+15            }
+16            else {
+17                start = mid;
+18            }
+19        }
+20        return ans;
+21    }
+22
+23    private boolean helper(double mid, int[][] squares, double sum) {
+24        double area = 0;
+25        for (int[] square : squares) {
+26            int y = square[1], l = square[2];
+27            if (y < mid) {
+28                area += (double) l * Math.min(mid - y, (double) l);
+29            }
+30        }
+31        return area >= sum / 2;
+32    }
+33}
