9d31fc3d83c93d003a658df0305de2d5e852719d
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-01-17 15:51:45 -0500

    File updated at 1/17/2026, 3:51:45 PM

--- leetcode-sub/3499-MaximizeActiveSectionwithTradeI/3499-MaximizeActiveSectionwithTradeI.java
+++ leetcode-sub/3499-MaximizeActiveSectionwithTradeI/3499-MaximizeActiveSectionwithTradeI.java
@@ -1,4 +1,4 @@
-// Last updated: 1/17/2026, 3:49:18 PM
+// Last updated: 1/17/2026, 3:51:45 PM
 1class Solution {
 2    public int maxActiveSectionsAfterTrade(String s) {
 3        char[] arr = s.toCharArray();
@@ -13,13 +13,11 @@
 12            }
 13            list.add(new int[]{c - '0', m});
 14            count += c == '1' ? m : 0;
-15        }
-16        int k = list.size();
-17        for (int i = 1; i < k - 1; i++) {
-18            if (list.get(i)[0] == 0) continue;
-19            int ans = list.get(i - 1)[1] + list.get(i + 1)[1];
-20            max = Math.max(max, ans);
-21        }
-22        return max + count;
-23    }
-24}
+15        } // can just add streak len 'm' to count all 1s in s[]
+16        for (int i = 1; i < list.size() - 1; i++) {
+17            if (list.get(i)[0] == 0) continue; // skip 0-streaks
+18            max = Math.max(max, list.get(i - 1)[1] + list.get(i + 1)[1]);
+19        } // for 1-streaks, add sum of streak len of 0s on left and right
+20        return max + count;
+21    }
+22}
