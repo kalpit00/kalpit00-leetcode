1ba7bb93e461467e599ca018b17f40ab272be985
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-01-20 01:40:08 -0500

    File updated at 1/20/2026, 1:40:08 AM

--- leetcode-sub/705-DesignHashSet/705-DesignHashSet.java
+++ leetcode-sub/705-DesignHashSet/705-DesignHashSet.java
@@ -1,19 +1,16 @@
-// Last updated: 1/20/2026, 1:36:43 AM
+// Last updated: 1/20/2026, 1:40:08 AM
 1class MyHashSet {
 2    private boolean[] set;
 3    public MyHashSet() {
 4        set = new boolean[1000001];
 5    }
-6    
-7    public void add(int key) {
-8        set[key] = true;
-9    }
-10    
-11    public void remove(int key) {
-12        set[key] = false;
-13    }
-14    
-15    public boolean contains(int key) {
-16        return set[key];
-17    }
-18}
+6    public void add(int key) {
+7        set[key] = true;
+8    }
+9    public void remove(int key) {
+10        set[key] = false;
+11    }
+12    public boolean contains(int key) {
+13        return set[key];
+14    }
+15}
