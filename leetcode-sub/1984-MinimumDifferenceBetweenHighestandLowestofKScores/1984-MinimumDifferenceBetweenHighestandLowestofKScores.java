5f363fb6727740d56fb2f3f7698549533726ba68
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-01-24 19:34:59 -0500

    Create file

---
+++ leetcode-sub/1984-MinimumDifferenceBetweenHighestandLowestofKScores/1984-MinimumDifferenceBetweenHighestandLowestofKScores.java
@@ -0,0 +1,11 @@
+// Last updated: 1/24/2026, 7:34:59 PM
+1class Solution {
+2    public int minimumDifference(int[] nums, int k) {
+3        int n = nums.length, min = Integer.MAX_VALUE;
+4        Arrays.sort(nums);
+5        for (int i = 0; i <= n - k; ++i) {
+6            min = Math.min(min, nums[i + k - 1] - nums[i]);
+7        }
+8        return min;
+9    }
+10}
