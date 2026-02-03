f421a654348511671dbb9f20047595ed2894c876
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-02-02 20:19:52 -0500

    File updated at 2/2/2026, 8:19:52 PM

--- leetcode-sub/3637-TrionicArrayI/3637-TrionicArrayI.java
+++ leetcode-sub/3637-TrionicArrayI/3637-TrionicArrayI.java
@@ -1,19 +1,18 @@
-// Last updated: 2/2/2026, 8:19:02 PM
+// Last updated: 2/2/2026, 8:19:52 PM
 1class Solution {
 2    public boolean isTrionic(int[] nums) {
-3        int n = nums.length, i = 1;
-4        while (i < n && nums[i - 1] < nums[i]) {
-5            i++;
-6        }
-7        int p = i - 1;
-8        while (i < n && nums[i - 1] > nums[i]) {
-9            i++;
-10        }
-11        int q = i - 1;
-12        while (i < n && nums[i - 1] < nums[i]) {
-13            i++;
+3        if (nums[0] >= nums[1]) {
+4            return false;
+5        }
+6        int n = nums.length, count = 1;
+7        for (int i = 2; i < n; i++) {
+8            if (nums[i - 1] == nums[i]) {
+9                return false;
+10            }
+11            if ((nums[i - 2] - nums[i - 1]) * (nums[i - 1] - nums[i]) < 0) {
+12                count++;
+13            }
 14        }
-15        int flag = i - 1;
-16        return (p != 0) && (q != p) && (flag == n - 1 && flag != q);
-17    }
-18}
+15        return count == 3;
+16    }
+17}
