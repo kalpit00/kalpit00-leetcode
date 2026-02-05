de44b13f50939696caba818a7b098c81a113e73c
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-02-04 20:06:11 -0500

    File updated at 2/4/2026, 8:06:11 PM

--- leetcode-sub/3379-TransformedArray/3379-TransformedArray.java
+++ leetcode-sub/3379-TransformedArray/3379-TransformedArray.java
@@ -1,11 +1,11 @@
-// Last updated: 10/4/2025, 6:26:05 AM
-class Solution {
-    public int[] constructTransformedArray(int[] A) {
-        int n = A.length;
-        int res[] = new int[n];
-        for (int i = 0; i < n; ++i) {
-            res[i] = A[(i + A[i] % n + n) % n];
-        }
-        return res;
-    }
-}
+// Last updated: 2/4/2026, 8:06:11 PM
+1class Solution {
+2    public int[] constructTransformedArray(int[] nums) {
+3        int n = nums.length;
+4        int res[] = new int[n];
+5        for (int i = 0; i < n; ++i) {
+6            res[i] = nums[(i + nums[i] % n + n) % n];
+7        }
+8        return res;
+9    }
+10}
