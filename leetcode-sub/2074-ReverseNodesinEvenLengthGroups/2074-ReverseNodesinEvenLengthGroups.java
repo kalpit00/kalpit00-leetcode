6ae93e9f3359d6a9d2de1eb55fee36a875da2c99
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-02-10 23:36:25 -0500

    File updated at 2/10/2026, 11:36:25 PM

--- leetcode-sub/2074-ReverseNodesinEvenLengthGroups/2074-ReverseNodesinEvenLengthGroups.java
+++ leetcode-sub/2074-ReverseNodesinEvenLengthGroups/2074-ReverseNodesinEvenLengthGroups.java
@@ -1,34 +1,41 @@
-// Last updated: 2/10/2026, 4:08:27 PM
+// Last updated: 2/10/2026, 11:36:25 PM
 1class Solution {
 2    public ListNode reverseEvenLengthGroups(ListNode head) {
-3        ListNode prev = head;
-4        int i = 2;
-5        while (prev.next != null) {
-6            ListNode temp = prev.next;
-7            int j = 0;
-8            while (j < i && temp != null) {
-9                j++;
-10                temp = temp.next;
-11            }            
-12            if (j % 2 == 0) {
-13                prev.next = reverse(prev.next, j);
-14            }            
-15            for (int k = 0; k < j; k++) {
-16                prev = prev.next;
+3        int n = -1, i = 2;
+4        ListNode temp = head, prev = head, curr = head.next;
+5        while (temp != null) {
+6            temp = temp.next;
+7            n++;
+8        }
+9        while (curr != null && n > 0) {
+10            int k = Math.min(i, n);
+11            temp = curr;
+12            if (k % 2 == 0) {
+13                ListNode ans = reverse(temp, k);
+14                prev.next = ans;
+15                prev = temp;
+16                curr = temp.next;
 17            }
-18            i++;
-19        }
-20        return head;
-21    }
-22    private ListNode reverse(ListNode head, int count) {
-23        ListNode prev = null, curr = head;
-24        for (int i = 0; i < count; i++) {
-25            ListNode next = curr.next;
-26            curr.next = prev;
-27            prev = curr;
-28            curr = next;
-29        }
-30        head.next = curr;
-31        return prev;
-32    }
-33}
+18            else {
+19                for (int j = 0; j < k; j++) {
+20                    prev = curr;
+21                    curr = curr.next;
+22                }
+23            }
+24            n -= k;
+25            i++;
+26        }
+27        return head;
+28    }
+29    private ListNode reverse(ListNode head, int k) {
+30        ListNode prev = null, curr = head; // use for-k loop instead of while
+31        for (int i = 0; i < k && curr != null; i++) {
+32            ListNode next = curr.next;
+33            curr.next = prev;
+34            prev = curr;
+35            curr = next;
+36        }
+37        head.next = curr;
+38        return prev;
+39    }
+40}
