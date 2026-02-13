8071ae49376b0ac27f8a19af71d184daa29b14e2
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-02-12 23:29:19 -0500

    File updated at 2/12/2026, 11:29:19 PM

--- leetcode-sub/1964-FindtheLongestValidObstacleCourseatEachPosition/1964-FindtheLongestValidObstacleCourseatEachPosition.java
+++ leetcode-sub/1964-FindtheLongestValidObstacleCourseatEachPosition/1964-FindtheLongestValidObstacleCourseatEachPosition.java
@@ -1,4 +1,4 @@
-// Last updated: 2/12/2026, 9:25:54 PM
+// Last updated: 2/12/2026, 11:29:19 PM
 1class Solution {
 2    public int[] longestObstacleCourseAtEachPosition(int[] nums) {
 3        int n = nums.length, len = 0;
@@ -11,20 +11,19 @@
 10        }
 11        return lis;
 12    }
-13
-14    private int upperBound(int[] dp, int n, int x) {
-15        int start = 0, end = n, ans = n;
-16        while (start <= end) {
-17            int mid = start + (end - start) / 2;
-18            if (dp[mid] > x) {
-19                ans = mid;
-20                end = mid - 1;
-21            }                
-22            else {
-23                start = mid + 1;
-24            }
-25        }
-26        return ans;
-27    }
-28}
-29
+13    private int upperBound(int[] dp, int n, int x) {
+14        int start = 0, end = n, ans = n;
+15        while (start <= end) {
+16            int mid = start + (end - start) / 2;
+17            if (dp[mid] > x) {
+18                ans = mid;
+19                end = mid - 1;
+20            }                
+21            else {
+22                start = mid + 1;
+23            }
+24        }
+25        return ans;
+26    }
+27}
+28
