61a58076804b27dbf319d3c7016f5d3ad5833197
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-02-14 17:46:40 -0500

    File updated at 2/14/2026, 5:46:40 PM

--- leetcode-sub/3240-MinimumNumberofFlipstoMakeBinaryGridPalindromicII/3240-MinimumNumberofFlipstoMakeBinaryGridPalindromicII.java
+++ leetcode-sub/3240-MinimumNumberofFlipstoMakeBinaryGridPalindromicII/3240-MinimumNumberofFlipstoMakeBinaryGridPalindromicII.java
@@ -1,18 +1,15 @@
-// Last updated: 2/14/2026, 5:44:37 PM
+// Last updated: 2/14/2026, 5:46:40 PM
 1class Solution {
 2    public long removeZeros(long n) {
-3        List<Long> list = new ArrayList<>();
-4        while (n > 0) {
-5            long d = n % 10;
-6            if (d != 0) {
-7                list.add(d);
-8            }
-9            n /= 10;
-10        }
-11        long res = 0;
-12        for (int i = list.size() - 1; i >= 0; i--) {
-13            res = res * 10 + list.get(i);
-14        }
-15        return res;
-16    }
-17}
+3        long res = 0;
+4        int i = 0;
+5        while (n > 0) {
+6            long d = n % 10;
+7            if (d != 0) {
+8                res = d * (long) Math.pow(10, i++) + res;
+9            }
+10            n /= 10;
+11        }
+12        return res;
+13    }
+14}
