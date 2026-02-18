b1098cc08bd46dbd32c339d7a7b9519dbf594eb2
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-02-17 22:44:05 -0500

    Create file

---
+++ leetcode-sub/191-Numberof1Bits/191-Numberof1Bits.java
@@ -0,0 +1,12 @@
+// Last updated: 2/17/2026, 10:44:04 PM
+1class Solution {
+2    public int hammingWeight(int n) {
+3        int k = 32 - Integer.numberOfLeadingZeros(n), count = 0;
+4        for (int i = 0; i < k; i++) {
+5            int mask = 1 << i;
+6            int b1 = (n & mask) != 0 ? 1 : 0;
+7            count += b1;
+8        }
+9        return count;
+10    }
+11}
