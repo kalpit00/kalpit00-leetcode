54f7b2f2435d33bb0f4e474e1994557d838d0747
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-02-19 18:44:36 -0500

    File updated at 2/19/2026, 6:44:36 PM

--- leetcode-sub/525-ContiguousArray/525-ContiguousArray.java
+++ leetcode-sub/525-ContiguousArray/525-ContiguousArray.java
@@ -1,22 +1,19 @@
-// Last updated: 10/3/2025, 5:48:26 PM
-class Solution {
-    public int findMaxLength(int[] nums) {
-        int n = nums.length, max = 0, min = Integer.MAX_VALUE;
-        int[] pre0 = new int[n + 1], pre1 = new int[n + 1], 
-        arr = new int[n + 1];
-        Integer[] map = new Integer[200002];
-        for (int i = 1; i <= n; i++) {
-            pre0[i] = pre0[i - 1] + (nums[i - 1] == 0 ? 1 : 0);
-            pre1[i] = pre1[i - 1] + (nums[i - 1] == 1 ? 1 : 0);
-            arr[i] = pre0[i] - pre1[i];            
-            min = Math.min(min, arr[i]);
-        }
-        min = min >= 0 ? 0 : min;
-        for (int i = 0; i <= n; i++) {
-            int idx = arr[i] - min;
-            map[idx] = map[idx] == null ? i : map[idx];
-            max = Math.max(max, i - map[idx]);
-        }
-        return max;
-    }
-}
+// Last updated: 2/19/2026, 6:44:36 PM
+1class Solution {
+2    public int findMaxLength(int[] nums) {
+3        int n = nums.length, max = 0;
+4        int[] pre0 = new int[n + 1], pre1 = new int[n + 1], 
+5        arr = new int[n + 1];
+6        for (int i = 1; i <= n; i++) {
+7            pre0[i] = pre0[i - 1] + (nums[i - 1] == 0 ? 1 : 0);
+8            pre1[i] = pre1[i - 1] + (nums[i - 1] == 1 ? 1 : 0);
+9            arr[i] = pre0[i] - pre1[i];            
+10        }
+11        Map<Integer, Integer> map = new HashMap<>();
+12        for (int i = 0; i <= n; i++) {
+13            map.putIfAbsent(arr[i], i);
+14            max = Math.max(max, i - map.get(arr[i]));
+15        }
+16        return max;
+17    }
+18}
