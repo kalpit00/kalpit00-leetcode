1780adf250b62c68b542ddae7e7ba557127c256e
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-02-19 03:26:55 -0500

    File updated at 2/19/2026, 3:26:55 AM

--- leetcode-sub/2121-IntervalsBetweenIdenticalElements/2121-IntervalsBetweenIdenticalElements.java
+++ leetcode-sub/2121-IntervalsBetweenIdenticalElements/2121-IntervalsBetweenIdenticalElements.java
@@ -1,37 +1,30 @@
-// Last updated: 2/19/2026, 3:20:25 AM
-1class Solution {
-2    public long[] getDistances(int[] nums) {
-3        int n = nums.length;
-4        long[] arr = new long[n];
-5        Map<Integer, List<Integer>> map = new HashMap<>();
-6        for (int i = 0; i < n; i++) {
-7            map.putIfAbsent(nums[i], new ArrayList<>());
-8            map.get(nums[i]).add(i);
-9        }
-10        for (int key : map.keySet()) {
-11            List<Integer> group = map.get(key);
-12            long[] res = getSumAbsoluteDifferences(group);
-13            for (int i = 0; i < group.size(); i++) {
-14                arr[group.get(i)] = res[i];
-15            }
-16        }
-17        return arr;
-18    }
-19
-20    private long[] getSumAbsoluteDifferences(List<Integer> nums) {
-21        int n = nums.size();
-22        long[] pre = new long[n], suf = new long[n], res = new long[n];
-23        for (int i = 1; i < n; i++) {
-24            pre[i] = pre[i - 1] + nums.get(i - 1);
-25        }
-26        for (int i = n - 2; i >= 0; i--) {
-27            suf[i] = suf[i + 1] + nums.get(i + 1);
-28        }
-29        for (int i = 0; i < n; i++) {
-30            long val = nums.get(i);
-31            res[i] = (suf[i] - (n - i - 1) * val) - 
-32            (pre[i] - i * val);
-33        }
-34        return res;
-35    }
-36}
+// Last updated: 2/19/2026, 3:26:55 AM
+class Solution {
+    public long[] getDistances(int[] arr) {
+        int n = arr.length;
+
+        Map<Integer, List<Integer>> indexMap = new HashMap<>();
+
+        for(int i=0;i<n;i++){
+            indexMap.computeIfAbsent(arr[i], k-> new ArrayList<>()).add(i);
+        }
+        long[] result = new long[n];
+        for (List<Integer> indices : indexMap.values()) {
+            int k = indices.size();
+            long totalSum = 0;
+            for (int idx : indices) totalSum += idx;
+
+            long prefixSum = 0;
+            for (int i = 0; i < k; i++) {
+                int currentIdx = indices.get(i);
+                
+                long leftSide = (long) i * currentIdx - prefixSum;
+                long rightSide = (totalSum - prefixSum - currentIdx) - (long) (k - 1 - i) * currentIdx;
+                
+                result[currentIdx] = leftSide + rightSide;
+                prefixSum += currentIdx;
+            }
+        }
+        return result;
+    }
+}
