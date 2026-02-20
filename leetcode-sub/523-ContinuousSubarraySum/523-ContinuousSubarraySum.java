200066a4c2a16e2fedd0c9167afd5dbb7b6ee271
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-02-19 19:05:09 -0500

    File updated at 2/19/2026, 7:05:09 PM

--- leetcode-sub/523-ContinuousSubarraySum/523-ContinuousSubarraySum.java
+++ leetcode-sub/523-ContinuousSubarraySum/523-ContinuousSubarraySum.java
@@ -1,18 +1,20 @@
-// Last updated: 2/19/2026, 7:00:04 PM
+// Last updated: 2/19/2026, 7:05:09 PM
 1class Solution {
 2    public boolean checkSubarraySum(int[] nums, int k) {
 3        int n = nums.length;
-4        int[] sum = new int[n + 1];
-5        for (int i = 1; i <= n; i++) {
-6            sum[i] = sum[i - 1] + nums[i - 1];
+4        int[] pre = new int[n + 1];
+5        for (int i = 0; i < n; i++) {
+6            pre[i + 1] = pre[i] + nums[i];
 7        }
-8        Set<Integer> set = new HashSet<>();
-9        for (int i = 2; i <= n; i++) {
-10            set.add(sum[i - 2] % k);
-11            if (set.contains(sum[i] % k)) { 
-12                return true;
-13            }
-14        }
-15        return false;
-16    }
-17}
+8        Map<Integer, Integer> map = new HashMap<>();
+9        map.put(0, 0);
+10        for (int i = 1; i <= n; i++) {
+11            int pre_i = ((pre[i] % k) + k) % k; // avoids negative pre[i]
+12            if (map.containsKey(pre_i) && i - map.get(pre_i) > 1) {
+13                return true;
+14            }
+15            map.putIfAbsent(pre_i, i);
+16        }
+17        return false;
+18    }
+19}
