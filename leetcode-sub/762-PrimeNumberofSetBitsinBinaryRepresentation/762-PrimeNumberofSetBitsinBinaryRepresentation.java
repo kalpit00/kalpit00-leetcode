ff36dfc28f888b2078d51893ed41f50e1c58daf1
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-02-21 03:16:31 -0500

    File updated at 2/21/2026, 3:16:31 AM

--- leetcode-sub/762-PrimeNumberofSetBitsinBinaryRepresentation/762-PrimeNumberofSetBitsinBinaryRepresentation.java
+++ leetcode-sub/762-PrimeNumberofSetBitsinBinaryRepresentation/762-PrimeNumberofSetBitsinBinaryRepresentation.java
@@ -1,26 +1,14 @@
-// Last updated: 4/28/2025, 3:48:13 PM
-class Solution {
-    public int countPrimeSetBits(int left, int right) {
-        int count = 0;
-        for (int i = left; i <= right; i++) {
-            count += isPrime(Integer.bitCount(i)) ? 1 : 0;
-        }
-        return count;
-    }
-    private boolean isPrime(int x) {
-        return (x == 2 || x == 3 || x == 5 || x == 7 ||
-                x == 11 || x == 13 || x == 17 || x == 19);
-    }
-    private boolean[] sieve(int n) {
-        boolean[] notPrime = new boolean[n];
-        notPrime[0] = notPrime[1] = true;
-        for (int i = 2; i < n; i++) {
-            if (!notPrime[i]) {
-                for (int j = 2; i*j < n; j++) {
-                    notPrime[i*j] = true;
-                }
-            }
-        }
-        return notPrime;
-    }
-}
+// Last updated: 2/21/2026, 3:16:31 AM
+1class Solution {
+2    public int countPrimeSetBits(int left, int right) {
+3        int count = 0;
+4        for (int i = left; i <= right; i++) {
+5            count += isPrime(Integer.bitCount(i)) ? 1 : 0;
+6        }
+7        return count;
+8    }
+9    private boolean isPrime(int x) {
+10        return (x == 2 || x == 3 || x == 5 || x == 7 ||
+11                x == 11 || x == 13 || x == 17 || x == 19);
+12    }
+13}
