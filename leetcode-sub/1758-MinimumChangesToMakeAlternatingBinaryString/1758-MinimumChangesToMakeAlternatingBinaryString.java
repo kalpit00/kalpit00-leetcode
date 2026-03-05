039a7c47b03fdbc98c411b854066cdef689f3a03
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-03-04 21:19:50 -0500

    File updated at 3/4/2026, 9:19:50 PM

--- leetcode-sub/1758-MinimumChangesToMakeAlternatingBinaryString/1758-MinimumChangesToMakeAlternatingBinaryString.java
+++ leetcode-sub/1758-MinimumChangesToMakeAlternatingBinaryString/1758-MinimumChangesToMakeAlternatingBinaryString.java
@@ -1,25 +1,12 @@
-// Last updated: 3/4/2026, 9:18:31 PM
+// Last updated: 3/4/2026, 9:19:50 PM
 1class Solution {
 2    public int minOperations(String s) {
-3        int start0 = 0;
-4        int start1 = 0;
-5        
-6        for (int i = 0; i < s.length(); i++) {
-7            if (i % 2 == 0) {
-8                if (s.charAt(i) == '0') {
-9                    start1++;
-10                } else {
-11                    start0++;
-12                }
-13            } else {
-14                if (s.charAt(i) == '1') {
-15                    start1++;
-16                } else {
-17                    start0++;
-18                }
-19            }
-20        }
-21        
-22        return Math.min(start0, start1);
-23    }
-24}
+3        char[] arr = s.toCharArray();
+4        int count = 0, n = arr.length;
+5        for (int i = 0; i < n; i++) {
+6            count += (i % 2 == 0 && arr[i] == '1') || 
+7            (i % 2 != 0 && arr[i] == '0') ? 1 : 0;            
+8        }
+9        return Math.min(count, n - count);
+10    }
+11}
