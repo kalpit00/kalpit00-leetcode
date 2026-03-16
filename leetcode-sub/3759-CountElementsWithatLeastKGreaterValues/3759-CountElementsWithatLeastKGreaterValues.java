00f8683dc569f5ece442d23beed1265df1796476
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-03-16 04:12:21 -0400

    File updated at 3/16/2026, 4:12:21 AM

--- leetcode-sub/3759-CountElementsWithatLeastKGreaterValues/3759-CountElementsWithatLeastKGreaterValues.java
+++ leetcode-sub/3759-CountElementsWithatLeastKGreaterValues/3759-CountElementsWithatLeastKGreaterValues.java
@@ -1,50 +1,51 @@
-// Last updated: 3/16/2026, 4:11:49 AM
+// Last updated: 3/16/2026, 4:12:21 AM
 1class Solution {
 2    public int countElements(int[] nums, int k) {
 3        int n = nums.length, count = 0;
 4        if (k == 0) return n;
-5        quickSelect(nums, 0, n - 1, n - k);
-6        for (int num : nums) {
-7            count += num < nums[n - k] ? 1 : 0;
-8        }
-9        return count;
-10    }
-11    private int quickSelect(int[] nums, int start, int end, int k) {
-12        if (start >= end) {
-13            return nums[start];
-14        } // take random pivot choice between index [start .. end]
-15        int pivotIndex = start + new Random().nextInt(end - start + 1);
-16        int pivot[] = partition(nums, start, end, pivotIndex);
-17        if (pivot[0] > k) {
-18            return quickSelect(nums, start, pivot[0] - 1, k);
-19        }
-20        else if (pivot[1] < k) {
-21            return quickSelect(nums, pivot[1] + 1, end, k);
-22        }
-23        else {
-24            return nums[k];
-25        }
-26    }
-27    // Dutch National Flag Algorithm
-28    private int[] partition(int[] nums, int start, int end, int pivotIndex) {
-29        int pivot = nums[pivotIndex]; 
-30        int i = start, idx = start, j = end;
-31        while (idx <= j && i < j) {
-32            if (nums[idx] < pivot) {
-33                swap(nums, i++, idx++);
-34            }
-35            else if (nums[idx] > pivot) {
-36                swap(nums, j--, idx);
-37            }
-38            else {
-39                idx++;
-40            }
-41        }
-42        return new int[]{i, j};
-43    }
-44    private void swap(int[] nums, int i, int j) {
-45        int temp = nums[i];
-46        nums[i] = nums[j];
-47        nums[j] = temp;
-48    }
-49}
+5        // quickSelect(nums, 0, n - 1, n - k);
+6        Arrays.sort(nums);
+7        for (int num : nums) {
+8            count += num < nums[n - k] ? 1 : 0;
+9        }
+10        return count;
+11    }
+12    private int quickSelect(int[] nums, int start, int end, int k) {
+13        if (start >= end) {
+14            return nums[start];
+15        } // take random pivot choice between index [start .. end]
+16        int pivotIndex = start + new Random().nextInt(end - start + 1);
+17        int pivot[] = partition(nums, start, end, pivotIndex);
+18        if (pivot[0] > k) {
+19            return quickSelect(nums, start, pivot[0] - 1, k);
+20        }
+21        else if (pivot[1] < k) {
+22            return quickSelect(nums, pivot[1] + 1, end, k);
+23        }
+24        else {
+25            return nums[k];
+26        }
+27    }
+28    // Dutch National Flag Algorithm
+29    private int[] partition(int[] nums, int start, int end, int pivotIndex) {
+30        int pivot = nums[pivotIndex]; 
+31        int i = start, idx = start, j = end;
+32        while (idx <= j && i < j) {
+33            if (nums[idx] < pivot) {
+34                swap(nums, i++, idx++);
+35            }
+36            else if (nums[idx] > pivot) {
+37                swap(nums, j--, idx);
+38            }
+39            else {
+40                idx++;
+41            }
+42        }
+43        return new int[]{i, j};
+44    }
+45    private void swap(int[] nums, int i, int j) {
+46        int temp = nums[i];
+47        nums[i] = nums[j];
+48        nums[j] = temp;
+49    }
+50}
