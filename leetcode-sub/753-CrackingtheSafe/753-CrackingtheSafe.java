26e6a6b08a792868188421e4081c358136b879e2
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-03-17 03:34:34 -0400

    File updated at 3/17/2026, 3:34:34 AM

--- leetcode-sub/753-CrackingtheSafe/753-CrackingtheSafe.java
+++ leetcode-sub/753-CrackingtheSafe/753-CrackingtheSafe.java
@@ -1,28 +1,26 @@
-// Last updated: 3/17/2026, 3:31:25 AM
+// Last updated: 3/17/2026, 3:34:34 AM
 1class Solution {
 2    public String crackSafe(int n, int k) {
-3        if (n == 1 && k == 1) return "0";
-4        Set<String> visited = new HashSet<>();
-5        List<Integer> res = new LinkedList<>();
-6        String src = "0".repeat(n - 1);
-7        dfs(src, k, visited, res);
-8        Collections.reverse(res);
-9        StringBuilder sb = new StringBuilder(src);
-10        for (int i : res) {
-11            sb.append(i);
-12        }
-13        return sb.toString();
-14    }
-15
-16    private void dfs(String node, int k, Set<String> visited,
-17    List<Integer> res) {
-18        for (int i = 0; i < k; i++) {
-19            String neighbor = node + i;
-20            if (!visited.contains(neighbor)) {
-21                visited.add(neighbor);
-22                dfs(neighbor.substring(1), k, visited, res);
-23                res.add(i);
-24            }
-25        }
-26    }
-27}
+3        StringBuilder res = new StringBuilder();
+4        int mod = (int) Math.pow(k, n - 1);
+5        boolean[] visited = new boolean[mod * k]; 
+6        dfs(0, mod, visited, res, k);
+7        for (int i = 0; i < n - 1; i++) {
+8            res.append('0');
+9        }
+10        return res.toString();        
+11    }
+12
+13    private void dfs(int node, int mod, boolean[] visited,
+14    StringBuilder res, int k) {
+15        for (int d = 0; d < k; d++) {
+16            int edge = node * k + d;
+17            if (!visited[edge]) {
+18                visited[edge] = true;
+19                int nextNode = edge % mod;
+20                dfs(nextNode, mod, visited, res, k);
+21                res.append(d);
+22            }
+23        }        
+24    }
+25}
