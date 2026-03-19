5f405dc80d708df6035a47c2dfd31f9e02ba7871
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-03-19 01:13:42 -0400

    File updated at 3/19/2026, 1:13:41 AM

--- leetcode-sub/2493-DivideNodesIntotheMaximumNumberofGroups/2493-DivideNodesIntotheMaximumNumberofGroups.java
+++ leetcode-sub/2493-DivideNodesIntotheMaximumNumberofGroups/2493-DivideNodesIntotheMaximumNumberofGroups.java
@@ -1,89 +1,67 @@
-// Last updated: 3/18/2026, 10:01:36 PM
+// Last updated: 3/19/2026, 1:13:41 AM
 1class Solution {
 2    public int magnificentSets(int n, int[][] edges) {
-3        DSU dsu = new DSU(n + 1);
-4        List<List<Integer>> adj = new ArrayList<>();
-5        for (int i = 0; i <= n; i++) {
-6            adj.add(new ArrayList<>());
-7        }
-8        for (int[] edge : edges) {
-9            int u = edge[0], v = edge[1];
-10            adj.get(u).add(v);
-11            adj.get(v).add(u);
-12            dsu.union(u, v);
-13        }
-14        Map<Integer, Integer> map = new HashMap<>();
-15        for (int i = 1; i <= n; i++) {
-16            int max = bfs(adj, i, n);
-17            if (max == -1) {
-18                return -1;
-19            }
-20            int root = dsu.findParent(i);
-21            map.put(root, Math.max(max, map.getOrDefault(root, 0)));
-22        }
-23        int sum = 0;
-24        for (int count : map.values()) {
-25            sum += count;
+3        List<List<Integer>> adj = new ArrayList<>();
+4        for (int i = 0; i <= n; i++) {
+5            adj.add(i, new ArrayList<>());
+6        }
+7        for (int[] edge : edges) {
+8            int u = edge[0], v = edge[1];
+9            adj.get(u).add(v);
+10            adj.get(v).add(u);
+11        }
+12        int sum = 0;
+13        int[] visited = new int[n + 1];
+14        for (int i = 1; i <= n; i++) {
+15            if (visited[i] == 0) { // explore in components!
+16                List<Integer> component = new ArrayList<>();
+17                if (!dfs(i, visited, adj, 1, component)) {
+18                    return -1;
+19                } // now bfs starting from each node in each cc : O(N^2)!
+20                int max = 0;
+21                for (int node : component) {
+22                    max = Math.max(max, bfs(adj, node, n));
+23                } // sum the highest level from all O(N) bfs calls in each cc
+24                sum += max;
+25            }
 26        }
 27        return sum;
-28    }
-29    private int bfs(List<List<Integer>> adj, int src, int n) {
-30        Queue<Integer> queue = new LinkedList<>();
-31        int[] visited = new int[n + 1];
-32        Arrays.fill(visited, -1);
-33        queue.offer(src);
-34        visited[src] = 0;
-35        int level = 0;
-36        while (!queue.isEmpty()) {
-37            int size = queue.size();
-38            for (int i = 0; i < size; i++) {
-39                int node = queue.poll();
-40                for (int neighbor : adj.get(node)) {
-41                    if (visited[neighbor] == -1) {
-42                        visited[neighbor] = level + 1;
-43                        queue.offer(neighbor);
-44                    }
-45                    else if (visited[neighbor] == level) {
-46                        return -1;
-47                    }
-48                }
-49            }
-50            level++;
-51        }
-52        return level;
-53    }
-54    class DSU {
-55        int[] size, parent;
-56        int componentCount;
-57        public DSU(int n) {
-58            size = new int[n];
-59            parent = new int[n];
-60            componentCount = n;
-61            for (int i = 0; i < n; i++) {
-62                size[i] = 1;
-63                parent[i] = i;
-64            }
-65        }
-66
-67        public int findParent(int node) {
-68            if (node == parent[node]) {
-69                return node;
-70            }
-71            return parent[node] = findParent(parent[node]);
-72        }
-73
-74        public void union(int u, int v) {
-75            int pu = findParent(u), pv = findParent(v);
-76            if (pu == pv) return;
-77
-78            if (size[pu] < size[pv]) {
-79                parent[pu] = pv;
-80                size[pv] += size[pu];
-81            } else {
-82                parent[pv] = pu;
-83                size[pu] += size[pv];
-84            }
-85            componentCount--;
-86        }
-87    }
-88}
+28    } // isGraphBipartite dfs() coloring!!!
+29    private boolean dfs(int node, int[] visited, List<List<Integer>> adj,
+30    int color, List<Integer> component) {
+31        visited[node] = color;
+32        component.add(node);
+33        for (int neighbor : adj.get(node)) {
+34            if (visited[neighbor] == color) { 
+35                return false;
+36            }
+37            else if (visited[neighbor] == 0) {
+38                if (!dfs(neighbor, visited, adj, -color, component)) {
+39                    return false;
+40                }
+41            }
+42        }
+43        return true;
+44    } // classic level order bfs!!
+45    private int bfs(List<List<Integer>> adj, int src, int n) {
+46        Queue<Integer> queue = new LinkedList<>();
+47        boolean[] visited = new boolean[n + 1];
+48        queue.offer(src);
+49        visited[src] = true;
+50        int level = 0;
+51        while (!queue.isEmpty()) {
+52            int size = queue.size();
+53            for (int i = 0; i < size; i++) {
+54                int node = queue.poll();
+55                for (int neighbor : adj.get(node)) {
+56                    if (!visited[neighbor]) {
+57                        visited[neighbor] = true;
+58                        queue.offer(neighbor);
+59                    }
+60                }
+61            }
+62            level++;
+63        }
+64        return level;
+65    }
+66}
