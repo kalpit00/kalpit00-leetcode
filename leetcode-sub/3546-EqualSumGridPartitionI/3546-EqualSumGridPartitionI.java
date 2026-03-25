af1441e9ab3078d3adcee4683efd2d4542d59b69
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-03-24 22:57:45 -0400

    File updated at 3/24/2026, 10:57:45 PM

--- leetcode-sub/3546-EqualSumGridPartitionI/3546-EqualSumGridPartitionI.java
+++ leetcode-sub/3546-EqualSumGridPartitionI/3546-EqualSumGridPartitionI.java
@@ -1,32 +1,30 @@
-// Last updated: 3/24/2026, 10:29:45 PM
+// Last updated: 3/24/2026, 10:57:45 PM
 1class Solution {
 2    public boolean canPartitionGrid(int[][] grid) {
 3        int m = grid.length, n = grid[0].length;
-4        long rowSum = 0, colSum = 0, sum = 0;
-5        long[] preRow = new long[m], preCol = new long[n];
-6        for (int i = 0; i < m; i++) {
-7            for (int j = 0; j < n; j++) {
-8                preRow[i] += grid[i][j];
-9                preCol[j] += grid[i][j];
-10            }
-11        }
-12        for (long r : preRow) {
-13            rowSum += r;
-14        }
-15        colSum = rowSum;
-16        for (int i = 0; i < m - 1; i++) {
-17            sum += preRow[i];
-18            if (rowSum == 2 * sum) {
-19                return true;
-20            }
+4        long[] preRow = new long[m], preCol = new long[n];
+5        for (int i = 0; i < m; i++) {
+6            for (int j = 0; j < n; j++) {
+7                preRow[i] += grid[i][j];
+8                preCol[j] += grid[i][j];
+9            }
+10        }
+11        return pivotIndex(preRow) || pivotIndex(preCol);
+12    }
+13    public boolean pivotIndex(long[] nums) {
+14        int n = nums.length;
+15        long[] pre = new long[n], suf = new long[n];
+16        for (int i = 1; i < n; i++) {
+17            pre[i] = pre[i - 1] + nums[i - 1];
+18        }
+19        for (int i = n - 2; i >= 0; i--) {
+20            suf[i] = suf[i + 1] + nums[i + 1];
 21        }
-22        sum = 0;
-23        for (int j = 0; j < n - 1; j++) {
-24            sum += preCol[j];
-25            if (colSum == 2 * sum) {
-26                return true;
-27            }
-28        }
-29        return false;
-30    }
-31}
+22        for (int i = 0; i < n; i++) {
+23            if (pre[i] + nums[i] == suf[i]) {
+24                return true;
+25            }
+26        }
+27        return false;
+28    }
+29}
