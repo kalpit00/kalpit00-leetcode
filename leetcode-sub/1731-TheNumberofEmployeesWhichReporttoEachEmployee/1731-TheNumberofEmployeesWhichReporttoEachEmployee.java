dbbd4322e1d78f306b204df38706c61438a449c0
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-03-31 16:12:23 -0400

    File updated at 3/31/2026, 4:12:22 PM

--- leetcode-sub/1731-TheNumberofEmployeesWhichReporttoEachEmployee/1731-TheNumberofEmployeesWhichReporttoEachEmployee.java
+++ leetcode-sub/1731-TheNumberofEmployeesWhichReporttoEachEmployee/1731-TheNumberofEmployeesWhichReporttoEachEmployee.java
@@ -1,51 +1,50 @@
-// Last updated: 3/24/2025, 9:01:55 AM
-import java.util.*;
-
-class Solution {
-    public int minimumTeachings(int n, int[][] languages, int[][] friendships) {
-        int m = languages.length; // Number of users
-        Set<Integer>[] set = new HashSet[m + 1];
-
-        // Convert languages to sets for each user
-        for (int i = 1; i <= m; i++) {
-            set[i] = new HashSet<>();
-            for (int language : languages[i - 1]) { // 0-based index
-                set[i].add(language);
-            }
-        }
-
-        // Find users who cannot communicate
-        Set<Integer> toTeach = new HashSet<>();
-        for (int[] friendship : friendships) {
-            int u = friendship[0], v = friendship[1]; // 1-based indices
-            Set<Integer> langU = set[u], langV = set[v];
-
-            boolean canCommunicate = false;
-            for (int lang : langU) {
-                if (langV.contains(lang)) {
-                    canCommunicate = true;
-                    break;
-                }
-            }
-            if (!canCommunicate) {
-                toTeach.add(u);
-                toTeach.add(v);
-            }
-        }
-
-        // Find the minimum number of people to teach for an optimal language
-        Map<Integer, Integer> langCount = new HashMap<>();
-        for (int user : toTeach) {
-            for (int lang : set[user]) {
-                langCount.put(lang, langCount.getOrDefault(lang, 0) + 1);
-            }
-        }
-
-        int maxCommon = 0;
-        for (int count : langCount.values()) {
-            maxCommon = Math.max(maxCommon, count);
-        }
-
-        return toTeach.size() - maxCommon;
-    }
-}
+// Last updated: 3/31/2026, 4:12:22 PM
+1class Solution {
+2    public int minimumNumbers(int num, int k) {
+3        if (num == 0) return 0;
+4        if (k == 0) {
+5            if (num % 10 == 0) {
+6                return 1;
+7            }
+8            return -1;
+9        }
+10        List<Integer> list = new ArrayList<>();
+11        for (int i = 0; i <= num; i++) {
+12            if (i % 10 == k) {
+13                list.add(i);
+14            }
+15        }
+16        int[] coins = new int[list.size()];
+17        for (int i = 0; i < coins.length; i++) {
+18            coins[i] = list.get(i);
+19        }
+20        return coinChange(coins, num);
+21    }
+22    public int coinChange(int[] coins, int amount) {
+23        int n = coins.length;
+24        Integer[][] dp = new Integer[n][amount + 1];
+25        int res = solve(0, coins, 0, amount, dp);
+26        return res == Integer.MAX_VALUE ? -1 : res;
+27    }
+28    
+29    private int solve(int i, int[] nums, int sum, int target, Integer[][] dp) {
+30        if (sum == target) {
+31            return 0;
+32        }
+33        if (i >= nums.length || sum > target) {
+34            return Integer.MAX_VALUE;
+35        }
+36        if (dp[i][sum] != null) {
+37            return dp[i][sum];
+38        }
+39        int notTake = solve(i + 1, nums, sum, target, dp);
+40        int take = Integer.MAX_VALUE;
+41        if (nums[i] <= target - sum) {
+42            int res = solve(i, nums, sum + nums[i], target, dp);
+43            if (res != Integer.MAX_VALUE) {
+44                take = 1 + res;
+45            }
+46        }
+47        return dp[i][sum] = Math.min(take, notTake);
+48    }
+49}
