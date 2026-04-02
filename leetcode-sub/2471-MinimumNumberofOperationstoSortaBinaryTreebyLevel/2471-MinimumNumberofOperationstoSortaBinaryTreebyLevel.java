4559567ab9213e0dd33232628200aac009cb43f9
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-04-02 03:56:57 -0400

    File updated at 4/2/2026, 3:56:57 AM

--- leetcode-sub/2471-MinimumNumberofOperationstoSortaBinaryTreebyLevel/2471-MinimumNumberofOperationstoSortaBinaryTreebyLevel.java
+++ leetcode-sub/2471-MinimumNumberofOperationstoSortaBinaryTreebyLevel/2471-MinimumNumberofOperationstoSortaBinaryTreebyLevel.java
@@ -1,41 +1,80 @@
-// Last updated: 4/2/2026, 3:50:28 AM
-1class Solution {
-2    public int minimumOperations(TreeNode root) {
-3        Queue<TreeNode> queue = new LinkedList<>();
-4        queue.offer(root);
-5        int sum = 0;
-6        while (!queue.isEmpty()) {
-7            List<Integer> unsorted = new ArrayList<>();
-8            int size = queue.size();
-9            for (int i = 0; i < size; i++) {
-10                TreeNode node = queue.poll();
-11                unsorted.add(node.val);
-12                if (node.left != null) {
-13                    queue.offer(node.left);
-14                }
-15                if (node.right != null) {
-16                    queue.offer(node.right);
-17                }
-18            }
-19            List<Integer> sorted = new ArrayList<>(unsorted);
-20            Collections.sort(sorted);
-21            sum += countSwaps(sorted, unsorted);
-22        }
-23        return sum;
-24    }
-25    public int countSwaps(List<Integer> sorted, List<Integer> unsorted) {
-26        int count = 0, n = sorted.size();
-27        Map<Integer, Integer> map = new HashMap<>();
-28        for (int i = 0; i < n; i++) {
-29            map.put(sorted.get(i), i);
-30        }
-31        for (int i = 0; i < n; i++) {
-32            while (i != map.get(unsorted.get(i))) {
-33                int correctIdx = map.get(unsorted.get(i));
-34                Collections.swap(unsorted, i, correctIdx);
-35                count++;
-36            }
-37        }
-38        return count;
-39    }
-40}
+// Last updated: 4/2/2026, 3:56:57 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minimumOperations(TreeNode root) {
+         Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        int totalSwaps = 0;
+
+        while (!q.isEmpty()) {
+
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+
+            // BFS level
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                level.add(node.val);
+
+                if (node.left != null) q.offer(node.left);
+                if (node.right != null) q.offer(node.right);
+            }
+
+            totalSwaps += minSwaps(level);
+        }
+
+        return totalSwaps;
+    }
+
+    private int minSwaps(List<Integer> arr) {
+        int n = arr.size();
+
+        List<int[]> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(new int[]{arr.get(i), i});
+        }
+
+        // sort by value
+        list.sort((a, b) -> a[0] - b[0]);
+
+        boolean[] visited = new boolean[n];
+        int swaps = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (visited[i] || list.get(i)[1] == i)
+                continue;
+
+            int cycleSize = 0;
+            int j = i;
+
+            while (!visited[j]) {
+                visited[j] = true;
+                j = list.get(j)[1];
+                cycleSize++;
+            }
+
+            if (cycleSize > 1) {
+                swaps += (cycleSize - 1);
+            }
+        }
+
+        return swaps;
+    }
+
+}
