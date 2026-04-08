c24ef6beebe5a771550f22825beb6b343e1cdbab
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-04-08 02:40:36 -0400

    File updated at 4/8/2026, 2:40:36 AM

--- leetcode-sub/2617-MinimumNumberofVisitedCellsinaGrid/2617-MinimumNumberofVisitedCellsinaGrid.java
+++ leetcode-sub/2617-MinimumNumberofVisitedCellsinaGrid/2617-MinimumNumberofVisitedCellsinaGrid.java
@@ -1,90 +1,41 @@
-// Last updated: 4/8/2026, 2:31:57 AM
-1class Solution {
-2    public int minimumVisitedCells(int[][] grid) {
-3        int m = grid.length, n = grid[0].length, steps = 1;
-4        DSU[] rows = new DSU[m], cols = new DSU[n];
-5        for (int i = 0; i < m; i++) {
-6            rows[i] = new DSU(n);
-7        }
-8        for (int j = 0; j < n; j++) {
-9            cols[j] = new DSU(m);
-10        }
-11        boolean[][] visited = new boolean[m][n];
-12        Queue<int[]> queue = new LinkedList<>();
-13        queue.offer(new int[]{0, 0});
-14        visited[0][0] = true;
-15        while (!queue.isEmpty()) {
-16            int size = queue.size();
-17            for (int t = 0; t < size; t++) {
-18                int[] node = queue.poll();
-19                int i = node[0], j = node[1];
-20                if (i == m - 1 && j == n - 1) {
-21                    return steps;
-22                }
-23                if (grid[i][j] == 0) continue;
-24                int r = Math.min(m - 1, i + grid[i][j]);
-25                int c = Math.min(n - 1, j + grid[i][j]);
-26                while (rows[i].findParent(j) < c) {
-27                    int k = rows[i].findParent(j) + 1;
-28                    rows[i].unionByParent(j, k);
-29                    queue.offer(new int[]{i, k});
-30                    visited[i][k] = true;
-31                }
-32                while (cols[j].findParent(i) < r) {
-33                    int k = cols[j].findParent(i) + 1;
-34                    cols[j].unionByParent(i, k);
-35                    queue.offer(new int[]{k, j});
-36                    visited[k][j] = true;
-37                }                
-38            }
-39            steps++;
-40        }
-41        return -1;
-42    }
-43    public class DSU {
-44        int[] parent;
-45        int[] size;
-46        public DSU (int n) {
-47            parent = new int[n];
-48            size = new int[n];
-49            for (int i = 0; i < n; i++) {
-50                parent[i] = i;
-51                size[i] = 1;
-52            }
-53        }
-54        public int findParent(int node) {
-55            if (node == parent[node]) {
-56                return node;
-57            }
-58            return parent[node] = findParent(parent[node]);
-59        }
-60        public boolean union(int u, int v) {
-61            int pu = findParent(u), pv = findParent(v);
-62            if (pu == pv) {
-63                return false;
-64            }
-65            if (size[pu] < size[pv]) {
-66                parent[pu] = pv;
-67                size[pv] += size[pu];
-68            }
-69            else {
-70                parent[pv] = pu;
-71                size[pu] += size[pv];
-72            }
-73            return true;
-74        }
-75        public boolean unionByParent(int u, int v) {
-76            int pu = findParent(u), pv = findParent(v);
-77            if (pu == pv) {
-78                return false;
-79            } // just compare parent indices, not size or rank!
-80            if (pu < pv) {
-81                parent[pu] = pv;
-82            }
-83            else {
-84                parent[pv] = pu;
-85            }
-86            return true;
-87        }
-88    }
-89}
+// Last updated: 4/8/2026, 2:40:36 AM
+class Solution {
+    public int minimumVisitedCells(int[][] grid) {
+        int M = grid.length, N = grid[0].length;
+        int [][]maxRow = new int[M][N];
+        int [][]maxCol = new int[M][N];
+
+        Queue<int[]> que = new ArrayDeque<>();
+        que.add(new int[]{0, 0});
+        int minVisits = 0;
+        while(!que.isEmpty()) {
+            int size = que.size();
+            while(size-- > 0) {
+                int []cur = que.remove();
+                int r = cur[0], c = cur[1], cellVal = grid[r][c];
+                if(r == M-1 && c == N-1)
+                    return minVisits + 1;
+
+                for(int k = r + 1; k <= r + cellVal && k < M; k++) {
+                    if(maxRow[k][c] == 0) {
+                        maxRow[k][c] = cellVal + r;
+                        que.add(new int[]{k, c});
+                    } else {
+                        k = maxRow[k][c]; // skip till max row, if already evaluted before
+                    }
+                }
+
+                for(int k = c + 1; k <= c + cellVal && k < N; k++) {
+                    if(maxCol[r][k] == 0) {
+                        maxCol[r][k] = cellVal + c;
+                        que.add(new int[]{r, k});
+                    } else {
+                        k = maxCol[r][k]; // skip till max col, if already evaluted before
+                    }
+                }
+            }
+            minVisits++;
+        }
+        return -1;
+    }
+}
