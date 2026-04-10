26c5bc490ca908ac77f74117cad2c00029940aff
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-04-09 21:22:37 -0400

    Create file

---
+++ leetcode-sub/3741-MinimumDistanceBetweenThreeEqualElementsII/3741-MinimumDistanceBetweenThreeEqualElementsII.java
@@ -0,0 +1,22 @@
+// Last updated: 4/9/2026, 9:22:37 PM
+1class Solution {
+2    public int minimumDistance(int[] nums) {
+3        int n = nums.length, min = Integer.MAX_VALUE;
+4        int[] freq = new int[100001];
+5        int[][] map = new int[100001][3];
+6        for (int i = 0; i < n; i++) {
+7            if (freq[nums[i]] < 3) {
+8                map[nums[i]][freq[nums[i]]] = i;
+9                freq[nums[i]]++;
+10            } 
+11            else {
+12                map[nums[i]][0] = map[nums[i]][1];
+13                map[nums[i]][1] = map[nums[i]][2];
+14                map[nums[i]][2] = i;
+15            }
+16            min = freq[nums[i]] >= 3 ? Math.min(min, 2 * (i - map[nums[i]][0])) 
+17            : min;
+18        }
+19        return min == Integer.MAX_VALUE ? -1 : min;
+20    }
+21}
