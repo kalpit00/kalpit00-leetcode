806da260130c02f8c4c9b7957ee947f59c48f524
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-04-09 21:22:06 -0400

    File updated at 4/9/2026, 9:22:06 PM

--- leetcode-sub/3740-MinimumDistanceBetweenThreeEqualElementsI/3740-MinimumDistanceBetweenThreeEqualElementsI.java
+++ leetcode-sub/3740-MinimumDistanceBetweenThreeEqualElementsI/3740-MinimumDistanceBetweenThreeEqualElementsI.java
@@ -1,25 +1,22 @@
-// Last updated: 4/9/2026, 9:20:37 PM
+// Last updated: 4/9/2026, 9:22:06 PM
 1class Solution {
 2    public int minimumDistance(int[] nums) {
 3        int n = nums.length, min = Integer.MAX_VALUE;
 4        int[] freq = new int[101];
 5        int[][] map = new int[101][3];
 6        for (int i = 0; i < n; i++) {
-7            int count = freq[nums[i]];
-8            if (count < 3) {
-9                map[nums[i]][count] = i;
-10                freq[nums[i]]++;
-11                if (freq[nums[i]] == 3) {
-12                    min = Math.min(min, 2 * (i - map[nums[i]][0]));
-13                }
-14            } 
-15            else {
-16                map[nums[i]][0] = map[nums[i]][1];
-17                map[nums[i]][1] = map[nums[i]][2];
-18                map[nums[i]][2] = i;
-19                min = Math.min(min, 2 * (i - map[nums[i]][0]));
-20            }
-21        }
-22        return min == Integer.MAX_VALUE ? -1 : min;
-23    }
-24}
+7            if (freq[nums[i]] < 3) {
+8                map[nums[i]][freq[nums[i]]] = i;
+9                freq[nums[i]]++;
+10            } 
+11            else {
+12                map[nums[i]][0] = map[nums[i]][1];
+13                map[nums[i]][1] = map[nums[i]][2];
+14                map[nums[i]][2] = i;
+15            }
+16            min = freq[nums[i]] >= 3 ? Math.min(min, 2 * (i - map[nums[i]][0])) 
+17            : min;
+18        }
+19        return min == Integer.MAX_VALUE ? -1 : min;
+20    }
+21}
