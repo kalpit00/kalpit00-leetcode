df4ed7b5cfdabd7a83e788f44ba0bfa04bfff555
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-04-10 16:41:26 -0400

    File updated at 4/10/2026, 4:41:26 PM

--- leetcode-sub/1888-MinimumNumberofFlipstoMaketheBinaryStringAlternating/1888-MinimumNumberofFlipstoMaketheBinaryStringAlternating.java
+++ leetcode-sub/1888-MinimumNumberofFlipstoMaketheBinaryStringAlternating/1888-MinimumNumberofFlipstoMaketheBinaryStringAlternating.java
@@ -1,21 +1,23 @@
-// Last updated: 3/6/2026, 9:16:56 PM
+// Last updated: 4/10/2026, 4:41:26 PM
 1class Solution {
-2    public int minFlips(String s) {
-3        char[] arr = s.toCharArray();
-4        int count1 = 0, count2 = 0, min1 = 0, min2 = 0, n = arr.length;
-5        for (int i = 0; i < n; i++) {
-6            if (i % 2 == arr[i] - '0') { // if parity of i matches 0 or 1!
-7                count1++; // count parity matches!
-8                min1++; // i.e, if 0 is on even indices and 1 is on odd indices!
-9            }
-10            else { // also use a 2nd parity match counter to reflect min
-11                count2++;
-12                min2++;
-13            } // min each with the other "global" counter
-14            min1 = Math.min(min1, count2);
-15            min2 = Math.min(min2, count1);
-16        }
-17        int min = Math.min(count1, count2);
-18        return n % 2 == 0 ? min : Math.min(min, Math.min(min1, min2));
-19    }
-20}
+2    public int minimumFlips(int n) {
+3        char[] s = Integer.toBinaryString(n).toCharArray();
+4        char[] rev = s.clone();
+5        reverse(rev);
+6        int count = 0;
+7        for (int i = 0; i < s.length; i++) {
+8            count += s[i] != rev[i] ? 1 : 0;
+9        }
+10        return count;
+11    }
+12    private void reverse(char[] s) {
+13        int i = 0, j = s.length - 1;
+14        while (i < j) {
+15            char temp = s[i];
+16            s[i] = s[j];
+17            s[j] = temp;
+18            i++;
+19            j--;
+20        }
+21    }
+22}
