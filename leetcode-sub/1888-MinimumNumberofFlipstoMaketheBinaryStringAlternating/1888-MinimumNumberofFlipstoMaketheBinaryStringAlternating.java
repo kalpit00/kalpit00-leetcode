318d853f5e91ebf6d0b2c86afece74b90c3b901c
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-04-10 22:43:37 -0400

    File updated at 4/10/2026, 10:43:37 PM

--- leetcode-sub/1888-MinimumNumberofFlipstoMaketheBinaryStringAlternating/1888-MinimumNumberofFlipstoMaketheBinaryStringAlternating.java
+++ leetcode-sub/1888-MinimumNumberofFlipstoMaketheBinaryStringAlternating/1888-MinimumNumberofFlipstoMaketheBinaryStringAlternating.java
@@ -1,4 +1,4 @@
-// Last updated: 4/10/2026, 10:21:31 PM
+// Last updated: 4/10/2026, 10:43:37 PM
 1class Solution {
 2    public int minArraySum(int[] nums, int k, int op1, int op2) {
 3        int n = nums.length;
@@ -6,31 +6,30 @@
 5        return solve(nums, 0, n, op1, op2, dp, k);
 6    }
 7    private int solve(int[] nums, int i, int n, int op1, int op2,
-8    Integer[][][] dp, int k) {
+8        Integer[][][] dp, int k) {
 9        if (i >= n) {
 10            return 0;
 11        }
 12        if (dp[i][op1][op2] != null) {
 13            return dp[i][op1][op2];
-14        }
-15        int min = Integer.MAX_VALUE;
-16        min = nums[i] + solve(nums, i + 1, n, op1, op2, dp, k);
-17        if (op1 > 0) {
-18            min = Math.min(min, (nums[i] + 1) / 2 + solve(nums, i + 1, n, op1 - 1, op2, dp, k));
-19        }        
-20        if (op2 > 0 && k <= nums[i]) {
-21            min = Math.min(min, (nums[i] - k) + solve(nums, i + 1, n, op1, op2 - 1, dp, k));
-22        }        
-23        if (op1 > 0 && op2 > 0) {
-24            int val = (nums[i] + 1) / 2;
-25            if (k <= val) {
-26                min = Math.min(min, (val - k) + solve(nums, i + 1, n, op1 - 1, op2 - 1, dp, k));
-27            }
-28            if (k <= nums[i]) {
-29                val = nums[i] - k;
-30                min = Math.min(min, (val + 1) / 2 + solve(nums, i + 1, n, op1 - 1, op2 - 1, dp, k));
-31            }
-32        }        
-33        return dp[i][op1][op2] = min;
-34    }
-35}
+14        }        
+15        int notTake = nums[i] + solve(nums, i + 1, n, op1, op2, dp, k);        
+16        int take1 = (op1 > 0) ? solve(nums, i + 1, n, op1 - 1, op2, dp, k) + 
+17        (nums[i] + 1) / 2 : Integer.MAX_VALUE;
+18        int take2 = (op2 > 0 && k <= nums[i]) ? (nums[i] - k) + 
+19        solve(nums, i + 1, n, op1, op2 - 1, dp, k) : Integer.MAX_VALUE;
+20        int takeBoth = Integer.MAX_VALUE;
+21        if (op1 > 0 && op2 > 0) {
+22            int val = (nums[i] + 1) / 2;
+23            if (k <= val) {
+24                takeBoth = Math.min(takeBoth, (val - k) + solve(nums, i + 1, n, op1 - 1, op2 - 1, dp, k));
+25            }            
+26            if (k <= nums[i]) {
+27                val = nums[i] - k;
+28                takeBoth = Math.min(takeBoth, (val + 1) / 2 + solve(nums, i + 1, n, op1 - 1, op2 - 1, dp, k));
+29            }
+30        }        
+31        int min = Math.min(notTake, Math.min(take1, Math.min(take2, takeBoth)));
+32        return dp[i][op1][op2] = min;
+33    }
+34}
