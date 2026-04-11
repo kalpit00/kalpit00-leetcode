172b9bff8e6f4f863f5fad67011114b5d5468a92
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-04-10 22:21:31 -0400

    File updated at 4/10/2026, 10:21:31 PM

--- leetcode-sub/1888-MinimumNumberofFlipstoMaketheBinaryStringAlternating/1888-MinimumNumberofFlipstoMaketheBinaryStringAlternating.java
+++ leetcode-sub/1888-MinimumNumberofFlipstoMaketheBinaryStringAlternating/1888-MinimumNumberofFlipstoMaketheBinaryStringAlternating.java
@@ -1,23 +1,36 @@
-// Last updated: 4/10/2026, 4:41:26 PM
+// Last updated: 4/10/2026, 10:21:31 PM
 1class Solution {
-2    public int minimumFlips(int n) {
-3        char[] s = Integer.toBinaryString(n).toCharArray();
-4        char[] rev = s.clone();
-5        reverse(rev);
-6        int count = 0;
-7        for (int i = 0; i < s.length; i++) {
-8            count += s[i] != rev[i] ? 1 : 0;
-9        }
-10        return count;
-11    }
-12    private void reverse(char[] s) {
-13        int i = 0, j = s.length - 1;
-14        while (i < j) {
-15            char temp = s[i];
-16            s[i] = s[j];
-17            s[j] = temp;
-18            i++;
-19            j--;
-20        }
-21    }
-22}
+2    public int minArraySum(int[] nums, int k, int op1, int op2) {
+3        int n = nums.length;
+4        Integer[][][] dp = new Integer[n + 1][op1 + 1][op2 + 1];
+5        return solve(nums, 0, n, op1, op2, dp, k);
+6    }
+7    private int solve(int[] nums, int i, int n, int op1, int op2,
+8    Integer[][][] dp, int k) {
+9        if (i >= n) {
+10            return 0;
+11        }
+12        if (dp[i][op1][op2] != null) {
+13            return dp[i][op1][op2];
+14        }
+15        int min = Integer.MAX_VALUE;
+16        min = nums[i] + solve(nums, i + 1, n, op1, op2, dp, k);
+17        if (op1 > 0) {
+18            min = Math.min(min, (nums[i] + 1) / 2 + solve(nums, i + 1, n, op1 - 1, op2, dp, k));
+19        }        
+20        if (op2 > 0 && k <= nums[i]) {
+21            min = Math.min(min, (nums[i] - k) + solve(nums, i + 1, n, op1, op2 - 1, dp, k));
+22        }        
+23        if (op1 > 0 && op2 > 0) {
+24            int val = (nums[i] + 1) / 2;
+25            if (k <= val) {
+26                min = Math.min(min, (val - k) + solve(nums, i + 1, n, op1 - 1, op2 - 1, dp, k));
+27            }
+28            if (k <= nums[i]) {
+29                val = nums[i] - k;
+30                min = Math.min(min, (val + 1) / 2 + solve(nums, i + 1, n, op1 - 1, op2 - 1, dp, k));
+31            }
+32        }        
+33        return dp[i][op1][op2] = min;
+34    }
+35}
