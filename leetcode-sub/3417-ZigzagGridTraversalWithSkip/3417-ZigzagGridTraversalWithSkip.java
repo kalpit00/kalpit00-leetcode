b4f21df3fca1ff5c7c3a85bfc4162cfa3c0f0f25
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-04-15 19:14:01 -0400

    File updated at 4/15/2026, 7:14:01 PM

--- leetcode-sub/3417-ZigzagGridTraversalWithSkip/3417-ZigzagGridTraversalWithSkip.java
+++ leetcode-sub/3417-ZigzagGridTraversalWithSkip/3417-ZigzagGridTraversalWithSkip.java
@@ -1,25 +1,24 @@
-// Last updated: 10/4/2025, 6:27:34 AM
-class Solution {
-    public List<Integer> zigzagTraversal(int[][] grid) {
-        ArrayList<Integer> l=new ArrayList<>();
-        int m=grid.length,n=grid[0].length;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j+=2){
-                l.add(grid[i][j]);
-            }
-            i++;
-            if(i==m){
-                break;
-            }
-            int j=n-2;
-            if(n%2==0){
-                j=n-1;
-            }
-            for(;j>=0;j-=2){
-                l.add(grid[i][j]);
-                
-            }
-        }
-        return l;
-    }
-}
+// Last updated: 4/15/2026, 7:14:01 PM
+1class Solution {
+2    public List<Integer> zigzagTraversal(int[][] grid) {
+3        List<Integer> list = new ArrayList<>();
+4        int m = grid.length, n = grid[0].length;
+5        boolean flag = true, dir = true;
+6        for (int i = 0; i < m; i++) {
+7            for (int j = 0; j < n; j++) {
+8                if (flag) {
+9                    if (dir) {
+10                        list.add(grid[i][j]);
+11                    }
+12                    else {
+13                        list.add(grid[i][n - j - 1]);
+14                    }
+15                }
+16                flag = !flag;
+17         
+18            }
+19            dir = !dir;
+20        }
+21        return list;
+22    }
+23}
