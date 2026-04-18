f880b70d70e773473740feb6e6ec3f391eaef8be
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-04-17 20:42:55 -0400

    File updated at 4/17/2026, 8:42:55 PM

--- leetcode-sub/2399-CheckDistancesBetweenSameLetters/2399-CheckDistancesBetweenSameLetters.java
+++ leetcode-sub/2399-CheckDistancesBetweenSameLetters/2399-CheckDistancesBetweenSameLetters.java
@@ -1,17 +1,16 @@
-// Last updated: 4/17/2026, 8:42:04 PM
-1class Solution {
-2    public boolean checkDistances(String str, int[] distance) {
-3        Integer[] map = new Integer[26];
-4        char[] s = str.toCharArray();
-5        int n = s.length;
-6        for (int i = 0; i < n; i++) {
-7            if (map[s[i] - 'a'] != null) {
-8                if (i - map[s[i] - 'a'] - 1 != distance[s[i] - 'a']) {
-9                    return false;
-10                }
-11            }
-12            map[s[i] - 'a'] = i;
-13        }
-14        return true;
-15    }
-16}
+// Last updated: 4/17/2026, 8:42:55 PM
+class Solution {
+    public boolean checkDistances(String s, int[] distance) {
+        int[] first = new int[26];
+        for(int i = 0;i < 26;i++){
+            first[i] = -1;}
+        for(int i = 0;i < s.length();i++){
+            int idx = s.charAt(i) - 'a';
+            if(first[idx] == -1){
+                first[idx] = i;
+            } else{
+                int dist = i-first[idx]-1;
+                if(dist != distance[idx]){
+                    return false;}}}
+               return true;
+    }}
