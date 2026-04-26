c233a2a3e32d6feb1cb29f4b21ea428d2e3362dc
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-04-25 21:41:51 -0400

    File updated at 4/25/2026, 9:41:51 PM

--- leetcode-sub/1559-DetectCyclesin2DGrid/1559-DetectCyclesin2DGrid.java
+++ leetcode-sub/1559-DetectCyclesin2DGrid/1559-DetectCyclesin2DGrid.java
@@ -1,4 +1,4 @@
-// Last updated: 4/25/2026, 9:40:05 PM
+// Last updated: 4/25/2026, 9:41:51 PM
 1class Solution {
 2    int[][] dir = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 3    public boolean containsCycle(char[][] grid) {
@@ -7,7 +7,7 @@
 6        for (int i = 0; i < m; i++) {
 7            for (int j = 0; j < n; j++) {
 8                if (!visited[i][j]) {
-9                    if (dfs(i, j, m, n, visited, grid, -1, -1, 0)) {
+9                    if (dfs(i, j, m, n, visited, grid, new int[]{-1, -1}, 0)) {
 10                        return true;
 11                    }
 12                }
@@ -16,7 +16,7 @@
 15        return false;
 16    }
 17    private boolean dfs(int i, int j, int m, int n, boolean[][] visited,
-18    char[][] grid, int px, int py, int steps) {
+18    char[][] grid, int[] parent, int steps) {
 19        if (visited[i][j]) {
 20            return steps >= 4;
 21        }
@@ -24,9 +24,9 @@
 23        for (int[] d : dir) {
 24            int r = i + d[0], c = j + d[1];
 25            if (r < 0 || r >= m || c < 0 || c >= n) continue;
-26            if (r == px && c == py) continue;
+26            if (r == parent[0] && c == parent[1]) continue;
 27            if (grid[r][c] == grid[i][j]) {
-28                if (dfs(r, c, m, n, visited, grid, i, j, steps + 1)) {
+28                if (dfs(r, c, m, n, visited, grid, new int[]{i, j}, steps+1)) {
 29                    return true;
 30                }
 31            }
