cc7d43014eadf2b5e64a75f7e37cd5a177a09a29
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-04-28 15:37:34 -0400

    File updated at 4/28/2026, 3:37:34 PM

--- leetcode-sub/2595-NumberofEvenandOddBits/2595-NumberofEvenandOddBits.java
+++ leetcode-sub/2595-NumberofEvenandOddBits/2595-NumberofEvenandOddBits.java
@@ -1,9 +1,9 @@
-// Last updated: 4/28/2026, 3:37:04 PM
+// Last updated: 4/28/2026, 3:37:34 PM
 1class Solution {
 2    public int[] evenOddBit(int num) {
 3        char[] s = Integer.toBinaryString(num).toCharArray();
 4        int n = s.length, even = 0, odd = 0;
-5        for (int i = n - 1; i >= 0; i--) {
+5        for (int i = 0; i < n; i++) {
 6            even += (n - i - 1) % 2 == 0 && s[i] == '1' ? 1 : 0;
 7            odd += (n - i - 1) % 2 == 1 && s[i] == '1' ? 1 : 0;
 8        }
