a80a921a0488602789be9704063f5b45c7ee0609
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-05-01 21:57:13 -0400

    File updated at 5/1/2026, 9:57:13 PM

--- leetcode-sub/788-RotatedDigits/788-RotatedDigits.java
+++ leetcode-sub/788-RotatedDigits/788-RotatedDigits.java
@@ -1,20 +1,26 @@
-// Last updated: 5/1/2026, 9:54:58 PM
-1class Solution {
-2    public int rotatedDigits(int n) {
-3        int res = 0;
-4        outer:
-5        for (int i = 1; i <= n; i++) {
-6            int num = i, count = 0;
-7            while (num > 0) {
-8                int d = num % 10;
-9                if (d == 3 || d == 4 || d == 7) {
-10                    continue outer;
-11                }
-12                count += d == 2 || d == 5 || d == 6 || d == 9 ? 1 : 0;
-13                num /= 10;
-14            }
-15            res += count > 0 ? 1 : 0;
-16        }
-17        return res;
-18    }
-19}
+// Last updated: 5/1/2026, 9:57:13 PM
+class Solution {
+    public int rotatedDigits(int n) {
+        int count=0;
+        for(int i=1;i<=n;i++){
+            if(isValid(i)){
+                count++;
+            }
+        }
+        return count;
+        
+    }
+    public boolean isValid(int num){
+        boolean valid=false;
+        while(num>0){
+            if(num%10==2||num%10==5||num%10==6||num%10==9){
+                valid=true;
+            }
+            if(num%10==3||num%10==4||num%10==7){
+                return false;
+            }
+            num/=10;
+        }
+        return valid;
+    }
+}
