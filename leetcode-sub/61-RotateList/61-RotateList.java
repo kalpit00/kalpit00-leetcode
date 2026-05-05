74e90dd5bf6254ccca3c79127f63fd1590b2f579
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-05-04 22:07:50 -0400

    File updated at 5/4/2026, 10:07:50 PM

--- leetcode-sub/61-RotateList/61-RotateList.java
+++ leetcode-sub/61-RotateList/61-RotateList.java
@@ -1,50 +1,51 @@
-// Last updated: 5/26/2025, 4:58:55 AM
-class Solution {
-    public ListNode rotateRight(ListNode head, int k) {
-        if (head == null || head.next == null || k == 0) {
-            return head;
-        }
-        int n = 0;
-        ListNode temp = head;
-        while (temp != null) {
-            n++;
-            temp = temp.next;
-        }
-        k = k % n;
-        if (k == 0) return head;
-        head = reverseBetween(head, 1, n);
-        head = reverseBetween(head, 1, k);
-        head = reverseBetween(head, k + 1, n);
-        return head;
-    }
-
-    public ListNode reverseBetween(ListNode head, int left, int right) {
-        if (head == null || left == right) {
-            return head;
-        }
-        ListNode dummy = new ListNode(-1), prev = dummy;
-        dummy.next = head;
-        for (int i = 1; i < left; i++) {
-            prev = prev.next;
-        }
-        ListNode curr = prev.next, after = curr;
-        for (int i = left; i <= right; i++) {
-            after = after.next;
-        }
-        ListNode ans = reverseList(curr, after);
-        prev.next = ans;
-        curr.next = after;
-        return dummy.next;
-    }
-
-    public ListNode reverseList(ListNode head, ListNode after) {
-        ListNode prev = null, curr = head;
-        while (curr != after) {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
-    }
-}
+// Last updated: 5/4/2026, 10:07:50 PM
+1class Solution {
+2    public ListNode rotateRight(ListNode head, int k) {
+3        if (head == null || head.next == null || k == 0) {
+4            return head;
+5        }
+6        int n = 0;
+7        ListNode temp = head;
+8        while (temp != null) {
+9            n++;
+10            temp = temp.next;
+11        }
+12        k = k % n;
+13        if (k == 0) return head;
+14        head = reverseBetween(head, 1, n);
+15        head = reverseBetween(head, 1, k);
+16        head = reverseBetween(head, k + 1, n);
+17        return head;
+18    }
+19
+20    public ListNode reverseBetween(ListNode head, int left, int right) {
+21        if (head == null || left == right) {
+22            return head;
+23        }
+24        ListNode dummy = new ListNode(-1), prev = dummy;
+25        dummy.next = head;
+26        for (int i = 1; i < left; i++) {
+27            prev = prev.next;
+28        }
+29        ListNode curr = prev.next, after = curr;
+30        for (int i = left; i <= right; i++) {
+31            after = after.next;
+32        }
+33        ListNode ans = reverseList(curr, after);
+34        prev.next = ans;
+35        curr.next = after;
+36        return dummy.next;
+37    }
+38
+39    public ListNode reverseList(ListNode head, ListNode after) {
+40        ListNode prev = null, curr = head;
+41        while (curr != after) {
+42            ListNode next = curr.next;
+43            curr.next = prev;
+44            prev = curr;
+45            curr = next;
+46        }
+47        return prev;
+48    }
+49}
+50
