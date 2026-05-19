af6b0c30e53f7b45a1cec20b1070d4cd739758ed
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-05-19 06:48:21 -0400

    File updated at 5/19/2026, 6:48:20 AM

--- leetcode-sub/1252-CellswithOddValuesinaMatrix/1252-CellswithOddValuesinaMatrix.java
+++ leetcode-sub/1252-CellswithOddValuesinaMatrix/1252-CellswithOddValuesinaMatrix.java
@@ -1,26 +1,18 @@
-// Last updated: 5/19/2026, 6:47:38 AM
+// Last updated: 5/19/2026, 6:48:20 AM
 1class Solution {
 2    public int oddCells(int m, int n, int[][] indices) {
-3        boolean[] row = new boolean[m];
-4        boolean[] col = new boolean[n];
-5        
-6        for (int i = 0; i < indices.length; i++) {
-7            row[indices[i][0]] ^= true;
-8            col[indices[i][1]] ^= true;
-9        }
-10        int rCount = 0;
-11        int cCount = 0;
-12        
-13        for (int i = 0; i < m; i++) {
-14            if (row[i]) {
-15                rCount++;
-16            }
-17        }
-18        for (int i = 0; i < n; i++) {
-19            if (col[i]) {
-20                cCount++;
-21            }
-22        }
-23        return rCount*n + cCount*m - 2*rCount*cCount;
-24    }
-25}
+3        int[] rowSum = new int[m], colSum = new int[n];
+4        for (int[] arr : indices) {
+5            int i = arr[0], j = arr[1];
+6            rowSum[i]++;
+7            colSum[j]++;
+8        }
+9        int count = 0;
+10        for (int i = 0; i < m; i++) {
+11            for (int j = 0; j < n; j++) {
+12                count += (rowSum[i] + colSum[j]) % 2 == 1 ? 1 : 0;
+13            }
+14        }
+15        return count;
+16    }
+17}
