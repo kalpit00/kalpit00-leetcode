31d609e2654495f8d12b37eb287f4d9224495435
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-05-20 01:53:00 -0400

    File updated at 5/20/2026, 1:53:00 AM

--- leetcode-sub/2657-FindthePrefixCommonArrayofTwoArrays/2657-FindthePrefixCommonArrayofTwoArrays.java
+++ leetcode-sub/2657-FindthePrefixCommonArrayofTwoArrays/2657-FindthePrefixCommonArrayofTwoArrays.java
@@ -1,20 +1,14 @@
-// Last updated: 5/20/2026, 1:50:14 AM
+// Last updated: 5/20/2026, 1:53:00 AM
 1class Solution {
 2    public int[] findThePrefixCommonArray(int[] A, int[] B) {
-3        int n = A.length, count = 0;
-4        long mask = 0;
+3        int n = A.length;
+4        long maskA = 0, maskB = 0;
 5        int[] res = new int[n];
 6        for (int i = 0; i < n; i++) {
-7            if ((mask & (1L << A[i])) != 0) {
-8                count++;
-9            }
-10            mask |= (1L << A[i]);
-11            if ((mask & (1L << B[i])) != 0) {
-12                count++;
-13            }
-14            mask |= (1L << B[i]);
-15            res[i] = count;
-16        }
-17        return res;
-18    }
-19}
+7            maskA |= (1L << A[i]);
+8            maskB |= (1L << B[i]);
+9            res[i] = Long.bitCount(maskA & maskB);
+10        }
+11        return res;
+12    }
+13}
