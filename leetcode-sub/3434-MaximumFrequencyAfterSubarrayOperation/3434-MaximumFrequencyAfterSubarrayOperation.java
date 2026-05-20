eb1f993462028b1ec15379b57aa416c01f7af032
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-05-20 19:42:24 -0400

    File updated at 5/20/2026, 7:42:23 PM

--- leetcode-sub/3434-MaximumFrequencyAfterSubarrayOperation/3434-MaximumFrequencyAfterSubarrayOperation.java
+++ leetcode-sub/3434-MaximumFrequencyAfterSubarrayOperation/3434-MaximumFrequencyAfterSubarrayOperation.java
@@ -1,35 +1,25 @@
-// Last updated: 12/30/2025, 8:03:28 AM
-class Solution {
-    public int maxFrequency(int[] nums, int k) {
-        int[] freq = new int[51];
-        int ans = 0, cnt = 0;
-        for(int n : nums){
-            freq[n] = Math.max(freq[n], cnt) + 1;
-            if(n == k){
-                ans++;
-                cnt++;
-            }
-            ans = Math.max(ans, freq[n]);
-        }
-        return ans;
-
-        /*
-        int orig = 0, ans = 0;
-        for(int n : nums)
-            if(n == k) 
-                orig++;
-        for(int i = 1; i <= 50; i++){
-            if(i == k)
-                continue;
-            int cnt = 0, mx = 0;
-            for(int n : nums){
-                cnt += (n == i)? 1: (n == k)? -1: 0;
-                cnt = Math.max(cnt, 0);
-                mx = Math.max(mx, cnt);
-            }
-            ans = Math.max(ans, mx);
-        }
-        return orig + ans;
-        */
-    }
-}
+// Last updated: 5/20/2026, 7:42:23 PM
+1class Solution {
+2    public int maxFrequency(int[] nums, int k) {
+3        int n = nums.length, count = 0, max = 0;
+4        Set<Integer> set = new HashSet<>();
+5        for (int num : nums) {
+6            set.add(num);
+7            count += num == k ? 1 : 0;
+8        }
+9        for (int x : set) {
+10            if (x == k) continue;
+11            max = Math.max(max, kadane(nums, x, k));
+12        }
+13        return count + max;
+14    }
+15    public int kadane(int[] nums, int x, int k) {
+16        int sum = 0, max = Integer.MIN_VALUE;
+17        for (int num : nums) {
+18            sum += num == x ? 1 : num == k ? -1 : 0;
+19            max = Math.max(max, sum);
+20            sum = sum < 0 ? 0 : sum;
+21        }
+22        return max;
+23    }
+24}
