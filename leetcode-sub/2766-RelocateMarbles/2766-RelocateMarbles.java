cea07ed3059e47578765ac9a89075be40bda12a4
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-05-20 01:32:55 -0400

    File updated at 5/20/2026, 1:32:54 AM

--- leetcode-sub/2766-RelocateMarbles/2766-RelocateMarbles.java
+++ leetcode-sub/2766-RelocateMarbles/2766-RelocateMarbles.java
@@ -1,16 +1,17 @@
-// Last updated: 5/20/2026, 1:30:55 AM
+// Last updated: 5/20/2026, 1:32:54 AM
 1class Solution {
 2    public List<Integer> relocateMarbles(int[] nums, int[] moveFrom, int[] moveTo) {
-3        TreeSet<Integer> set = new TreeSet<>();
-4        List<Integer> res = new ArrayList<>();
-5        int n = nums.length, m = moveFrom.length;
-6        for (int i = 0; i < n; i++) {
-7            set.add(nums[i]);
-8        }
-9        for (int i = 0; i < m; i++) {
-10            set.remove(moveFrom[i]);
-11            set.add(moveTo[i]);
-12        }
-13        return new ArrayList<>(set);
-14    }
-15}
+3        Set<Integer> set = new HashSet<>();
+4        int n = nums.length, m = moveFrom.length;
+5        for (int i = 0; i < n; i++) {
+6            set.add(nums[i]);
+7        }
+8        for (int i = 0; i < m; i++) {
+9            set.remove(moveFrom[i]);
+10            set.add(moveTo[i]);
+11        }
+12        List<Integer> res = new ArrayList<>(set);
+13        Collections.sort(res);
+14        return res;
+15    }
+16}
