c3e76b44cf3bf289b34147babbdbc2b1dffe6b4b
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-05-24 02:32:42 -0400

    File updated at 5/24/2026, 2:32:41 AM

--- leetcode-sub/1340-JumpGameV/1340-JumpGameV.java
+++ leetcode-sub/1340-JumpGameV/1340-JumpGameV.java
@@ -1,37 +1,26 @@
-// Last updated: 2/17/2026, 2:55:59 AM
-class Solution {
-    int[] dp;
-    int n;
-    
-    public int maxJumps(int[] arr, int d) {
-        n = arr.length;
-        dp = new int[n];
-        
-        int max = 1;
-        for(int i = 0; i < n; i++) {
-            max = Math.max(max, dfs(arr, i, d));
-        }
-        
-        return max;
-    }
-    
-    private int dfs(int[] arr, int i, int d) {
-        if(dp[i] != 0) return dp[i];
-        
-        int max = 1; // at least itself
-        
-        // check left
-        for(int j = i - 1; j >= Math.max(0, i - d); j--) {
-            if(arr[j] >= arr[i]) break; // blocked
-            max = Math.max(max, 1 + dfs(arr, j, d));
-        }
-        
-        // check right
-        for(int j = i + 1; j <= Math.min(n - 1, i + d); j++) {
-            if(arr[j] >= arr[i]) break; // blocked
-            max = Math.max(max, 1 + dfs(arr, j, d));
-        }
-        
-        return dp[i] = max;
-    }
-}
+// Last updated: 5/24/2026, 2:32:41 AM
+1class Solution {
+2    public int maxJumps(int[] nums, int d) {
+3        int n = nums.length, max = 1;
+4        Integer[] dp = new Integer[n];
+5        for (int i = 0; i < n; i++) {
+6            max = Math.max(max, dfs(i, nums, dp, d, n));
+7        }
+8        return max;
+9    }
+10    private int dfs(int i, int[] nums, Integer[] dp, int d, int n) {
+11        if (dp[i] != null) {
+12            return dp[i];
+13        }
+14        int max = 1;
+15        for (int j = i + 1; j < n && j - i <= d; j++) {
+16            if (nums[j] >= nums[i]) break;
+17            max = Math.max(max, 1 + dfs(j, nums, dp, d, n));
+18        }
+19        for (int j = i - 1; j >= 0 && i - j <= d; j--) {
+20            if (nums[j] >= nums[i]) break;
+21            max = Math.max(max, 1 + dfs(j, nums, dp, d, n));
+22        }
+23        return dp[i] = max;
+24    }
+25}
