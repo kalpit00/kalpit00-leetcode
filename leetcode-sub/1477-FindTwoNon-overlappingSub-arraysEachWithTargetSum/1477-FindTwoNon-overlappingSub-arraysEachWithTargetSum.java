25dac80df3e22406e7b49dcbc2cba0e3faf70253
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-05-25 06:00:43 -0400

    File updated at 5/25/2026, 6:00:43 AM

--- leetcode-sub/1477-FindTwoNon-overlappingSub-arraysEachWithTargetSum/1477-FindTwoNon-overlappingSub-arraysEachWithTargetSum.java
+++ leetcode-sub/1477-FindTwoNon-overlappingSub-arraysEachWithTargetSum/1477-FindTwoNon-overlappingSub-arraysEachWithTargetSum.java
@@ -1,9 +1,9 @@
-// Last updated: 5/25/2026, 5:52:03 AM
+// Last updated: 5/25/2026, 6:00:43 AM
 1class Solution {
 2    public int minSumOfLengths(int[] nums, int target) {
 3        Map<Integer, Integer> map = new HashMap<>();
-4        int n = nums.length, sum = 0, min = Integer.MAX_VALUE;
-5        map.put(0, -1);
+4        int n = nums.length, min = Integer.MAX_VALUE;
+5        map.put(0, -1); // put 0 with -1, so j == -1 means only 1 subarr found
 6        int[] dp = new int[n], pre = new int[n];
 7        pre[0] = nums[0];
 8        for (int i = 1; i < n; i++) {
@@ -13,10 +13,10 @@
 12            dp[i] = i > 0 ? dp[i - 1] : Integer.MAX_VALUE;
 13            if (map.containsKey(pre[i] - target)) {
 14                int j = map.get(pre[i] - target);
-15                dp[i] = Math.min(dp[i], i - j);
-16                if (j != -1 && dp[j] != Integer.MAX_VALUE) {
+15                dp[i] = Math.min(dp[i], i - j); // subarr[j .. i] has sum == k
+16                if (j != -1 && dp[j] != Integer.MAX_VALUE) { // 2 subarrs exist
 17                    min = Math.min(min, dp[j] + i - j);
-18                }
+18                } // sum the two subarray lengths, dp[j] is min subarr
 19            }
 20            map.put(pre[i], i);
 21        }
