b4dfb5e1038d131597b7282f1615e5bbbb927e5b
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-05-25 22:07:37 -0400

    File updated at 5/25/2026, 10:07:37 PM

--- leetcode-sub/1477-FindTwoNon-overlappingSub-arraysEachWithTargetSum/1477-FindTwoNon-overlappingSub-arraysEachWithTargetSum.java
+++ leetcode-sub/1477-FindTwoNon-overlappingSub-arraysEachWithTargetSum/1477-FindTwoNon-overlappingSub-arraysEachWithTargetSum.java
@@ -1,32 +1,31 @@
-// Last updated: 5/25/2026, 9:47:58 PM
-1class Solution {
-2    public int minSumOfLengths(int[] nums, int target) {
-3        int n = nums.length, sum = 0, j = 0, 
-4        min = Integer.MAX_VALUE, res = Integer.MAX_VALUE;
-5        int[] pre = new int[n], suf = new int[n];
-6        for (int i = 0; i < n; i++) {
-7            sum += nums[i];
-8            while (j <= i && sum > target) {
-9                sum -= nums[j++];
-10            }
-11            pre[i] = (sum == target) ? (min = Math.min(min, i - j + 1)) : min;
-12        }
-13        min = Integer.MAX_VALUE;
-14        sum = 0;
-15        j = n - 1;
-16        for (int i = n - 1; i >= 0; i--) { 
-17            sum += nums[i];           
-18            while (j > i && sum > target) {
-19                sum -= nums[j--];
-20            }
-21            suf[i] = (sum == target) ? (min = Math.min(min, j - i + 1)) : min;
-22        }
-23        for (int i = 0; i < n - 1; i++) {
-24            if (pre[i] == Integer.MAX_VALUE || suf[i+1] == Integer.MAX_VALUE) {
-25                continue;
-26            }
-27            res = Math.min(res, pre[i] + suf[i + 1]);
-28        }
-29        return res == Integer.MAX_VALUE ? -1 : res;
-30    }
-31}
+// Last updated: 5/25/2026, 10:07:37 PM
+class Solution {
+    public int minSumOfLengths(int[] arr, int target) {
+       int[] min_len = new int[arr.length];
+        Arrays.fill(min_len, Integer.MAX_VALUE);
+        
+        int sum = 0;
+        int start = 0;
+        int res = Integer.MAX_VALUE;
+        int min = Integer.MAX_VALUE;
+        
+        for(int end = 0; end < arr.length; end++){
+            sum += arr[end];
+            
+            while(sum > target){
+                sum -= arr[start++];
+            }
+            
+            if(sum == target){
+                int cur_len = end-start+1;
+                if(start > 0 && min_len[start-1] != Integer.MAX_VALUE){
+                    res = Math.min(res, cur_len + min_len[start-1]);
+                }
+                min = Math.min(min, cur_len);
+            }
+            min_len[end] = min;
+        }
+        
+        return res == Integer.MAX_VALUE ? -1 : res; 
+    }
+}
