2330d03f7caca0e029c57b505332c3a484b3d70f
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-05-25 21:47:58 -0400

    File updated at 5/25/2026, 9:47:58 PM

--- leetcode-sub/1477-FindTwoNon-overlappingSub-arraysEachWithTargetSum/1477-FindTwoNon-overlappingSub-arraysEachWithTargetSum.java
+++ leetcode-sub/1477-FindTwoNon-overlappingSub-arraysEachWithTargetSum/1477-FindTwoNon-overlappingSub-arraysEachWithTargetSum.java
@@ -1,25 +1,32 @@
-// Last updated: 5/25/2026, 6:00:43 AM
+// Last updated: 5/25/2026, 9:47:58 PM
 1class Solution {
 2    public int minSumOfLengths(int[] nums, int target) {
-3        Map<Integer, Integer> map = new HashMap<>();
-4        int n = nums.length, min = Integer.MAX_VALUE;
-5        map.put(0, -1); // put 0 with -1, so j == -1 means only 1 subarr found
-6        int[] dp = new int[n], pre = new int[n];
-7        pre[0] = nums[0];
-8        for (int i = 1; i < n; i++) {
-9            pre[i] = pre[i - 1] + nums[i];
-10        }
-11        for (int i = 0; i < n; i++) {
-12            dp[i] = i > 0 ? dp[i - 1] : Integer.MAX_VALUE;
-13            if (map.containsKey(pre[i] - target)) {
-14                int j = map.get(pre[i] - target);
-15                dp[i] = Math.min(dp[i], i - j); // subarr[j .. i] has sum == k
-16                if (j != -1 && dp[j] != Integer.MAX_VALUE) { // 2 subarrs exist
-17                    min = Math.min(min, dp[j] + i - j);
-18                } // sum the two subarray lengths, dp[j] is min subarr
-19            }
-20            map.put(pre[i], i);
-21        }
-22        return min == Integer.MAX_VALUE ? -1 : min;
-23    }
-24}
+3        int n = nums.length, sum = 0, j = 0, 
+4        min = Integer.MAX_VALUE, res = Integer.MAX_VALUE;
+5        int[] pre = new int[n], suf = new int[n];
+6        for (int i = 0; i < n; i++) {
+7            sum += nums[i];
+8            while (j <= i && sum > target) {
+9                sum -= nums[j++];
+10            }
+11            pre[i] = (sum == target) ? (min = Math.min(min, i - j + 1)) : min;
+12        }
+13        min = Integer.MAX_VALUE;
+14        sum = 0;
+15        j = n - 1;
+16        for (int i = n - 1; i >= 0; i--) { 
+17            sum += nums[i];           
+18            while (j > i && sum > target) {
+19                sum -= nums[j--];
+20            }
+21            suf[i] = (sum == target) ? (min = Math.min(min, j - i + 1)) : min;
+22        }
+23        for (int i = 0; i < n - 1; i++) {
+24            if (pre[i] == Integer.MAX_VALUE || suf[i+1] == Integer.MAX_VALUE) {
+25                continue;
+26            }
+27            res = Math.min(res, pre[i] + suf[i + 1]);
+28        }
+29        return res == Integer.MAX_VALUE ? -1 : res;
+30    }
+31}
