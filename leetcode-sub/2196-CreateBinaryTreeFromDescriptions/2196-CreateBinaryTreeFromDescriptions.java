8a940b04bce48f741d72d8aaa0f338c46e9cb584
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-06-06 23:34:56 -0400

    File updated at 6/6/2026, 11:34:56 PM

--- leetcode-sub/2196-CreateBinaryTreeFromDescriptions/2196-CreateBinaryTreeFromDescriptions.java
+++ leetcode-sub/2196-CreateBinaryTreeFromDescriptions/2196-CreateBinaryTreeFromDescriptions.java
@@ -1,25 +1,27 @@
-// Last updated: 6/6/2026, 11:32:09 PM
+// Last updated: 6/6/2026, 11:34:56 PM
 1class Solution {
 2    public TreeNode createBinaryTree(int[][] descriptions) {
 3        boolean[] set = new boolean[100001];
-4        Map<Integer, TreeNode> map = new HashMap<>();
+4        TreeNode[] map = new TreeNode[100001];
 5        for (int[] description : descriptions) {
 6            int parent = description[0], child = description[1];
-7            map.putIfAbsent(parent, new TreeNode(parent));
-8            map.putIfAbsent(child, new TreeNode(child));
-9            set[child] = true;
-10            if (description[2] == 1) {
-11                map.get(parent).left = map.get(child);
-12            }
-13            else {
-14                map.get(parent).right = map.get(child);
-15            }
-16        }
-17        for (int node : map.keySet()) {
-18            if (!set[node]) {
-19                return map.get(node);
-20            }
-21        }
-22        return null;
-23    }
-24}
+7            map[parent] = map[parent] == null ? new TreeNode(parent) :
+8            map[parent];
+9            map[child] = map[child] == null ? new TreeNode(child) : map[child];
+10            set[child] = true;
+11            if (description[2] == 1) {
+12                map[parent].left = map[child];
+13            }
+14            else {
+15                map[parent].right = map[child];
+16            }
+17        }
+18        for (int i = 0; i < map.length; i++) {
+19            if (map[i] == null) continue;
+20            if (!set[i]) {
+21                return map[i];
+22            }
+23        }
+24        return null;
+25    }
+26}
