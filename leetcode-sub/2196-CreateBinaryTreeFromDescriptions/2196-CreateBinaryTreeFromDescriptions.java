85a9814192c4552338f6907efc1325bcf7ea50ed
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-06-06 23:32:09 -0400

    File updated at 6/6/2026, 11:32:09 PM

--- leetcode-sub/2196-CreateBinaryTreeFromDescriptions/2196-CreateBinaryTreeFromDescriptions.java
+++ leetcode-sub/2196-CreateBinaryTreeFromDescriptions/2196-CreateBinaryTreeFromDescriptions.java
@@ -1,13 +1,13 @@
-// Last updated: 6/6/2026, 11:30:40 PM
+// Last updated: 6/6/2026, 11:32:09 PM
 1class Solution {
 2    public TreeNode createBinaryTree(int[][] descriptions) {
-3        Map<Integer, TreeNode> map = new HashMap<>();
-4        Set<Integer> children = new HashSet<>();
+3        boolean[] set = new boolean[100001];
+4        Map<Integer, TreeNode> map = new HashMap<>();
 5        for (int[] description : descriptions) {
 6            int parent = description[0], child = description[1];
 7            map.putIfAbsent(parent, new TreeNode(parent));
 8            map.putIfAbsent(child, new TreeNode(child));
-9            children.add(child);
+9            set[child] = true;
 10            if (description[2] == 1) {
 11                map.get(parent).left = map.get(child);
 12            }
@@ -16,7 +16,7 @@
 15            }
 16        }
 17        for (int node : map.keySet()) {
-18            if (!children.contains(node)) {
+18            if (!set[node]) {
 19                return map.get(node);
 20            }
 21        }
