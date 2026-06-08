017f4a7433d49315a6727996eb8b48e6297bd296
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-06-07 20:30:18 -0400

    File updated at 6/7/2026, 8:30:17 PM

--- leetcode-sub/2161-PartitionArrayAccordingtoGivenPivot/2161-PartitionArrayAccordingtoGivenPivot.java
+++ leetcode-sub/2161-PartitionArrayAccordingtoGivenPivot/2161-PartitionArrayAccordingtoGivenPivot.java
@@ -1,19 +1,22 @@
-// Last updated: 6/7/2026, 8:30:01 PM
+// Last updated: 6/7/2026, 8:30:17 PM
 1class Solution {
 2    public int[] pivotArray(int[] nums, int pivot) {
-3        int n = nums.length, left = 0, right = n - 1;
+3        int n = nums.length, idx = 0, count = 0;
 4        int[] res = new int[n];
-5        for (int i = 0, j = n - 1; i < n; i++, j--) {
+5        for (int i = 0; i < n; i++) {
 6            if (nums[i] < pivot) {
-7                res[left++] = nums[i];
+7                res[idx++] = nums[i];
 8            }
-9            if (nums[j] > pivot) {
-10                res[right--] = nums[j];
-11            }
-12        }
-13        while (left <= right) {
-14            res[left++] = pivot;
-15        }
-16        return res;
-17    }
-18}
+9            count += nums[i] == pivot ? 1 : 0;
+10        }
+11        for (int i = 0; i < count; i++) {
+12            res[idx++] = pivot;
+13        }
+14        for (int i = 0; i < n; i++) {
+15            if (nums[i] > pivot) {
+16                res[idx++] = nums[i];
+17            }
+18        }
+19        return res;
+20    }
+21}
