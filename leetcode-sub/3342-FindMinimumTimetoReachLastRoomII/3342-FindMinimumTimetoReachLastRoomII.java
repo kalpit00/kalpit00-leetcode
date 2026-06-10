e4433312fe7223ab8136fcea9c6e1983f2f63ab7
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-06-09 20:00:57 -0400

    File updated at 6/9/2026, 8:00:56 PM

--- leetcode-sub/3342-FindMinimumTimetoReachLastRoomII/3342-FindMinimumTimetoReachLastRoomII.java
+++ leetcode-sub/3342-FindMinimumTimetoReachLastRoomII/3342-FindMinimumTimetoReachLastRoomII.java
@@ -1,27 +1,28 @@
-// Last updated: 5/8/2025, 1:52:45 AM
-class Solution {
-    public int minTimeToReach(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
-        boolean[][] visited = new boolean[m][n];
-        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> a[2] - b[2]);
-        queue.offer(new int[]{0, 0, 0, 1}); 
-        int[][] dir = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        while (!queue.isEmpty()) {
-            int[] node = queue.poll();
-            int x = node[0], y = node[1], parentMax = node[2], parity = node[3];
-            if (x == m - 1 && y == n - 1) {
-                return parentMax;
-            }
-            for (int[] d : dir) {
-                int r = x + d[0], c = y + d[1];
-                if (r >= 0 && r < m && c >= 0 && c < n && !visited[r][c]) {
-                    visited[r][c] = true;
-                    int wait = (parity == 1) ? 1 : 2;
-                    int childMax = wait + Math.max(parentMax, grid[r][c]);
-                    queue.offer(new int[]{r, c, childMax, 1 - parity});
-                }
-            }
-        }
-        return -1;
-    }
-}
+// Last updated: 6/9/2026, 8:00:56 PM
+1class Solution {
+2    public int minTimeToReach(int[][] grid) {
+3        int m = grid.length, n = grid[0].length;
+4        boolean[][] visited = new boolean[m][n];
+5        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+6        queue.offer(new int[]{0, 0, 0, 1}); 
+7        int[][] dir = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+8        while (!queue.isEmpty()) {
+9            int[] node = queue.poll();
+10            int x = node[0], y = node[1], parentMax = node[2], parity = node[3];
+11            if (x == m - 1 && y == n - 1) {
+12                return parentMax;
+13            }
+14            for (int[] d : dir) {
+15                int r = x + d[0], c = y + d[1];
+16                if (r >= 0 && r < m && c >= 0 && c < n && !visited[r][c]) {
+17                    visited[r][c] = true;
+18                    int wait = (parity == 1) ? 1 : 2;
+19                    int childMax = wait + Math.max(parentMax, grid[r][c]);
+20                    queue.offer(new int[]{r, c, childMax, 1 - parity});
+21                }
+22            }
+23        }
+24        return -1;
+25    }
+26}
+27
