1061c3f7a4beae7110dfcd171b591093cc1027d0
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-06-11 05:27:09 -0400

    File updated at 6/11/2026, 5:27:08 AM

--- leetcode-sub/3558-NumberofWaystoAssignEdgeWeightsI/3558-NumberofWaystoAssignEdgeWeightsI.java
+++ leetcode-sub/3558-NumberofWaystoAssignEdgeWeightsI/3558-NumberofWaystoAssignEdgeWeightsI.java
@@ -1,7 +1,7 @@
-// Last updated: 6/11/2026, 5:24:07 AM
+// Last updated: 6/11/2026, 5:27:08 AM
 1class Solution {
 2    public int assignEdgeWeights(int[][] edges) {
-3        int n = edges.length + 1, mod = 1000000007, max = 0;
+3        int n = edges.length + 1, mod = 1000000007, max = 0, count = 1;
 4        List<List<Integer>> adj = new ArrayList<>(n + 1);
 5        for (int i = 0; i <= n; i++) adj.add(new ArrayList<>());
 6        for (int[] edge : edges) {
@@ -11,7 +11,7 @@
 10        }
 11        Queue<int[]> queue = new LinkedList<>();
 12        boolean[] visited = new boolean[n + 1];
-13        queue.offer(new int[]{1, 1});
+13        queue.offer(new int[]{1, 0});
 14        visited[1] = true;
 15        while (!queue.isEmpty()) {
 16            int[] node = queue.poll();
@@ -23,18 +23,11 @@
 22                    queue.offer(new int[]{neighbor, depth + 1});
 23                }
 24            }
-25        }
-26        return modPow(2, max - 2, mod);
-27    }
-28    // 2^x, b^exp in logN
-29    private int modPow(int base, int exp, int mod) {
-30        long res = 1, b = base;
-31        while (exp > 0) {
-32            if ((exp & 1) == 1) res = res * b % mod;
-33            b = b * b % mod;
-34            exp >>= 1;
-35        }
-36        return (int) res;
-37    }
-38}
-39
+25        } // 2^(max - 2)
+26        for (int i = 0; i < max - 1; i++) {
+27            count = (count * 2) % mod;
+28        }
+29        return count;
+30    }
+31}
+32
