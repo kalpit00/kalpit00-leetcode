2eba56c0cf7a7a6f1637355592c0636b02bdb2bd
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-06-11 21:17:40 -0400

    File updated at 6/11/2026, 9:17:39 PM

--- leetcode-sub/222-CountCompleteTreeNodes/222-CountCompleteTreeNodes.java
+++ leetcode-sub/222-CountCompleteTreeNodes/222-CountCompleteTreeNodes.java
@@ -1,19 +1,22 @@
-// Last updated: 7/29/2025, 12:13:15 AM
-class Solution {
-    public int countNodes(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        TreeNode left = root.right, right = root.right;
-        int height = 0;
-        while (right != null) { // go till right is null
-            left = left.left;
-            right = right.right;
-            height++;
-        }
-        if (left == null && right == null) { // left == right, 2^h - 1
-            return (1 << height) + countNodes(root.left);
-        }
-        return (1 << (height + 1)) + countNodes(root.right);
-    }
-}
+// Last updated: 6/11/2026, 9:17:39 PM
+1class Solution { // logN * logN
+2    public int countNodes(TreeNode root) {
+3        if (root == null) {
+4            return 0;
+5        }
+6        int left = depth(root, true), right = depth(root, false);
+7        if (left == right) { // its complete BT, there are 2^h - 1 nodes
+8            return (1 << left) - 1;
+9        } // else, using normal O(N) dfs to count
+10        return 1 + countNodes(root.left) + countNodes(root.right);
+11    }
+12    
+13    public int depth(TreeNode root, boolean isLeft) {
+14        int count = 0;
+15        while (root != null) {
+16            count++;
+17            root = isLeft ? root.left : root.right;
+18        }
+19        return count;
+20    }
+21}
