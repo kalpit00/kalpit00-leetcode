aff9f2ca22f3e9452a1240d489a5313faf629221
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-06-17 02:46:34 -0400

    File updated at 6/17/2026, 2:46:34 AM

--- leetcode-sub/3614-ProcessStringwithSpecialOperationsII/3614-ProcessStringwithSpecialOperationsII.java
+++ leetcode-sub/3614-ProcessStringwithSpecialOperationsII/3614-ProcessStringwithSpecialOperationsII.java
@@ -1,38 +1,41 @@
-// Last updated: 6/17/2026, 2:38:51 AM
+// Last updated: 6/17/2026, 2:46:34 AM
 1class Solution {
 2    public char processStr(String s, long k) {
 3        long n = 0;
 4        for (char c : s.toCharArray()) {
 5            if (Character.isLetter(c)) {
-6                n++;
+6                n++; // for char, append, so len++
 7            }
-8            else if (c == '*') {
+8            else if (c == '*') { // delete, so len--
 9                if (n > 0) n--;
 10            }
-11            else if (c == '#') {
+11            else if (c == '#') { // duplicate, so len *= 2
 12                n *= 2;
 13            }
-14        }
-15        if (n < k + 1) return '.';
-16        for (int i = s.length() - 1; i >= 0; i--) {
-17            char c = s.charAt(i);
-18            if (Character.isLetter(c)) {
-19                if (n == k + 1) return c;
-20                n--;
-21            }
-22            else if (c == '*') {
-23                n++;
+14            else { // reverse
+15                // no change in length for reverse
+16            }
+17        }
+18        if (k >= n) return '.';
+19        for (int i = s.length() - 1; i >= 0; i--) {
+20            char c = s.charAt(i);
+21            if (Character.isLetter(c)) { // check if its kth char (1 indexed)
+22                if (n == k + 1) return c;
+23                n--; // opp of append is len--
 24            }
-25            else if (c == '#') {
-26                if ((n + 1) / 2 < k + 1) {
-27                    k -= n / 2;
-28                }
-29                n = (n + 1) / 2;
-30            }
-31            else {
-32                k = n - k - 1;
+25            else if (c == '*') {
+26                n++; // opp of delete is len++
+27            }
+28            else if (c == '#') {
+29                if (k >= n / 2) {
+30                    k -= n / 2;
+31                } // opp of duplicate is len /= 2
+32                n /= 2;
 33            }
-34        }
-35        return '.';
-36    }
-37}
+34            else {
+35                k = n - k - 1;
+36            }  // for reverse, the position would be at other end k steps away
+37        } // (its like doing n - i - 1 from right end!)
+38        return '.';
+39    }
+40}
