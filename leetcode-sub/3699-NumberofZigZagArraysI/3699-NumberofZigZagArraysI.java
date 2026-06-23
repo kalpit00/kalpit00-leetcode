e0bc46a5e4734d4e4bc6f18c58498090ca9c577c
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-06-22 20:04:15 -0400

    File updated at 6/22/2026, 8:04:15 PM

--- leetcode-sub/3699-NumberofZigZagArraysI/3699-NumberofZigZagArraysI.java
+++ leetcode-sub/3699-NumberofZigZagArraysI/3699-NumberofZigZagArraysI.java
@@ -1,35 +1,29 @@
-// Last updated: 6/22/2026, 8:02:12 PM
-1public class Solution {
-2
-3    private static final int MOD = 1_000_000_007;
-4
-5    public int zigZagArrays(int n, int l, int r) {
-6        int[] dp0 = new int[r + 1];
-7        int[] dp1 = new int[r + 1];
-8        int[] sum0 = new int[r + 2];
-9        int[] sum1 = new int[r + 2];
-10
-11        for (int i = l; i <= r; i++) {
-12            dp0[i] = 1;
-13            dp1[i] = 1;
-14            sum0[i] = i - l + 1;
-15            sum1[i] = i - l + 1;
-16        }
-17
-18        for (int i = 1; i < n; i++) {
-19            for (int j = l; j <= r; j++) {
-20                dp0[j] = (sum1[r] - sum1[j] + MOD) % MOD;
-21                dp1[j] = sum0[j - 1];
-22            }
-23
-24            sum0[l] = dp0[l];
-25            sum1[l] = dp1[l];
-26            for (int j = l + 1; j <= r; j++) {
-27                sum0[j] = (sum0[j - 1] + dp0[j]) % MOD;
-28                sum1[j] = (sum1[j - 1] + dp1[j]) % MOD;
-29            }
-30        }
-31
-32        return (sum0[r] + sum1[r]) % MOD;
-33    }
-34}
+// Last updated: 6/22/2026, 8:04:15 PM
+1class Solution {
+2    public int zigZagArrays(int n, int l, int r) {
+3        int MOD = 1_000_000_007;
+4        r -= l;
+5        int[] dp = new int[r + 1];
+6        Arrays.fill(dp, 1);
+7        for (int i = 1; i < n; i++) {
+8            int pre = 0, pre2;
+9            if ((i & 1) == 1) {
+10                for (int v = 0; v <= r; v++) {
+11                    pre2 = pre + dp[v];
+12                    dp[v] = pre;
+13                    pre = pre2 % MOD;
+14                }
+15            } else {
+16                for (int v = r; v >= 0; v--) {
+17                    pre2 = pre + dp[v];
+18                    dp[v] = pre;
+19                    pre = pre2 % MOD;
+20                }
+21            }
+22        }
+23        int res = 0;
+24        for (int v : dp)
+25            res = (res + v) % MOD;
+26        return res * 2 % MOD;
+27    }
+28}
