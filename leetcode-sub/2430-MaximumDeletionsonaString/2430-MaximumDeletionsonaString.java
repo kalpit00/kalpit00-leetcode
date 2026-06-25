4a39b0ae4f0128f95d5216bcab57fdca926208e4
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-06-25 03:13:41 -0400

    File updated at 6/25/2026, 3:13:41 AM

--- leetcode-sub/2430-MaximumDeletionsonaString/2430-MaximumDeletionsonaString.java
+++ leetcode-sub/2430-MaximumDeletionsonaString/2430-MaximumDeletionsonaString.java
@@ -1,22 +1,22 @@
-// Last updated: 6/25/2026, 12:40:42 AM
+// Last updated: 6/25/2026, 3:13:41 AM
 1class Solution {
 2    public int deleteString(String s) {
-3        Map<String, Integer> dp = new HashMap<>();
-4        return solve(s, dp);
-5    }
-6    private int solve(String s, Map<String, Integer> dp) {
-7        if (s.length() == 0) return 0;
-8        if (dp.containsKey(s)) return dp.get(s);
-9        int[] lps = kmp(s);
-10        int max = 1;
-11        for (int i = 1; i < lps.length; i++) {
-12            int j = (i + 1) / 2;
-13            if (lps[i] == j && i % 2 == 1) {
-14                max = Math.max(max, 1 + solve(s.substring(j), dp));
-15            }
-16        }
-17        dp.put(s, max);
-18        return max;
+3        int n = s.length();
+4        Integer[] dp = new Integer[n];
+5        return solve(0, n, s, dp);
+6    }
+7    private int solve(int idx, int n, String s, Integer[] dp) {
+8        if (idx == n) return 0; // or s.length() == 0 || s.isEmpty()
+9        if (dp[idx] != null) return dp[idx];
+10        int[] lps = kmp(s);
+11        int max = 1;
+12        for (int i = 1; i < lps.length; i++) {
+13            int j = (i + 1) / 2;
+14            if (lps[i] == j && i % 2 == 1) {
+15                max = Math.max(max, 1 + solve(idx + j, n,  s.substring(j), dp));
+16            }
+17        }
+18        return dp[idx] = max;
 19    }
 20    private int[] kmp(String s) {
 21        char[] p = s.toCharArray();
