b69ecd3ff5dea970d6a28f494319151a47ff19e8
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-06-25 22:38:27 -0400

    File updated at 6/25/2026, 10:38:26 PM

--- leetcode-sub/3737-CountSubarraysWithMajorityElementI/3737-CountSubarraysWithMajorityElementI.java
+++ leetcode-sub/3737-CountSubarraysWithMajorityElementI/3737-CountSubarraysWithMajorityElementI.java
@@ -1,18 +1,22 @@
-// Last updated: 6/24/2026, 10:16:46 PM
+// Last updated: 6/25/2026, 10:38:26 PM
 1class Solution {
-2
-3    public int countMajoritySubarrays(int[] nums, int target) {
-4        int n = nums.length;
-5        int ans = 0;
-6        for (int i = 0; i < n; ++i) {
-7            int cnt = 0;
-8            for (int j = i; j < n; ++j) {
-9                cnt += (nums[j] == target ? 1 : -1);
-10                if (cnt > 0) {
-11                    ++ans;
-12                }
-13            }
-14        }
-15        return ans;
-16    }
-17}
+2    public long countMajoritySubarrays(int[] nums, int target) {
+3        int n = nums.length, count = n;
+4        int[] pre = new int[n * 2 + 1];
+5        pre[n] = 1;
+6        long res = 0, sum = 0;
+7        for (int i = 0; i < n; ++i) {
+8            if (nums[i] == target) {
+9                sum += pre[count];
+10                count++;
+11            } 
+12            else {
+13                count--;
+14                sum -= pre[count];
+15            }
+16            pre[count]++;
+17            res += sum;
+18        }
+19        return res;
+20    }
+21}
