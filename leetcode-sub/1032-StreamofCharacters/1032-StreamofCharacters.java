1bd06aac829c40bf6cdad60e18416dec6d5e1a7f
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-06-29 02:59:40 -0400

    File updated at 6/29/2026, 2:59:40 AM

--- leetcode-sub/1032-StreamofCharacters/1032-StreamofCharacters.java
+++ leetcode-sub/1032-StreamofCharacters/1032-StreamofCharacters.java
@@ -1,55 +1,54 @@
-// Last updated: 6/29/2026, 2:58:35 AM
+// Last updated: 6/29/2026, 2:59:40 AM
 1class StreamChecker {
 2    Trie trie;
-3    StringBuilder sb;
+3    List<Character> list;
 4    public StreamChecker(String[] words) {
 5        trie = new Trie();
-6        sb = new StringBuilder();
+6        list = new ArrayList<>();
 7        for (String word : words) {
 8            trie.insert(word);
 9        }
 10    }
-11    
-12    public boolean query(char letter) {
-13        sb.append(letter);
-14        return trie.search(sb.toString());
-15    }
-16    class Trie {
-17        private TrieNode root;
-18        
-19        public Trie() {
-20            root = new TrieNode();
-21        }
-22        
-23        public void insert(String word) {
-24            TrieNode node = root;
-25            for (int i = word.length() - 1; i >= 0; i--) {
-26                char ch = word.charAt(i);
-27                if (node.child[ch - 'a'] == null) {
-28                    node.child[ch - 'a'] = new TrieNode();
-29                }
-30                node = node.child[ch - 'a'];
-31            }
-32            node.isEnd = true;
-33        }   
-34        
-35        public boolean search(String word) {
-36            TrieNode node = root;
-37            for (int i = word.length() - 1; i >= 0; i--) {
-38                char ch = word.charAt(i);
-39                if (node.child[ch - 'a'] == null) {
-40                    return false;
-41                }
-42                node = node.child[ch - 'a'];
-43                if (node.isEnd) {
-44                    return true;
-45                }
-46            }
-47            return node.isEnd;
-48        }
-49        class TrieNode {
-50            TrieNode[] child = new TrieNode[26];
-51            boolean isEnd = false;
-52        }
-53    }
-54}
+11    public boolean query(char letter) {
+12        list.add(letter);
+13        return trie.search(list);
+14    }
+15    class Trie {
+16        private TrieNode root;
+17        
+18        public Trie() {
+19            root = new TrieNode();
+20        }
+21        
+22        public void insert(String word) {
+23            TrieNode node = root;
+24            for (int i = word.length() - 1; i >= 0; i--) {
+25                char ch = word.charAt(i);
+26                if (node.child[ch - 'a'] == null) {
+27                    node.child[ch - 'a'] = new TrieNode();
+28                }
+29                node = node.child[ch - 'a'];
+30            }
+31            node.isEnd = true;
+32        }   
+33        
+34        public boolean search(List<Character> word) {
+35            TrieNode node = root;
+36            for (int i = word.size() - 1; i >= 0; i--) {
+37                char ch = word.get(i);
+38                if (node.child[ch - 'a'] == null) {
+39                    return false;
+40                }
+41                node = node.child[ch - 'a'];
+42                if (node.isEnd) {
+43                    return true;
+44                }
+45            }
+46            return node.isEnd;
+47        }
+48        class TrieNode {
+49            TrieNode[] child = new TrieNode[26];
+50            boolean isEnd = false;
+51        }
+52    }
+53}
