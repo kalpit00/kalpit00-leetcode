22784113cbcd4a5d6953a0930f899f42932e13ac
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-07-18 15:00:33 -0400

    File updated at 7/18/2026, 3:00:33 PM

--- leetcode-sub/3737-CountSubarraysWithMajorityElementI/3737-CountSubarraysWithMajorityElementI.java
+++ leetcode-sub/3737-CountSubarraysWithMajorityElementI/3737-CountSubarraysWithMajorityElementI.java
@@ -1,22 +1,26 @@
-// Last updated: 6/25/2026, 10:38:26 PM
-1class Solution {
-2    public long countMajoritySubarrays(int[] nums, int target) {
-3        int n = nums.length, count = n;
-4        int[] pre = new int[n * 2 + 1];
-5        pre[n] = 1;
-6        long res = 0, sum = 0;
-7        for (int i = 0; i < n; ++i) {
-8            if (nums[i] == target) {
-9                sum += pre[count];
-10                count++;
-11            } 
-12            else {
-13                count--;
-14                sum -= pre[count];
-15            }
-16            pre[count]++;
-17            res += sum;
-18        }
-19        return res;
-20    }
-21}
+// Last updated: 7/18/2026, 3:00:33 PM
+class Solution {
+    public int countMajoritySubarrays(int[] nums, int target) {
+        int[] prefix_sum = new int[2*nums.length+1];
+        prefix_sum[nums.length] = 1;
+        int cnt = nums.length;
+        int presum = 0;
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                presum += prefix_sum[cnt];
+                cnt += 1;
+                prefix_sum[cnt] += 1;
+            } else {
+                cnt -= 1;
+                presum -= prefix_sum[cnt];
+                prefix_sum[cnt] += 1;
+            }
+            result += presum;
+
+        }
+
+
+        return result;
+    }
+}
