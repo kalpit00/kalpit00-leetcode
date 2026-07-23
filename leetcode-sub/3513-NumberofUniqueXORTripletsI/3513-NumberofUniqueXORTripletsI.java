9b7c0cf3fb94818f4026f65710d62c00994580e6
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-07-22 23:12:50 -0400

    File updated at 7/22/2026, 11:12:49 PM

--- leetcode-sub/3513-NumberofUniqueXORTripletsI/3513-NumberofUniqueXORTripletsI.java
+++ leetcode-sub/3513-NumberofUniqueXORTripletsI/3513-NumberofUniqueXORTripletsI.java
@@ -1,11 +1,7 @@
-// Last updated: 7/22/2026, 11:08:48 PM
+// Last updated: 7/22/2026, 11:12:49 PM
 1class Solution {
 2    public int uniqueXorTriplets(int[] nums) {
-3        int n = nums.length, count = 0, x = n;
-4        while (x != 0) { // keep halving the length of nums[]
-5            x /= 2; // x >>= 1;
-6            count++;
-7        }
-8        return n <= 2 ? n : 1 << count; // ans = 2^count
-9    }
-10}
+3        int n = nums.length, m = 32 - Integer.numberOfLeadingZeros(n);
+4        return n <= 2 ? n : 1 << m; // ans = 2^bitLength of n in binary
+5    }
+6}
