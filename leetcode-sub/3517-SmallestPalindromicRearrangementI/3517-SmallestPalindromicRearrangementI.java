75da6566023eb8792b87bff8f541e57cb3f319fc
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-07-27 20:22:32 -0400

    File updated at 7/27/2026, 8:22:31 PM

--- leetcode-sub/3517-SmallestPalindromicRearrangementI/3517-SmallestPalindromicRearrangementI.java
+++ leetcode-sub/3517-SmallestPalindromicRearrangementI/3517-SmallestPalindromicRearrangementI.java
@@ -1,23 +1,23 @@
-// Last updated: 5/21/2025, 2:50:18 PM
-class Solution {
-    public String smallestPalindrome(String s) {
-        int[] map = new int[26];
-        for (char c : s.toCharArray()) {
-            map[c - 'a']++;
-        }
-        int n = s.length(), i = 0, j = n - 1;
-        char[] res = new char[n];
-        for (int k = 0; k < 26; k++) {
-            if (map[k] % 2 == 1) {
-                res[n / 2] = (char) (k + 'a');
-                map[k]--;
-            }
-            while (map[k] > 0) {
-                res[i++] = (char) (k + 'a');
-                res[j--] = (char) (k + 'a');
-                map[k] -= 2;
-            }
-        }
-        return String.valueOf(res);
-    }
-}
+// Last updated: 7/27/2026, 8:22:31 PM
+1class Solution {
+2    public String smallestPalindrome(String s) {
+3        int[] map = new int[26];
+4        for (char c : s.toCharArray()) {
+5            map[c - 'a']++;
+6        }
+7        int n = s.length(), i = 0, j = n - 1;
+8        char[] res = new char[n];
+9        for (int k = 0; k < 26; k++) {
+10            if (map[k] % 2 == 1) {
+11                res[n / 2] = (char) (k + 'a');
+12                map[k]--;
+13            }
+14            while (map[k] > 0) {
+15                res[i++] = (char) (k + 'a');
+16                res[j--] = (char) (k + 'a');
+17                map[k] -= 2;
+18            }
+19        }
+20        return String.valueOf(res);
+21    }
+22}
