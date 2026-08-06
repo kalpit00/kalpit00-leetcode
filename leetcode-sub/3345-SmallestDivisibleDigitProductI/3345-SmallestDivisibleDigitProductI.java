82598023600c200e92367c881f53a5d9d717cb60
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-08-05 20:11:38 -0400

    File updated at 8/5/2026, 8:11:38 PM

--- leetcode-sub/3345-SmallestDivisibleDigitProductI/3345-SmallestDivisibleDigitProductI.java
+++ leetcode-sub/3345-SmallestDivisibleDigitProductI/3345-SmallestDivisibleDigitProductI.java
@@ -1,18 +1,18 @@
-// Last updated: 6/19/2025, 2:58:55 AM
-class Solution {
-    public int smallestNumber(int n, int t) {
-        int i = n;
-        while (true) {
-            int prod = 1;
-            int num = i;
-            while (num > 0) {
-                prod *= num % 10;
-                num /= 10;
-            }
-            if (prod % t == 0) {
-                return i;
-            }
-            i++;
-        }
-    }
-}
+// Last updated: 8/5/2026, 8:11:38 PM
+1class Solution {
+2    public int smallestNumber(int n, int t) {
+3        int i = n;
+4        while (true) {
+5            int prod = 1;
+6            int num = i;
+7            while (num > 0) {
+8                prod *= num % 10;
+9                num /= 10;
+10            }
+11            if (prod % t == 0) {
+12                return i;
+13            }
+14            i++;
+15        }
+16    }
+17}
