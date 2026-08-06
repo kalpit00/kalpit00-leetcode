be76f10800203e324d451beb3c6ff4f9d17f09a8
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-08-06 00:44:20 -0400

    Create file

---
+++ leetcode-sub/3843-FirstElementwithUniqueFrequency/3843-FirstElementwithUniqueFrequency.java
@@ -0,0 +1,17 @@
+// Last updated: 8/6/2026, 12:44:20 AM
+1class Solution {
+2    public int firstUniqueFreq(int[] nums) {
+3        int[] map = new int[100001], buckets = new int[100001];
+4        for (int num : nums) {
+5            map[num]++;
+6        }
+7        for (int i = 0; i < map.length; i++) {
+8            if (map[i] == 0) continue;
+9            buckets[map[i]]++;
+10        }
+11        for (int i = 0; i < nums.length; i++) {
+12            if (buckets[map[nums[i]]] == 1) return nums[i];
+13        }
+14        return -1;
+15    }
+16}
