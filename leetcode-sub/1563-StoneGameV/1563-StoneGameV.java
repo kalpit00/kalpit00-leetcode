294b0e93a24dc35bfd4474b72b814af519ce900b
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-08-17 06:37:17 -0400

    File updated at 8/17/2026, 6:37:17 AM

--- leetcode-sub/1563-StoneGameV/1563-StoneGameV.java
+++ leetcode-sub/1563-StoneGameV/1563-StoneGameV.java
@@ -1,24 +1,38 @@
-// Last updated: 8/16/2026, 9:23:38 PM
+// Last updated: 8/17/2026, 6:37:17 AM
 1class Solution {
-2    public int stoneGameV(int[] stoneValue) {
-3        int n = stoneValue.length;
-4        int[][] dp = new int[n][n], max = new int[n][n];
-5        for(int i = 0; i < n; i++) {
-6            max[i][i] = stoneValue[i];
-7        }
-8        for(int j = 1; j < n; j++) {
-9            int mid = j, sum = stoneValue[j], rightHalf = 0;
-10            for(int i = j-1; i >= 0; i--) {
-11                sum += stoneValue[i];
-12                while((rightHalf + stoneValue[mid]) * 2 <= sum) {
-13                    rightHalf += stoneValue[mid--];
-14                }
-15                dp[i][j] = rightHalf * 2 == sum ? max[i][mid] : (mid == i ? 0 : max[i][mid - 1]);
-16                dp[i][j] = Math.max(dp[i][j], mid == j ? 0 : max[j][mid + 1]);
-17                max[i][j] = Math.max(max[i][j - 1], dp[i][j] + sum);
-18                max[j][i] = Math.max(max[j][i + 1], dp[i][j] + sum);
-19            }
-20        }
-21        return dp[0][n-1];
-22    }
-23}
+2    public int stoneGameV(int[] nums) {
+3        int n = nums.length;
+4        Long[][] dp = new Long[n][n];
+5        long[] pre = new long[n];
+6        pre[0] = nums[0];
+7        for (int i = 1; i < n; i++) {
+8            pre[i] = pre[i - 1] + nums[i];
+9        }
+10        return (int) solve(0, n - 1, n, nums, dp, pre);   
+11    }
+12	private long solve(int i, int j, int n, int[] nums, 
+13    Long[][] dp, long[] pre) {
+14        if (i >= j) {
+15            return 0;
+16        }
+17        if (dp[i][j] != null) {
+18            return dp[i][j];
+19        }
+20        long max = Long.MIN_VALUE;
+21        for (int k = i + 1; k <= j; k++) {
+22            long left = i > 0 ? pre[k - 1] - pre[i - 1] : pre[k - 1];
+23            long right = k > 0 ? pre[j] - pre[k - 1] : pre[j];
+24            if (left < right) {
+25                max = Math.max(max, left + solve(i, k - 1, n, nums, dp, pre));
+26            }
+27            else if (left > right) {
+28                max = Math.max(max, right + solve(k, j, n, nums, dp, pre));
+29            }
+30            else {
+31                max = Math.max(max, left + solve(i, k - 1, n, nums, dp, pre));
+32                max = Math.max(max, right + solve(k, j, n, nums, dp, pre));
+33            }
+34        }
+35        return dp[i][j] = max;
+36    }
+37}
