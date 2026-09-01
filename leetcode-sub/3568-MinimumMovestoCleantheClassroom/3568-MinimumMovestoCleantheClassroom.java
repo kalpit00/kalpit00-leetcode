b610cd565e9249ff6d0bc4a10a46746fbff974ce
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-09-01 03:18:49 -0400

    File updated at 9/1/2026, 3:18:49 AM

--- leetcode-sub/3568-MinimumMovestoCleantheClassroom/3568-MinimumMovestoCleantheClassroom.java
+++ leetcode-sub/3568-MinimumMovestoCleantheClassroom/3568-MinimumMovestoCleantheClassroom.java
@@ -1,85 +1,67 @@
-// Last updated: 6/1/2025, 3:53:39 AM
-class Solution {
-    static class State {
-        int x, y, energy, mask, steps;
-        State(int x, int y, int energy, int mask, int steps) {
-            this.x = x;
-            this.y = y;
-            this.energy = energy;
-            this.mask = mask;
-            this.steps = steps;
-        }
-    }
-
-    public int minMoves(String[] classroom, int energy) {
-        int m = classroom.length, n = classroom[0].length();
-        char[][] grid = new char[m][n];
-        for (int i = 0; i < m; i++) grid[i] = classroom[i].toCharArray();
-
-        int startX = -1, startY = -1;
-        List<int[]> lumetarkon = new ArrayList<>();
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                char c = grid[i][j];
-                if (c == 'S') {
-                    startX = i;
-                    startY = j;
-                } else if (c == 'L') {
-                    lumetarkon.add(new int[]{i, j});
-                }
-            }
-        }
-
-        int totalLitter = lumetarkon.size();
-        int allMask = (1 << totalLitter) - 1;
-
-        int[][][] visited = new int[m][n][1 << totalLitter];
-        for (int[][] layer : visited)
-            for (int[] row : layer)
-                Arrays.fill(row, -1);  
-
-        Queue<State> queue = new ArrayDeque<>();
-        queue.offer(new State(startX, startY, energy, 0, 0));
-        visited[startX][startY][0] = energy;
-
-        int[][] dirs = {{0,1}, {1,0}, {0,-1}, {-1,0}};
-
-        while (!queue.isEmpty()) {
-            State curr = queue.poll();
-
-            if (curr.mask == allMask) return curr.steps;
-
-            for (int[] dir : dirs) {
-                int nx = curr.x + dir[0];
-                int ny = curr.y + dir[1];
-
-                if (nx < 0 || ny < 0 || nx >= m || ny >= n || grid[nx][ny] == 'X') continue;
-
-                int nextEnergy = curr.energy - 1;
-                if (nextEnergy < 0) continue;
-
-                char cell = grid[nx][ny];
-                if (cell == 'R') nextEnergy = energy;
-
-                int nextMask = curr.mask;
-                if (cell == 'L') {
-                    for (int i = 0; i < lumetarkon.size(); i++) {
-                        int[] pos = lumetarkon.get(i);
-                        if (pos[0] == nx && pos[1] == ny) {
-                            nextMask |= (1 << i);
-                            break;
-                        }
-                    }
-                }
-
-                if (visited[nx][ny][nextMask] < nextEnergy) {
-                    visited[nx][ny][nextMask] = nextEnergy;
-                    queue.offer(new State(nx, ny, nextEnergy, nextMask, curr.steps + 1));
-                }
-            }
-        }
-
-        return -1;
-    }
-}
+// Last updated: 9/1/2026, 3:18:49 AM
+1class Solution {
+2    int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};    
+3    public int minMoves(String[] classroom, int energyMax) {
+4        int m = classroom.length, n = classroom[0].length(), x = -1, y = -1;
+5        char[][] grid = new char[m][n];
+6        List<int[]> list = new ArrayList<>();
+7        for (int i = 0; i < m; i++) {
+8            grid[i] = classroom[i].toCharArray();
+9            for (int j = 0; j < n; j++) {
+10                char ch = grid[i][j];
+11                if (ch == 'S') {
+12                    x = i;
+13                    y = j;
+14                }
+15                else if (ch == 'L') {
+16                    list.add(new int[]{i, j});
+17                }
+18            }
+19        }
+20        int count = list.size(), dest = (1 << count) - 1;
+21        Queue<int[]> queue = new LinkedList<>();
+22        queue.offer(new int[]{x, y, energyMax, 0, 0});
+23        int[][][] visited = new int[m][n][1 << count];
+24        for (int[][] mat : visited) {
+25            for (int[] a : mat) {
+26                Arrays.fill(a, -1);
+27            }
+28        }
+29        visited[x][y][0] = energyMax;        
+30        while (!queue.isEmpty()) {
+31            int[] node = queue.poll();
+32            int i = node[0], j = node[1], energy = node[2], 
+33            mask = node[3], steps = node[4];
+34            if (mask == dest) {
+35                return steps;
+36            }
+37            for (int[] d : dir) {
+38                int r = d[0] + i, c = d[1] + j;
+39                if (r < 0 || c < 0 || r >= m || c >= n || grid[r][c] == 'X') {
+40                    continue;
+41                }
+42                char ch = grid[r][c];
+43                int childEnergy = energy - 1;
+44                if (childEnergy < 0) continue;
+45                if (ch == 'R') {
+46                    childEnergy = energyMax;
+47                }
+48                int childMask = mask;
+49                if (ch == 'L') {
+50                    for (int k = 0; k < count; k++) {
+51                        int[] item = list.get(k);
+52                        if (item[0] == r && item[1] == c) {
+53                            childMask |= (1 << k);
+54                            break;
+55                        }
+56                    }
+57                }
+58                if (visited[r][c][childMask] < childEnergy) {
+59                    visited[r][c][childMask] = childEnergy;
+60                    queue.offer(new int[]{r, c, childEnergy, childMask, steps + 1});
+61                }
+62            }
+63        }
+64        return -1;
+65    }
+66}
