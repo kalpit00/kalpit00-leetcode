4c63481c03f541668146f5036263c2865829da0d
Kalpit Patel <kalpit.k.patel@gmail.com>
2026-09-22 20:03:17 -0400

    Create file

---
+++ leetcode-sub/1658-MinimumOperationstoReduceXtoZero/version/java/1658-MinimumOperationstoReduceXtoZero_20260922_200316.java
@@ -0,0 +1,18 @@
+// Last updated: 9/22/2026, 8:03:16 PM
+1class Solution {
+2    public int minOperations(int[] nums, int x) {
+3        int n = nums.length, left = 0, right = 0, sum = 0, max = -1,
+4        windowSum = 0;
+5        for (int num : nums) {
+6            sum += num;
+7        }
+8        while (right < n) {
+9            windowSum += nums[right++];
+10            while (left < n && windowSum > sum - x) {
+11                windowSum -= nums[left++];
+12            }
+13            max = windowSum == sum - x ? Math.max(max, right - left) : max;
+14        }
+15        return max == -1 ? -1 : n - max;
+16    }
+17}
